99c5e5c38d016e6ae7fa6a5166c0a586ae0e0c33
Jordan Husney <jordan.husney@gmail.com>
2016-01-18 09:55:29 -0500

    Working draft of falcor/model get pattern.

--- api/models/meeting.js
+++ api/models/meeting.js
@@ -4,7 +4,12 @@ const type = Database.type;
 
 const Meeting = Database.createModel('Meeting', {
   id: type.string(),
+  createdAt: Date,
   content: type.string(),
 });
+
+Meeting.save([
+  { createdAt: Date.now(), content: 'this is a new test' }
+]);
 
 export default Meeting;
