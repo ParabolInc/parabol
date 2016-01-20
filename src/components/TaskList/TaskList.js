742620c9f7699f764d5d23b60349d639aa0b98c1
Terry Acker <ackernaut@gmail.com>
2016-01-20 16:54:45 -0600

    Progress on task list items

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
-import { TaskListItem } from 'components';
+import {TaskListItem} from 'components';
+
+const exampleItems = [
+  {
+    id: 0,
+    checked: false,
+    label: 'Project',
+    team: 'Engineering',
+    timePassed: '1 month ago',
+    title: 'Refactor routes in the app'
+  },
+  {
+    id: 1,
+    checked: true,
+    label: 'Task',
+    team: 'Core',
+    timePassed: '2 days ago',
+    title: 'Update important policy'
+  },
+  {
+    id: 2,
+    checked: false,
+    label: 'Project',
+    team: 'Design',
+    timePassed: '1 hour ago',
+    title: 'Create clickable feature prototype'
+  }
+];
 
 export default class TaskList extends Component {
   render() {
     const styles = require('./TaskList.scss');
+    const handleCheckboxChanged = (id) => {
+      console.log(`Checkbox for item[${id}] was clicked`);
+    };
     return (
       <div className={styles.list}>
-        Task List<br />
-        <TaskListItem />
-        <TaskListItem />
-        <TaskListItem />
-        <TaskListItem />
-        <TaskListItem />
+        {exampleItems.map(item =>
+          <TaskListItem {...item} key={item.id} onCheckboxChanged={() => handleCheckboxChanged(item.id)} />
+        )}
       </div>
     );
   }
--- src/components/TaskListItem/TaskListItem.js
+++ src/components/TaskListItem/TaskListItem.js
@@ -1,11 +1,35 @@
-import React, {Component} from 'react';
+import React, {PropTypes, Component} from 'react';
 
 export default class TaskListItem extends Component {
+  static propTypes = {
+    checked: PropTypes.bool.isRequired,
+    label: PropTypes.string.isRequired,
+    onCheckboxChanged: PropTypes.func.isRequired,
+    team: PropTypes.string.isRequired,
+    timePassed: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+  }
   render() {
     const styles = require('./TaskListItem.scss');
+    const props = this.props;
+    const itemClassName = props.checked ? styles.itemSelected : styles.item;
     return (
-      <div className={styles.item}>
-        Project—Team—Title—Timestamp
+      <div className={itemClassName}>
+        <input type="checkbox"
+               checked={props.checked ? 'checked' : false}
+               onChange={props.onCheckboxChanged} />
+        <div className={styles.tag}>
+          {props.label}
+        </div>
+        <div className={styles.team}>
+          {props.team}
+        </div>
+        <div className={styles.title}>
+          {props.title}
+        </div>
+        <div className={styles.timePassed}>
+          {props.timePassed}
+        </div>
       </div>
     );
   }
