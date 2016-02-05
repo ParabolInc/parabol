158790c7ff3feafc238cf42feab0292153c0ac45
Jordan Husney <jordan.husney@gmail.com>
2016-02-04 19:17:59 -0500

    Cleanup of unused boilerplate source.

--- src/components/index.js
+++ src/components/index.js
@@ -5,9 +5,6 @@
  *
  */
 
-export CounterButton from './CounterButton/CounterButton';
-export GithubButton from './GithubButton/GithubButton';
-export InfoBar from './InfoBar/InfoBar';
 export ListFilterSet from './ListFilterSet/ListFilterSet';
 export MeetingHeader from './MeetingHeader/MeetingHeader';
 export MeetingLink from './MeetingLink/MeetingLink';
@@ -18,11 +15,9 @@ export MiniInfoBar from './MiniInfoBar/MiniInfoBar';
 export NotificationBar from './NotificationBar/NotificationBar';
 export StatusNav from './StatusNav/StatusNav';
 export StatusNavItem from './StatusNavItem/StatusNavItem';
-export SurveyForm from './SurveyForm/SurveyForm';
 export TaskList from './TaskList/TaskList';
 export TaskListItem from './TaskListItem/TaskListItem';
 export UserDrawer from './UserDrawer/UserDrawer';
 export UserHub from './UserHub/UserHub';
 export UserInput from './UserInput/UserInput';
 export UserMenu from './UserMenu/UserMenu';
-export WidgetForm from './WidgetForm/WidgetForm';
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,20 +1,8 @@
 export App from './App/App';
-export LandingLayout from './LandingLayout/LandingLayout';
+export AppLayout from './AppLayout/AppLayout';
 export Landing from './Landing/Landing';
+export LandingLayout from './LandingLayout/LandingLayout';
 export Login from './Login/Login';
 export LoginSuccess from './LoginSuccess/LoginSuccess';
-export AppLayout from './AppLayout/AppLayout';
 export MeetingLayout from './MeetingLayout/MeetingLayout';
 export NotFound from './NotFound/NotFound';
-
-/*
-export App from './boilerplate/App/App';
-export Home from './boilerplate/Home/Home';
-export Login from './boilerplate/Login/Login';
-export LoginSuccess from './boilerplate/LoginSuccess/LoginSuccess';
-export NotFound from './boilerplate/NotFound/NotFound';
-export About from './boilerplate/About/About';
-export Chat from './boilerplate/Chat/Chat';
-export Survey from './boilerplate/Survey/Survey';
-export Widgets from './boilerplate/Widgets/Widgets';
-*/
