9af46902412b3c5feab89fa6ed2575a4e4990c71
Jordan Husney <jordan.husney@gmail.com>
2016-02-09 14:19:25 -0500

    Merge cleanup

--- assets/js/index.jsx
+++
@@ -1,7 +0,0 @@
-import '../styles/base.scss';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('app'));
