645d212b54a4da01915502ba313db2d1361626b9
Jordan Husney <jordan.husney@gmail.com>
2016-04-08 21:49:52 -0500

    Dokku #36 merge (#37)
    * First pass at Dokku staging/production configuration
    
    Added template `.env` as `.env.example`
    
    Modified various files to use .env options
    
    Added app.json with postdeploy hook.
    
    Updated package.json:
       * Updated description
       * `npm start` now starts production mode by default
       * Readded engines section to make dokku happy
    
    Added dotenv processing in two spots:
       * `src/server/database/setupDB.babel.js` and,
       * `src/server/server.js`
    
    Removed `src/server/secrets.js`.
    
    * Added .env expansion to client webpack configuration.
    
    Uses webpack's EnvironmentPlugin to expand...
    
       * AUTH0_CLIENT_ID
       * AUTH0_DOMAIN
       * HOST
       * PORT
    
    ...in client code.
    
    Also fixed a few eslint warnings. One file at a time...
    
    Bumped node and npm version in package.json.

--- src/server/server.js
+++ src/server/server.js
@@ -1,22 +1,28 @@
 import {SocketCluster} from 'socketcluster';
-import {auth0Secret} from './secrets';
 import path from 'path';
-// import os from 'os';
+import { getDotenv } from '../universal/utils/dotenv';
+
+// Import .env and expand variables:
+getDotenv();
+
+const secretKey = process.env.AUTH0_CLIENT_SECRET ||
+  'BksPeQQrRkXhDrugzQDg5Nw-IInub9RkQ-pSWohUM9s6Oii4xoGVCrK2_OcUCfYZ';
 
 // const numCpus = os.cpus().length;
 export const options = {
-  authKey: new Buffer(auth0Secret, 'base64'),
+  authKey: new Buffer(secretKey, 'base64'),
   logLevel: 1,
   // change this to scale vertically
   workers: 1,
   brokers: 1,
-  port: 3000,
+  host: process.env.HOST || '127.0.0.1',
+  port: process.env.PORT || 3000,
   appName: 'Meatier',
   allowClientPublish: false,
   initController: path.join(__dirname, '/init.js'),
   workerController: path.join(__dirname, '/worker.js'),
   brokerController: path.join(__dirname, '/broker.js'),
   socketChannelLimit: 1000,
-  rebootWorkerOnCrash: true
+  rebootWorkerOnCrash: true,
 };
 new SocketCluster(options); // eslint-disable-line no-new
