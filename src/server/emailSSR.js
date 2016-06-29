e4e6532cc0413cfa4f5a39037a7fbc6cf015ae4d
Terry Acker <ackernaut@gmail.com>
2016-06-29 11:24:38 -0500

    Small updates to invite email

--- src/server/emailSSR.js
+++ src/server/emailSSR.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Oy from 'oy-vey';
-// import TeamInvite from 'universal/modules/email/containers/TeamInvite/TeamInvite';
-import WelcomeEmail from 'universal/modules/email/containers/WelcomeEmail/WelcomeEmail';
+import TeamInvite from 'universal/modules/email/containers/TeamInvite/TeamInvite';
+// import WelcomeEmail from 'universal/modules/email/containers/WelcomeEmail/WelcomeEmail';
 
 export default function emailSSR(req, res) {
-  const html = Oy.renderTemplate(<WelcomeEmail />, {
+  const html = Oy.renderTemplate(<TeamInvite />, {
     title: 'Welcome to Action by Parabol',
     previewText: 'Welcome to Action by Parabol'
   });
--- src/universal/modules/email/containers/TeamInvite/TeamInvite.js
+++ src/universal/modules/email/containers/TeamInvite/TeamInvite.js
@@ -65,12 +65,11 @@ export default class TeamInvite extends Component {
                 </td>
                 <td style={{paddingLeft: '16px'}}>
                   Jordan Husney<br />
-                  <span style={colorCool}>jordan@parabol.co</span>
+                  <span style={{...colorCool, fontSize: '14px'}}>jordan@parabol.co</span>
                 </td>
               </tr>
             </tbody>
           </table>
-          <br />
           <EmptySpace height={40} />
           <b>Hi <span style={colorWarm}>terry@parabol.co</span>!<br />
           Jordan has invited you to join a team on Action:</b>
