8810247903a124666e19db683a2f341bc2dfc49e
Jordan Husney <jordan.husney@gmail.com>
2016-07-01 08:16:34 +0200

    promisified mailcompose, working sendEmail()!

--- src/server/email/sendEmail.js
+++ src/server/email/sendEmail.js
@@ -9,34 +9,34 @@ export default async function sendEmail(to, template, props) {
     throw new Error(`Email template for ${template} does not exist!`);
   }
 
-  console.log('--------------- moe ---------------');
-  console.log(emailFactory(props));
-
-  const mail = mailcomposer(Object.assign(
+  const mailOptions = Object.assign(
     emailFactory(props), // render the html and text email
-    getMailgunOptions()  // assign default from: address
-  ));
+    getMailgunOptions(), // assign default from: address
+    { to }               // assign to: address
+  );
+
+  const buildMail = (options) => new Promise((resolve, reject) => {
+    mailcomposer(options).build((error, message) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(message);
+    });
+  });
 
   let message = null;
-
   try {
     // TODO: this is returning undefined. We must begin our investigation here.
-    message = await mail.build();
+    message = await buildMail(mailOptions);
   } catch (e) {
     console.warn(`mailcomposer: unable to build message ${e}`);
     return false;
   }
 
-  console.log('--------------- larry ---------------');
-  console.log(message);
-
   const data = ({
     to,
     message: message.toString('ascii')
   });
-
-  console.log('--------------- curly ---------------');
-  console.log(message.toString('ascii'));
 
   try {
     await mailgun.messages().sendMime(data);
