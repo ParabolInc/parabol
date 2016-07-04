12aeee75908cf376dea30b80799995a2d7da0091
Jordan Husney <jordan.husney@gmail.com>
2016-07-04 17:31:27 +0200

    Fix migrations.
    Can now go up all/down all smoothly.

--- src/server/database/migrations/20160624120200-update-user-profile.js
+++ src/server/database/migrations/20160624120200-update-user-profile.js
@@ -1,13 +1,26 @@
 exports.up = async r => {
-  return await r.table('UserProfile').update({
+  await r.table('UserProfile').update({
     isNew: false,
     welcomeSentAt: null,
     preferredName: '',
   });
+  await r.table('CachedUser').forEach(cu =>
+    r.table('UserProfile').insert(
+      r.table('UserProfile').get(cu('userProfileId')).without('id')
+        .merge({
+          id: cu('id')
+        })
+    )
+  );
+  return await r.table('CachedUser').replace(r.row.without('userProfileId'));
 };
 
 exports.down = async r => {
-  return await r.table('UserProfile').replace(
+  // TODO: write down query that restores CachedUser.userProfileId
+  await r.table('UserProfile').replace(
     r.row.without('isNew', 'welcomeSentAt', 'preferredName')
+  );
+  return await r.table('CachedUser').forEach(cu =>
+    cu.update(cu.merge({ userProfileId: cu('id') }))
   );
 };
