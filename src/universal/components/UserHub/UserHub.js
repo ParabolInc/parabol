b5f0441f723a85060b8f9c533ea86c54b5196704
Terry Acker <ackernaut@gmail.com>
2016-07-08 15:13:54 -0500

    Adds Sign Out link to UserHub

--- src/universal/components/UserHub/UserHub.js
+++ src/universal/components/UserHub/UserHub.js
@@ -5,7 +5,9 @@ import theme from 'universal/styles/theme';
 import FontAwesome from 'react-fontawesome';
 
 let styles = {};
-
+const faStyle = {
+  lineHeight: 'inherit'
+};
 
 const UserHub = (props) => {
   const {email, profile: {preferredName}} = props.user;
@@ -17,23 +19,73 @@ const UserHub = (props) => {
     dispatch(push('/me/settings'));
   };
 
+  const onCloseSettingsClick = (event) => {
+    const {dispatch} = props;
+    event.preventDefault();
+    dispatch(push('/me'));
+  };
+
+  const onSignOutClick = (event) => {
+    event.preventDefault();
+    console.log('onSignOutClick');
+  };
+
   return (
-    <div className={styles.root}>
-      <img alt="Me" className={styles.avatar} src={avatar} />
-      <div className={styles.info}>
-        <div className={styles.name}>{preferredName}</div>
-        <div className={styles.email}>{email}</div>
-      </div>
-      <div className={styles.settings}>
-        <div className={styles.settingsIcon} title="My Preferences">
-          <FontAwesome name="cog" onClick={(e) => onSettingsClick(e)} />
+    <div>
+      {props.activeArea !== 'settings' ?
+        <div className={styles.root}>
+          <img alt="Me" className={styles.avatar} src={avatar} />
+          <div className={styles.info}>
+            <div className={styles.name}>{preferredName}</div>
+            <div className={styles.email}>{email}</div>
+          </div>
+          <div className={styles.settings}>
+            <div className={styles.settingsIcon} title="My Preferences">
+              <FontAwesome
+                name="cog"
+                onClick={(e) => onSettingsClick(e)}
+                style={faStyle}
+              />
+            </div>
+          </div>
+        </div> :
+        <div className={styles.root}>
+          <a
+            className={styles.link}
+            href="#"
+            onClick={(e) => onSignOutClick(e)}
+            title="Sign Out"
+          >
+            <div className={styles.linkIcon}>
+              <FontAwesome
+                name="sign-out"
+                style={faStyle}
+              />
+            </div>
+            <div className={styles.linkLabel}>
+              Sign Out
+            </div>
+          </a>
+          <div className={styles.closeIcon} title="My Preferences">
+            <FontAwesome
+              name="times-circle"
+              onClick={(e) => onCloseSettingsClick(e)}
+              style={faStyle}
+              title="Close My Preferences"
+            />
+          </div>
         </div>
-      </div>
+      }
     </div>
   );
 };
 
 UserHub.propTypes = {
+  activeArea: PropTypes.oneOf([
+    'outcomes',
+    'settings',
+    'team'
+  ]).isRequired,
   dispatch: PropTypes.func.isRequired,
   user: PropTypes.object
 };
@@ -56,6 +108,7 @@ styles = StyleSheet.create({
   info: {
     alignItems: 'flex-start',
     display: 'flex',
+    flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     paddingLeft: '1rem'
@@ -65,7 +118,7 @@ styles = StyleSheet.create({
     fontSize: theme.typography.sBase,
     fontWeight: 700,
     lineHeight: '1.375rem',
-    maxWidth: '8rem',
+    maxWidth: '132px',
     overflow: 'hidden',
     paddingTop: '.125rem',
     textOverflow: 'ellipsis',
@@ -82,8 +135,9 @@ styles = StyleSheet.create({
     alignItems: 'center',
     fontSize: theme.typography.s3,
     display: 'flex',
-    flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    minWidth: '2rem',
+    width: '2rem'
   },
 
   settingsIcon: {
@@ -93,6 +147,58 @@ styles = StyleSheet.create({
     lineHeight: '14px',
     textAlign: 'center',
     width: '14px',
+
+    ':hover': {
+      opacity: '.5'
+    },
+    ':focus': {
+      opacity: '.5'
+    }
+  },
+
+  link: {
+    color: 'inherit',
+    cursor: 'pointer',
+    display: 'block',
+    flex: 1,
+    fontSize: 0,
+    height: '2.75rem',
+    lineHeight: '2.75rem',
+    marginRight: '3.5rem',
+    paddingLeft: '2.375rem',
+
+    ':hover': {
+      color: 'inherit',
+      opacity: '.5'
+    },
+    ':focus': {
+      color: 'inherit',
+      opacity: '.5'
+    }
+  },
+
+  linkIcon: {
+    display: 'inline-block',
+    fontSize: theme.typography.s3,
+    marginRight: '.5rem',
+    verticalAlign: 'middle',
+    width: theme.typography.s3,
+  },
+
+  linkLabel: {
+    display: 'inline-block',
+    fontSize: theme.typography.s3,
+    fontWeight: 700,
+    verticalAlign: 'middle'
+  },
+
+  closeIcon: {
+    cursor: 'pointer',
+    fontSize: theme.typography.s3,
+    height: '2.75rem',
+    lineHeight: '2.75rem',
+    textAlign: 'center',
+    width: '2rem',
 
     ':hover': {
       opacity: '.5'
