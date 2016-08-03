d543eae7047341e6582a291670a3f0b3f0c1b998
Jordan Husney <jordan.husney@gmail.com>
2016-08-03 13:50:10 -0400

    Fix for #146

--- src/universal/modules/teamDashboard/containers/Team/Team.js
+++ src/universal/modules/teamDashboard/containers/Team/Team.js
@@ -1,6 +1,39 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {cashay} from 'cashay';
 import Team from 'universal/modules/teamDashboard/components/Team/Team';
 import requireAuth from 'universal/decorators/requireAuth/requireAuth';
+
+const getUserAndMemberships = `
+query {
+  user: getCurrentUser {
+    id,
+    email,
+    id,
+    isNew,
+    picture,
+    preferredName
+    memberships {
+      id,
+      team {
+       id,
+       name
+      },
+      isLead,
+      isActive,
+      isFacilitator
+    }
+  }
+}`;
+
+const queryOpts = {
+  component: 'TeamContainer',
+  localOnly: true
+};
+
+const mapStateToProps = () => ({
+  user: cashay.query(getUserAndMemberships, queryOpts).data.user
+});
 
 const TeamContainer = (props) => {
   const {params: {teamId}, user, ...otherProps} = props;
@@ -11,7 +44,11 @@ TeamContainer.propTypes = {
   params: PropTypes.shape({
     teamId: PropTypes.string.isRequired
   }),
-  user: PropTypes.object
+  user: PropTypes.shape({
+    name: PropTypes.string,
+    memberships: PropTypes.array,
+    preferredName: PropTypes.string
+  })
 };
 
-export default requireAuth(TeamContainer);
+export default connect(mapStateToProps)(requireAuth(TeamContainer));
