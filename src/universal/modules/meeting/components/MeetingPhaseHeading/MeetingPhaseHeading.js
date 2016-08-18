65dd43ff31377955c1abeaa99fd2ba1b352683fd
Terry Acker <ackernaut@gmail.com>
2016-08-18 12:07:03 -0500

    Correctly naming the thing

--- src/universal/modules/meeting/components/MeetingPhaseHeading/MeetingPhaseHeading.js
+++ src/universal/modules/meeting/components/MeetingPhaseHeading/MeetingPhaseHeading.js
@@ -1,13 +1,13 @@
 import React, {PropTypes} from 'react';
 import Type from 'universal/components/Type/Type';
 
-const MeetingSectionHeading = (props) =>
+const MeetingPhaseHeading = (props) =>
   <Type align="center" bold family="serif" scale="s7" theme="warm">
     {props.children}
   </Type>;
 
-MeetingSectionHeading.propTypes = {
+MeetingPhaseHeading.propTypes = {
   children: PropTypes.any
 };
 
-export default MeetingSectionHeading;
+export default MeetingPhaseHeading;
