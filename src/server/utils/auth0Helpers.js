7d76fe4efca6ab6ea8a507dcf158c783cfe10c42
Jordan Husney <jordan.husney@gmail.com>
2016-08-19 11:18:59 -0400

    Closes #205 – adds auth0 management token for dev sandbox

--- src/server/utils/auth0Helpers.js
+++ src/server/utils/auth0Helpers.js
@@ -4,13 +4,10 @@ import {getDotenv} from '../../universal/utils/dotenv';
 // Import .env and expand variables:
 getDotenv();
 
-if (!process.env.AUTH0_MANAGEMENT_TOKEN) {
-  throw new Error('Please set AUTH0_MANAGEMENT_TOKEN environment variable');
-}
-
 export const auth0ManagementClient = new ManagementClient({
   domain: process.env.AUTH0_DOMAIN || 'parabol.auth0.com',
-  token: process.env.AUTH0_MANAGEMENT_TOKEN
+  // eslint-disable-next-line max-len
+  token: process.env.AUTH0_MANAGEMENT_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ3M21CcmhpWEdzRTNEc3JTTWdwQXoyYlM2VnpMaE95SiIsInNjb3BlcyI6eyJ1c2VycyI6eyJhY3Rpb25zIjpbInVwZGF0ZSJdfSwidXNlcnNfYXBwX21ldGFkYXRhIjp7ImFjdGlvbnMiOlsicmVhZCIsInVwZGF0ZSIsImRlbGV0ZSIsImNyZWF0ZSJdfX0sImlhdCI6MTQ3MTYxOTUxOSwianRpIjoiOTlhZWMwOGMwNTU4MmFhMjg2MmZhMmNjNzk2YjQwODIifQ.f08OPPuvDQOaH6avwxKA9gHafpScF3pWYGfun7GkbDs'
 });
 
 export const clientSecret = process.env.AUTH0_CLIENT_SECRET ||
