afebeaae6b8f9a001b19ebefc639377127093664
Jordan Husney <jordan.husney@gmail.com>
2016-08-26 14:48:35 -0400

    Fix & a13n to tables called out in meeting migration

--- src/server/database/migrations/20160722161700-meeting.js
+++ src/server/database/migrations/20160722161700-meeting.js
@@ -1,31 +1,32 @@
 exports.up = async (r) => {
   const meetingTables = [
-    r.tableCreate('Meeting'),
-    r.tableCreate('Project'),
     r.tableCreate('Action'),
     r.tableCreate('AgendaItem'),
+    r.tableCreate('Meeting'),
     r.tableCreate('Participant'),
+    r.tableCreate('Project'),
     r.tableCreate('Outcome'),
     r.tableCreate('TaskOutcomeDiff')
   ];
   await Promise.all(meetingTables);
   const meetingIndices = [
-    r.table('Meeting').indexCreate('teamId'),
-    r.table('Project').indexCreate('teamMemberId'),
     r.table('Action').indexCreate('userId'),
     r.table('AgendaItem').indexCreate('teamId'),
+    r.table('Meeting').indexCreate('teamId'),
     r.table('Participant').indexCreate('meetingId'),
     r.table('Participant').indexCreate('userId'),
+    r.table('Project').indexCreate('teamMemberId')
   ];
   return await Promise.all(meetingIndices);
 };
 
 exports.down = async (r) => {
   const meetingTables = [
-    r.tableDrop('Meeting'),
-    r.tableDrop('Task'),
+    r.tableDrop('Action'),
     r.tableDrop('AgendaItem'),
+    r.tableDrop('Meeting'),
     r.tableDrop('Participant'),
+    r.tableDrop('Project'),
     r.tableDrop('Outcome'),
     r.tableDrop('TaskOutcomeDiff')
   ];
