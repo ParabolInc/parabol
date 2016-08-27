c82dd45f835ddc00a996a3e030679aa83aec3a2b
Jordan Husney <jordan.husney@gmail.com>
2016-08-27 18:09:38 -0400

    Adds client application versioning
    Closes #218

--- src/universal/utils/clientOptions.js
+++ src/universal/utils/clientOptions.js
@@ -1,20 +1,21 @@
+import {APP_NAME} from './constants';
+
 export const auth0 = {
   clientId: process.env.AUTH0_CLIENT_ID || 'w3mBrhiXGsE3DsrSMgpAz2bS6VzLhOyJ',
   account: process.env.AUTH0_DOMAIN || 'parabol.auth0.com'
 };
-export const APP_NAME = 'Action';
 
 export const head = {
   titleTemplate: '%s | Parabol, Inc.',
   meta: [
     {name: 'description', content: 'Team transparency, made easy.'},
     {charset: 'utf-8'},
-    {property: 'og:site_name', content: 'Action'},
+    {property: 'og:site_name', content: APP_NAME},
 // {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
 // {property: 'og:image:width', content: '200'},
 // {property: 'og:image:height', content: '200'},
     {property: 'og:locale', content: 'en_US'},
-    {property: 'og:title', content: 'Action'},
+    {property: 'og:title', content: APP_NAME},
     {property: 'og:description', content: 'Team transparency, made easy.'},
     {property: 'og:card', content: 'summary'},
     {property: 'og:site', content: '@jrhusney'},
