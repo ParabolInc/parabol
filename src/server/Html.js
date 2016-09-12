1383aedc0933eca821b9e1bcd8d730ec4c9c4f90
mattkrick <matt.krick@gmail.com>
2016-09-12 10:11:22 -0700

    begin #263 extract font-awesome

--- src/server/Html.js
+++ src/server/Html.js
@@ -22,33 +22,40 @@ export default function Html({
   const initialState = `window.__INITIAL_STATE__ = ${JSON.stringify(store.getState())}`;
 
   const root = PROD && renderToString(
-    <LookRoot config={lookConfig}>
-      <Provider store={store}>
-        <RouterContext {...renderProps} />
-      </Provider>
-    </LookRoot>
-  );
-
+      <LookRoot config={lookConfig}>
+        <Provider store={store}>
+          <RouterContext {...renderProps} />
+        </Provider>
+      </LookRoot>
+    );
+  const fontAwesomeUrl = PROD ?
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css' :
+    '/static/css/font-awesome.css';
   return (
     <html>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="description" content="Team transparency, made easy." />
-        <style dangerouslySetInnerHTML={{__html: lookCSSToken}} id={lookConfig.styleElementId} />
-        <title>{title}</title>
-        {/* segment.io analytics */}
-        {process.env.SEGMENT_WRITE_KEY && <script type="text/javascript" async src={segmentCdnSrc} />}
-      </head>
-      <body>
-        <script dangerouslySetInnerHTML={{__html: initialState}} />
-        {PROD ?
-          <div id="root" dangerouslySetInnerHTML={{__html: root}}></div> :
-          <div id="root"></div>}
-        {PROD && <script dangerouslySetInnerHTML={{__html: manifest.text}} />}
-        {PROD && <script src={vendor.js} />}
-        <script src={PROD ? app.js : '/static/app.js'} />
-      </body>
+    <head>
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <meta property="description" content="Team transparency, made easy."/>
+      <style dangerouslySetInnerHTML={{__html: lookCSSToken}} id={lookConfig.styleElementId}/>
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href={fontAwesomeUrl}
+      />
+      <title>{title}</title>
+      {/* segment.io analytics */}
+      {process.env.SEGMENT_WRITE_KEY && <script type="text/javascript" async src={segmentCdnSrc}/>}
+    </head>
+    <body>
+    <script dangerouslySetInnerHTML={{__html: initialState}}/>
+    {PROD ?
+      <div id="root" dangerouslySetInnerHTML={{__html: root}}></div> :
+      <div id="root"></div>}
+    {PROD && <script dangerouslySetInnerHTML={{__html: manifest.text}}/>}
+    {PROD && <script src={vendor.js}/>}
+    <script src={PROD ? app.js : '/static/app.js'}/>
+    </body>
     </html>
   );
 }
