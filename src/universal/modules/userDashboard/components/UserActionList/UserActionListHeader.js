2f6d5c43eb7d1a569cc35606ec29808e9a972e59
Terry Acker <ackernaut@gmail.com>
2016-09-27 15:32:18 -0500

    More pixel-tweaking for list header control

--- src/universal/modules/userDashboard/components/UserActionList/UserActionListHeader.js
+++ src/universal/modules/userDashboard/components/UserActionList/UserActionListHeader.js
@@ -6,12 +6,18 @@ import ui from 'universal/styles/ui';
 import {Type} from 'universal/components';
 import FontAwesome from 'react-fontawesome';
 
+const height = '1.25rem';
+const lineHeight = height;
+
 const iconStyle = {
   ...ib,
   color: theme.palette.mid,
   fontSize: ui.iconSize,
+  height,
+  lineHeight,
   marginRight: '.25rem',
   textAlign: 'center',
+  verticalAlignt: 'middle',
   width: '1rem'
 };
 
@@ -21,7 +27,7 @@ const UserActionListEmpty = (props) => {
   return (
     <div className={styles.root}>
       <FontAwesome name="plus-circle" onClick={onCreateNewAction} style={iconStyle} />
-      <Type bold display="inlineBlock" lineHeight="1.125rem" onClick={onCreateNewAction} scale="s3" width="auto">
+      <Type bold display="inlineBlock" lineHeight={lineHeight} onClick={onCreateNewAction} scale="s3" width="auto">
         Add New Action
       </Type>
     </div>
@@ -35,7 +41,11 @@ UserActionListEmpty.propTypes = {
 UserActionListEmpty.styles = StyleSheet.create({
   root: {
     backgroundColor: ui.actionCardBgColor,
+    boxSizing: 'content-box',
     cursor: 'pointer',
+    fontSize: 0,
+    height,
+    lineHeight,
     padding: '.25rem .4375rem',
 
     ':hover': {
