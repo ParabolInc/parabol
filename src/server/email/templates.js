343f68ff1cbbcf2db100e0ca696e2447d0b296b9
Jordan Husney <jordan.husney@gmail.com>
2016-10-20 12:23:40 -0400

    Modified emailSSR to actually send email.
    Also added summaryEmail to email templates factory.

--- src/server/email/templates.js
+++ src/server/email/templates.js
@@ -1,7 +1,9 @@
+import summaryEmail from 'universal/modules/email/containers/SummaryEmail/index';
 import teamInvite from 'universal/modules/email/containers/TeamInvite/index';
 import welcomeEmail from 'universal/modules/email/containers/WelcomeEmail/index';
 
 export default ({
+  summaryEmail,
   teamInvite,
   welcomeEmail
 });
