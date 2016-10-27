ca9844f330b1b3bb861427f56c85d304d54ffc47
mattkrick <matt.krick@gmail.com>
2016-10-27 15:34:41 -0700

    remove makeSSRAppLink

--- src/universal/utils/makeHref.js
+++ src/universal/utils/makeHref.js
@@ -1,9 +1,7 @@
-import makeSSRAppLink from 'server/utils/makeAppLink';
-
 export default function makeHref(relativeLink) {
   if (typeof window !== 'undefined') {
     return `${window.location.origin}${relativeLink}`;
   }
   // default for SSR
-  return makeSSRAppLink(relativeLink);
+  return 'https://www.parabol.co'
 }
