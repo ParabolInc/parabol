9c392022748b67c712e0f09a1c2dc8eebac97a1e
Terry Acker <ackernaut@gmail.com>
2016-11-03 17:24:19 -0500

    Style up some user row action links

--- src/universal/modules/teamDashboard/components/TeamSettings/TeamSettings.js
+++ src/universal/modules/teamDashboard/components/TeamSettings/TeamSettings.js
@@ -1,25 +1,26 @@
 import React, {PropTypes} from 'react';
 import withStyles from 'universal/styles/withStyles';
 import {css} from 'aphrodite-local-styles/no-important';
+import appTheme from 'universal/styles/theme/appTheme';
 import {overflowTouch} from 'universal/styles/helpers';
 import {reduxForm} from 'redux-form';
-import Button from 'universal/components/Button/Button';
 import InviteUser from 'universal/components/InviteUser/InviteUser';
 import UserRow from 'universal/components/UserRow/UserRow';
 
 const TeamSettings = (props) => {
   const {teamMembers, styles} = props;
   const userRowActions = (user) => {
-    const removeUser = () =>
-      console.log(`remove ${user.preferredName}: ${user.id}`);
     return (
-      <div>
-        <Button
-          borderRadius=".25rem"
-          colorPalette="gray"
-          label="Remove" size="smallest"
-          onClick={removeUser}
-        />
+      <div className={css(styles.actionLinkBlock)}>
+        <div className={css(styles.actionLink)}>
+          Promote {user.preferredName} to Team Lead
+        </div>
+        <div className={css(styles.actionLink)}>
+          Remove
+        </div>
+        <div className={css(styles.actionLink)}>
+          Leave Team
+        </div>
       </div>
     );
   };
@@ -83,6 +84,26 @@ const styleThunk = () => ({
     position: 'absolute',
     right: 0,
     top: 0,
+  },
+
+  actionLinkBlock: {
+    fontSize: 0
+  },
+
+  actionLink: {
+    color: appTheme.palette.dark,
+    cursor: 'pointer',
+    display: 'inline-block',
+    fontSize: appTheme.typography.s3,
+    fontWeight: 700,
+    lineHeight: appTheme.typography.s5,
+    marginLeft: '1.25rem',
+    textDecoration: 'underline',
+    verticalAlign: 'middle',
+
+    ':hover': {
+      opacity: '.5'
+    }
   }
 });
 
