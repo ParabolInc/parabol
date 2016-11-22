32933690e6c2c36ca2e93a5a06ed59a2cabd25ff
Terry Acker <ackernaut@gmail.com>
2016-11-22 17:32:55 -0600

    Project drag layer min width per area

--- src/universal/containers/ProjectCard/ProjectDragLayer.js
+++ src/universal/containers/ProjectCard/ProjectDragLayer.js
@@ -23,11 +23,15 @@ function getItemStyles(props) {
   const {x, y} = currentOffset;
   const transform = `translate3d(${x}px, ${y}px, 0px)`;
   // NOTE: Widths are calculated based on the results of UI constants, but yeah, manual (TA)
+  const dashMinWidth = '9.9375rem';
+  const meetingMinWidth = '12.4375rem';
+  const minWidth = area === 'meeting' ? meetingMinWidth : dashMinWidth;
   const dashWidth = 'calc((100vw - 39.375rem) / 4)';
   const meetingWidth = 'calc((100vw - 25.25rem) / 4)';
   const width = area === 'meeting' ? meetingWidth : dashWidth;
   return {
     ...layerStyles,
+    minWidth,
     transform,
     WebkitTransform: transform,
     width
