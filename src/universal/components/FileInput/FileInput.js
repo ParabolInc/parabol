8db12ed61b3855f59788a12858d699929af0a84a
Jordan Husney <jordan.husney@gmail.com>
2017-01-06 00:28:35 -0500

    lint

--- src/universal/components/FileInput/FileInput.js
+++ src/universal/components/FileInput/FileInput.js
@@ -1,5 +1,4 @@
 import React, {Component, PropTypes} from 'react';
-import FieldBlock from 'universal/components/FieldBlock/FieldBlock';
 import FieldHelpText from 'universal/components/FieldHelpText/FieldHelpText';
 
 class FileInput extends Component {
