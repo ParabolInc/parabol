2c1945e24d65b359f7db0814a1fa1874735f5eb0
Terry Acker <ackernaut@gmail.com>
2017-01-16 11:49:47 -0600

    Adds new notification bars

---
+++ src/universal/components/ActiveTrialNotificationBar/ActiveTrialNotificationBar.js
@@ -0,0 +1,65 @@
+import React, {PropTypes} from 'react';
+import withStyles from 'universal/styles/withStyles';
+import {css} from 'aphrodite-local-styles/no-important';
+import ui from 'universal/styles/ui';
+import {Link} from 'react-router';
+import FontAwesome from 'react-fontawesome';
+import NotificationBar from 'universal/components/NotificationBar/NotificationBar';
+
+const ActiveTrialNotificationBar = (props) => {
+  const {accountLink, styles} = props;
+  const iconStyle = {
+    fontSize: ui.iconSize
+  };
+  return (
+    <NotificationBar colorPalette="cool">
+      <div className={css(styles.inlineBlock, styles.message)}>
+        Add 30 days to your trial!
+      </div>
+      <div className={css(styles.inlineBlock)}>
+        <Link className={css(styles.link)} title="Go to My Account" to={accountLink}>
+          <span className={css(styles.underline)}>Go to My Account</span> <FontAwesome name="arrow-right" style={iconStyle} />
+        </Link>
+      </div>
+    </NotificationBar>
+  );
+};
+
+ActiveTrialNotificationBar.propTypes = {
+  accountLink: PropTypes.string,
+  styles: PropTypes.object
+};
+
+const styleThunk = () => ({
+  inlineBlock: {
+    display: 'inline-block',
+    margin: '0 1rem',
+    verticalAlign: 'top'
+  },
+
+  link: {
+    color: 'inherit',
+    textDecoration: 'none',
+
+    ':hover': {
+      color: 'inherit',
+      opacity: '.5'
+    },
+    ':focus': {
+      color: 'inherit',
+      opacity: '.5'
+    },
+    paddingRight: '1rem'
+  },
+
+  underline: {
+    textDecoration: 'underline'
+  },
+
+  message: {
+    fontWeight: 700,
+    paddingLeft: '1rem'
+  }
+});
+
+export default withStyles(styleThunk)(ActiveTrialNotificationBar);
---
+++ src/universal/components/ExpiredTrialNotificationBar/ExpiredTrialNotificationBar.js
@@ -0,0 +1,67 @@
+import React, {PropTypes} from 'react';
+import withStyles from 'universal/styles/withStyles';
+import {css} from 'aphrodite-local-styles/no-important';
+import appTheme from 'universal/styles/theme/appTheme';
+import ui from 'universal/styles/ui';
+import {Link} from 'react-router';
+import FontAwesome from 'react-fontawesome';
+import NotificationBar from 'universal/components/NotificationBar/NotificationBar';
+
+const ExpiredTrialNotificationBar = (props) => {
+  const {accountLink, styles} = props;
+  const iconStyle = {
+    fontSize: ui.iconSize
+  };
+  return (
+    <NotificationBar colorPalette="light">
+      <div className={css(styles.inlineBlock, styles.message)}>
+        Your trial has expired!
+      </div>
+      <div className={css(styles.inlineBlock)}>
+        <Link className={css(styles.link)} title="Add Billing Information" to={accountLink}>
+          <span className={css(styles.underline)}>Add Billing Information</span> <FontAwesome name="arrow-right" style={iconStyle} />
+        </Link>
+      </div>
+    </NotificationBar>
+  );
+};
+
+ExpiredTrialNotificationBar.propTypes = {
+  accountLink: PropTypes.string,
+  styles: PropTypes.object
+};
+
+const styleThunk = () => ({
+  inlineBlock: {
+    color: appTheme.palette.warm,
+    display: 'inline-block',
+    margin: '0 1rem',
+    verticalAlign: 'top'
+  },
+
+  link: {
+    color: 'inherit',
+    textDecoration: 'none',
+
+    ':hover': {
+      color: 'inherit',
+      opacity: '.5'
+    },
+    ':focus': {
+      color: 'inherit',
+      opacity: '.5'
+    },
+    paddingRight: '1rem'
+  },
+
+  underline: {
+    textDecoration: 'underline'
+  },
+
+  message: {
+    fontWeight: 700,
+    paddingLeft: '1rem'
+  }
+});
+
+export default withStyles(styleThunk)(ExpiredTrialNotificationBar);
