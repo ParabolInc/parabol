569f4d5794aaf8250699be9186b9a53415f20545
mattkrick <matt.krick@gmail.com>
2017-02-07 14:31:10 -0800

    add content to proj input

--- src/server/graphql/models/Project/projectSchema.js
+++ src/server/graphql/models/Project/projectSchema.js
@@ -104,6 +104,7 @@ export const ProjectInput =  new GraphQLInputObjectType({
   name: 'ProjectInput',
   fields: () => ({
     id: {type: GraphQLID, description: 'The unique team ID'},
+    content: {type: GraphQLString},
     name: {type: GraphQLString, description: 'The name of the team'},
     orgId: {type: GraphQLID, description: 'The unique orginization ID that pays for the team'},
     teamMemberId: {type: GraphQLID},
