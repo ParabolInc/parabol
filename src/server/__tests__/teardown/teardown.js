15ed0d805303b7d12a97ab7f412544080e6ca42f
mattkrick <matt.krick@gmail.com>
2017-02-27 18:15:59 -0800

    occassionally cleanup DB, no force exit

---
+++ src/server/__tests__/teardown/teardown.js
@@ -0,0 +1,20 @@
+import getRethink from '../../database/rethinkDriver';
+import junit from 'jest-junit-reporter';
+
+export default async function teardown(results) {
+  junit(results);
+
+  // maybe consider thinking about flushing the DB
+  if (process.pid % 10 === 0) {
+    const r = getRethink();
+    const docs = await r.table('User').count();
+    if (docs > 1000) {
+      // flush the DB
+      const list = await r.tableList().filter((name) => name !== '_migrations');
+      const promises = list.map((table) => r.table(table).delete());
+      await Promise.all(promises);
+    }
+    await r.getPoolMaster().drain()
+  }
+  process.exit()
+};
--- src/server/billing/debug.js
+++ src/server/billing/debug.js
@@ -53,9 +53,9 @@ foo('invoices', 'retrieveLines', 'in_19pVJzFLaFINmHnBC5uwabBm');
 //   const {user, organization} = await mockDB.init()
 //     // .newNotification(undefined, {type: TRIAL_EXPIRED})
 //     .org(0, {periodEnd: new Date(now.getTime() + 1)});
-//   fs.writeFile('./debug.json', JSON.stringify(organization), function(err) {
-//     if (err) console.log(err);
-//   });
+  fs.writeFile('./debug.json', JSON.stringify(organization), function(err) {
+    if (err) console.log(err);
+  });
 // }
 // foo();
 // const orgDoc = {
