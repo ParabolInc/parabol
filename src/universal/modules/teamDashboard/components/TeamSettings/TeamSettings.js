a83d38d77f6970172b2043a52a8e72d5192603ee
Nolan Dey <ndey96@gmail.com>
2017-03-02 19:53:08 -0500

    Add delete team button to TeamSettings

--- src/universal/modules/teamDashboard/components/TeamSettings/TeamSettings.js
+++ src/universal/modules/teamDashboard/components/TeamSettings/TeamSettings.js
@@ -6,12 +6,14 @@ import {overflowTouch} from 'universal/styles/helpers';
 import {reduxForm} from 'redux-form';
 import InviteUser from 'universal/components/InviteUser/InviteUser';
 import UserRow from 'universal/components/UserRow/UserRow';
+import Row from 'universal/components/Row/Row';
 import fromNow from 'universal/utils/fromNow';
 import {cashay} from 'cashay';
 import RemoveTeamMemberModal from 'universal/modules/teamDashboard/components/RemoveTeamMemberModal/RemoveTeamMemberModal';
 import PromoteTeamMemberModal from 'universal/modules/teamDashboard/components/PromoteTeamMemberModal/PromoteTeamMemberModal';
 import LeaveTeamModal from 'universal/modules/teamDashboard/components/LeaveTeamModal/LeaveTeamModal';
 import {showSuccess} from 'universal/modules/toast/ducks/toastDuck';
+import Button from 'universal/components/Button/Button';
 
 const TeamSettings = (props) => {
   const {
@@ -103,6 +105,17 @@ const TeamSettings = (props) => {
       </div>
     );
   };
+
+  const deleteTeam = () => {console.log('BLAWH')}
+  const deleteTeamButton = (
+    <Button
+      colorPalette="mid"
+      label="Delete Team"
+      size="smallest"
+      onClick={deleteTeam}
+    />
+  )
+
   return (
     <div className={css(styles.root)}>
       <div className={css(styles.inviteBlock)}>
@@ -150,6 +163,9 @@ const TeamSettings = (props) => {
             );
           })
           }
+          <Row
+            children={deleteTeamButton}
+          />
         </div>
       </div>
     </div>
