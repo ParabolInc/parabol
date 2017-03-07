01f51f13235bc2b7175061ff5bd68af70975fb51
mattkrick <matt.krick@gmail.com>
2017-03-07 15:51:10 -0800

    lint

--- src/server/__tests__/utils/expectAsyncToThrow.js
+++ src/server/__tests__/utils/expectAsyncToThrow.js
@@ -3,7 +3,7 @@ export default async function expectAsyncToThrow(promise, idLookup = []) {
     await promise;
     throw new Error(`Jest: promise did not throw! ${Math.random()}`);
   } catch (e) {
-    idLookup.forEach((key,idx) => e.message = e.message.replace(key, idx));
+    idLookup.forEach((key, idx) => { e.message = e.message.replace(key, idx); });
     expect(() => { throw e; }).toThrowErrorMatchingSnapshot();
   }
 };
