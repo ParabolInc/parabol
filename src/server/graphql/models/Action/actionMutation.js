2975295cb0283ee629a805d88030d9750166a1b9
mattkrick <matt.krick@gmail.com>
2017-03-07 20:59:44 -0800

    add deleteAction unit tests

--- src/server/graphql/models/Action/actionMutation.js
+++ src/server/graphql/models/Action/actionMutation.js
@@ -7,31 +7,12 @@ import {
 import {requireSUOrTeamMember, requireWebsocket} from 'server/utils/authorization';
 import updateAction from 'server/graphql/models/Action/updateAction/updateAction';
 import createAction from 'server/graphql/models/Action/createAction/createAction';
+import deleteAction from 'server/graphql/models/Action/deleteAction/deleteAction';
 
 export default {
   updateAction,
   createAction,
-  deleteAction: {
-    type: GraphQLBoolean,
-    description: 'Delete an action',
-    args: {
-      actionId: {
-        type: new GraphQLNonNull(GraphQLID),
-        description: 'The actionId (teamId::shortid) to delete'
-      }
-    },
-    async resolve(source, {actionId}, {authToken, socket}) {
-      const r = getRethink();
-
-      // AUTH
-      const [teamId] = actionId.split('::');
-      requireSUOrTeamMember(authToken, teamId);
-      requireWebsocket(socket);
-
-      // RESOLUTION
-      await r.table('Action').get(actionId).delete();
-    }
-  },
+  deleteAction,
   makeProject: {
     type: GraphQLBoolean,
     description: 'Turn an action into a project',
