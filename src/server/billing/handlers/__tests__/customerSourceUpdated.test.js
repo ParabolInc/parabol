ce9395d86d25bf730b329cd1509dd3e6dc2b43f9
mattkrick <matt.krick@gmail.com>
2017-03-11 13:08:13 -0800

    debug CI

--- src/server/billing/handlers/__tests__/customerSourceUpdated.test.js
+++ src/server/billing/handlers/__tests__/customerSourceUpdated.test.js
@@ -11,25 +11,52 @@ console.error = jest.fn();
 describe('customerSourceUpdated', () => {
   test('handles stripe updating the credit card', async() => {
     // SETUP
+    try {
+      const r = getRethink();
+    } catch(e) {
+      throw e
+    }
     const r = getRethink();
     const dynamicSerializer = new DynamicSerializer();
     const mockDB = new MockDB();
     const oldToken = 'tok_4012888888881881';
     const oldCard = creditCardByToken[oldToken];
+    try {
+      const {organization} = await mockDB
+        .newOrg({creditCard: oldCard});
+    } catch(e) {
+      throw e
+    }
     const {organization} = await mockDB
       .newOrg({creditCard: oldCard});
+    console.log('made mockdb')
     const org = organization[0];
     stripe.__setMockData(org);
     const {id: orgId, stripeId} = org;
     stripe.__db.customers[stripeId].sources.data[0].exp_year = '2030';
 
     // TEST
-    await customerSourceUpdated(stripeId);
+    try {
+      await customerSourceUpdated(stripeId);
+    } catch(e) {
+      console.log('mut itself', e)
+      throw e
+    }
 
     // VERIFY
+    console.log('pre-verify')
+    try {
+
     const db = await fetchAndSerialize({
       organization: r.table('Organization').get(orgId),
     }, dynamicSerializer);
+    } catch(e) {
+      throw e
+    }
+    const db = await fetchAndSerialize({
+      organization: r.table('Organization').get(orgId),
+    }, dynamicSerializer);
+    console.log('post fetch seri')
     expect(db).toMatchSnapshot();
     expect(stripe.__snapshot(org.stripeId, dynamicSerializer)).toMatchSnapshot();
   });
