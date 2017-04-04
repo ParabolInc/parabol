a19960b42eae5314337ab7d63974ec163e2d1c7e
Nolan Dey <ndey96@gmail.com>
2017-04-03 23:07:07 -0400

    Remove state from ArchiveTeam and stick it in ArchiveTeamContainer

--- src/universal/modules/teamDashboard/components/ArchiveTeam/ArchiveTeam.js
+++ src/universal/modules/teamDashboard/components/ArchiveTeam/ArchiveTeam.js
@@ -1,77 +1,61 @@
-import React, {Component, PropTypes} from 'react';
+import React, {PropTypes} from 'react';
 import {reduxForm, Field} from 'redux-form';
 import InputField from 'universal/components/InputField/InputField';
-import {cashay} from 'cashay';
 import Button from 'universal/components/Button/Button';
 
-@reduxForm({form: 'archiveTeamConfirmation'})
-export default class ArchiveTeamConfirmation extends Component {
+const ArchiveTeam = (props) => {
+  const {
+    teamName,
+    handleSubmit,
+    handleClick,
+    handleFormBlur,
+    handleFormSubmit,
+    showConfirmationField,
+    submitting
+  } = props;
 
-  static propTypes = {
-    teamId: PropTypes.string.isRequired,
-    teamName: PropTypes.string.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    router: PropTypes.object.isRequired,
-    submitting: PropTypes.bool.isRequired
-  }
+  const archiveTeamClick = () => {
+    if (!submitting) handleClick();
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {showConfirmationField: false};
-  }
+  const validate = (value) => {
+    return value !== teamName && 'The team name entered was incorrect';
+  };
 
-  archiveTeamClick = () => {
-    const {submitting} = this.props;
-    if (!submitting) {
-      this.setState({showConfirmationField: true});
-    }
-  }
+  return (
+    <div>
+      {!showConfirmationField ?
+        <Button
+          colorPalette="warm"
+          label="Archive Team"
+          size="smallest"
+          onClick={archiveTeamClick}
+        /> :
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <Field
+            autoFocus
+            onBlur={handleFormBlur}
+            colorPalette="gray"
+            component={InputField}
+            name="archivedTeamName"
+            placeholder="Type the team name to confirm"
+            type="text"
+            validate={validate}
+          />
+        </form>
+      }
+    </div>
+  );
+};
 
-  validate = (value) => {
-    const {teamName} = this.props;
-    return teamName !== value && 'The team name entered was incorrect';
-  }
+ArchiveTeam.propTypes = {
+  teamName: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleFormSubmit: PropTypes.func.isRequired,
+  handleFormBlur: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  submitting: PropTypes.bool.isRequired,
+  showConfirmationField: PropTypes.bool.isRequired
+};
 
-  archiveTeam = () => {
-    return new Promise((resolve) => {
-      const {teamId, router} = this.props;
-      const variables = {teamId};
-      cashay.mutate('archiveTeam', {variables});
-      router.push('/me');
-      resolve();
-    });
-  }
-
-  formBlurred = () => {
-    this.setState({showConfirmationField: false});
-  }
-
-  render() {
-    const {handleSubmit} = this.props;
-    const {showConfirmationField} = this.state;
-    return (
-      <div>
-        {!showConfirmationField ?
-          <Button
-            colorPalette="warm"
-            label="Archive Team"
-            size="smallest"
-            onClick={this.archiveTeamClick}
-          /> :
-          <form onSubmit={handleSubmit(this.archiveTeam)}>
-            <Field
-              autoFocus
-              onBlur={this.formBlurred}
-              colorPalette="gray"
-              component={InputField}
-              name="archivedTeamName"
-              placeholder="Type the team name to confirm"
-              type="text"
-              validate={this.validate}
-            />
-          </form>
-        }
-      </div>
-    );
-  }
-}
+export default reduxForm({form: 'archiveTeamConfirmation'})(ArchiveTeam);
--- src/universal/modules/teamDashboard/containers/ArchiveTeamContainer/ArchiveTeamContainer.js
+++ src/universal/modules/teamDashboard/containers/ArchiveTeamContainer/ArchiveTeamContainer.js
@@ -1,27 +1,51 @@
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {withRouter} from 'react-router';
 import ArchiveTeam from 'universal/modules/teamDashboard/components/ArchiveTeam/ArchiveTeam';
+import {cashay} from 'cashay';
 
-const ArchiveTeamContainer = (props) => {
-  const {
-    teamId,
-    teamName,
-    router
-  } = props;
+@withRouter
+export default class ArchiveTeamContainer extends Component {
 
-  return (
-    <ArchiveTeam
-      teamId={teamId}
-      teamName={teamName}
-      router={router}
-    />
-  );
-};
+  static propTypes = {
+    teamId: PropTypes.string.isRequired,
+    teamName: PropTypes.string.isRequired,
+    router: PropTypes.object.isRequired
+  }
 
-ArchiveTeamContainer.propTypes = {
-  teamId: PropTypes.string.isRequired,
-  teamName: PropTypes.string.isRequired,
-  router: PropTypes.object.isRequired
-};
+  constructor(props) {
+    super(props);
+    this.state = {showConfirmationField: false};
+  }
 
-export default withRouter(ArchiveTeamContainer);
+  handleClick = () => {
+    this.setState({showConfirmationField: true});
+  }
+
+  formBlurred = () => {
+    this.setState({showConfirmationField: false});
+  }
+
+  archiveTeam = () => {
+    return new Promise((resolve) => {
+      const {teamId, router} = this.props;
+      const variables = {teamId};
+      cashay.mutate('archiveTeam', {variables});
+      router.push('/me');
+      resolve();
+    });
+  }
+
+  render() {
+    const {teamName} = this.props;
+    const {showConfirmationField} = this.state;
+    return (
+      <ArchiveTeam
+        teamName={teamName}
+        handleClick={this.handleClick}
+        handleFormBlur={this.formBlurred}
+        handleFormSubmit={this.archiveTeam}
+        showConfirmationField={showConfirmationField}
+      />
+    );
+  }
+}
