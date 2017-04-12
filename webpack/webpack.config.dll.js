6ea58fc01efb33c5fefd55b2dc9ea3a9e7c56514
Terry Acker <ackernaut@gmail.com>
2017-04-12 09:38:56 -0500

    Webpack config for dll vendors

--- webpack/webpack.config.dll.js
+++ webpack/webpack.config.dll.js
@@ -15,6 +15,7 @@ module.exports = {
       'graphiql',
       'graphql',
       'html-entities', // server
+      'markdown-it',
       'react',
       'react-async-hoc',
       'react-copy-to-clipboard',
@@ -22,14 +23,13 @@ module.exports = {
       'react-dnd-html5-backend',
       'react-dom',
       'react-fontawesome',
+      'react-githubish-mentions',
       'react-helmet',
       'react-hotkey-hoc',
-      'react-markdown',
       'react-notification-system',
       'react-portal-hoc',
       'react-redux',
       'react-router',
-      'react-textarea-autosize',
       'redbox-react',
       'redux',
       'redux-form',
@@ -69,4 +69,3 @@ module.exports = {
     ]
   }
 };
-
