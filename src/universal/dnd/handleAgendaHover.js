1fc362b2a6a94b815ae713b23f53d776d8327182
mattkrick <matt.krick@gmail.com>
2017-04-12 15:41:10 -0700

    fix #896 sorting first agenda item

--- src/universal/dnd/handleAgendaHover.js
+++ src/universal/dnd/handleAgendaHover.js
@@ -1,23 +1,24 @@
 import {cashay} from 'cashay';
 import checkDragForUpdate from 'universal/dnd/checkDragForUpdate';
-import {DND_THROTTLE} from 'universal/utils/constants';
+import {DND_THROTTLE, AGENDA_ITEMS} from 'universal/utils/constants';
+import actionMeeting from 'universal/modules/meeting/helpers/actionMeeting';
 
 let lastSentAt = 0;
 export default function handleAgendaHover(targetProps, monitor) {
   const now = new Date();
   if (lastSentAt > (now - DND_THROTTLE)) return;
-  const {agenda, agendaPhaseItem, dragState, teamId} = targetProps;
+  const {agenda, agendaPhaseItem, dragState, facilitatorPhase, teamId} = targetProps;
   // dont let current or previous items get dragged
   const {id} = monitor.getItem();
-  const currentItemIdx = agenda.findIndex((i) => i.isComplete === false);
+  const startedAgendaItems = actionMeeting[facilitatorPhase].index >= actionMeeting[AGENDA_ITEMS].index;
+  const currentItemIdx = startedAgendaItems ? agenda.findIndex((i) => i.isComplete === false) : -1;
   const dragItemIdx = agenda.findIndex((i) => i.id === id);
   if (dragItemIdx <= currentItemIdx) return;
-
 
   const updatedVariables = checkDragForUpdate(monitor, dragState, agenda, false);
   if (!updatedVariables) return;
   const {updatedDoc: updatedAgendaItem} = updatedVariables;
-  if (agendaPhaseItem) {
+  if (startedAgendaItems && agendaPhaseItem) {
     const currentItem = agenda[agendaPhaseItem - 1];
     const {sortOrder: minSort} = currentItem;
     if (updatedAgendaItem.sortOrder <= minSort) {
