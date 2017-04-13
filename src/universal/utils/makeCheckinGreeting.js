37f1eaffaae91305bda3d2a192a103a07d558727
Terry Acker <ackernaut@gmail.com>
2017-04-13 10:43:19 -0500

    First pass, greeting languages

--- src/universal/utils/makeCheckinGreeting.js
+++ src/universal/utils/makeCheckinGreeting.js
@@ -1,19 +1,64 @@
 const greetings = [
-  'Hola',
-  'Bonjour',
-  'Guten tag',
-  'Ciao',
-  'Namaste',
-  'Jambo',
-  'Oi',
-  'Salut',
-  'Hallo',
-  'Hej',
-  'Nǐ hǎo',
-  'Yeoboseyo',
-  'Shalom',
-  'Merhaba',
-  'Zdravo'
+  {
+    content: 'Hola',
+    language: 'Spanish'
+  },
+  {
+    content: 'Bonjour',
+    language: 'French'
+  },
+  {
+    content: 'Guten tag',
+    language: 'German'
+  },
+  {
+    content: 'Ciao',
+    language: 'Italian'
+  },
+  {
+    content: 'Namaste',
+    language: 'Hindi'
+  },
+  {
+    content: 'Jambo',
+    language: 'Swahili'
+  },
+  {
+    content: 'Oi',
+    language: 'Portuguese'
+  },
+  {
+    content: 'Salut',
+    language: 'Catalan'
+  },
+  {
+    content: 'Hallo',
+    language: 'German'
+  },
+  {
+    content: 'Hej',
+    language: 'Swedish'
+  },
+  {
+    content: 'Nǐ hǎo',
+    language: 'Chinese'
+  },
+  {
+    content: 'Yeoboseyo',
+    language: 'Korean'
+  },
+  {
+    content: 'Shalom',
+    language: 'Hebrew'
+  },
+  {
+    content: 'Merhaba',
+    language: 'Turkish'
+  },
+  {
+    content: 'Zdravo',
+    language: 'Slovenian'
+  }
 ];
 
 const questions = [
@@ -27,7 +72,8 @@ const questions = [
   'What are you cooking next',
   'Learn any new slang lately',
   'Heard any good advice lately',
-  'If you could have one superpower today, what would it be'
+  'If you could have one superpower today, what would it be',
+  'What’s got your attention this week'
 ];
 
 export const makeCheckinGreeting = (week) => {
