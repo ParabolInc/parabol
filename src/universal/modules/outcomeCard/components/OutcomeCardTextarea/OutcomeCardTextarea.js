4c959e3f7f3f1ea3e07966a64640610c64dbe7bc
Terry Acker <ackernaut@gmail.com>
2017-04-13 15:40:47 -0500

    Card min heights updated

--- src/universal/modules/outcomeCard/components/OutcomeCardTextarea/OutcomeCardTextarea.js
+++ src/universal/modules/outcomeCard/components/OutcomeCardTextarea/OutcomeCardTextarea.js
@@ -198,8 +198,6 @@ const descriptionBreakpoint = '@media (min-width: 90rem)';
 const styleThunk = () => ({
   content: {
     ...contentBase,
-    // TODO: only set this during the agenda round to match the card placeholder row height?
-    // minHeight: '3.3125rem',
     padding: `0 ${ui.cardPaddingBase} .1875rem`,
 
     ':focus': {
