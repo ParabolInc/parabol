ddb762786d8085513086f37d624d571b8dad4165
Terry Acker <ackernaut@gmail.com>
2017-05-16 15:02:18 -0500

    revert change on this file

--- src/server/sendICS.js
+++ src/server/sendICS.js
@@ -4,6 +4,6 @@ export default async function sendICS(req, res) {
   const {teamName, createdAt, meetingUrl} = req.query;
   const startDate = new Date(Number(createdAt));
   const icsText = createICS(startDate, meetingUrl, teamName);
-  res.set('Content-Type', 'text/calendar').set('Content-disposition', 'attachment; filename=Action Meeting.ics');
+  res.set('Content-Type', 'text/calendar').set('Content-disposition', 'attachment; filename=Parabol Action Meeting.ics');
   res.send(icsText);
 }
