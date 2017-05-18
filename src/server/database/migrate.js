d377b55689b3abe2ebd2db6059e6068c7c4cfc1d
Matt Krick <matt.krick@gmail.com>
2017-05-17 20:12:52 -0700

    fix migrate.js

--- src/server/database/migrate.js
+++ src/server/database/migrate.js
@@ -4,15 +4,24 @@ import {parse} from 'url';
 
 getDotenv();
 
-const [,, direction, count = '--all'] = process.argv;
-const all = count === '--all';
-if (process.env.NODE_ENV === 'test') {
-  console.log('NODE_ENV is test, loading .env.test...');
-}
+const startMigration = async () => {
+  const [,, direction, count] = process.argv;
+  const all = count === '--all';
+  if (process.env.NODE_ENV === 'test') {
+    console.log('NODE_ENV is test, loading .env.test...');
+  }
 
-const {hostname, port, path} = parse(process.env.RETHINKDB_URL);
-process.env.host = hostname;
-process.env.port = port;
-process.env.db = path.slice(1);
-process.env.r = process.cwd();
-migrate[direction]({all, root: __dirname});
+  const {hostname, port, path} = parse(process.env.RETHINKDB_URL);
+  process.env.host = hostname;
+  process.env.port = port;
+  process.env.db = path.slice(1);
+  process.env.r = process.cwd();
+
+  try {
+    await migrate[direction]({all, root: __dirname});
+  } finally {
+    process.exit();
+  }
+};
+
+startMigration();
---
+++ src/server/database/migrations/20170316073600-segmentIdentify.js
@@ -0,0 +1,7 @@
+exports.up = async (r) => {
+
+};
+
+exports.down = async (r) => {
+
+};
