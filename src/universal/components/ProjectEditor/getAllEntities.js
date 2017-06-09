32f433cebfae63a135e7c23ee71013ad5b3a9d95
Matt Krick <matt.krick@gmail.com>
2017-06-09 14:37:18 -0700

    add getAllEntities

---
+++ src/universal/components/ProjectEditor/getAllEntities.js
@@ -0,0 +1,21 @@
+// hopefully draft adds this to their API because it's just silly
+const getAllEntities = (contentState) => {
+  const entityIds = new Set();
+  const blockMap = contentState.getBlockMap();
+  blockMap.forEach((block) => {
+    const charList = block.getCharacterList();
+    charList.forEach((charMeta) => {
+      const entityId = charMeta.getEntity();
+      if (entityId) {
+        entityIds.add(entityId);
+      }
+    })
+  });
+  const entities = [];
+  entityIds.forEach((id) => {
+    entities.push(contentState.getEntity(id))
+  });
+  return entities;
+};
+
+export default getAllEntities;
