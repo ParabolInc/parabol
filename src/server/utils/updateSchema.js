c5abb103c10ebc774e301feff4c75f4f05c124d3
Matt Krick <matt.krick@gmail.com>
2017-07-13 10:49:28 -0700

    fix build

--- src/server/utils/updateSchema.js
+++ src/server/utils/updateSchema.js
@@ -3,11 +3,15 @@ import {graphql, printSchema, introspectionQuery} from 'graphql';
 import path from 'path';
 import schema from 'server/graphql/rootSchema';
 
-const schemaPath = path.join(__dirname, '../../../build/schema.graphql');
-const jsonPath = path.join(__dirname, '../../../build/schema.json');
+const buildDir = path.join(process.cwd(), 'build');
+const schemaPath = path.join(buildDir, 'schema.graphql');
+const jsonPath = path.join(buildDir, 'schema.json');
 
 (async () => {
   const result = await graphql(schema, introspectionQuery);
+  if (!fs.existsSync(buildDir)) {
+    fs.mkdirSync(buildDir);
+  }
   fs.writeFileSync(schemaPath, printSchema(schema));
   // use json for IDE plugins
   fs.writeFileSync(jsonPath, JSON.stringify(result, null, 2));
