87a40d126561d537b78a13a9cef456b0d3849afa
Matt Krick <matt.krick@gmail.com>
2017-07-31 14:45:11 -0700

    add localhost fallback

--- src/server/integrations/handleGitHubEntry.js
+++ src/server/integrations/handleGitHubEntry.js
@@ -4,11 +4,12 @@ const handleGitHubEntry = (req, res) => {
   const {query} = req;
   const delimiterIdx = query.state.indexOf('::');
   const origin = query.state.substring(0, delimiterIdx);
+  const publicOrigin = origin.indexOf('localhost') !== -1 ? 'http://dev.parabol.ultrahook.com' : origin;
   const qs = {
     ...query,
     state: query.state.substring(delimiterIdx + 2)
   };
-  const newUrl = `${origin}/auth/github`;
+  const newUrl = `${publicOrigin}/auth/github`;
   req.pipe(request({qs, uri: newUrl})).pipe(res);
 };
 
