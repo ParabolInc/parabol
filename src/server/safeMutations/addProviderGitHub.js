be9e4c7174975e9a42fca006408b65f3ac0d6224
Matt Krick <matt.krick@gmail.com>
2017-07-31 14:12:21 -0700

    remove redirect_uri for GH

--- src/server/safeMutations/addProviderGitHub.js
+++ src/server/safeMutations/addProviderGitHub.js
@@ -59,7 +59,7 @@ const addProviderGitHub = async (code, teamId, userId) => {
     client_id: process.env.GITHUB_CLIENT_ID,
     client_secret: process.env.GITHUB_CLIENT_SECRET,
     code,
-    redirect_uri: makeAppLink('auth/github/entry')
+    //redirect_uri: makeAppLink('auth/github/entry')
   };
   const uri = `https://github.com/login/oauth/access_token?${stringify(queryParams)}`;
   const ghRes = await fetch(uri, postOptions);
