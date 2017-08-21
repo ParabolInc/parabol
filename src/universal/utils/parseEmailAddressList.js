d3b410bd53b2968b9cba2a472f4606ae1b88778e
Matt Krick <matt.krick@gmail.com>
2017-08-21 15:14:07 -0700

    solve for null case on #1206

--- src/universal/utils/parseEmailAddressList.js
+++ src/universal/utils/parseEmailAddressList.js
@@ -1,6 +1,6 @@
 import emailAddresses from 'email-addresses';
 
-const parseEmailAddressList = (rawStr) => {
+const parseEmailAddressList = (rawStr = '') => {
   // this breaks RFC5322 standards, but people are not standard :-(
   const commaDelimStr = rawStr.replace(';', ',');
   return emailAddresses.parseAddressList(commaDelimStr);
