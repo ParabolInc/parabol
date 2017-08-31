fa80caae397f4fe20d64b33bebc2b7d60f04f6e4
Matt Krick <matt.krick@gmail.com>
2017-08-31 12:15:09 -0700

    add tests for unicode

--- src/universal/utils/draftjs/__tests__/truncateCard.test.js
+++ src/universal/utils/draftjs/__tests__/truncateCard.test.js
@@ -41,6 +41,19 @@ describe('truncateCard', () => {
     const outStr = res.getPlainText();
     expect(outStr).toEqual(inStr);
   });
+  test('does not truncate when emoji takes char 51 and 52', async () => {
+    // SETUP
+    const inStr = 'this is a string that is exactly 52 chars in lengt😀';
+    const contentState = ContentState.createFromText(inStr);
+    const content = JSON.stringify(convertToRaw(contentState));
+
+    // TEST
+    const res = truncateCard(content);
+
+    // VERIFY
+    const outStr = res.getPlainText();
+    expect(outStr).toEqual(inStr);
+  });
   test('truncates last block when max blocks in exceeded', async () => {
     // SETUP
     const inStr = 'this\nis\na\nstring\nthat\nis exactly 52 chars in lengthx';
@@ -81,6 +94,20 @@ describe('truncateCard', () => {
     // VERIFY
     const outStr = res.getPlainText();
     const expectedStr = 'this\nis\na\nstring\nthat is exactly 52 chars in leng...';
+    expect(outStr).toEqual(expectedStr);
+  });
+  test('truncates the full unicode, not half', async () => {
+    // SETUP
+    const inStr = 'this is a string that is exactly 52 chars in length😀';
+    const contentState = ContentState.createFromText(inStr);
+    const content = JSON.stringify(convertToRaw(contentState));
+
+    // TEST
+    const res = truncateCard(content);
+
+    // VERIFY
+    const outStr = res.getPlainText();
+    const expectedStr = 'this is a string that is exactly 52 chars in leng...';
     expect(outStr).toEqual(expectedStr);
   });
 });
