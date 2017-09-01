312c5488f03d225e5e4017fda39ae7de3ed5d93b
Terry Acker <ackernaut@gmail.com>
2017-09-01 10:37:20 -0500

    patch rejoin button, fresh yarnage

--- src/universal/modules/meeting/components/RejoinFacilitatorButton/RejoinFacilitatorButton.js
+++ src/universal/modules/meeting/components/RejoinFacilitatorButton/RejoinFacilitatorButton.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import withStyles from 'universal/styles/withStyles';
 import {css} from 'aphrodite-local-styles/no-important';
+import ui from 'universal/styles/ui';
 import Button from 'universal/components/Button/Button';
 
 const RejoinFacilitatorButton = (props) => {
@@ -13,7 +14,7 @@ const RejoinFacilitatorButton = (props) => {
         depth={2}
         icon="user"
         label="Rejoin Facilitator"
-      onClick={onClickHandler}
+        onClick={onClickHandler}
         size="small"
       />
     </div>
