840d8bbced5cbca78f7130b57d10e2bc571cd974
Terry Acker <ackernaut@gmail.com>
2017-09-11 17:14:48 -0500

    meeting updates, fixes #1202, #1282, #1023

--- src/universal/modules/email/components/SummaryEmail/index.js
+++ src/universal/modules/email/components/SummaryEmail/index.js
@@ -3,12 +3,12 @@ import Oy from 'oy-vey';
 import {summaryEmailText} from './SummaryEmail';
 import SummaryEmailContainer from 'universal/modules/email/containers/SummaryEmail/SummaryEmailContainer';
 import makeDateString from 'universal/utils/makeDateString';
-
+import {MEETING_NAME} from 'universal/utils/constants';
 
 export default (props) => {
   const {meeting: {teamName, endedAt}} = props;
   const dateStr = makeDateString(endedAt, false);
-  const subject = `${teamName} ${dateStr} Action Meeting Summary`;
+  const subject = `${teamName} ${dateStr} ${MEETING_NAME} Summary`;
   return {
     subject,
     body: summaryEmailText(props),
