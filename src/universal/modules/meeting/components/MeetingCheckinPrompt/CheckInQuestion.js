7fb6c26cfb2ce12ee8ec13c6cb24a2cbca657e18
Daniel Friedman <dfriedman58@gmail.com>
2017-10-20 14:06:16 -0400

    Implement Personal check-in question

--- src/universal/modules/meeting/components/MeetingCheckinPrompt/CheckInQuestion.js
+++ src/universal/modules/meeting/components/MeetingCheckinPrompt/CheckInQuestion.js
@@ -4,10 +4,31 @@ import PropTypes from 'prop-types';
 
 // import Editable from 'universal/components/Editable/Editable';
 // import MeetingPrompt from 'universal/modules/meeting/components/MeetingPrompt/MeetingPrompt';
+import Button from 'universal/components/Button/Button';
+import Tooltip from 'universal/components/Tooltip/Tooltip';
 
-const StaticCheckinQuestion = ({checkInQuestion}) => (
-  <span>{checkInQuestion}?</span>
-);
+const StaticCheckinQuestion = ({checkInQuestion}) => {
+  const upgradeCopy = 'Upgrade to a Pro Account to customize the Social Check-in question.';
+  return (
+    <Tooltip
+      tip={<div>{upgradeCopy}</div>}
+      originAnchor={{vertical: 'bottom', horizontal: 'center'}}
+      targetAnchor={{vertical: 'top', horizontal: 'center'}}
+    >
+      <span>
+        {checkInQuestion}?
+        <Button
+          aria-label={upgradeCopy}
+          title={upgradeCopy}
+          compact
+          disabled
+          buttonStyle="flat"
+          icon="pencil"
+        />
+      </span>
+    </Tooltip>
+  );
+};
 
 StaticCheckinQuestion.propTypes = {
   checkInQuestion: PropTypes.string.isRequired
