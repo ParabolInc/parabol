a019d6e9a15bc5ff5608dd63640138e42bf0cee1
Matt Krick <matt.krick@gmail.com>
2017-10-23 20:14:00 -0700

    fix migration

--- src/server/database/migrations/20171023195700-checkinQuestion.js
+++ src/server/database/migrations/20171023195700-checkinQuestion.js
@@ -1,11 +1,14 @@
-import {ContentState} from 'draft-js';
+import {ContentState, convertToRaw} from 'draft-js';
 
 exports.up = async (r) => {
   const teamsWithActiveMeetings = await r.table('Team')
     .filter((team) => team('checkInQuestion').default(null).ne(null))
     .pluck('id', 'checkInQuestion');
+
   await Promise.all(teamsWithActiveMeetings.map((team) => {
-    const checkInQuestion = ContentState.createFromText(team.checkInQuestion);
+    const contentState = ContentState.createFromText(team.checkInQuestion);
+    const raw = convertToRaw(contentState);
+    const checkInQuestion = JSON.stringify(raw);
     return r.table('Team').get(team.id).update({checkInQuestion});
   }));
 };
