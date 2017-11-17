db61a71771513aafe778f87d9db491f3cb34683a
Matt Krick <matt.krick@gmail.com>
2017-11-17 14:31:51 -0800

    fix in-meeting notifications, notifications bug from email

--- src/universal/modules/notifications/containers/Notifications/NotificationsContainer.js
+++ src/universal/modules/notifications/containers/Notifications/NotificationsContainer.js
@@ -5,6 +5,8 @@ import Notifications from 'universal/modules/notifications/components/Notificati
 
 const NotificationsContainer = (props) => {
   const {dispatch, notifications} = props;
+  // to avoid a waterfall we push notifications down the DOMs throat, but here it's worth the wait
+  if (!notifications) return null;
   return (
     <Notifications dispatch={dispatch} notifications={notifications} />
   );
