26db1f7b1f8976cbf64a5083a8ba85727a630982
Terry Acker <ackernaut@gmail.com>
2017-11-17 11:05:58 -0600

    Request notif, show money matters for PRO only, fixes #1496

--- src/server/graphql/types/NotifyInvitation.js
+++ src/server/graphql/types/NotifyInvitation.js
@@ -1,5 +1,7 @@
 import {GraphQLID, GraphQLNonNull, GraphQLObjectType, GraphQLString} from 'graphql';
 import Notification, {notificationInterfaceFields} from 'server/graphql/types/Notification';
+import getRethink from 'server/database/rethinkDriver';
+import {Team} from 'server/graphql/models/Team/teamSchema';
 
 const NotifyInvitation = new GraphQLObjectType({
   name: 'NotifyInvitation',
@@ -25,6 +27,15 @@ const NotifyInvitation = new GraphQLObjectType({
     teamName: {
       type: new GraphQLNonNull(GraphQLString),
       description: 'The team name the inviteeEmail is being invited to'
+    },
+    team: {
+      type: Team,
+      description: 'The team the project is on',
+      resolve: ({teamId}) => {
+        // FIXME after merging in the dataloader PR
+        const r = getRethink();
+        return r.table('Team').get(teamId).run();
+      }
     },
     ...notificationInterfaceFields
   })
