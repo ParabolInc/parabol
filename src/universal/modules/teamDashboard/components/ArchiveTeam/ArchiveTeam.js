95676a5e7a2c300c52c3dd46b416a1211ab086f1
Daniel Friedman <dfriedman58@gmail.com>
2017-12-01 08:55:00 -0500

    Fix delete team validation UI

--- src/universal/modules/teamDashboard/components/ArchiveTeam/ArchiveTeam.js
+++ src/universal/modules/teamDashboard/components/ArchiveTeam/ArchiveTeam.js
@@ -1,68 +1,71 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form';
 import InputField from 'universal/components/InputField/InputField';
 import Button from 'universal/components/Button/Button';
 import Type from 'universal/components/Type/Type';
 
-const ArchiveTeam = (props) => {
-  const {
-    teamName,
-    handleSubmit,
-    handleClick,
-    handleFormBlur,
-    handleFormSubmit,
-    showConfirmationField,
-    submitting
-  } = props;
+@reduxForm({form: 'archiveTeamConfirmation'})
+class ArchiveTeam extends Component {
+  static propTypes = {
+    teamName: PropTypes.string.isRequired,
+    handleFormBlur: PropTypes.func.isRequired,
+    handleFormSubmit: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    handleClick: PropTypes.func.isRequired,
+    submitting: PropTypes.bool.isRequired,
+    showConfirmationField: PropTypes.bool.isRequired
+  };
 
-  const archiveTeamClick = () => {
+  archiveTeamClick = () => {
+    const {submitting, handleClick} = this.props;
     if (!submitting) handleClick();
   };
 
-  const validate = (value) => {
-    return value !== teamName && 'The team name entered was incorrect';
-  };
-
-  return (
-    <div>
-      {!showConfirmationField ?
-        <div>
-          <Button
-            buttonSize="small"
-            colorPalette="warm"
-            label="Delete Team"
-            onClick={archiveTeamClick}
-          />
-          <Type width="auto" marginTop=".5rem" scale="s2">
-            <b>Note</b>: {'Currently, this can’t be undone.'}
-          </Type>
-        </div> :
-        <form onSubmit={handleSubmit(handleFormSubmit)}>
-          <Field
-            autoFocus
-            onBlur={handleFormBlur}
-            colorPalette="gray"
-            component={InputField}
-            name="archivedTeamName"
-            placeholder="Type the team name to confirm"
-            type="text"
-            validate={validate}
-          />
-        </form>
-      }
-    </div>
+  validate = (value) => (
+    value !== this.props.teamName
+      ? 'The team name entered was incorrect'
+      : undefined
   );
-};
 
-ArchiveTeam.propTypes = {
-  teamName: PropTypes.string.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  handleFormSubmit: PropTypes.func.isRequired,
-  handleFormBlur: PropTypes.func.isRequired,
-  handleClick: PropTypes.func.isRequired,
-  submitting: PropTypes.bool.isRequired,
-  showConfirmationField: PropTypes.bool.isRequired
-};
+  render() {
+    const {
+      handleFormBlur,
+      handleFormSubmit,
+      handleSubmit,
+      showConfirmationField
+    } = this.props;
 
-export default reduxForm({form: 'archiveTeamConfirmation'})(ArchiveTeam);
+    return (
+      <div>
+        {!showConfirmationField ?
+          <div>
+            <Button
+              buttonSize="small"
+              colorPalette="warm"
+              label="Delete Team"
+              onClick={this.archiveTeamClick}
+            />
+            <Type width="auto" marginTop=".5rem" scale="s2">
+              <b>Note</b>: {'Currently, this can’t be undone.'}
+            </Type>
+          </div> :
+          <form onSubmit={handleSubmit(handleFormSubmit)}>
+            <Field
+              autoFocus
+              onBlur={handleFormBlur}
+              colorPalette="gray"
+              component={InputField}
+              name="archivedTeamName"
+              placeholder="Type the team name to confirm"
+              type="text"
+              validate={this.validate}
+            />
+          </form>
+        }
+      </div>
+    );
+  }
+}
+
+export default ArchiveTeam;
