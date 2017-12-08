e78fbb6d9432c948829388f930b91f8566fccf5d
Matt Krick <matt.krick@gmail.com>
2017-12-07 21:59:37 -0800

    fix tests for shared-dataloader

--- src/server/__tests__/setup/makeDataLoader.js
+++ src/server/__tests__/setup/makeDataLoader.js
@@ -1,8 +1,8 @@
 import RethinkDataLoader from 'server/utils/RethinkDataLoader';
-import SharedDataLoader from 'server/utils/SharedDataLoader';
+import SharedDataLoader from 'shared-dataloader';
 
 const makeDataLoader = (authToken) => {
-  const sharedDataLoader = new SharedDataLoader({ttl: 1000});
+  const sharedDataLoader = new SharedDataLoader({ttl: 1000, onShare: '_share'});
   const getDataLoader = sharedDataLoader.add(new RethinkDataLoader(authToken, {cache: false}));
   const operationId = Object.keys(sharedDataLoader.warehouse)[0];
   const store = sharedDataLoader.warehouse[operationId];
