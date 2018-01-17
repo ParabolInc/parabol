1e9cb6366a6798b9d9bd4f42c035a50608e7bf7d
Matt Krick <matt.krick@gmail.com>
2018-01-17 11:43:11 -0800

    remove unused test utils

--- src/server/__tests__/utils/refreshAuthToken.js
+++
@@ -1,8 +0,0 @@
-import mintToken from './mintToken';
-import getRethink from '../../database/rethinkDriver';
-
-export default async function refreshAuthToken(userId) {
-  const r = getRethink();
-  const user = await r.table('User').get(userId).default({tms: null});
-  return mintToken(userId, {tms: user.tms});
-}
