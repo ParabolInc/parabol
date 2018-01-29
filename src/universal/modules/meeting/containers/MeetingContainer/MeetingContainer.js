4e5ac11956aba9a8d571c832c895c4702b157c56
Daniel Friedman <dfriedman58@gmail.com>
2018-01-29 12:03:20 -0500

    Lint

--- src/universal/modules/meeting/containers/MeetingContainer/MeetingContainer.js
+++ src/universal/modules/meeting/containers/MeetingContainer/MeetingContainer.js
@@ -32,10 +32,8 @@ import EndMeetingMutation from 'universal/mutations/EndMeetingMutation';
 import KillMeetingMutation from 'universal/mutations/KillMeetingMutation';
 import MoveMeetingMutation from 'universal/mutations/MoveMeetingMutation';
 import PromoteFacilitatorMutation from 'universal/mutations/PromoteFacilitatorMutation';
-import UpdateAgendaItemMutation from 'universal/mutations/UpdateAgendaItemMutation';
 import {
-  AGENDA_ITEMS, CHECKIN, FIRST_CALL, LAST_CALL, LOBBY, phaseArray, SORT_STEP,
-  UPDATES
+  AGENDA_ITEMS, CHECKIN, FIRST_CALL, LAST_CALL, LOBBY, phaseArray, UPDATES
 } from 'universal/utils/constants';
 import withMutationProps from 'universal/utils/relay/withMutationProps';
 
