da1f10e5655eb3e847a6ba689263f9dd5b808261
Daniel Friedman <dfriedman58@gmail.com>
2018-02-02 10:13:49 -0500

    Cleanup post-cherry-pick

--- src/universal/modules/teamDashboard/components/AgendaInput/AgendaInput.js
+++ src/universal/modules/teamDashboard/components/AgendaInput/AgendaInput.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import withStyles from 'universal/styles/withStyles';
+import ui from 'universal/styles/ui';
 import {css} from 'aphrodite-local-styles/no-important';
 import {reduxForm, Field} from 'redux-form';
 import AgendaInputField from './AgendaInputField';
