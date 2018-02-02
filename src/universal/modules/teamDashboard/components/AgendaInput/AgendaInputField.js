0f208133457fbb204c45ad517dbc32fb9272887f
Daniel Friedman <dfriedman58@gmail.com>
2018-02-02 15:32:24 -0500

    Save Agenda Item Input focus, even through updates

--- src/universal/modules/teamDashboard/components/AgendaInput/AgendaInputField.js
+++ src/universal/modules/teamDashboard/components/AgendaInput/AgendaInputField.js
@@ -1,6 +1,6 @@
 import {css} from 'aphrodite-local-styles/no-important';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {Component} from 'react';
 import FontAwesome from 'react-fontawesome';
 import withHotkey from 'react-hotkey-hoc';
 import {createFragmentContainer} from 'react-relay';
@@ -29,25 +29,59 @@ const iconStyle = {
   width: ui.iconSize2x,
   zIndex: 200
 };
-const AgendaInputField = (props) => {
-  const {
-    afterSubmitAgendaItem,
-    atmosphere,
-    bindHotkey,
-    disabled,
-    handleSubmit,
-    setAgendaInputRef,
-    styles,
-    team: {teamId, agendaItems}
-  } = props;
-  let inputRef;
-  const setRef = (c) => {
-    inputRef = c;
+
+const hasFocus = (element) => (
+  element && document.activeElement === element
+);
+
+class AgendaInputField extends Component {
+  static propTypes = {
+    atmosphere: PropTypes.object.isRequired,
+    bindHotkey: PropTypes.func,
+    disabled: PropTypes.bool,
+    handleSubmit: PropTypes.func,
+    input: PropTypes.object,
+    afterSubmitAgendaItem: PropTypes.func.isRequired,
+    setAgendaInputRef: PropTypes.func,
+    styles: PropTypes.object,
+    team: PropTypes.object.isRequired
+  };
+
+  componentDidMount() {
+    const {disabled, bindHotkey} = this.props;
+    if (!disabled) {
+      bindHotkey('+', this.focusOnInput);
+    }
+  }
+
+  componentWillUpdate() {
+    this.maybeSaveFocus();
+  }
+
+  componentDidUpdate() {
+    this.maybeRefocus();
+  }
+
+  setRef = (c) => {
+    const {setAgendaInputRef} = this.props;
+    this.inputRef = c;
     if (setAgendaInputRef) {
       setAgendaInputRef(c);
     }
   };
-  const handleAgendaItemSubmit = (submittedData) => {
+
+  inputRef = null;
+  refocusAfterUpdate = false;
+
+  focusOnInput = (e) => {
+    e.preventDefault();
+    if (this.inputRef) {
+      this.inputRef.focus();
+    }
+  };
+
+  handleAgendaItemSubmit = (submittedData) => {
+    const {afterSubmitAgendaItem, atmosphere, team: {agendaItems, teamId}} = this.props;
     const content = submittedData.agendaItem;
     if (!content) return;
     const newAgendaItem = {
@@ -57,83 +91,92 @@ const AgendaInputField = (props) => {
       teamMemberId: toTeamMemberId(teamId, atmosphere.userId)
     };
     AddAgendaItemMutation(atmosphere, newAgendaItem);
-    inputRef.focus();
     afterSubmitAgendaItem();
   };
 
-  const focusOnInput = (e) => {
-    e.preventDefault();
-    if (inputRef) {
-      inputRef.focus();
-    }
+  makeForm = () => {
+    const {disabled, handleSubmit, styles} = this.props;
+    const rootStyles = css(
+      styles.root,
+      disabled && styles.rootDisabled
+    );
+    const inputStyles = css(
+      styles.input,
+      !disabled && styles.inputNotDisabled
+    );
+    return (
+      <form className={rootStyles} onSubmit={handleSubmit(this.handleAgendaItemSubmit)}>
+        <input
+          {...this.props.input}
+          autoCapitalize="off"
+          autoComplete="off"
+          className={inputStyles}
+          disabled={disabled}
+          maxLength="63"
+          onKeyDown={this.maybeBlur}
+          placeholder="Add Agenda Topic…"
+          ref={this.setRef}
+          type="text"
+        />
+        <FontAwesome name="plus-square-o" style={iconStyle} />
+      </form>
+    );
   };
-  const maybeBlur = (e) => {
-    if (e.key === 'Escape') {
-      inputRef.blur();
-    }
-  };
-  const rootStyles = css(
-    styles.root,
-    disabled && styles.rootDisabled
-  );
-  const inputStyles = css(
-    styles.input,
-    !disabled && styles.inputNotDisabled
-  );
-  if (!disabled) {
-    bindHotkey('+', focusOnInput);
-  }
-  const tip = (
-    <div style={{textAlign: 'center'}}>{'Add meeting topics to discuss,'}<br />{'like “upcoming vacation”'}</div>);
-  const input = (
-    <form className={rootStyles} onSubmit={handleSubmit(handleAgendaItemSubmit)}>
-      <input
-        {...props.input}
-        autoCapitalize="off"
-        autoComplete="off"
-        className={inputStyles}
-        disabled={disabled}
-        maxLength="63"
-        onKeyDown={maybeBlur}
-        placeholder="Add Agenda Topic…"
-        ref={setRef}
-        type="text"
-      />
-      <FontAwesome name="plus-square-o" style={iconStyle} />
-    </form>
-  );
-  const showTooltip = Boolean(agendaItems.length > 0 && !disabled);
-  return (
-    <div>
-      {showTooltip ?
-        <Tooltip
-          delay={1000}
-          hideOnFocus
-          tip={tip}
-          maxHeight={52}
-          maxWidth={224}
-          originAnchor={{vertical: 'top', horizontal: 'center'}}
-          targetAnchor={{vertical: 'bottom', horizontal: 'center'}}
-        >
-          {input}
-        </Tooltip> :
-        input
-      }
+
+  makeTooltip = () => (
+    <div style={{textAlign: 'center'}}>
+      {'Add meeting topics to discuss,'}<br />{'like “upcoming vacation”'}
     </div>
   );
-};
 
-AgendaInputField.propTypes = {
-  atmosphere: PropTypes.object.isRequired,
-  bindHotkey: PropTypes.func,
-  disabled: PropTypes.bool,
-  handleSubmit: PropTypes.func,
-  input: PropTypes.object,
-  afterSubmitAgendaItem: PropTypes.func.isRequired,
-  setAgendaInputRef: PropTypes.func,
-  styles: PropTypes.object,
-  team: PropTypes.object.isRequired
-};
+  maybeBlur = (e) => {
+    if (e.key === 'Escape') {
+      this.inputRef.blur();
+    }
+  };
+
+  maybeRefocus = () => {
+    if (this.inputRef && this.refocusAfterUpdate) {
+      this.inputRef.focus();
+      this.refocusAfterUpdate = false;
+    }
+  };
+
+  maybeSaveFocus = () => {
+    if (hasFocus(this.inputRef)) {
+      this.refocusAfterUpdate = true;
+    }
+  };
+
+  render() {
+    const {
+      disabled,
+      team: {agendaItems}
+    } = this.props;
+
+    const form = this.makeForm();
+    const showTooltip = Boolean(agendaItems.length > 0 && !disabled);
+    return (
+      <div>
+        {showTooltip ? (
+          <Tooltip
+            delay={1000}
+            hideOnFocus
+            tip={this.makeTooltip()}
+            maxHeight={52}
+            maxWidth={224}
+            originAnchor={{vertical: 'top', horizontal: 'center'}}
+            targetAnchor={{vertical: 'bottom', horizontal: 'center'}}
+          >
+            {form}
+          </Tooltip>
+        ) : (
+          form
+        )}
+      </div>
+    );
+  }
+}
 
 const inputPlaceholderStyles = makePlaceholderStyles(appTheme.palette.mid60l);
 
