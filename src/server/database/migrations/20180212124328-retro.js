d2a42a5503947acaf878918400707950379d32a9
Matt Krick <matt.krick@gmail.com>
2018-02-13 17:26:41 -0800

    add thoughts to migration

--- src/server/database/migrations/20180212124328-retro.js
+++ src/server/database/migrations/20180212124328-retro.js
@@ -2,14 +2,20 @@ exports.up = async (r) => {
   try {
     await Promise.all([
       r.tableCreate('CustomPhaseItem'),
-      r.tableCreate('NewMeeting')
+      r.tableCreate('NewMeeting'),
+      r.tableCreate('RetroThought'),
+      r.tableCreate('RetroThoughtGroup'),
   } catch (e) {
     // noop
   }
   try {
     await Promise.all([
       r.table('CustomPhaseItem').indexCreate('teamId'),
-      r.table('NewMeeting').indexCreate('teamId')
+      r.table('NewMeeting').indexCreate('teamId'),
+      r.table('RetroThought').indexCreate('meetingId'),
+      r.table('RetroThought').indexCreate('teamId'),
+      r.table('RetroThoughtGroup').indexCreate('meetingId'),
+      r.table('RetroThoughtGroup').indexCreate('teamId'),
     ]);
   } catch (e) {
     // noop
