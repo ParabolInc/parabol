344e0fe7c6b173f691af5a617b37aa8bc6d88127
Daniel Friedman <dfriedman58@gmail.com>
2018-02-20 10:54:31 -0500

    Remove old ::-moz-placeholder rules

--- src/universal/styles/helpers/makePlaceholderStyles.js
+++ src/universal/styles/helpers/makePlaceholderStyles.js
@@ -1,15 +1,11 @@
 export default function makePlaceholderStyles(color) {
   return {
+    '::placeholder': {color},
+
     // Chrome/Opera/Safari
     '::-webkit-input-placeholder': {color},
 
-    // Firefox 19+
-    '::-moz-placeholder': {color},
-
     // IE 10+
-    ':-ms-input-placeholder': {color},
-
-    // Firefox 18-
-    ':-moz-placeholder': {color}
+    ':-ms-input-placeholder': {color}
   };
 }
