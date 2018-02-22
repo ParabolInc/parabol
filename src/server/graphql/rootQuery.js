33459fd25638c238557ce363e10e369e3c878933
Matt Krick <matt.krick@gmail.com>
2018-02-21 19:44:57 -0800

    fix #1752 activeProUserCount

--- src/server/graphql/rootQuery.js
+++ src/server/graphql/rootQuery.js
@@ -1,10 +1,12 @@
 import {GraphQLObjectType} from 'graphql';
 import User from 'server/graphql/types/User';
 import {getUserId} from 'server/utils/authorization';
+import activeProUserCount from 'server/graphql/queries/activeProUserCount';
 
 export default new GraphQLObjectType({
   name: 'Query',
   fields: () => ({
+    activeProUserCount,
     viewer: {
       type: User,
       resolve: (source, args, {authToken, dataLoader}) => {
