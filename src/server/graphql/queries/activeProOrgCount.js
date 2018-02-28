05f8d0227a40c49210c691ea47ac77db75b53ea7
Jordan Husney <jordan.husney@gmail.com>
2018-02-27 17:56:13 -0800

    Add activeProOrgCount query, implements #1803

---
+++ src/server/graphql/queries/activeProOrgCount.js
@@ -0,0 +1,26 @@
+import {GraphQLInt} from 'graphql';
+import getRethink from 'server/database/rethinkDriver';
+import {requireSU} from 'server/utils/authorization';
+
+export default {
+  type: GraphQLInt,
+  async resolve(source, args, {authToken}) {
+    const r = getRethink();
+
+    // AUTH
+    requireSU(authToken);
+
+    // RESOLUTION
+    return r.table('Organization')
+      .filter((org) => org('tier').eq('pro'))
+      .map( (org) => org('orgUsers')
+        // calculate whether the org is active or not:
+        //   reduces [bool, bool, bool] => bool
+        //   where at least one false => true (active org)
+        //   all true => false (inactive org)
+        .fold(false, (acc, orgUser) => acc.or(r.not(orgUser('inactive'))))
+      )
+      // count true values in sequence (# of active orgs)
+      .count( (possiblyActiveOrg) => possiblyActiveOrg );
+  }
+};
