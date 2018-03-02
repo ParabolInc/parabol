23b4adfcabb934079f063e17f6ed99e5ba0a8116
Daniel Friedman <dfriedman58@gmail.com>
2018-03-02 10:10:59 -0500

    Finish AddReflectionButton

--- src/universal/components/AddReflectionButton/AddReflectionButton.js
+++ src/universal/components/AddReflectionButton/AddReflectionButton.js
@@ -8,16 +8,29 @@ import React from 'react';
 import styled from 'react-emotion';
 
 import PlainButton from 'universal/components/PlainButton/PlainButton';
+import appTheme from 'universal/styles/theme/appTheme';
 
 type Props = {
   handleClick: () => any
 };
 
+const Plus = styled(
+  (props) => <span aria-hidden="true" {...props}>+</span>
+)({
+  paddingRight: '0.5rem'
+});
+
 const AddReflectionButton = (props: Props) => (
-  <PlainButton {...props} onClick={props.handleClick}>Add a reflection</PlainButton>
+  <PlainButton {...props} onClick={props.handleClick}>
+    <Plus /><span>Add a reflection</span>
+  </PlainButton>
 );
 
 export default styled(AddReflectionButton)({
+  border: `2px dashed ${appTheme.palette.mid}`,
+  borderRadius: 3,
+  color: appTheme.palette.dark,
+  padding: '0.8rem',
   textAlign: 'center',
   width: '20rem'
 });
