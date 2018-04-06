e33a1fb2aeb0099fbff2e9eccc664c9c09045b8b
Jordan Husney <jordan.husney@gmail.com>
2018-04-06 00:15:49 -0700

    Rename suActiveUserCount -> suUserCount; adds params

--- src/server/graphql/queries/__tests__/suUserCount.test.js
+++ src/server/graphql/queries/__tests__/suUserCount.test.js
@@ -1,11 +1,34 @@
 import mockAuthToken from 'server/__tests__/setup/mockAuthToken';
 import MockDB from 'server/__tests__/setup/MockDB';
 import expectAsyncToThrow from 'server/__tests__/utils/expectAsyncToThrow';
-import suActiveProUserCount from 'server/graphql/queries/suActiveProUserCount';
-import {PRO} from 'universal/utils/constants';
+import suUserCount from 'server/graphql/queries/suUserCount';
+import {PERSONAL, PRO} from 'universal/utils/constants';
 import shortid from 'shortid';
 
 console.error = jest.fn();
+
+const defaultResolverArgs = {
+  includeInactive: false,
+  tier: PRO
+};
+
+test('counts the number of Personal users', async () => {
+  // SETUP
+  const mockDB = new MockDB();
+  const {user} = await mockDB.init();
+  const authToken = mockAuthToken(user[1], {rol: 'su'});
+  // TEST
+  const next = await suUserCount.resolve(
+    undefined,
+    {
+      ...defaultResolverArgs,
+      tier: PERSONAL
+    },
+    {authToken});
+
+  // VERIFY
+  expect(next >= 2).toBe(true);
+});
 
 test('counts the number of Pro users', async () => {
   // SETUP
@@ -13,7 +36,7 @@ test('counts the number of Pro users', async () => {
   const {user} = await mockDB.init();
   const authToken = mockAuthToken(user[1], {rol: 'su'});
   // TEST
-  const next = await suActiveProUserCount.resolve(undefined, undefined, {authToken});
+  const next = await suUserCount.resolve(undefined, defaultResolverArgs, {authToken});
 
   // VERIFY
   expect(next >= 0).toBe(true);
@@ -29,7 +52,7 @@ test('new Pro org increments number of Pro users', async () => {
   await mockDB
     .newOrg({name: shortid.generate(), tier: PRO})
     .newOrg({name: shortid.generate(), tier: PRO});
-  const next = await suActiveProUserCount.resolve(undefined, undefined, {authToken});
+  const next = await suUserCount.resolve(undefined, defaultResolverArgs, {authToken});
 
   // VERIFY
   // Tests run concurrently, so anything that counts across the entire database must be atomic (no initial, then next)
@@ -44,6 +67,6 @@ test('user token requires su role', async () => {
 
   // TEST & VERIFY
   await expectAsyncToThrow(
-    suActiveProUserCount.resolve(undefined, undefined, {authToken})
+    suUserCount.resolve(undefined, defaultResolverArgs, {authToken})
   );
 });
--- src/server/graphql/rootQuery.js
+++ src/server/graphql/rootQuery.js
@@ -1,18 +1,18 @@
 import {GraphQLObjectType} from 'graphql';
 import User from 'server/graphql/types/User';
 import {getUserId} from 'server/utils/authorization';
-import suActiveProUserCount from 'server/graphql/queries/suActiveProUserCount';
 import suCountTiersForUser from 'server/graphql/queries/suCountTiersForUser';
 import suOrgCount from 'server/graphql/queries/suOrgCount';
 import suProOrgInfo from 'server/graphql/queries/suProOrgInfo';
+import suUserCount from 'server/graphql/queries/suUserCount';
 
 export default new GraphQLObjectType({
   name: 'Query',
   fields: () => ({
-    suActiveProUserCount,
     suCountTiersForUser,
-    suOrgCount,
+    suUserCount,
     suProOrgInfo,
+    suOrgCount,
     viewer: {
       type: User,
       resolve: (source, args, {authToken, dataLoader}) => {
