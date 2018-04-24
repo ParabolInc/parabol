b0c1730fcb2f0ad445d8e229b7a059693f5fbd4c
Matt Krick <matt.krick@gmail.com>
2018-04-24 16:24:15 -0700

    fix #2026 ungroup by dragging anywhere

--- src/universal/components/MeetingPhaseWrapper.js
+++ src/universal/components/MeetingPhaseWrapper.js
@@ -4,6 +4,7 @@ import styled from 'react-emotion';
 const MeetingPhaseWrapper = styled('div')({
   display: 'flex',
   justifyContent: 'space-around',
+  height: '100%',
   margin: '0 auto',
   maxWidth: ui.meetingTopicPhaseMaxWidth,
   width: '100%'
