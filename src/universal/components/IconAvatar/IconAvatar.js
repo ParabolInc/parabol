f2a697dc51d8f83ebecd809d3e1780a0cf8129cd
Terry Acker <ackernaut@gmail.com>
2018-04-25 11:43:06 -0500

    Adds presentational component for upgrade nudge notification

--- src/universal/components/IconAvatar/IconAvatar.js
+++ src/universal/components/IconAvatar/IconAvatar.js
@@ -1,33 +1,61 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import withStyles from 'universal/styles/withStyles';
-import {css} from 'aphrodite-local-styles/no-important';
-import FontAwesome from 'react-fontawesome';
+import styled from 'react-emotion';
+import StyledFontAwesome from 'universal/components/StyledFontAwesome';
 import ui from 'universal/styles/ui';
 import appTheme from 'universal/styles/theme/appTheme';
 
+const smallSize = '2rem';
+const mediumSize = '2.75rem';
+const largeSize = '4rem';
+
+const iconSizes = {
+  small: {
+    fontSize: ui.iconSize,
+    height: smallSize,
+    width: smallSize
+  },
+
+  medium: {
+    fontSize: ui.iconSizeAvatar,
+    height: mediumSize,
+    width: mediumSize
+  },
+
+  large: {
+    fontSize: ui.iconSize2x,
+    height: largeSize,
+    width: largeSize
+  }
+};
+
+const IconAvatarRoot = styled('div')(({size}) => {
+  const {fontSize, height, width} = iconSizes[size];
+  return ({
+    alignItems: 'center',
+    backgroundColor: appTheme.palette.mid70l,
+    borderRadius: '100%',
+    color: '#fff',
+    display: 'flex',
+    fontSize,
+    height,
+    justifyContent: 'center',
+    textAlign: 'center',
+    width
+  });
+});
+
+const IconStyled = styled(StyledFontAwesome)({
+  fontSize: 'inherit',
+  lineHeight: ui.iconSize
+});
+
 const IconAvatar = (props) => {
-  const {
-    icon,
-    size,
-    styles,
-    title
-  } = props;
-
-  const rootStyles = css(
-    styles.root,
-    styles[size]
-  );
-
-  const iconStyle = {
-    fontSize: 'inherit',
-    lineHeight: ui.iconSize
-  };
-
+  const {icon, size, title} = props;
   return (
-    <div className={rootStyles} title={title}>
-      <FontAwesome name={icon} style={iconStyle} />
-    </div>
+    <IconAvatarRoot size={size || 'small'} title={title || 'Icon Avatar'}>
+      <IconStyled name={icon} />
+    </IconAvatarRoot>
   );
 };
 
@@ -38,48 +66,7 @@ IconAvatar.propTypes = {
     'medium',
     'large'
   ]),
-  styles: PropTypes.object,
   title: PropTypes.string
 };
 
-IconAvatar.defaultProps = {
-  size: 'small',
-  title: 'Icon Avatar'
-};
-
-const styleThunk = () => {
-  const smallSize = '2rem';
-  const mediumSize = '2.75rem';
-  const largeSize = '4rem';
-  return {
-    root: {
-      alignItems: 'center',
-      backgroundColor: appTheme.palette.mid70l,
-      borderRadius: '100%',
-      color: '#fff',
-      display: 'flex',
-      justifyContent: 'center',
-      textAlign: 'center'
-    },
-
-    small: {
-      fontSize: ui.iconSize,
-      height: smallSize,
-      width: smallSize
-    },
-
-    medium: {
-      fontSize: ui.iconSizeAvatar,
-      height: mediumSize,
-      width: mediumSize
-    },
-
-    large: {
-      fontSize: ui.iconSize2x,
-      height: largeSize,
-      width: largeSize
-    }
-  };
-};
-
-export default withStyles(styleThunk)(IconAvatar);
+export default IconAvatar;
