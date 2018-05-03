121560d9c7356a7fcef8a48d68d9e70ad1efe24c
Terry Acker <ackernaut@gmail.com>
2018-05-03 17:16:52 -0500

    Style due date toggle, due soon, past due

--- src/universal/components/DueDateToggle.js
+++ src/universal/components/DueDateToggle.js
@@ -6,17 +6,34 @@ import LoadableDueDatePicker from 'universal/components/LoadableDueDatePicker';
 import FontAwesome from 'react-fontawesome';
 import {createFragmentContainer} from 'react-relay';
 import {shortMonths} from 'universal/utils/makeDateString';
+import ui from 'universal/styles/ui';
+import StyledFontAwesome from 'universal/components/StyledFontAwesome';
 
 const Toggle = styled('div')(
   {
     alignItems: 'center',
-    cursor: 'pointer'
+    borderRadius: '.125rem',
+    cursor: 'pointer',
+    display: 'flex',
+    padding: '.0625rem .1875rem'
   },
   ({dueDate}) => ({
-    color: dueDate && 'red',
-    backgroundColor: dueDate && 'rgba(255,0,0, 0.2)'
+    color: dueDate && ui.dueDateColor,
+    backgroundColor: dueDate && ui.dueDateBg
+  }),
+  ({isDueSoon}) => ({
+    color: isDueSoon && ui.dueDateSoonColor,
+    backgroundColor: isDueSoon && ui.dueDateSoonBg
+  }),
+  ({isPastDue}) => ({
+    color: isPastDue && ui.dueDatePastColor,
+    backgroundColor: isPastDue && ui.dueDatePastBg
   })
 );
+
+const DueDateIcon = styled(StyledFontAwesome)({
+  fontSize: ui.iconSize
+});
 
 const DateString = styled('span')({
   marginLeft: '0.25rem'
@@ -44,12 +61,23 @@ const formatDueDate = (dueDate) => {
   return `${monthStr} ${day}`;
 };
 
+const getDateDiff = (dueDate) => {
+  const today = new Date();
+  const date = new Date(dueDate);
+  const timeDiff = date.getTime() - today.getTime();
+  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  return diffDays;
+};
+
 const DueDateToggle = (props: Props) => {
   const {task} = props;
   const {dueDate} = task;
+  const dateDiff = dueDate && getDateDiff(dueDate);
+  const isDueSoon =  dueDate && dateDiff > -1 && dateDiff < 2;
+  const isPastDue =  dueDate && dateDiff < 0;
   const toggle = (
-    <Toggle dueDate={dueDate}>
-      <FontAwesome name="clock-o" />
+    <Toggle dueDate={dueDate} isDueSoon={isDueSoon} isPastDue={isPastDue}>
+      <DueDateIcon name="clock-o" />
       {dueDate && <DateString>{formatDueDate(dueDate)}</DateString>}
     </Toggle>
   );
