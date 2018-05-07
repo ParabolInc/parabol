4844d5a60dfffe2a3532e34dc1d74190b8554c26
Terry Acker <ackernaut@gmail.com>
2018-05-07 13:34:30 -0500

    upgrade modal wip

--- src/universal/components/UpgradeModal.js
+++ src/universal/components/UpgradeModal.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import styled from 'react-emotion';
+import ui from 'universal/styles/ui';
 
 const ModalBoundary = styled('div')({
-  width: 200,
-  height: 200,
-  background: 'white'
+  background: ui.palette.white,
+  borderRadius: ui.modalBorderRadius,
+  display: 'flex',
+  height: 280,
+  width: 600
 });
 
+const CenteredModalBoundary = styled(ModalBoundary)({
+  alignItems: 'center',
+  flexDirection: 'column',
+  justifyContent: 'center'
+});
+
+const ModalHeading = styled('h2')({
+  fontSize: '1.5rem',
+  fontWeight: 600,
+  lineHeight: '1.5',
+  margin: 0
+});
+
+const ModalCopy = styled('p')({
+  fontSize: '1rem',
+  lineHeight: '2rem',
+  margin: 0
+});
+
+const UpgradeModalConfirmation = (
+  <CenteredModalBoundary>
+    <ModalHeading>{'Upgrade Successful!'}</ModalHeading>
+    <ModalCopy>{'Your organization is now on the '}<b>{'Pro'}</b>{' tier.'}</ModalCopy>
+  </CenteredModalBoundary>
+);
+
 const UpgradeModal = () => {
-  return (
-    <ModalBoundary>{'Upgrade Modal'}</ModalBoundary>
-  );
+  return UpgradeModalConfirmation;
+  // return (
+  //   <ModalBoundary>{'Upgrade Modal'}</ModalBoundary>
+  // );
 };
 
 export default UpgradeModal;
