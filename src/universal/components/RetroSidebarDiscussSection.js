7a0ae7c9e8bbb374ae5a4bed091c42ce259c5c77
Matt Krick <matt.krick@gmail.com>
2018-05-23 08:38:22 -0700

    add topic sorting fix #2088

--- src/universal/components/RetroSidebarDiscussSection.js
+++ src/universal/components/RetroSidebarDiscussSection.js
@@ -7,9 +7,18 @@ import StyledFontAwesome from 'universal/components/StyledFontAwesome'
 import ui from 'universal/styles/ui'
 import MeetingSidebarLabelBlock from 'universal/components/MeetingSidebarLabelBlock'
 import MeetingSubnavItem from 'universal/components/MeetingSubnavItem'
-import {RETRO_TOPIC_LABEL, RETRO_VOTED_LABEL} from 'universal/utils/constants'
+import {
+  RETRO_TOPIC_LABEL,
+  RETRO_VOTED_LABEL,
+  DISCUSSION_TOPIC,
+  SORT_STEP
+} from 'universal/utils/constants'
 import plural from 'universal/utils/plural'
 import LabelHeading from 'universal/components/LabelHeading/LabelHeading'
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
+import dndNoise from 'universal/utils/dndNoise'
+import withAtmosphere from 'universal/decorators/withAtmosphere/withAtmosphere'
+import DragDiscussionTopicMutation from 'universal/mutations/DragDiscussionTopicMutation'
 
 type Props = {|
   gotoStageId: (stageId: string) => void,
@@ -30,66 +39,129 @@ const CheckIcon = styled(StyledFontAwesome)(({isUnsyncedFacilitatorStage}) => ({
   color: isUnsyncedFacilitatorStage ? ui.palette.warm : ui.palette.mid
 }))
 
+const DraggableMeetingSubnavItem = styled('div')(({isDragging}) => ({
+  boxShadow: isDragging && ui.shadow[2]
+}))
+
 const RetroSidebarDiscussSection = (props: Props) => {
   const {
+    atmosphere,
     gotoStageId,
     viewer: {
       team: {newMeeting}
     }
   } = props
-  const {localPhase, localStage, facilitatorStageId} = newMeeting || {}
+  const {localPhase, localStage, facilitatorStageId, meetingId} = newMeeting || {}
   if (!localPhase || !localPhase.stages || !localStage) return null
   const {stages} = localPhase
   const {localStageId} = localStage
   const inSync = localStageId === facilitatorStageId
 
+  const onDragEnd = (result) => {
+    const {source, destination} = result
+
+    if (
+      !destination ||
+      destination.droppableId !== DISCUSSION_TOPIC ||
+      source.droppableId !== DISCUSSION_TOPIC
+    ) {
+      return
+    }
+
+    const sourceTopic = stages[source.index]
+    const destinationTopic = stages[destination.index]
+
+    let sortOrder
+    if (destination.index === 0) {
+      sortOrder = destinationTopic.sortOrder - SORT_STEP + dndNoise()
+    } else if (destination.index === stages.length - 1) {
+      sortOrder = destinationTopic.sortOrder + SORT_STEP + dndNoise()
+    } else {
+      sortOrder =
+        (stages[destination.index - 1].sortOrder + destinationTopic.sortOrder) / 2 + dndNoise()
+    }
+
+    const {id: stageId} = sourceTopic
+    const variables = {meetingId, stageId, sortOrder}
+    DragDiscussionTopicMutation(atmosphere, variables)
+  }
   return (
-    <SidebarPhaseItemChild>
-      <MeetingSidebarLabelBlock>
-        <LabelHeading>
-          {plural(stages.length, `${RETRO_VOTED_LABEL} ${RETRO_TOPIC_LABEL}`)}
-        </LabelHeading>
-      </MeetingSidebarLabelBlock>
-      {stages.map((stage, idx) => {
-        const {reflectionGroup} = stage
-        if (!reflectionGroup) return null
-        const {title, voteCount} = reflectionGroup
-        // the local user is at another stage than the facilitator stage
-        const isUnsyncedFacilitatorStage = !inSync && stage.id === facilitatorStageId
-        const navState = {
-          isActive: localStage.localStageId === stage.id, // the local user is at this stage
-          isComplete: stage.isComplete, // this stage is complete
-          isDisabled: !stage.isNavigable,
-          isUnsyncedFacilitatorStage
-        }
-        const voteMeta = (
-          <VoteTally>
-            <CheckIcon isUnsyncedFacilitatorStage={isUnsyncedFacilitatorStage} name='check' />
-            {' x '}
-            {voteCount}
-          </VoteTally>
-        )
-        return (
-          <MeetingSubnavItem
-            key={stage.id}
-            label={title}
-            metaContent={voteMeta}
-            onClick={() => gotoStageId(stage.id)}
-            orderLabel={`${idx + 1}.`}
-            {...navState}
-          />
-        )
-      })}
-    </SidebarPhaseItemChild>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <SidebarPhaseItemChild>
+        <MeetingSidebarLabelBlock>
+          <LabelHeading>
+            {plural(stages.length, `${RETRO_VOTED_LABEL} ${RETRO_TOPIC_LABEL}`)}
+          </LabelHeading>
+        </MeetingSidebarLabelBlock>
+        <Droppable droppableId={DISCUSSION_TOPIC}>
+          {(provided, snapshot) => {
+            return (
+              <div ref={provided.innerRef}>
+                {stages.map((stage, idx) => {
+                  const {reflectionGroup, sortOrder} = stage
+                  if (!reflectionGroup) return null
+                  const {title, voteCount} = reflectionGroup
+                  console.log('title', title)
+                  // the local user is at another stage than the facilitator stage
+                  const isUnsyncedFacilitatorStage = !inSync && stage.id === facilitatorStageId
+                  const navState = {
+                    isActive: localStage.localStageId === stage.id, // the local user is at this stage
+                    isComplete: stage.isComplete, // this stage is complete
+                    isDisabled: !stage.isNavigable,
+                    isUnsyncedFacilitatorStage
+                  }
+                  const voteMeta = (
+                    <VoteTally>
+                      <CheckIcon
+                        isUnsyncedFacilitatorStage={isUnsyncedFacilitatorStage}
+                        name='check'
+                      />
+                      {' x '}
+                      {voteCount}
+                    </VoteTally>
+                  )
+                  return (
+                    <Draggable key={stage.id} draggableId={stage.id} index={idx}>
+                      {(dragProvided, dragSnapshot) => {
+                        return (
+                          <DraggableMeetingSubnavItem
+                            isDragging={dragSnapshot.isDragging}
+                            innerRef={dragProvided.innerRef}
+                            {...dragProvided.draggableProps}
+                            {...dragProvided.dragHandleProps}
+                          >
+                            <MeetingSubnavItem
+                              key={stage.id}
+                              isDragging={dragSnapshot.isDragging}
+                              label={title}
+                              metaContent={voteMeta}
+                              onClick={() => gotoStageId(stage.id)}
+                              orderLabel={`${idx + 1}.`}
+                              sortOrder={sortOrder}
+                              {...navState}
+                            />
+                          </DraggableMeetingSubnavItem>
+                        )
+                      }}
+                    </Draggable>
+                  )
+                })}
+              </div>
+            )
+          }}
+        </Droppable>
+      </SidebarPhaseItemChild>
+    </DragDropContext>
   )
 }
 
 export default createFragmentContainer(
-  RetroSidebarDiscussSection,
+  withAtmosphere(RetroSidebarDiscussSection),
   graphql`
     fragment RetroSidebarDiscussSection_viewer on User {
       team(teamId: $teamId) {
         newMeeting {
+          meetingId: id
           localStage {
             localStageId: id
           }
@@ -106,6 +178,7 @@ export default createFragmentContainer(
                     title
                     voteCount
                   }
+                  sortOrder
                 }
               }
             }
@@ -119,6 +192,7 @@ export default createFragmentContainer(
                     title
                     voteCount
                   }
+                  sortOrder
                 }
               }
             }
