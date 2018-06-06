76aac807e511265ce45606e43b5c139f5aa8b0b1
Matt Krick <matt.krick@gmail.com>
2018-06-06 06:59:20 -0700

    works with react-virtualized

--- src/server/graphql/mutations/helpers/updateReflectionLocation/handleUpdateLocation.js
+++ src/server/graphql/mutations/helpers/updateReflectionLocation/handleUpdateLocation.js
@@ -2,24 +2,18 @@ import addReflectionToGroup from 'server/graphql/mutations/helpers/updateReflect
 import removeReflectionFromGroup from 'server/graphql/mutations/helpers/updateReflectionLocation/removeReflectionFromGroup'
 import moveReflectionGroup from 'server/graphql/mutations/helpers/updateReflectionLocation/moveReflectionGroup'
 
-const handleUpdatedLocation = (
-  reflectionId,
-  reflectionGroupId,
-  retroPhaseItemId,
-  sortOrder,
-  context
-) => {
-  // adding to group requires reflectionId and reflectionGroupId
-  // removing from group (into a column) requires reflectionId, retroPhaseItemId, and a null reflectionGroupId
-  // moving a group requires reflectionGroupId & retroPhaseItemId
+const handleUpdatedLocation = (reflectionId, reflectionGroupId, sortOrder, context) => {
+  // adding/reordering within the same group requires reflectionId and reflectionGroupId
+  // removing from group (into its own new group) requires reflectionId and a null reflectionGroupId
+  // moving a group requires reflectionGroupId and a null reflectionId
   if (reflectionId && reflectionGroupId) {
     return addReflectionToGroup(reflectionId, reflectionGroupId, sortOrder, context)
   }
-  if (reflectionId && !reflectionGroupId && retroPhaseItemId) {
-    return removeReflectionFromGroup(reflectionId, retroPhaseItemId, sortOrder, context)
+  if (reflectionId && !reflectionGroupId) {
+    return removeReflectionFromGroup(reflectionId, sortOrder, context)
   }
-  if (!reflectionId && reflectionGroupId && retroPhaseItemId) {
-    return moveReflectionGroup(reflectionGroupId, retroPhaseItemId, sortOrder, context)
+  if (!reflectionId && reflectionGroupId) {
+    return moveReflectionGroup(reflectionGroupId, sortOrder, context)
   }
   return undefined
 }
