ff6893a0018e47bfd4398b0c78026fed76dedd62
Terry Acker <ackernaut@gmail.com>
2018-08-15 15:30:22 -0500

    Adds Elevation story

--- stories/Elevation.stories.js
+++ stories/Elevation.stories.js
@@ -8,13 +8,44 @@ import React from 'react'
 import {storiesOf} from '@storybook/react'
 import RetroBackground from './components/RetroBackground'
 import StoryContainer from './components/StoryContainer'
+import styled from 'react-emotion'
+import elevation from 'universal/styles/elevation'
+import ui from 'universal/styles/ui'
 
-storiesOf('Loadable Help Dialog', module).add('toggle the help dialog', () => (
+const demoCardSize = '8rem'
+
+const Grid = styled('div')({
+  display: 'grid',
+  gridGap: '2.5rem',
+  gridTemplateColumns: `repeat(auto-fill, ${demoCardSize})`
+})
+
+const Elevation = styled('div')(({depth, index}) => ({
+  backgroundColor: 'white',
+  boxShadow: `${depth}`,
+  borderRadius: '.125rem',
+  color: ui.palette.midGray,
+  height: demoCardSize,
+  padding: '1rem',
+  width: demoCardSize,
+  '::after': {
+    content: `"${index}dp"`
+  }
+}))
+
+const RenderBlock = () => (
+  <Grid>
+    {elevation.map((depth, index) => {
+      return <Elevation depth={depth} index={index} key={`${index}dp`} />
+    })}
+  </Grid>
+)
+
+storiesOf('Elevation', module).add('elevations on parade', () => (
   <RetroBackground>
     <StoryContainer
-      description={`This demonstrates a help dialog (available for all teammates)
-          and an optional tip (for the facilitator in the control bar)`}
-      render={() => <div>Elevation</div>}
+      description={`This shows example elevations, a link to Material defaults as a reference, and a working list of how we use elevation in our app`}
+      render={RenderBlock}
     />
   </RetroBackground>
 ))
