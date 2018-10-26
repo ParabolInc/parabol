89ec8c327ee25f00f145f56981adce020cf220e9
Matt Krick <matt.krick@gmail.com>
2018-10-25 18:20:10 -0700

    fix filename

--- src/universal/modules/email/components/SummaryEmail/ExportToCSV.tsx
+++ src/universal/modules/email/components/SummaryEmail/ExportToCSV.tsx
@@ -1,3 +1,4 @@
+import Json2csv from 'json2csv'
 import React, {Component} from 'react'
 import {fetchQuery, graphql} from 'react-relay'
 import FlatButton from 'universal/components/FlatButton'
@@ -5,6 +6,7 @@ import IconLabel from 'universal/components/IconLabel'
 import withAtmosphere, {
   WithAtmosphereProps
 } from 'universal/decorators/withAtmosphere/withAtmosphere'
+import extractTextFromDraftString from 'universal/utils/draftjs/extractTextFromDraftString'
 import withMutationProps, {WithMutationProps} from 'universal/utils/relay/withMutationProps'
 
 interface Props extends WithAtmosphereProps, WithMutationProps {
@@ -15,6 +17,10 @@ const query = graphql`
   query ExportToCSVQuery($meetingId: ID!) {
     viewer {
       newMeeting(meetingId: $meetingId) {
+        team {
+          name
+        }
+        endedAt
         ... on RetrospectiveMeeting {
           reflectionGroups {
             reflections {
@@ -24,12 +30,20 @@ const query = graphql`
               content
             }
             title
+            voteCount
           }
         }
       }
     }
   }
 `
+
+interface CSVRow {
+  title: string
+  votes: number
+  type: 'Task' | 'Reflection'
+  content: string
+}
 
 class ExportToCSV extends Component<Props> {
   exportToCSV = async () => {
@@ -38,7 +52,49 @@ class ExportToCSV extends Component<Props> {
     submitMutation()
     const data = await fetchQuery(atmosphere, query, {meetingId})
     onCompleted()
-    console.log('data', data)
+    const {
+      viewer: {
+        newMeeting: {
+          reflectionGroups,
+          endedAt,
+          team: {name: teamName}
+        }
+      }
+    } = data
+    const rows = [] as Array<CSVRow>
+    reflectionGroups.forEach((group) => {
+      const {reflections, tasks, title, voteCount: votes} = group
+      tasks.forEach((task) => {
+        rows.push({
+          title,
+          votes,
+          type: 'Task',
+          content: extractTextFromDraftString(task.content)
+        })
+      })
+      reflections.forEach((reflection) => {
+        rows.push({
+          title,
+          votes,
+          type: 'Reflection',
+          content: extractTextFromDraftString(reflection.content)
+        })
+      })
+    })
+    const fields = ['title', 'votes', 'type', 'content']
+    const parser = new Json2csv.Parser(fields)
+    const csv = parser.parse(rows)
+    const csvContent = 'data:text/csv;charset=utf-8,' + csv
+    const date = new Date(endedAt)
+    const numDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    // copied from https://stackoverflow.com/questions/18848860/javascript-array-to-csv/18849208#18849208
+    const encodedUri = encodeURI(csvContent)
+    const link = document.createElement('a')
+    link.setAttribute('href', encodedUri)
+    link.setAttribute('download', `ParabolRetrospective_${teamName}_${numDate}.csv`)
+    document.body.appendChild(link) // Required for FF
+    link.click()
+    document.body.removeChild(link)
     return
   }
 
@@ -47,9 +103,10 @@ class ExportToCSV extends Component<Props> {
     return (
       <FlatButton
         aria-label={`Export to CSV`}
-        size='medium'
+        size='small'
         onClick={this.exportToCSV}
         waiting={submitting}
+        style={{margin: '16px auto 0'}}
       >
         <IconLabel icon='cloud_download' iconColor='green' iconLarge label={'Export to CSV'} />
       </FlatButton>
