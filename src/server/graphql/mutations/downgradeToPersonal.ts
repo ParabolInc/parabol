a9d65f24898a987a6db8aca58bd491e8256d944d
Matt Krick <matt.krick@gmail.com>
2018-11-16 07:24:21 -0800

    support superUser requests for teams, orgs, and downgrades

--- src/server/graphql/mutations/downgradeToPersonal.ts
+++ src/server/graphql/mutations/downgradeToPersonal.ts
@@ -3,7 +3,7 @@ import stripe from 'server/billing/stripe'
 import getRethink from 'server/database/rethinkDriver'
 import DowngradeToPersonalPayload from 'server/graphql/types/DowngradeToPersonalPayload'
 import {sendAlreadyPersonalTierError} from 'server/utils/alreadyMutatedErrors'
-import {getUserId, getUserOrgDoc, isOrgBillingLeader} from 'server/utils/authorization'
+import {getUserId, getUserOrgDoc, isOrgBillingLeader, isSuperUser} from 'server/utils/authorization'
 import {sendOrgLeadAccessError} from 'server/utils/authorizationErrors'
 import publish from 'server/utils/publish'
 import sendSegmentEvent, {sendSegmentIdentify} from 'server/utils/sendSegmentEvent'
@@ -25,9 +25,11 @@ export default {
     const subOptions = {mutatorId, operationId}
 
     // AUTH
-    const userId = getUserId(authToken)
-    const userOrgDoc = await getUserOrgDoc(userId, orgId)
-    if (!isOrgBillingLeader(userOrgDoc)) return sendOrgLeadAccessError(authToken, userOrgDoc)
+    const viewerId = getUserId(authToken)
+    if (!isSuperUser(authToken)) {
+      const userOrgDoc = await getUserOrgDoc(viewerId, orgId)
+      if (!isOrgBillingLeader(userOrgDoc)) return sendOrgLeadAccessError(authToken, userOrgDoc)
+    }
 
     // VALIDATION
     const {stripeSubscriptionId, tier} = await r.table('Organization').get(orgId)
@@ -66,18 +68,18 @@ export default {
         )('changes')('new_val')('id')
         .default([])
     })
-    sendSegmentEvent('Downgrade to personal', userId, {orgId})
+    sendSegmentEvent('Downgrade to personal', viewerId, {orgId})
     const data = {orgId, teamIds}
     publish(ORGANIZATION, orgId, DowngradeToPersonalPayload, data, subOptions)
 
     teamIds.forEach((teamId) => {
       // I can't readily think of a clever way to use the data obj and filter in the resolver so I'll reduce here.
-      // This is probably a smelly piece of code telling me I should be sending this per-userId or per-org
+      // This is probably a smelly piece of code telling me I should be sending this per-viewerId or per-org
       const teamData = {orgId, teamIds: [teamId]}
       publish(TEAM, teamId, DowngradeToPersonalPayload, teamData, subOptions)
     })
     // the count of this users tier stats just changed, update:
-    await sendSegmentIdentify(userId)
+    await sendSegmentIdentify(viewerId)
     return data
   }
 }
