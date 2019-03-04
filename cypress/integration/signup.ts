00ec9625f3e26465c7798c69fbde1e650f984f71
Matt Krick <matt.krick@gmail.com>
2019-03-04 15:08:57 -0800

    begin CI testing 12

--- cypress/integration/signup.ts
+++ cypress/integration/signup.ts
@@ -5,7 +5,7 @@ describe('signup', () => {
       cy.get('input[name="email"]').type(`cypress+${Date.now()}@parabol.co`)
       cy.get('input[name="password"]').type('cypress')
       cy.root().submit()
-      cy.location('pathname', {timeout: 10000}).should('eq', '/me')
+      cy.location('pathname', {timeout: 20000}).should('eq', '/me')
     })
   })
   it('performs a login when matches existing credentials', () => {
@@ -14,7 +14,7 @@ describe('signup', () => {
       cy.get('input[name="email"]').type(`cypress@parabol.co`)
       cy.get('input[name="password"]').type('cypress')
       cy.root().submit()
-      cy.location('pathname').should('eq', '/me')
+      cy.location('pathname', {timeout: 20000}).should('eq', '/me')
     })
   })
 })
