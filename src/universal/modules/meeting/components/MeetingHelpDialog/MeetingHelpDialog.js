741352d1b6bd19fb0b86a89e516c87f0e593eddd
Matt Krick <matt.krick@gmail.com>
2019-04-29 16:43:15 -0700

    refactor old meeting HelpMenus

--- src/universal/modules/meeting/components/MeetingHelpDialog/MeetingHelpDialog.js
+++ src/universal/modules/meeting/components/MeetingHelpDialog/MeetingHelpDialog.js
@@ -6,10 +6,11 @@
 
 import React from 'react'
 import styled from 'react-emotion'
-import LoadableMeetingHelpDialogMenu from 'universal/modules/meeting/components/MeetingHelpDialog/LoadableMeetingHelpDialogMenu'
-import LoadableMenu from 'universal/components/LoadableMenu'
 import FloatingActionButton from 'universal/components/FloatingActionButton'
 import IconLabel from 'universal/components/IconLabel'
+import useMenu from 'universal/hooks/useMenu'
+import {MenuPosition} from 'universal/hooks/useCoords'
+import lazyPreload from 'universal/utils/lazyPreload'
 
 type Props = {
   phase: string
@@ -22,33 +23,22 @@ const StyledButton = styled(FloatingActionButton)({
   width: '2rem'
 })
 
-const originAnchor = {
-  vertical: 'bottom',
-  horizontal: 'right'
-}
-
-const targetAnchor = {
-  vertical: 'bottom',
-  horizontal: 'right'
-}
+const MeetingHelpDialogMenu = lazyPreload(() =>
+  import(/* webpackChunkName: 'MeetingHelpDialogMenu' */
+    'universal/modules/meeting/components/MeetingHelpDialog/MeetingHelpDialogMenu')
+)
 
 const MeetingHelpDialog = ({phase}: Props) => {
-  const iconButtonToggle = (
-    <StyledButton palette='white'>
-      <IconLabel icon='help_outline' />
-    </StyledButton>
-  )
-
+  const {togglePortal, originRef, menuPortal, menuProps} = useMenu(MenuPosition.LOWER_RIGHT)
   return (
-    <LoadableMenu
-      LoadableComponent={LoadableMeetingHelpDialogMenu}
-      maxWidth={280}
-      maxHeight={320}
-      originAnchor={originAnchor}
-      queryVars={{phase}}
-      targetAnchor={targetAnchor}
-      toggle={iconButtonToggle}
-    />
+    <>'     '<StyledButton
+      palette='white'
+      onClick={togglePortal}
+      innerRef={originRef}
+      onMouseEnter={MeetingHelpDialogMenu.preload}
+    >
+      <IconLabel icon='help_outline' />
+    </StyledButton>'     '{menuPortal(<MeetingHelpDialogMenu phase={phase} menuProps={menuProps} />)}'   '</>
   )
 }
 
