7a383aca542aaf9a108c61af6047907e58a7c9f7
Terry Acker <ackernaut@gmail.com>
2019-04-30 16:39:07 -0500

    Disabled state when waiting

--- src/universal/components/GoogleOAuthButton.tsx
+++ src/universal/components/GoogleOAuthButton.tsx
@@ -3,40 +3,44 @@ import styled from 'react-emotion'
 import RaisedButton from 'universal/components/RaisedButton'
 import GoogleSVG from 'universal/components/GoogleSVG'
 
-const StyledButton = styled(RaisedButton)({
-  backgroundColor: 'white',
+interface Props {
+  label: string
+  onClick: () => void
+  waiting?: boolean
+}
+
+const StyledButton = styled(RaisedButton)(({waiting}: {waiting: boolean}) => ({
+  backgroundColor: waiting ? '#ebebeb' : 'white',
   borderRadius: 20,
+  color: waiting ? '#8D8D8D' : '#757575',
   height: 40,
   margin: '0 auto',
+  opacity: '1 !important',
   padding: '0 16px',
   width: 240
-})
+}))
 
-const IconBlock = styled('div')({
+const IconBlock = styled('div')(({waiting}: {waiting: boolean}) => ({
   marginRight: 16,
   '& svg': {
     display: 'block',
     height: 18,
-    width: 18
+    width: 18,
+    '& path': {
+      fill: waiting ? '#8D8D8D' : undefined
+    }
   }
-})
+}))
 
 const Label = styled('div')({
-  color: 'rgba(0, 0, 0, .54)',
   fontSize: 14
 })
-
-interface Props {
-  label: string
-  onClick: () => void
-  waiting: boolean | undefined
-}
 
 function GoogleOAuthButton (props: Props) {
   const {onClick, label, waiting} = props
   return (
     <StyledButton onClick={onClick} waiting={waiting}>
-      <IconBlock>
+      <IconBlock waiting={waiting}>
         <GoogleSVG />
       </IconBlock>
       <Label>{label}</Label>
