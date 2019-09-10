cdedb3fca46e07d36ff4415178b285f1d1cb3264
Matt Krick <matt.krick@gmail.com>
2019-09-10 08:04:48 -0700

    fix ts

--- packages/server/utils/RethinkDataLoader.ts
+++ packages/server/utils/RethinkDataLoader.ts
@@ -25,7 +25,7 @@ import OrganizationUser from '../database/types/OrganizationUser'
 import Reflection from '../database/types/Reflection'
 import SuggestedAction from '../database/types/SuggestedAction'
 import Task from '../database/types/Task'
-import TeamInvitation from 'database/types/TeamInvitation'
+import TeamInvitation from '../database/types/TeamInvitation'
 import User from '../database/types/User'
 import ReflectionGroup from '../database/types/ReflectionGroup'
 import Notification from '../database/types/Notification'
@@ -95,7 +95,7 @@ export default class RethinkDataLoader {
   dataLoaderOptions: DataLoader.Options<any, any>
   authToken: null | AuthToken
 
-  constructor(
+  constructor (
     authToken: AuthToken | null = null,
     dataLoaderOptions: DataLoader.Options<any, any> = {}
   ) {
@@ -103,7 +103,7 @@ export default class RethinkDataLoader {
     this.dataLoaderOptions = dataLoaderOptions
   }
 
-  private fkLoader<T = any>(
+  private fkLoader<T = any> (
     standardLoader: DataLoader<string, T>,
     field: string,
     fetchFn: (ids: string[]) => any[] | Promise<any[]>
@@ -118,7 +118,7 @@ export default class RethinkDataLoader {
     return new DataLoader<string, T[]>(batchFn, this.dataLoaderOptions)
   }
 
-  private pkLoader<T extends keyof Tables>(table: T) {
+  private pkLoader<T extends keyof Tables> (table: T) {
     // don't pass in a a filter here because they requested a specific ID, they know what they want
     const batchFn = async (keys) => {
       const r = getRethink()
