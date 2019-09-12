1674a16e2a96fa7e0701faa2fb11fa19b555b9d0
Matt Krick <matt.krick@gmail.com>
2019-09-12 10:26:03 -0700

    support moving multiple teams to org fix authToken expiry

---
+++ packages/server/safeMutations/safeArchiveEmptyPersonalOrganization.ts
@@ -0,0 +1,25 @@
+import getRethink from '../database/rethinkDriver'
+import {TierEnum} from 'parabol-client/types/graphql'
+
+// Only does something if the organization is empty & not paid
+// safeArchiveTeam & downgradeToPersonal should be called before calling this
+
+const safeArchiveEmptyPersonalOrganization = async (orgId: string) => {
+  const r = getRethink()
+  const now = new Date()
+  const teamCountRemainingOnOldOrg = await r.table('Team')
+    .getAll(orgId, {index: 'orgId'})
+    .count() as number
+
+  if (teamCountRemainingOnOldOrg > 0) return
+  const org = await r.table('Organization').get(orgId)
+  if (org.tier !== TierEnum.personal) return
+
+  await r
+    .table('OrganizationUser')
+    .getAll(orgId, {index: 'orgId'})
+    .filter({removedAt: null})
+    .update({removedAt: now})
+}
+
+export default safeArchiveEmptyPersonalOrganization
