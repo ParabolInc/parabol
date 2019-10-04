e159f68ef442351e46c20f5c9d5373d066d2f8fa
Terry Acker <ackernaut@gmail.com>
2019-10-04 17:08:36 -0500

    improve menu sort by preferredName

--- packages/client/components/AssignFacilitatorMenu.tsx
+++ packages/client/components/AssignFacilitatorMenu.tsx
@@ -24,8 +24,8 @@ const AssignFacilitatorMenu = (props: Props) => {
   const {teamMembers} = team || {teamMembers: []}
   const assignees = useMemo(
     () => teamMembers
-      .filter((teamMember) => teamMember.userId !== facilitatorUserId)
-      .sort((a,b) => (a.preferredName > b.preferredName) ? 1 : ((b.preferredName > a.preferredName) ? -1 : 0)),
+    .filter((teamMember) => teamMember.userId !== facilitatorUserId)
+    .sort((a,b) => a.preferredName.toLowerCase() < b.preferredName.toLowerCase() ? -1 : 1),
     [facilitatorUserId, teamMembers]
   )
   const atmosphere = useAtmosphere()
