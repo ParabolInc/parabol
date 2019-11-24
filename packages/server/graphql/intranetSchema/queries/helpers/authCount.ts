d0f5eafdee02cc57f5bdfe1ad3fe0989fcd7b10d
Matt Krick <matt.krick@gmail.com>
2019-11-24 09:43:47 -0600

    add logins and signups query to private schema

---
+++ packages/server/graphql/intranetSchema/queries/helpers/authCount.ts
@@ -0,0 +1,21 @@
+import getRethink from '../../../../database/rethinkDriver'
+
+type FilterField = 'createdAt' | 'lastSeenAt'
+
+const authCount = async (
+  after: Date | null | undefined,
+  isActive: boolean | null | undefined,
+  filterField: FilterField
+) => {
+  const r = await getRethink()
+  const activeFilter = isActive ? {inactive: false} : {}
+  const afterFilter = after ? (row) => row(filterField).ge(after) : {}
+  return r
+    .table('User')
+    .filter(activeFilter)
+    .filter(afterFilter)
+    .count()
+    .run()
+}
+
+export default authCount
