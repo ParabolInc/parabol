7290d4ecab82feb4e5f1f7fa93ad20db6af523e9
Matt Krick <matt.krick@gmail.com>
2019-12-18 15:40:37 -0600

    fix tests for no auth0

--- packages/cypress/support/commands.ts
+++ packages/cypress/support/commands.ts
@@ -37,7 +37,7 @@ const login = (_overrides = {}) => {
   const exp = toEpochSeconds(now + JWT_LIFESPAN)
   const iat = toEpochSeconds(now)
   const tokenObj = {
-    sub: 'auth0|5c75e3a068d77f71b39513a9',
+    sub: 'local|wnVeDjF-n',
     aud: Cypress.env('AUTH0_CLIENT_ID'),
     iss: window.location.origin,
     exp,
