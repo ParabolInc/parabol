9327c7806614c8782c587902fd3d1a9d7a669d9c
Matt Krick <matt.krick@gmail.com>
2020-01-29 11:39:24 -0500

    fix #3525 vertical height in meeting
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/components/MeetingPhaseWrapper.tsx
+++ packages/client/components/MeetingPhaseWrapper.tsx
@@ -3,6 +3,8 @@ import styled from '@emotion/styled'
 const MeetingPhaseWrapper = styled('div')({
   display: 'flex',
   justifyContent: 'space-around',
+  // https://github.com/ParabolInc/action/issues/3525
+  overflow: 'hidden',
   height: '100%',
   margin: '0 auto',
   width: '100%'
