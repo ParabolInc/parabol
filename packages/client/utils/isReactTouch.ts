340273ef8a76c8039e584535ed6083abca4b42c5
Matt Krick <matt.krick@gmail.com>
2020-02-05 13:16:16 -0500

    better touchstart detection
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/utils/isReactTouch.ts
+++ packages/client/utils/isReactTouch.ts
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const isReactTouch = (e: React.MouseEvent | React.TouchEvent): e is React.TouchEvent => {
-  return (e as any).touches !== undefined
+  return e.type === 'touchstart'
 }
 
 export default isReactTouch
