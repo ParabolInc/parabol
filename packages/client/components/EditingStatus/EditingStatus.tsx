8784866895d27552a9027a12643211fa0cbbe6b0
Matt Krick <matt.krick@gmail.com>
2020-02-11 17:19:09 -0500

    fix editing flicker
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/components/EditingStatus/EditingStatus.tsx
+++ packages/client/components/EditingStatus/EditingStatus.tsx
@@ -46,10 +46,10 @@ const EditingStatus = (props: Props) => {
   const otherEditors = editors.filter((editor) => editor.userId !== viewerId)
   const isEditing = editors.length > otherEditors.length
   const [timestampType, setTimestampType] = useState<TimestampType>('createdAt')
-  const toggleTimestamp = () => {
+  const toggleTimestamp = (e: React.MouseEvent) => {
+    e.preventDefault()
     closeTooltip()
     setTimestampType(timestampType === 'createdAt' ? 'updatedAt' : 'createdAt')
-    ;(document as any).activeElement?.blur()
   }
   const {tooltipPortal, openTooltip, closeTooltip, originRef: tipRef} = useTooltip<HTMLDivElement>(
     MenuPosition.UPPER_CENTER,
