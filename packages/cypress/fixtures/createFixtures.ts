58f2ad9d61e6440ffc4b9735da16de680e0fa4ec
Matt Krick <matt.krick@gmail.com>
2020-03-02 12:14:20 -0500

    remove contenth-length for cypress tests
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/cypress/fixtures/createFixtures.ts
+++ packages/cypress/fixtures/createFixtures.ts
@@ -1,12 +1,12 @@
 /*
  * To create fixtures:
- * Set the DB to test in .env
+ * Set the DB to test in the server .env
  * Migrate the DB: `yarn db:migrate`
  * Run a signup (manually, cypress@parabol.co, PW: cypress)
  * Update the userId to match the newly created user in ../support/commands.ts L40
- * Set durability to soft: `node packages/server/__tests__/setup/softenDurability.babel.js`
+ * Set durability to soft: `c`
  * Clone DB to another DB namespace 'cypress': `node packages/server/database/cloneTestToCypress.babel.js`
  * Dump test and cypress DBs to rdb_test.tar.gz: `docker run --rm --link action-rethink -v $(pwd):/backup petecoop/rethinkdb-driver rethinkdb-dump -c 172.17.0.2:28015 -f /backup/packages/cypress/fixtures/rdb_test.tar.gz -e test -e cypress`
  * FOR MACOS (WITHOUT DOCKER): `rethinkdb-dump -f packages/cypress/fixtures/rdb_test2.tar.gz -e test -e cypress`
- * `chown $USER rdb_test2.tar.gz && rm packages/cypress/fixtures/rdb_test.tar.gz && mv packages/cypress/fixtures/rdb_test2.tar.gz packages/cypress/fixtures/rdb_test.tar.gz`
+ * `chown $USER packages/cypress/fixtures/rdb_test2.tar.gz && rm packages/cypress/fixtures/rdb_test.tar.gz && mv packages/cypress/fixtures/rdb_test2.tar.gz packages/cypress/fixtures/rdb_test.tar.gz`
  */
