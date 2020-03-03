bc2d6fb87453b98951d8f4295e8bf9d5637e5d2e
Terry Acker <ackernaut@gmail.com>
2020-03-03 15:28:01 -0600

    Migration, rename action meeting to check-in

--- packages/server/database/migrations/20200303123942-actionToCheckIn.ts
+++ packages/server/database/migrations/20200303123942-actionToCheckIn.ts
@@ -4,13 +4,10 @@ export const up = async function(r: R) {
   try {
     await r
       .table('NewMeeting')
-      .filter((meeting) => meeting('name').match('Action meeting'))
-      .update(
-        (meeting) => ({
-          name: r.js(`${meeting}("name").replace("Action meeting", "Check-in")`)
-        }),
-        {nonAtomic: true}
-      )
+      .filter((meeting) => meeting('name').match('Action meeting #'))
+      .update((row) => ({
+        name: r('Check-in #').add(row('meetingNumber').coerceTo('string'))
+      }))
       .run()
   } catch (e) {
     console.log(e)
@@ -21,13 +18,10 @@ export const down = async function(r: R) {
   try {
     await r
       .table('NewMeeting')
-      .filter((meeting) => meeting('name').match('Check-in'))
-      .update(
-        (meeting) => ({
-          name: r.js(`${meeting}("name").replace("Check-in", "Action meeting")`)
-        }),
-        {nonAtomic: true}
-      )
+      .filter((meeting) => meeting('name').match('Check-in #'))
+      .update((row) => ({
+        name: r('Action meeting #').add(row('meetingNumber').coerceTo('string'))
+      }))
       .run()
   } catch (e) {
     console.log(e)
