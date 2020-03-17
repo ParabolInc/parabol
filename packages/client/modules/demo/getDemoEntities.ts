15c754fd2c6c0282cd6aae7e3da231eb2949b1e0
NikAiyer <nick.aiyer@gmail.com>
2020-03-16 21:57:13 -0500

    Added a demoLookup function to getDemoEntities.ts to skip fetching data from google when environment is not production

--- packages/client/modules/demo/getDemoEntities.ts
+++ packages/client/modules/demo/getDemoEntities.ts
@@ -2,6 +2,7 @@ import graphql from 'babel-plugin-relay/macro'
 import {fetchQuery} from 'relay-runtime'
 import Atmosphere from '../../Atmosphere'
 import {getDemoEntitiesQuery} from '__generated__/getDemoEntitiesQuery.graphql'
+import PROD from 'parabol-server/PROD';
 
 const query = graphql`
   query getDemoEntitiesQuery($text: String!) {
@@ -14,13 +15,20 @@ const query = graphql`
     }
   }
 `
+const demoLookup = (text) => {
+  return [{name: text, salience: 1, lemma: text}]
+}
 
 const getDemoEntities = async (text: string) => {
   if (!text || text.length <= 2) return []
   const remoteAtmosphere = new Atmosphere()
+  if(!PROD){
+    const lookupEntities = demoLookup(text)
+    return lookupEntities || []
+  }
   const res = await fetchQuery<getDemoEntitiesQuery>(remoteAtmosphere, query, {text})
   const {getDemoEntities} = res
-  const {entities} = getDemoEntities!
+  const {entities} = getDemoEntities
   return entities || []
 }
 
