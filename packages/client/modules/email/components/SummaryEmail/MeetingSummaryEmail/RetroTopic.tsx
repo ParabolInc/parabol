b5931670c520e18a2ec3b5fa716f64f929a52877
Terry Acker <ackernaut@gmail.com>
2020-04-21 17:30:16 -0500

    fix #3753 retro comment count in summary

--- packages/client/modules/email/components/SummaryEmail/MeetingSummaryEmail/RetroTopic.tsx
+++ packages/client/modules/email/components/SummaryEmail/MeetingSummaryEmail/RetroTopic.tsx
@@ -8,19 +8,22 @@ import {RetroTopic_topic} from '../../../../../__generated__/RetroTopic_topic.gr
 import emailDir from '../../../emailDir'
 import AnchorIfEmail from './AnchorIfEmail'
 import EmailReflectionCard from './EmailReflectionCard'
+import plural from '../../../../../utils/plural'
 
 const topicThemeHeading = {
-  color: PALETTE.TEXT_BLUE,
+  color: PALETTE.TEXT_MAIN,
   display: 'block',
   fontFamily: FONT_FAMILY.SANS_SERIF,
   fontSize: 16,
-  fontWeight: 600
+  fontWeight: 600,
+  textDecoration: 'none'
 }
 
 const votesBlock = {
   fontSize: 14,
   fontWeight: 600,
-  lineHeight: ICON_SIZE.MD18
+  lineHeight: ICON_SIZE.MD18,
+  padding: '8px 0px'
 }
 
 const voteCountStyle = {
@@ -33,6 +36,15 @@ const imageStyle = {
   verticalAlign: 'text-bottom'
 }
 
+const commentLinkStyle = {
+  color: PALETTE.TEXT_BLUE,
+  display: 'block',
+  fontFamily: FONT_FAMILY.SANS_SERIF,
+  fontSize: 13,
+  fontWeight: 600,
+  textDecoration: 'none'
+}
+
 interface Props {
   isEmail: boolean
   topic: RetroTopic_topic
@@ -41,17 +53,26 @@ interface Props {
 
 const RetroTopic = (props: Props) => {
   const {isEmail, to, topic} = props
-  const {reflections, title, voteCount} = topic
+  const {reflections, title, voteCount, thread} = topic
   const imageSource = isEmail ? 'static' : 'local'
   const icon = imageSource === 'local' ? 'thumb_up_18.svg' : 'thumb_up_18@3x.png'
   const src = `${emailDir}${icon}`
   const grid = useEmailItemGrid(reflections, 3)
+  const commentLength = thread.edges.length
+  const commentLinkLabel =
+    commentLength === 0
+      ? 'No Comments'
+      : commentLength >= 101
+      ? 'See 100+ Comments'
+      : `See ${commentLength} ${plural(commentLength, 'Comment')}`
   return (
     <>
       <tr>
         <td align='center' style={{paddingTop: 20}}>
           <AnchorIfEmail href={to} isEmail={isEmail} style={topicThemeHeading}>
+            {'“'}
             {title}
+            {'”'}
           </AnchorIfEmail>
         </td>
       </tr>
@@ -62,6 +83,13 @@ const RetroTopic = (props: Props) => {
           </AnchorIfEmail>
           <AnchorIfEmail href={to} isEmail={isEmail} style={voteCountStyle}>
             {voteCount}
+          </AnchorIfEmail>
+        </td>
+      </tr>
+      <tr>
+        <td align='center'>
+          <AnchorIfEmail href={to} isEmail={isEmail} style={commentLinkStyle}>
+            {commentLinkLabel}
           </AnchorIfEmail>
         </td>
       </tr>
@@ -81,6 +109,13 @@ export default createFragmentContainer(RetroTopic, {
     fragment RetroTopic_topic on RetroReflectionGroup {
       title
       voteCount
+      thread(first: 101) @connection(key: "DiscussionThread_thread") {
+        edges {
+          node {
+            threadId
+          }
+        }
+      }
       reflections {
         ...EmailReflectionCard_reflection
       }
