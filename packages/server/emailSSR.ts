22c7de74b5a554fd8d4c3230c3f9336830593e7b
Matt Krick <matt.krick@gmail.com>
2020-05-12 12:00:57 -0700

    add email ssr debugging

--- packages/server/emailSSR.ts
+++ packages/server/emailSSR.ts
@@ -1,73 +1,11 @@
-// import makeAppLink from './utils/makeAppLink'
+import notificationSummaryCreator from './email/components/notificationSummaryCreator'
+import {uWSHandler} from './graphql/uWSAsyncHandler'
 
-// const ORG_MEMBERS_LINK = makeAppLink('me/organizations/tzsqFnuzh/members')
+const emailSSR: uWSHandler = (res) => {
+  const preferredName = 'Terry'
+  const notificationCount = 5
+  const {html} = notificationSummaryCreator({preferredName, notificationCount})
+  res.end(html)
+}
 
-// const EMAIL_ALL_PROPS = {
-//   summaryEmail: {
-//     meeting: {
-//       teamName: 'Team 123',
-//       endedAt: new Date().toJSON(),
-//       agendaItemsCompleted: 3,
-//       invitees: []
-//     }
-//   },
-//   notificationSummary: {
-//     userId: '123',
-//     email: 'fake@123.co',
-//     name: 'First Last',
-//     numNotifications: '4'
-//   },
-//   teamInvite: {
-//     inviteLink: '#inviteLink',
-//     inviteeName: undefined,
-//     inviteeEmail: 'nolan@example.co',
-//     inviterName: 'Sara',
-//     inviterEmail: 'sara@example.co',
-//     teamName: 'Example Team'
-//   },
-//   UpcomingInvoiceEmailTemplate: {
-//     memberUrl: ORG_MEMBERS_LINK,
-//     periodEndStr: 'January 31',
-//     newUsers: [
-//       {
-//         email: 'nolan@company.co',
-//         name: 'Nolan'
-//       },
-//       {
-//         email: 'sara@another.co',
-//         name: 'Sara'
-//       }
-//     ]
-//   }
-// }
-// export default async function emailSSR(req, res) {
-//   const {template} = req.params
-//   // const emailFactory = templates[template]
-//   const props = EMAIL_ALL_PROPS[template]
-//   /*
-//    * Render and send email
-//    *
-//    * Don't forget to set the MAILGUN_API_KEY, MAILGUN_DOMAIN, and MAIL_FROM
-//    * environment variables if you want to send the email for reals.
-//    */
-//   // const sharedDataLoader = new DataLoaderWarehouse({
-//   //   onShare: '_share',
-//   //   ttl: 2000
-//   // })
-//   // const authToken = {}
-//   // const dataLoader = sharedDataLoader.add(new RethinkDataLoader(authToken, { cache: false }))
-
-//   // const context = {
-//   //   socketId: 'foo',
-//   //   rateLimiter: null,
-//   //   // dataLoader,
-//   //   authToken
-//   // }
-
-//   // const email = await emailFactory({...props, context}).catch(console.log)
-//   // Render and show container:
-//   res.set('Cache-Control', 'no-cache, no-store, must-revalidate')
-//   res.set('Pragma', 'no-cache')
-//   res.set('Expires', '0')
-//   // res.send(email.html)
-// }
+export default emailSSR
