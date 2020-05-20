014d5e28013a64407916c3b5a7ab267958f2f6dd
nickoferrall <nickoferrall@gmail.com>
2020-05-20 13:50:00 +0100

    margin update

--- packages/client/components/NewMeetingSidebarPhaseListItem.tsx
+++ packages/client/components/NewMeetingSidebarPhaseListItem.tsx
@@ -107,7 +107,7 @@ const PhaseCountBlock = styled('div')({
 const StyledBadge = styled(Badge)({
   backgroundColor: PALETTE.BACKGROUND_GRAY,
   boxShadow: 'none',
-  marginRight: 2,
+  marginRight: 3,
   minWidth: 24,
   textShadow: 'none'
 })
