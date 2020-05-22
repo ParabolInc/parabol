b38304da42eea5b6abc9f990ad7f4ddfb9a3591f
nickoferrall <nickoferrall@gmail.com>
2020-05-22 21:10:52 +0100

    removed cypress test

--- packages/cypress/integration/checkinMeeting.ts
+++
@@ -1,7 +0,0 @@
-describe('Test check-in meeting', () => {
-  it('Check that all personal votes are remaining and no team votes have been used', () => {
-    cy.login()
-    cy.wait(1000)
-    cy.contains('Start meeting')
-  })
-})
