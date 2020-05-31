d84ea249727ea91d1b5a8c0d0d84e193b949c03b
Tiffany Han <tiffanyhan@tiffanys-mbp.lan>
2020-05-30 18:17:10 -0700

    real fix for bug wherein you can't add a task from board bc it was passing undefined to data loader

--- packages/server/graphql/mutations/validateThreadableReflectionGroupId.ts
+++ packages/server/graphql/mutations/validateThreadableReflectionGroupId.ts
@@ -7,14 +7,19 @@ const validateThreadableReflectionGroupId = async (
   meetingId: string | null | undefined,
   dataLoader: DataLoaderWorker
 ) => {
+  if (!meetingId)
+    return undefined
+
   const meeting = await dataLoader.get('newMeetings').load(meetingId)
   switch (meeting.meetingType) {
     case MeetingTypeEnum.retrospective:
       const reflectionGroupId =
         threadSource === ThreadSourceEnum.REFLECTION_GROUP ? threadId : undefined
-      const reflectionGroup = await dataLoader.get('retroReflectionGroups').load(reflectionGroupId)
-      if (!reflectionGroup || reflectionGroup.meetingId !== meetingId)
-        return 'Invalid reflection group ID. ef yeah'
+      if (reflectionGroupId) {
+        const reflectionGroup = await dataLoader.get('retroReflectionGroups').load(reflectionGroupId)
+        if (!reflectionGroup || reflectionGroup.meetingId !== meetingId)
+          return 'Invalid reflection group ID. ef yeah'
+      }
       break
     case MeetingTypeEnum.action:
       // TODO: add validation for action items here, plus change filename
