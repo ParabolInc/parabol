803ad954f74dc027ccccab6983e52df01ff05a1e
Matt Krick <matt.krick@gmail.com>
2020-06-05 12:47:47 -0700

    add email to track event
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/mutations/helpers/bootstrapNewUser.ts
+++ packages/server/graphql/mutations/helpers/bootstrapNewUser.ts
@@ -13,10 +13,12 @@ import createTeamAndLeader from './createTeamAndLeader'
 
 // no waiting necessary, it's just analytics
 const handleSegment = async (user: User, isInvited: boolean) => {
-  const {id: userId, email, segmentId, preferredName} = user
+  const {id: userId, createdAt, email, segmentId, picture, preferredName} = user
   segmentIo.identify({
     userId,
     traits: {
+      avatar: picture,
+      createdAt,
       email,
       name: preferredName
     },
--- packages/server/utils/segmentIo.ts
+++ packages/server/utils/segmentIo.ts
@@ -1,27 +1,54 @@
 import SegmentIo from 'analytics-node'
 import crypto from 'crypto'
+import getRethink from '../database/rethinkDriver'
 import PROD from '../PROD'
 import {toEpochSeconds} from './epochTime'
+import getRedis from './getRedis'
+import sendToSentry from './sendToSentry'
 
 const {SEGMENT_WRITE_KEY, SERVER_SECRET} = process.env
+
+const getEmail = async (userId: string) => {
+  const redis = getRedis()
+  const key = `email:${userId}`
+  const email = await redis.get(key)
+  if (email) return email
+  const r = await getRethink()
+  const dbEmail = await r
+    .table('User')
+    .get(userId)('email')
+    .run()
+  if (!dbEmail) {
+    sendToSentry(new Error('Email for user not found'), {userId})
+    return ''
+  }
+  await redis.set(key, dbEmail)
+  return dbEmail
+}
 
 const segmentIo = new SegmentIo(SEGMENT_WRITE_KEY || 'x', {
   flushAt: PROD ? 20 : 1,
   enable: !!SEGMENT_WRITE_KEY
-})
-;(segmentIo as any)._track = segmentIo.track
-segmentIo.track = (options) => {
+}) as any
+segmentIo._track = segmentIo.track
+segmentIo.track = async (options) => {
   const now = new Date()
   const ts = String(toEpochSeconds(now))
   const parabolToken = crypto
     .createHmac('sha256', SERVER_SECRET!)
     .update(ts)
     .digest('base64')
-
+  const {userId, event, properties} = options
+  const email = await getEmail(options.userId as string)
   return (segmentIo as any)._track({
-    ...options,
+    userId,
+    event,
+    properties: {
+      ...properties,
+      email
+    },
     timestamp: now,
     parabolToken: parabolToken as any
   })
 }
-export default segmentIo
+export default segmentIo as SegmentIo
