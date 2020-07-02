d9ee3d54a59c5f0ba4ba729193a031586c326323
Matt Krick <matt.krick@gmail.com>
2020-07-02 10:23:06 -0700

    add gatekeeping to public
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/types/RetrospectiveMeetingSettings.ts
+++ packages/server/graphql/types/RetrospectiveMeetingSettings.ts
@@ -7,6 +7,33 @@ import CustomPhaseItem from './CustomPhaseItem'
 import ReflectTemplate, {ReflectTemplateConnection} from './ReflectTemplate'
 import TeamMeetingSettings, {teamMeetingSettingsFields} from './TeamMeetingSettings'
 
+const getPublicScoredTemplates = async (templates: {createdAt: Date; id: string}[]) => {
+  const sharedTemplateIds = templates.map(({id}) => id)
+  const sharedTemplateEndTimes = await db.readMany('endTimesByTemplateId', sharedTemplateIds)
+  const scoreByTemplateId = {} as {[templateId: string]: number}
+  const starterTemplates = new Set([
+    'sailboatTemplate',
+    'startStopContinueTemplate',
+    'workingStuckTemplate',
+    'fourLsTemplate',
+    'GladSadMadTemplate'
+  ])
+  templates.forEach((template, idx) => {
+    const {id: templateId, createdAt} = template
+    const endTimes = sharedTemplateEndTimes[idx]
+    const starterBonus = starterTemplates.has(templateId) ? 100 : 0
+    const minUsagePenalty = sharedTemplateEndTimes.length < 10 && !starterBonus
+    scoreByTemplateId[templateId] = minUsagePenalty
+      ? -1
+      : getTemplateScore(createdAt, endTimes, 0.2) + starterBonus
+  })
+  // mutative, but doesn't matter if we change the sort oder
+  templates.sort((a, b) => {
+    return scoreByTemplateId[a.id] > scoreByTemplateId[b.id] ? -1 : 1
+  })
+  return templates
+}
+
 const getScoredTemplates = async (
   templates: {createdAt: Date; id: string}[],
   newHotnessFactor: number
@@ -129,7 +156,7 @@ const RetrospectiveMeetingSettings = new GraphQLObjectType<any, GQLContext>({
       },
       resolve: async (_source, {first, after}) => {
         const publicTemplates = await db.read('publicTemplates', 'all')
-        const scoredTemplates = await getScoredTemplates(publicTemplates, 0.2)
+        const scoredTemplates = await getPublicScoredTemplates(publicTemplates)
         return connectionFromTemplateArray(scoredTemplates, first, after)
       }
     }
--- packages/server/graphql/types/UpdateTemplateScopePayload.ts
+++ packages/server/graphql/types/UpdateTemplateScopePayload.ts
@@ -1,28 +1,19 @@
 import {GraphQLNonNull, GraphQLObjectType} from 'graphql'
-import {MeetingTypeEnum} from 'parabol-client/types/graphql'
 import {GQLContext} from '../graphql'
 import makeMutationPayload from './makeMutationPayload'
-import RetrospectiveMeetingSettings from './RetrospectiveMeetingSettings'
+import ReflectTemplate from './ReflectTemplate'
 
 export const UpdateTemplateScopeSuccess = new GraphQLObjectType<any, GQLContext>({
   name: 'UpdateTemplateScopeSuccess',
   fields: () => ({
-    meetingSettings: {
-      type: GraphQLNonNull(RetrospectiveMeetingSettings),
-      description: 'The settings that contain the teamTemplates array that was modified',
-      resolve: ({teamId}, _args, {dataLoader}) => {
-        return dataLoader
-          .get('meetingSettingsByType')
-          .load({teamId, meetingType: MeetingTypeEnum.retrospective})
+    template: {
+      type: GraphQLNonNull(ReflectTemplate),
+      description:
+        'the template that was just updated, if downscoped, does not provide whole story',
+      resolve: async ({templateId}, _args, {dataLoader}) => {
+        return dataLoader.get('reflectTemplates').load(templateId)
       }
     }
-    // template: {
-    // type: GraphQLNonNull(ReflectTemplate),
-    // description: 'the template that was just updated, if downscoped, does not provide whole story',
-    // resolve: async ({templateId}, _args, {dataLoader}) => {
-    // return dataLoader.get('reflectTemplates').load(templateId)
-    // }
-    // }
   })
 })
 
