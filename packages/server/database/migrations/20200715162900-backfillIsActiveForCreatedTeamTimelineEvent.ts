82d2565766ffa6a97f5df8d5adf11807478536d5
Bruce Tian <tianrunhe@gmail.com>
2020-07-15 16:28:54 -0700

    Add Create TimelineEventTeamCreated DB type; Add migration script to backfill undefined isActive field

---
+++ packages/server/database/migrations/20200715162900-backfillIsActiveForCreatedTeamTimelineEvent.ts
@@ -0,0 +1,19 @@
+import {R} from 'rethinkdb-ts'
+
+export const up = async function(r: R) {
+  try {
+    await r
+      .table('TimelineEvent')
+      .filter((event) => event.hasFields('isActive').not())
+      .update({
+        isActive: true
+      })
+      .run()
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const down = async function() {
+  // noop
+}
