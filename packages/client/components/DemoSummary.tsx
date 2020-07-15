121f347e2794a8e68ea768e3929b5fa3cbe28422
NikAiyer <nick.aiyer@gmail.com>
2020-07-15 16:59:26 -0700

    reverted back to store-or-network for QueryRenderer and cleared LocalAtmosphere storage in DemoSumamry

--- packages/client/components/DemoSummary.tsx
+++ packages/client/components/DemoSummary.tsx
@@ -2,6 +2,8 @@ import useCanonical from '~/hooks/useCanonical'
 import React, {useEffect} from 'react'
 import NewMeetingSummaryRoot from '../modules/summary/components/NewMeetingSummaryRoot'
 import AtmosphereProvider from './AtmosphereProvider/AtmosphereProvider'
+import LocalAtmosphere from '~/modules/demo/LocalAtmosphere'
+import {MutableRecordSource} from 'relay-runtime/lib/store/RelayStoreTypes'
 
 const getLocalAtmosphere = () => {
   return import(/* webpackChunkName: 'LocalAtmosphere' */ '~/modules/demo/LocalAtmosphere')
@@ -11,6 +13,8 @@ const DemoSummary = () => {
   useCanonical('retrospective-demo-summary')
   useEffect(() => {
     return () => {
+      const localAtmosphere = new LocalAtmosphere().getStore().getSource() as MutableRecordSource
+      localAtmosphere.clear()
       window.localStorage.removeItem('retroDemo')
     }
   }, [])
