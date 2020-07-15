83a2a9170619356645b4bca8d4a9775eb88b4370
Bruce Tian <tianrunhe@gmail.com>
2020-07-15 16:43:45 -0700

    Revert regex for tsc config in lint-staged.config.js

--- packages/client/lint-staged.config.js
+++ packages/client/lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   'package/client/**/!(*graphql).{js,ts,tsx}': 'eslint --fix',
-  'package/client/**/!(*grphql).{ts,tsx}': () => 'tsc --noEmit -p tsconfig.json'
+  '**/*.{ts,tsx}': () => 'tsc --noEmit -p tsconfig.json'
 }
