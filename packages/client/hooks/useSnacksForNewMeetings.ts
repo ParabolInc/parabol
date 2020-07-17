4818e91285dce784eef7c715205e9adc6e4c3cdc
Matt Krick <matt.krick@gmail.com>
2020-07-16 17:32:23 -0700

    fix snackbar on start meeting
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/hooks/useSnacksForNewMeetings.ts
+++ packages/client/hooks/useSnacksForNewMeetings.ts
@@ -1,27 +1,33 @@
-import useAtmosphere from '~/hooks/useAtmosphere'
-import useRouter from '~/hooks/useRouter'
 import ms from 'ms'
 import {useEffect, useState} from 'react'
-import {TopBarMeetingsActiveMeetings} from '~/__generated__/TopBarMeetingsActiveMeetings.graphql'
+import useAtmosphere from '~/hooks/useAtmosphere'
+import useRouter from '~/hooks/useRouter'
 import fromTeamMemberId from '~/utils/relay/fromTeamMemberId'
+import {TopBarMeetingsActiveMeetings} from '~/__generated__/TopBarMeetingsActiveMeetings.graphql'
 
 const useSnacksForNewMeetings = (meetings: TopBarMeetingsActiveMeetings['activeMeetings']) => {
   const [dismissedMeetingIds] = useState(() => new Set<string>())
   const atmosphere = useAtmosphere()
   const {history} = useRouter()
   useEffect(() => {
+    const {viewerId} = atmosphere
     const fiveMinsAgo = new Date(Date.now() - ms('5m'))
+    const fiveSecsAgo = new Date(Date.now() - ms('5s'))
     const sortedMeetings = meetings
       .filter(
-        (meeting) =>
-          !dismissedMeetingIds.has(meeting.id) && new Date(meeting.createdAt) > fiveMinsAgo
-      )
+        (meeting) => {
+          if (dismissedMeetingIds.has(meeting.id)) return false
+          const createdAt = new Date(meeting.createdAt)
+          if (createdAt < fiveMinsAgo) return false
+          const {userId: facilitatorUserId} = fromTeamMemberId(meeting.facilitator.id)
+          if (facilitatorUserId === viewerId && createdAt > fiveSecsAgo) return false
+          return true
+        })
       .sort((a, b) => (new Date(a.createdAt) < new Date(b.createdAt) ? -1 : 1))
     const [snackedMeeting] = sortedMeetings
     if (!snackedMeeting) return
     const {id: meetingId, name: meetingName, facilitator} = snackedMeeting
     const {id: facilitatorId, preferredName} = facilitator
-    const {viewerId} = atmosphere
     const {userId: facilitatorUserId} = fromTeamMemberId(facilitatorId)
     const isInit = facilitatorUserId === viewerId
     const name = isInit ? 'You' : preferredName
