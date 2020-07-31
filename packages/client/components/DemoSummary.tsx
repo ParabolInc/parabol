172b7e73162b0cb0210418084f3a243239f8e14f
NikAiyer <nick.aiyer@gmail.com>
2020-07-30 18:02:04 -0700

    Made changes to DemoSummary to use getLocalAtmosphere function

--- packages/client/components/DemoSummary.tsx
+++ packages/client/components/DemoSummary.tsx
@@ -2,19 +2,25 @@ import useCanonical from '~/hooks/useCanonical'
 import React, {useEffect} from 'react'
 import NewMeetingSummaryRoot from '../modules/summary/components/NewMeetingSummaryRoot'
 import AtmosphereProvider from './AtmosphereProvider/AtmosphereProvider'
-import LocalAtmosphere from '~/modules/demo/LocalAtmosphere'
 import {MutableRecordSource} from 'relay-runtime/lib/store/RelayStoreTypes'
 
 const getLocalAtmosphere = () => {
   return import(/* webpackChunkName: 'LocalAtmosphere' */ '~/modules/demo/LocalAtmosphere')
 }
 
+async function clearLocalAtmosphere() {
+  const LocalAtmosphere = await getLocalAtmosphere()
+    .then((mod) => mod.default)
+    .catch()
+  const atmosphere = new LocalAtmosphere().getStore().getSource() as MutableRecordSource
+  atmosphere.clear()
+}
+
 const DemoSummary = () => {
   useCanonical('retrospective-demo-summary')
   useEffect(() => {
     return () => {
-      const localAtmosphere = new LocalAtmosphere().getStore().getSource() as MutableRecordSource
-      localAtmosphere.clear()
+      clearLocalAtmosphere()
       window.localStorage.removeItem('retroDemo')
     }
   }, [])
