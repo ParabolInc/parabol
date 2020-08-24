10e4ab9b8561f8f9258c523154207450b3cfd032
Bruce Tian <tianrunhe@gmail.com>
2020-08-24 14:25:34 -0700

    Add team filter integration test

--- packages/cypress/integration/userTaskView.ts
+++ packages/cypress/integration/userTaskView.ts
@@ -1,7 +1,12 @@
 describe('userTaskView', () => {
   beforeEach(() => {
+    cy.task('resetDb')
     cy.login()
     cy.visit('/me/tasks')
+  })
+
+  afterEach(() => {
+    cy.task('resetDb')
   })
 
   it('shows default team and team member filter on /me/tasks', () => {
@@ -22,5 +27,33 @@ describe('userTaskView', () => {
     cy.get('[data-cy=team_member_filter]').click()
     cy.get('[data-cy="team_member_filter_local|wnVeDjF-n"]').click()
     cy.url().should('include', 'userId=local|wnVeDjF-n')
+  })
+
+  it('filters tasks by team filter', () => {
+    // Create a new team
+    cy.get('nav')
+      .contains('Add a Team')
+      .click()
+    cy.get('form').within(() => {
+      cy.get('input[name="teamName"]').type(`cypress's own team`)
+      cy.root().submit()
+      cy.location('pathname', {timeout: 10000}).should('match', new RegExp('/team/(.+)'))
+      cy.wait(1000)
+    })
+
+    // Create a new task in the new team
+    cy.get('[data-cy=add_task_Future]').click()
+    cy.get('[data-cy=draggable-task-card-editor]')
+      .type("New Task under cypress's own team")
+      .type('{enter}')
+
+    cy.url().then((url) => {
+      const regex = /.+\/team\/(.+)/
+      const newTeamId = url.replace(regex, '$1')
+      cy.visit(`/me/tasks?teamId=${newTeamId}`)
+
+      cy.get('[data-cy=team_filter]').should('contain', `cypress's own team`)
+      cy.get('[data-cy=draggable-task-card-editor]').contains("New Task under cypress's own team")
+    })
   })
 })
