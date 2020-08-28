5369b7f476dad9519a9e37c871df7d45de1bf787
nickoferrall <nickoferrall@gmail.com>
2020-08-28 16:09:52 +0100

    added migration

---
+++ packages/server/database/migrations/20200828161500-samlDomainAsId.ts
@@ -0,0 +1,26 @@
+import {R} from 'rethinkdb-ts'
+
+export const up = async function(r: R) {
+  try {
+    await r
+      .table('SAML')
+      .update((row) => ({
+        id: row('domain')
+      }))
+      .run()
+  } catch (e) {
+    console.log(e)
+  }
+  try {
+    await r
+      .table('SAML')
+      .filter((row) => row('id').eq(row('domain')))
+      .run()
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+export const down = async function() {
+  // noop
+}
