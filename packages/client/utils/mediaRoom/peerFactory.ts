603457a44ef87648e01932b18bcd8247cb4cc3c6
Tiffany Han <hantiffany@gmail.com>
2020-09-02 14:17:01 -0700

    delete unused peerfactory

--- packages/client/utils/mediaRoom/peerFactory.ts
+++
@@ -1,7 +0,0 @@
-import protoo from 'protoo-client'
-import Room from './Room'
-
-export default function peerFactory(transport: protoo.WebSocketTransport, room: Room) {
-  const peer = new protoo.Peer(transport)
-  return [peer, room]
-}
