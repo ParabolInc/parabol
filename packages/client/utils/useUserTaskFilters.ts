d8e05c00527ced503a0ef034bcc55bbfdea49671
Bruce Tian <tianrunhe@gmail.com>
2020-09-09 13:40:09 -0700

    Use URLSearchParams instead of the query-string pacakge

--- packages/client/utils/useUserTaskFilters.ts
+++ packages/client/utils/useUserTaskFilters.ts
@@ -1,15 +1,14 @@
-import * as queryString from "query-string"
 import useRouter from '~/hooks/useRouter'
 import {useMemo} from 'react'
 
 const useUserTaskFilters = (viewerId: string) => {
   const {location} = useRouter()
   return useMemo(() => {
-    const parsed = queryString.parse(location.search, {parseBooleans: true})
-    const teamIds = parsed.teamIds ? (parsed.teamIds as string).split(',') : undefined
-    const userIdsArray = parsed.userIds ? (parsed.userIds as string).split(',') : undefined
+    const parsed = new URLSearchParams(location.search)
+    const teamIds = parsed.has('teamIds') ? (parsed.get('teamIds') as string).split(',') : undefined
+    const userIdsArray = parsed.has('userIds') ? (parsed.get('userIds') as string).split(',') : undefined
     const userIds = userIdsArray || (teamIds ? undefined : [viewerId])
-    const showArchived = !!parsed.archived
+    const showArchived = !!parsed.get('archived')
     return {userIds, teamIds, showArchived}
   }, [viewerId, location])
 }
