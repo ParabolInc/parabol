0b8ad859eec1f55e20f2408415f779319272af0f
Tiffany Han <hantiffany@gmail.com>
2020-09-11 17:58:11 -0700

    - build sfu server for prod - bump pm2

--- scripts/webpack/dev.servers.config.js
+++ scripts/webpack/dev.servers.config.js
@@ -18,45 +18,39 @@ module.exports = {
   devtool: 'eval-source-map',
   mode: 'development',
   node: {
-    __dirname: false,
+    __dirname: false
   },
   entry: {
     web: ['webpack/hot/poll?1000', DOTENV, path.join(SERVER_ROOT, 'server.dev.ts')],
     gqlExecutor: [DOTENV, path.join(GQL_ROOT, 'gqlExecutor.ts')],
-    sfu: [DOTENV, path.join(SFU_ROOT, 'server.ts')],
+    sfu: [DOTENV, path.join(SFU_ROOT, 'server.ts')]
   },
   output: {
     filename: '[name].js',
     path: path.join(PROJECT_ROOT, 'dev'),
     libraryTarget: 'commonjs',
     hotUpdateChunkFilename: 'hot/[id].[hash].hot-update.js',
-    hotUpdateMainFilename: 'hot/[hash].hot-update.json',
+    hotUpdateMainFilename: 'hot/[hash].hot-update.json'
   },
   resolve: {
     alias: {
       '~': path.join(CLIENT_ROOT),
       'parabol-server': SERVER_ROOT,
-      'parabol-client': CLIENT_ROOT,
+      'parabol-client': CLIENT_ROOT
     },
     extensions: ['.js', '.json', '.ts', '.tsx'],
     unsafeCache: true,
     // this is run outside the server dir, but we want to favor using modules from the server dir
-    modules: [
-      path.resolve(SERVER_ROOT, 'node_modules'),
-      path.resolve(PROJECT_ROOT, 'node_modules'),
-    ],
+    modules: [path.resolve(SERVER_ROOT, 'node_modules'), path.resolve(PROJECT_ROOT, 'node_modules')]
   },
   resolveLoader: {
-    modules: [
-      path.resolve(SERVER_ROOT, 'node_modules'),
-      path.resolve(PROJECT_ROOT, 'node_modules'),
-    ],
+    modules: [path.resolve(SERVER_ROOT, 'node_modules'), path.resolve(PROJECT_ROOT, 'node_modules')]
   },
   target: 'node',
   externals: [
     nodeExternals({
-      whitelist: ['webpack/hot/poll?1000', /parabol-client/, /parabol-server/],
-    }),
+      whitelist: ['webpack/hot/poll?1000', /parabol-client/, /parabol-server/]
+    })
   ],
   plugins: [new webpack.HotModuleReplacementPlugin()],
   module: {
@@ -69,18 +63,18 @@ module.exports = {
           {
             loader: '@sucrase/webpack-loader',
             options: {
-              transforms: ['jsx'],
-            },
-          },
-        ],
+              transforms: ['jsx']
+            }
+          }
+        ]
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
-        use: ['ignore-loader'],
-      },
-    ],
+        use: ['ignore-loader']
+      }
+    ]
   },
   optimization: {
-    noEmitOnErrors: true,
-  },
+    noEmitOnErrors: true
+  }
 }
