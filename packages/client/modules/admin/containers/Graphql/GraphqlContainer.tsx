eb4a57f612ec249ebf95799a3700b6ebea525bff
Tiffany Han <tiffany@parabol.co>
2020-09-18 07:25:17 -0700

    remove trailing slash frmo intranet graphql url (#4175)

--- packages/client/modules/admin/containers/Graphql/GraphqlContainer.tsx
+++ packages/client/modules/admin/containers/Graphql/GraphqlContainer.tsx
@@ -30,7 +30,7 @@ const GraphqlContainer = () => {
     window.localStorage.setItem(LocalStorageKey.GRAPHIQL_SCHEMA, value)
   }
   const fetcher = async ({query, variables}) => {
-    const url = `${window.location.origin}/intranet-graphql/`
+    const url = `${window.location.origin}/intranet-graphql`
     const res = await fetch(url, {
       method: 'POST',
       headers: {
