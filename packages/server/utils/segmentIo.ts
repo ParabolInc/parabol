0951f076da4e62d958ca3be4c545fa99de011a05
Jordan Husney <jordan.husney@gmail.com>
2020-09-22 22:28:31 -0700

    Correct Segment GA schema; default to userId if null

--- packages/server/utils/segmentIo.ts
+++ packages/server/utils/segmentIo.ts
@@ -24,27 +24,25 @@ segmentIo.track = async (options) => {
   return (segmentIo as any)._track({
     userId,
     event,
-    context: {
-      /*
-       * Specify the Google Analytics ID for use with the Google Analytics integration
-       *
-       * Why do we do this? GA is a sad, old bear. Here's the story: when
-       * a user new to Parabol visits our marketing website the Segment-generated
-       * anonymousId is hashed to create the GA User ID. Before this change,
-       * we assumed sending a identify() event that would tie the new, Parabol userId to the
-       * anonymousId, but Google Analytics apparently does not support tying users together
-       * in this way. When we would looked at our data, events coming from our app would
-       * be tallied as "direct" rather than acquired through a known channel (e.g.
-       * "Organic Search"), making it look like all these new users just dropped out of the
-       * sky.
-       *
-       * To work around this, we specify which a UUID Segment will consistently hash
-       * between our marketing site and app domains. We do this by passing in
-       * additional Segment context, as seen here:
-       */
-      integrations: {
-        'Google Analytics': {clientId: segmentId}
-      }
+    /*
+     * Specify the Google Analytics ID for use with the Google Analytics integration
+     *
+     * Why do we do this? GA is a sad, old bear. Here's the story: when
+     * a user new to Parabol visits our marketing website the Segment-generated
+     * anonymousId is hashed to create the GA User ID. Before this change,
+     * we assumed sending a identify() event that would tie the new, Parabol userId to the
+     * anonymousId, but Google Analytics apparently does not support tying users together
+     * in this way. When we would looked at our data, events coming from our app would
+     * be tallied as "direct" rather than acquired through a known channel (e.g.
+     * "Organic Search"), making it look like all these new users just dropped out of the
+     * sky.
+     *
+     * To work around this, we specify which a UUID Segment will consistently hash
+     * between our marketing site and app domains. We do this by passing in
+     * additional Segment context, as seen here:
+     */
+    integrations: {
+      'Google Analytics': {clientId: segmentId || userId}
     },
     properties: {
       ...properties,
