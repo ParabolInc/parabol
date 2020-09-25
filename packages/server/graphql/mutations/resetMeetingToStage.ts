142f2f1546412ffb266109c777d8911859c36937
Tiffany Han <hantiffany@gmail.com>
2020-09-25 14:23:38 -0700

    mutation deletes all threadables and clears votes

--- packages/server/graphql/mutations/resetMeetingToStage.ts
+++ packages/server/graphql/mutations/resetMeetingToStage.ts
@@ -1,4 +1,5 @@
 import {GraphQLBoolean, GraphQLID, GraphQLNonNull} from 'graphql'
+import getRethink from '../../database/rethinkDriver'
 
 const resetMeetingToStage = {
   type: GraphQLNonNull(GraphQLBoolean),
@@ -11,11 +12,34 @@ const resetMeetingToStage = {
       type: GraphQLNonNull(GraphQLID)
     }
   },
-  resolve: async (_source, {meetingId, stageId}, context) => {
-    console.log('reset meeting to stage mutation invoked on server!')
+  resolve: async (_source, {meetingId, stageId}, {dataLoader}) => {
     console.log('meetingId:', meetingId)
     console.log('stageId:', stageId)
-    console.log(context)
+    const reflectionGroups = await dataLoader
+      .get('retroReflectionGroupsByMeetingId')
+      .load(meetingId)
+    console.log('reflection groups:', reflectionGroups)
+    const reflectionGroupIds = reflectionGroups.map((rg) => rg.id)
+    console.log('reflection group ids:', reflectionGroupIds)
+    const r = await getRethink()
+    await Promise.all([
+      r
+        .table('Comment')
+        .getAll(r.args(reflectionGroupIds), {index: 'threadId'})
+        .delete()
+        .run(),
+      r
+        .table('Task')
+        .getAll(r.args(reflectionGroupIds), {index: 'threadId'})
+        .delete()
+        .run(),
+      r
+        .table('RetroReflectionGroup')
+        .getAll(r.args(reflectionGroupIds))
+        .update({voterIds: []})
+        .run()
+    ])
+
     return true
   }
 }
