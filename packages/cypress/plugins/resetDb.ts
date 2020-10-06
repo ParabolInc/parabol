4b0d8406cd35faa68da92ed8d09aaf402b0892e8
Bruce Tian <tianrunhe@gmail.com>
2020-10-05 17:04:47 -0700

    ResetDB in Cypress test should not wipe out the QueryMap db

--- packages/cypress/plugins/resetDb.ts
+++ packages/cypress/plugins/resetDb.ts
@@ -10,36 +10,34 @@ interface DBOptions {
 const resetDb = ({source, target}: DBOptions) => async () => {
   const r = await getRethink()
   // wipe out all records on the target
-  await r
+  const tableList: string[] = await r
     .db(target)
     .tableList()
-    .forEach(
-      (t: string) =>
-        t !== 'QueryMap' &&
-        r
-          .db(target)
-          .table(t)
-          .delete()
-    )
     .run()
+  const filteredTableList: string[] = tableList.filter((tableName) => tableName !== 'QueryMap')
+  filteredTableList.forEach((tableName) =>
+    r
+      .db(target)
+      .table(tableName)
+      .delete()
+      .run()
+  )
+
   // add source docs to target db
-  return r
-    .db(target)
-    .tableList()
-    .forEach((t: string) => {
-      if (t !== 'QueryMap') {
-        return r
-          .db(target)
-          .table(t)
-          .insert(
-            r
-              .db(source)
-              .table(t)
-              .coerceTo('array')
-          )
-      }
-    })
-    .run()
+  return Promise.all(
+    filteredTableList.map((t: any) =>
+      r
+        .db(target)
+        .table(t)
+        .insert(
+          r
+            .db(source)
+            .table(t)
+            .coerceTo('array')
+        )
+        .run()
+    )
+  )
 }
 
 export default resetDb
