c61f008011f797be8fb93b6955968931fbf3e104
Matt Krick <matt.krick@gmail.com>
2020-10-12 17:39:26 -0700

    add a CLI for CRUDs
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

---
+++ scripts/codeshift/baseClientMutation.ts
@@ -0,0 +1,42 @@
+import graphql from 'babel-plugin-relay/macro'
+import {commitMutation} from 'react-relay'
+import {StandardMutation} from '../types/relayMutations'
+import {PASCAL_MUTATIONMutation as TPASCAL_MUTATIONMutation} from '../__generated__/PASCAL_MUTATIONMutation.graphql'
+
+graphql`
+  fragment PASCAL_MUTATIONMutation_meeting on PASCAL_MUTATIONSuccess {
+    <<ADD FIELDS>>
+  }
+`
+
+const mutation = graphql`
+  mutation PASCAL_MUTATIONMutation() {
+    MUTATION_NAME() {
+      ... on ErrorPayload {
+        error {
+          message
+        }
+      }
+      ...PASCAL_MUTATIONMutation_meeting @relay(mask: false)
+    }
+  }
+`
+
+const PASCAL_MUTATIONMutation: StandardMutation<TPASCAL_MUTATIONMutation> = (
+  atmosphere,
+  variables,
+  {onError, onCompleted}
+) => {
+  return commitMutation<TPASCAL_MUTATIONMutation>(atmosphere, {
+    mutation,
+    variables,
+    optimisticUpdater: (store) => {
+      const {} = variables
+
+    },
+    onCompleted,
+    onError
+  })
+}
+
+export default PASCAL_MUTATIONMutation
