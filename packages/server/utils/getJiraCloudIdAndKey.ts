a881f812d8d0c61aa5b348adcfde6164199897e7
nickoferrall <nickoferrall@gmail.com>
2020-11-17 10:23:50 +0000

    update sucrase and eslint dependencies

--- packages/server/utils/getJiraCloudIdAndKey.ts
+++ packages/server/utils/getJiraCloudIdAndKey.ts
@@ -1,4 +1,4 @@
-const getJiraCloudIdAndKey = (id: string) => {
+const getJiraCloudIdAndKey = (id: string): [cloudId: string, issueKey: string] => {
   const [cloudId, issueKey] = id.split(':')
   return [cloudId, issueKey] as [cloudId: string, issueKey: string]
 }
