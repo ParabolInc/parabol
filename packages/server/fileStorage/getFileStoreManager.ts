42bd5ef24d4e9bb1ffb7fe4a87a2eca9e52b803a
Tiffany Han <hantiffany@gmail.com>
2020-11-17 15:57:55 -0800

    throw error if `FILE_STORE_PROVIDER` env var not defined

--- packages/server/fileStorage/getFileStoreManager.ts
+++ packages/server/fileStorage/getFileStoreManager.ts
@@ -10,8 +10,14 @@ const managers = {
 
 const getFileStoreManager = () => {
   if (!fileStoreManager) {
-    const fileStoreProvider = process.env.FILE_STORE_PROVIDER!
-    const Manager = managers[fileStoreProvider] ?? LocalFileSystemManager
+    const fileStoreProvider = process.env.FILE_STORE_PROVIDER
+    if (!fileStoreProvider) {
+      throw new Error('The env var `FILE_STORE_PROVIDER` must be defined in `.env` file')
+    }
+    if (!(fileStoreProvider in managers)) {
+      throw new Error('Got invalid value for `FILE_STORE_PROVIDER` env var')
+    }
+    const Manager = managers[fileStoreProvider]
     fileStoreManager = new Manager()
   }
   return fileStoreManager
