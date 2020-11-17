8a75c67f8d999f0c87239ba5b9915eaeaccb13fa
Tiffany Han <hantiffany@gmail.com>
2020-11-17 15:44:16 -0800

    mark protected members of FileStoreManager

--- packages/server/fileStorage/FileStoreManager.ts
+++ packages/server/fileStorage/FileStoreManager.ts
@@ -6,26 +6,22 @@ export interface PutFileOptions {
 }
 
 export default abstract class FileStoreManager {
-  _putFileCb: undefined | ((fullPath: string) => void)
-
-  abstract _putFile(fullPath: string, buffer: Buffer): Promise<void>
-
-  async putFile(options: PutFileOptions): Promise<string> {
-    const {partialPath, buffer} = options
-    const fullPath = this.prependPath(partialPath)
-    await this._putFile(fullPath, buffer)
-    return this.getPublicFileLocation(fullPath)
-  }
-
-  abstract prependPath(partialPath: string): string
-
-  abstract getPublicFileLocation(fullPath: string): string
-
   static getUserAvatarPath(userId: string, ext: string): string {
     return `User/${userId}/picture/${shortid.generate()}.${ext}`
   }
 
   static getOrgAvatarPath(orgId: string, ext: string): string {
     return `Organization/${orgId}/picture/${shortid.generate()}.${ext}`
+  }
+
+  protected abstract prependPath(partialPath: string): string
+  protected abstract _putFile(fullPath: string, buffer: Buffer): Promise<void>
+  protected abstract getPublicFileLocation(fullPath: string): string
+
+  async putFile(options: PutFileOptions): Promise<string> {
+    const {partialPath, buffer} = options
+    const fullPath = this.prependPath(partialPath)
+    await this._putFile(fullPath, buffer)
+    return this.getPublicFileLocation(fullPath)
   }
 }
