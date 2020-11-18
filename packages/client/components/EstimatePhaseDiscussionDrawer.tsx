e4c77f159238e763ec3848b7d14adc3e6aa999c7
nickoferrall <nickoferrall@gmail.com>
2020-11-18 09:37:17 +0000

    remove video from estimate phase drawer

--- packages/client/components/EstimatePhaseDiscussionDrawer.tsx
+++ packages/client/components/EstimatePhaseDiscussionDrawer.tsx
@@ -1,19 +1,16 @@
 import styled from '@emotion/styled'
-import React, {useEffect, useRef} from 'react'
+import React, {useRef} from 'react'
 import graphql from 'babel-plugin-relay/macro'
-import {commitLocalUpdate, createFragmentContainer} from 'react-relay'
+import {createFragmentContainer} from 'react-relay'
 import {useCoverable} from '~/hooks/useControlBarCovers'
 import {desktopSidebarShadow} from '~/styles/elevation'
 import {EstimatePhaseDiscussionDrawer_meeting} from '~/__generated__/EstimatePhaseDiscussionDrawer_meeting.graphql'
 import {DiscussionThreadEnum, MeetingControlBarEnum, ZIndex} from '../types/constEnums'
 import DiscussionThreadRoot from './DiscussionThreadRoot'
 import {PALETTE} from '~/styles/paletteV2'
-import Icon from './Icon'
-import {ICON_SIZE} from '~/styles/typographyV2'
-import PlainButton from './PlainButton/PlainButton'
 import LabelHeading from './LabelHeading/LabelHeading'
 import Avatar from './Avatar/Avatar'
-import useAtmosphere from '~/hooks/useAtmosphere'
+import SidebarToggle from './SidebarToggle'
 
 const Drawer = styled('div')<{isDesktop: boolean}>(({isDesktop}) => ({
   boxShadow: isDesktop ? desktopSidebarShadow : undefined,
@@ -30,12 +27,6 @@ const Drawer = styled('div')<{isDesktop: boolean}>(({isDesktop}) => ({
   zIndex: ZIndex.SIDEBAR
 }))
 
-const VideoContainer = styled('div')<{isShowingVideo: boolean}>(({isShowingVideo}) => ({
-  display: isShowingVideo ? 'flex' : 'none',
-  height: '200px',
-  padding: 6
-}))
-
 const ThreadColumn = styled('div')({
   alignItems: 'center',
   bottom: 0,
@@ -48,19 +39,6 @@ const ThreadColumn = styled('div')({
   overflow: 'auto',
   position: 'relative',
   width: '100%'
-})
-
-const ButtonGroup = styled('div')({
-  display: 'flex',
-  justifyContent: 'flex-end',
-  height: 16,
-  userSelect: 'none',
-  width: '100%'
-})
-
-const StyledIcon = styled(Icon)({
-  fontSize: ICON_SIZE.MD24,
-  cursor: 'pointer'
 })
 
 const AvatarGroup = styled(LabelHeading)({
@@ -84,19 +62,6 @@ const StyledAvatar = styled(Avatar)({
   transition: 'all 150ms'
 })
 
-const ShowVideoIcon = styled(Icon)({
-  color: '#FFFF',
-  fontSize: ICON_SIZE.MD24
-})
-
-const ShowVideoButton = styled(PlainButton)<{isShowingVideo: boolean}>(({isShowingVideo}) => ({
-  backgroundColor: PALETTE.TEXT_PURPLE,
-  borderRadius: '50%',
-  display: isShowingVideo ? 'none' : 'flex',
-  margin: '0px 3px',
-  padding: 8
-}))
-
 interface Props {
   isDesktop: boolean
   meeting: EstimatePhaseDiscussionDrawer_meeting
@@ -104,21 +69,10 @@ interface Props {
 
 const EstimatePhaseDiscussionDrawer = (props: Props) => {
   const {isDesktop, meeting} = props
-  const {id: meetingId, endedAt, isShowingVideo, localStage, viewerMeetingMember} = meeting
+  const {id: meetingId, endedAt, localStage, viewerMeetingMember} = meeting
   const {user} = viewerMeetingMember
   const {picture} = user
   const {serviceTaskId} = localStage
-  const atmosphere = useAtmosphere()
-  const setIsShowingVideo = (isShowing: boolean) => {
-    commitLocalUpdate(atmosphere, (store) => {
-      const meeting = store.get(meetingId)!
-      meeting.setValue(isShowing, 'isShowingVideo')
-    })
-  }
-  useEffect(() => {
-    setIsShowingVideo(true)
-  }, [])
-  // const [isShowingVideo, setIsShowingVideo] = useState(true)
   const ref = useRef<HTMLDivElement>(null)
   const meetingControlBarBottom = 16
   const coverableHeight = isDesktop ? MeetingControlBarEnum.HEIGHT + meetingControlBarBottom : 0
@@ -126,25 +80,13 @@ const EstimatePhaseDiscussionDrawer = (props: Props) => {
 
   return (
     <Drawer isDesktop={isDesktop} ref={ref}>
-      <VideoContainer isShowingVideo={isShowingVideo}>
-        <ButtonGroup>
-          <PlainButton>
-            <StyledIcon>fullscreen</StyledIcon>
-          </PlainButton>
-          <PlainButton onClick={() => setIsShowingVideo(false)}>
-            <StyledIcon>close</StyledIcon>
-          </PlainButton>
-        </ButtonGroup>
-      </VideoContainer>
       <DiscussingGroup>
         <AvatarGroup>
           {[1, 2, 3, 4].map((__example, idx) => {
             return <StyledAvatar key={idx} size={32} picture={picture} />
           })}
         </AvatarGroup>
-        <ShowVideoButton isShowingVideo={isShowingVideo} onClick={() => setIsShowingVideo(true)}>
-          <ShowVideoIcon>videocam</ShowVideoIcon>
-        </ShowVideoButton>
+        <SidebarToggle dataCy='right-sidebar' />
       </DiscussingGroup>
       <ThreadColumn>
         <DiscussionThreadRoot
@@ -167,7 +109,6 @@ export default createFragmentContainer(EstimatePhaseDiscussionDrawer, {
     fragment EstimatePhaseDiscussionDrawer_meeting on PokerMeeting {
       id
       endedAt
-      isShowingVideo
       localStage {
         ...EstimatePhaseDiscussionDrawerStage @relay(mask: false)
       }
