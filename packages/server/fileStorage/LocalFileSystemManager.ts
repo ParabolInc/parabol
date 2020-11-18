765447589c353ed7c634238b10dd70d7d51579e2
Tiffany Han <hantiffany@gmail.com>
2020-11-17 16:47:55 -0800

    use promisified version of fs.mkdir and fs.writeFile

--- packages/server/fileStorage/LocalFileSystemManager.ts
+++ packages/server/fileStorage/LocalFileSystemManager.ts
@@ -14,15 +14,7 @@ export default class LocalFileSystemManager extends FileStoreManager {
 
   async _putFile(fullPath: string, buffer: Buffer): Promise<void> {
     const fsAbsLocation = path.join(process.cwd(), fullPath)
-    await fs.mkdir(path.dirname(fsAbsLocation), {recursive: true}, async (err) => {
-      if (err) {
-        console.error(err)
-      }
-      await fs.writeFile(fsAbsLocation, buffer, (err) => {
-        if (err) {
-          console.error(err)
-        }
-      })
-    })
+    await fs.promises.mkdir(path.dirname(fsAbsLocation), {recursive: true})
+    await fs.promises.writeFile(fsAbsLocation, buffer)
   }
 }
