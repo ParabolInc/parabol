f54dd1498a1872a0a463c44abd4a0ac3f7d6a5f2
Tiffany Han <hantiffany@gmail.com>
2020-12-17 14:28:45 -0800

    sso domain getter returns domain with tld included

--- packages/client/utils/getSSODomainFromEmail.ts
+++ packages/client/utils/getSSODomainFromEmail.ts
@@ -1,9 +1,6 @@
 const getSSODomainFromEmail = (email) => {
   const [, domainWithTld] = email.toLowerCase().split('@')
-  if (!domainWithTld) return null
-  const lastDotIdx = domainWithTld.lastIndexOf('.')
-  const lastSafeDotIdx = lastDotIdx === -1 ? 1e6 : lastDotIdx
-  return domainWithTld.slice(0, lastSafeDotIdx)
+  return domainWithTld
 }
 
 export default getSSODomainFromEmail
