a8fc70cfed2580b5c602d425954f6f97a5e9322e
Tiffany Han <hantiffany@gmail.com>
2021-01-14 13:41:44 -0800

    separate web and metrics databases

--- packages/server/postgres/getPostgres.ts
+++ packages/server/postgres/getPostgres.ts
@@ -1,11 +1,28 @@
 import { Pool } from 'pg'
 
-let pool
-const getPostgres = () => {
-  if (!pool) {
-    pool = new Pool({ max: 1 })
+export enum dbKeys {
+  web = 'web',
+  metrics = 'metrics'
+}
+
+const pgUrlMap = {
+  [dbKeys.web]: process.env.PG_WEB_URL,
+  [dbKeys.metrics]: process.env.PG_METRICS_URL
+} as {[key in dbKeys]: string}
+
+const poolMap = {
+  [dbKeys.web]: null,
+  [dbKeys.metrics]: null
+} as {[key in dbKeys]: typeof Pool | null}
+
+const getPostgres = (dbKey: dbKeys = dbKeys.web) => {
+  if (!poolMap[dbKey]) {
+    poolMap[dbKey] = new Pool({
+      connectionString: pgUrlMap[dbKey],
+      max: 1
+    })
   }
-  return pool
+  return poolMap[dbKey]
 }
 
 export default getPostgres
