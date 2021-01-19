78b70f4e9f8646a863c6c07b3fab735c1cd92435
Tiffany Han <hantiffany@gmail.com>
2021-01-19 13:50:49 -0800

    container gets POSTGRES_PASSWORD from .env file (don't put value in string or it wont work)

--- packages/server/postgres/getPostgres.ts
+++ packages/server/postgres/getPostgres.ts
@@ -4,6 +4,7 @@ let pool
 const getPostgres = () => {
   if (!pool) {
     pool = new Pool({
+      connectionString: process.env.POSTGRES_URL,
       max: process.env.NODE_ENV === 'production' ? 85 : 5 // leave 15 conns for su management in production
     })
   }
