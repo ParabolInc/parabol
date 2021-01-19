0134fa8402644843a3ef83eefbe9e4c56c99412c
Tiffany Han <hantiffany@gmail.com>
2021-01-19 12:55:45 -0800

    Revert "separate web and metrics databases"
    This reverts commit a8fc70cfed2580b5c602d425954f6f97a5e9322e.

--- packages/server/postgres/getPostgres.ts
+++ packages/server/postgres/getPostgres.ts
@@ -1,28 +1,11 @@
 import { Pool } from 'pg'
 
-export enum dbKeys {
-  web = 'web',
-  metrics = 'metrics'
-}
-
-const pgUrlMap = {
-  [dbKeys.web]: process.env.PG_WEB_URL,
-  [dbKeys.metrics]: process.env.PG_METRICS_URL
-} as {[key in dbKeys]: string}
-
-const poolMap = {
-  [dbKeys.web]: null,
-  [dbKeys.metrics]: null
-} as {[key in dbKeys]: typeof Pool | null}
-
-const getPostgres = (dbKey: dbKeys = dbKeys.web) => {
-  if (!poolMap[dbKey]) {
-    poolMap[dbKey] = new Pool({
-      connectionString: pgUrlMap[dbKey],
-      max: process.env.NODE_ENV === 'production' ? 85 : 5 // leave 15 conns for su management in production
-    })
+let pool
+const getPostgres = () => {
+  if (!pool) {
+    pool = new Pool({ max: 1 })
   }
-  return poolMap[dbKey]
+  return pool
 }
 
 export default getPostgres
