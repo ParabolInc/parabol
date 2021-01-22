5058dfa4408b110906d37db1d9eb62db04dfd7c7
Matt Krick <matt.krick@gmail.com>
2021-01-22 15:24:21 -0800

    add pgm
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

---
+++ packages/server/postgres/pgmConfig.js
@@ -0,0 +1,12 @@
+
+const pgmConfig = {
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB,
+  tsconfig: 'packages/server/tsconfig.json',
+  'migrations-dir': 'packages/server/postgres/migrations'
+}
+
+module.exports = pgmConfig
