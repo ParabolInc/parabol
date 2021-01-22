f62ecff87678b50994807726b006b4f87839b6f3
Tiffany Han <hantiffany@gmail.com>
2021-01-21 17:15:37 -0800

    pushing updated branch for debug

---
+++ packages/server/postgres/migrations/web/1611277007145_addTable.ts
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.sql(`
+    CREATE TABLE audit (id int, name text);
+  `)
+};
+
+exports.down = pgm => {
+  pgm.sql(`
+    DROP TABLE audit;
+  `)
+};
