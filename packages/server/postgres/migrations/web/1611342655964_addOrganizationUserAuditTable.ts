4d584be04a94839fb03164db6d1f8ed75703d9a5
Tiffany Han <hantiffany@gmail.com>
2021-01-22 12:52:23 -0800

    check in migration to add OrganizationUserAudit table

---
+++ packages/server/postgres/migrations/web/1611342655964_addOrganizationUserAuditTable.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+    CREATE TABLE "OrganizationUserAudit" (
+      id SERIAL PRIMARY KEY,
+      "orgId" INT NOT NULL,
+      "userId" INT NOT NULL,
+      "eventDate" TIMESTAMP WITH TIME ZONE NOT NULL,
+      "eventType" INT NOT NULL
+    );
+  `)
+  pgm.sql(`
+    CREATE INDEX "idx_OrganizationUserAudit_orgId" ON "OrganizationUserAudit"("orgId");
+  `)
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+    DROP TABLE "OrganizationUserAudit";
+  `)
+}
