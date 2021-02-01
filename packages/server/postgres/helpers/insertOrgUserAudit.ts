b3b8e25a28c3ecebed6c91458100a2adaa5ba35e
Tiffany Han <hantiffany@gmail.com>
2021-02-01 00:26:55 -0800

    verified works

--- packages/server/postgres/helpers/insertOrgUserAudit.ts
+++ packages/server/postgres/helpers/insertOrgUserAudit.ts
@@ -6,9 +6,9 @@ import type * as s from 'zapatos/schema'
 const insertOrgUserAudit = async (
   orgIds: string[],
   userId: string,
-  eventType: 'activated' | 'added' | 'inactivated' | 'removed',
+  eventType: s.OrganizationUserAuditEventTypeEnum,
   eventDate: Date = new Date()
-) => {
+): Promise<s.OrganizationUserAudit.Selectable[]> => {
   const pg = getPg()
   const auditRows = orgIds.map(orgId => ({orgId, userId, eventType, eventDate}))
   const result = await z.insert('OrganizationUserAudit', auditRows).run(pg)
