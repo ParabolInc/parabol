26adbed73c40917753fe368dd76b3c4a63fe0f35
Tiffany Han <hantiffany@gmail.com>
2021-02-20 18:03:42 -0800

    wip account for race conditions

--- packages/server/postgres/migrations/postDeploy/1613857272859_backfillUserTable.ts
+++ packages/server/postgres/migrations/postDeploy/1613857272859_backfillUserTable.ts
@@ -10,20 +10,36 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 export async function up(pgm: MigrationBuilder): Promise<void> {
   const r = await getRethink()
   const batchSize = 1
-  let i = 0
-  
+  let lastStartTs
+
+  const doBackfill = async () => {
+    lastStartTs = lastStartTs ?? r.minval
+    const startTs = new Date()
+    let i = 0
+    let foundUsers = false
+
+    while (true) {
+      const offset = batchSize * i
+      const rethinkUsers = await r
+        .table('User')
+        .orderBy('createdAt')
+        .filter(row => row('createdAt').gt(lastStartTs))
+        .skip(offset)
+        .limit(batchSize)
+        .run()
+      if (!rethinkUsers.length) { break }
+      foundUsers = true
+      const pgUsers = mapUsers(rethinkUsers)
+      backupUserQuery.run({users: pgUsers}, getPg())
+      i += 1
+    }
+    lastStartTs = startTs
+    return foundUsers
+  }
+
   while (true) {
-    const offset = batchSize * i
-    const rethinkUsers = await r
-      .table('User')
-      .orderBy('createdAt')
-      .skip(offset)
-      .limit(batchSize)
-      .run()
-    if (!rethinkUsers.length) { break }
-    const pgUsers = mapUsers(rethinkUsers)
-    backupUserQuery.run({users: pgUsers}, getPg())
-    i += 1
+    const backfillFoundUsers = await doBackfill()
+    if (!backfillFoundUsers) { break }
   }
   await r.getPoolMaster().drain()
 }
