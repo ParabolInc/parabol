45acdb42e86640e050c817d819ebc741a51bd44d
Tiffany Han <hantiffany@gmail.com>
2021-02-23 15:32:31 -0800

    change batch size

--- packages/server/postgres/migrations/postDeploy/1613857272859_backfillUserTable.ts
+++ packages/server/postgres/migrations/postDeploy/1613857272859_backfillUserTable.ts
@@ -8,7 +8,7 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   const r = await getRethink()
-  const batchSize = 1
+  const batchSize = 5000
   const backfillStartTs = new Date()
   console.log('start ts:', backfillStartTs)
 
