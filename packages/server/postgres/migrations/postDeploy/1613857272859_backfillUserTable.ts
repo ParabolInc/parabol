5309c617787da12b5981c82987f8ff7c2076c4d7
Tiffany Han <hantiffany@gmail.com>
2021-02-23 12:02:20 -0800

    account for created and updated race conditions

--- packages/server/postgres/migrations/postDeploy/1613857272859_backfillUserTable.ts
+++ packages/server/postgres/migrations/postDeploy/1613857272859_backfillUserTable.ts
@@ -2,54 +2,65 @@
 import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
 import getRethink from '../../../database/rethinkDriver'
 import insertUser from '../../helpers/insertUser'
+import catchAndLog from '../../utils/catchAndLog'
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   const r = await getRethink()
   const batchSize = 1
-  let lastStartTs
+  const backfillStartTs = new Date()
+  console.log('start ts:', backfillStartTs)
 
-  const doBackfill = async () => {
-    lastStartTs = lastStartTs ?? r.minval
-    const startTs = new Date()
-    // todo: sleep long enough to manually insert another user in
+  const doBackfill = async (
+    usersByFieldChoice: ('createdAt' | 'updatedAt'),
+    afterTs?: Date
+  ) => {
     let i = 0
     let foundUsers = false
-    // todo: need to clean up rethink users with email DELETED
+    // todo: aghostuser has invalid value for `tms`
+    // todo: special case to account for rethink users with email DELETED
     // todo: check for duplicate values for id or email in rethink users
 
-    // todo: should also account for update race conditions??
-    // maybe the backup should do on conflict update then??
-    // console.log('starting backfill pass...')
-    // console.log('last start ts:', lastStartTs)
-    // console.log('start ts:', startTs)
+    console.log('starting backfill pass...')
+    console.log('after ts:', afterTs, 'by:', usersByFieldChoice)
     while (true) {
       console.log('i:', i)
       const offset = batchSize * i
       const rethinkUsers = await r
         .table('User')
-        .orderBy('createdAt')
-        .filter(row => row('createdAt').gt(lastStartTs))
+        .orderBy(usersByFieldChoice)
+        .filter(row => row(usersByFieldChoice).gt(afterTs ?? r.minval))
         .skip(offset)
         .limit(batchSize)
         .run()
       if (!rethinkUsers.length) { break }
       foundUsers = true
-      await insertUser(rethinkUsers)
+      await catchAndLog(() => insertUser(rethinkUsers))
       i += 1
     }
-    lastStartTs = startTs
-    // todo: another loop for race conditions updates???
     return foundUsers
   }
 
-  while (true) {
-    const backfillFoundUsers = await doBackfill()
-    console.log('backfillFoundUsers?', backfillFoundUsers)
-    // await new Promise(resolve => setTimeout(resolve, 1000*60*2))
-    if (!backfillFoundUsers) { break }
+  const doBackfillAccountingForRaceConditions = async (
+    usersByFieldChoice: ('createdAt' | 'updatedAt'),
+    usersAfterTs?: Date
+  ) => {
+    while (true) {
+      const thisBackfillStartTs = new Date()
+      const backfillFoundUsers = await doBackfill(
+        usersByFieldChoice,
+        usersAfterTs
+      )
+      console.log('backfillFoundUsers?', backfillFoundUsers)
+      // await new Promise(resolve => setTimeout(resolve, 1000*60*2)) // update user while sleeping
+      if (!backfillFoundUsers) { break }
+      usersAfterTs = thisBackfillStartTs
+    }
   }
-  console.log('finished')
+
+  await doBackfillAccountingForRaceConditions('createdAt')
+  await doBackfillAccountingForRaceConditions('updatedAt', backfillStartTs)
   await r.getPoolMaster().drain()
+  console.log('finished')
 }
