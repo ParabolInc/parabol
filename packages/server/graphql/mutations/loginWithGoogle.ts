0c29a41af69c983fb4dda3f916d5ec2321c5235e
Bruce Tian <tianrunhe@gmail.com>
2021-02-24 10:41:18 -0800

    Generate a new id for user who has signed in with Google and deleted their account before

--- packages/server/graphql/mutations/loginWithGoogle.ts
+++ packages/server/graphql/mutations/loginWithGoogle.ts
@@ -5,6 +5,7 @@ import AuthIdentityGoogle from '../../database/types/AuthIdentityGoogle'
 import AuthToken from '../../database/types/AuthToken'
 import User from '../../database/types/User'
 import db from '../../db'
+import generateUID from '../../generateUID'
 import encodeAuthToken from '../../utils/encodeAuthToken'
 import GoogleServerManager from '../../utils/GoogleServerManager'
 import standardError from '../../utils/standardError'
@@ -86,22 +87,28 @@ const loginWithGoogle = {
         }
       }
 
+      const userId = `google-oauth2|${sub}`
+      const deletedUser = await r
+        .table('User')
+        .get(userId)
+        .run()
+
       // it's a new user!
       const nickname = name || email.substring(0, email.indexOf('@'))
       const preferredName = nickname.length === 1 ? nickname.repeat(2) : nickname
-      const userId = `google-oauth2|${sub}`
       const identity = new AuthIdentityGoogle({
         id: sub,
         isEmailVerified: email_verified !== 'false'
       })
       const newUser = new User({
-        id: userId,
         preferredName,
         picture,
         email,
         identities: [identity],
         segmentId
       })
+      // if a deleted user with the same id exists, generate a new id
+      newUser.id = deletedUser && deletedUser.isRemoved ? `google-oauth2|${generateUID()}` : userId
       context.authToken = await bootstrapNewUser(newUser, !invitationToken)
       return {
         userId,
