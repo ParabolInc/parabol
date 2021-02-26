1e600b0f3f2d61c56be8a06cfc737e158ab475e1
Terry Acker <ackernaut@gmail.com>
2021-02-26 15:51:43 -0600

    palette v3 with used values

---
+++ packages/client/styles/paletteV3.ts
@@ -0,0 +1,57 @@
+export const enum PALETTE {
+  AQUA_400 = '#55C0CF',
+
+  FOREST_400 = '#73A153',
+
+  FUSCIA_400 = '#DB70DB',
+
+  GOLD_100 = '#FAEBD3',
+  GOLD_300 = '#FFCC63',
+  GOLD_500 = '#DE8E02',
+  GOLD_700 = '#855000',
+
+  GRADIENT_TOMATO_600_ROSE_500 = 'linear-gradient(to right, #F23B31 0, #ED4C86 100%)', // default: TOMATO_600 to ROSE_500
+  GRADIENT_TOMATO_700_ROSE_600 = 'linear-gradient(to right, #D5211A 0, #EB195F 100%)', // hover: TOMATO_700 to ROSE_600
+  GRADIENT_TOMATO_400_ROSE_300 = 'linear-gradient(to right, #FD7F77 0, #F099BF 100%)', // disabled: TOMATO_400 to ROSE_300
+
+  GRAPE_500 = '#A06BD6',
+  GRAPE_600 = '#7340B5',
+  GRAPE_700 = '#493272',
+  GRAPE_800 = '#2D1D53',
+
+  GRASS_300 = '#C4CF66',
+
+  JADE_400 = '#66BC8C',
+
+  LILAC_500 = '#7272E5',
+
+  ROSE_500 = '#ED4C86',
+
+  SKY_300 = '#8EC7F1',
+  SKY_400 = '#61B1EB',
+  SKY_500 = '#329AE5',
+  SKY_600 = '#14649E',
+
+  SLATE_100 = '#F8F7FC',
+  SLATE_200 = '#F1F0FA',
+  SLATE_300 = '#E0DDEC',
+  SLATE_400 = '#C3C0D8',
+  SLATE_500 = '#A7A3C2',
+  SLATE_600 = '#82809A',
+  SLATE_700 = '#444258',
+  SLATE_800 = '#2D2D39',
+  SLATE_900 = '#1C1C21',
+
+  SLATE_700_30 = '#4442584D', // SLATE_700 at 30% opacity
+  SLATE_700_80 = '#444258CD', // SLATE_700 at ~80% opacity
+  SLATE_900_32 = '#1C1C2152', // SLATE_900 at 32% opacity
+
+  TERRA_300 = '#FE975D',
+
+  TOMATO_100 = '#FFE2E0',
+  TOMATO_500 = '#FD6157',
+  TOMATO_600 = '#F23B31',
+  TOMATO_800 = '#9F201E',
+
+  WHITE = '#FFFFFF'
+}
