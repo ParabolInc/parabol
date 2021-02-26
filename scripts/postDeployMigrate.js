b3035561e2b869115e17eea1bd16af68476df7f4
Tiffany Han <tiffany@parabol.co>
2021-02-26 12:14:05 -0800

    add comment

--- scripts/postDeployMigrate.js
+++ scripts/postDeployMigrate.js
@@ -4,7 +4,7 @@ const path = require('path')
 const getProjectRoot = require('./webpack/utils/getProjectRoot')
 const PROJECT_ROOT = getProjectRoot()
 const pgMigrate = require('node-pg-migrate').default
-require('rethinkdb-ts-migrate')
+require('rethinkdb-ts-migrate') // hack to be able to use files for migration files
 
 const postDeployMigrate = async () => {
   const programmaticConfig = {
