4ca8405ed92bae454dacae6b7585eb8c9d77b3f1
Tiffany Han <hantiffany@gmail.com>
2021-03-04 17:07:21 -0800

    add update team by org id query

---
+++ packages/server/postgres/queries/src/updateTeamByOrgIdQuery.sql
@@ -0,0 +1,13 @@
+/*
+  @name updateTeamByOrgIdQuery
+*/
+UPDATE "Team" SET
+  "name" = COALESCE(:name, "name"),
+  "isArchived" = COALESCE(:isArchived, "isArchived"),
+  "isPaid" = COALESCE(:isPaid, "isPaid"),
+  "jiraDimensionFields" = COALESCE(:jiraDimensionFields, "jiraDimensionFields"),
+  "lastMeetingType" = COALESCE(:lastMeetingType, "lastMeetingType"),
+  "tier" = COALESCE(:tier, "tier"),
+  "orgId" = COALESCE(:orgId, "orgId"),
+  "updatedAt" = COALESCE(:updatedAt, "updatedAt")
+WHERE "orgId" = :orgId;
