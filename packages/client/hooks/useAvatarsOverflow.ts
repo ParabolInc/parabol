6c3f76b4e3179becdb20ffb008c14e97f03663d0
nickoferrall <nickoferrall@gmail.com>
2021-03-23 09:48:14 -0500

    update ref name

--- packages/client/hooks/useAvatarsOverflow.ts
+++ packages/client/hooks/useAvatarsOverflow.ts
@@ -2,18 +2,18 @@ import {RefObject, useLayoutEffect, useState} from 'react'
 import useResizeObserver from './useResizeObserver'
 import {dashAvatarWidth} from '../components/DashboardAvatars/DashboardAvatar'
 
-const useAvatarsOverflow = (rowRef: RefObject<HTMLDivElement>) => {
+const useAvatarsOverflow = (avatarsRef: RefObject<HTMLDivElement>) => {
   const [maxAvatars, setMaxAvatars] = useState(0)
   const margin = 16
   const checkOverflow = () => {
-    const {current: el} = rowRef
+    const {current: el} = avatarsRef
     if (!el) return
     const {clientWidth: totalWidth} = el
     const maxAvatars = Math.floor(totalWidth / (dashAvatarWidth + margin))
     setMaxAvatars(maxAvatars)
   }
   useLayoutEffect(checkOverflow, [])
-  useResizeObserver(checkOverflow, rowRef)
+  useResizeObserver(checkOverflow, avatarsRef)
   return maxAvatars
 }
 
