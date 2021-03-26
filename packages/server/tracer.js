2181f0036ddc1e908128c27b8f5599528d868113
Bruce Tian <tianrunhe@gmail.com>
2021-03-26 15:28:39 -0700

    Add hooks for GraphQL plugin

--- packages/server/tracer.js
+++ packages/server/tracer.js
@@ -1,5 +1,14 @@
 import tracer from 'dd-trace'
 tracer.init({
-  enabled: process.env.DD_TRACE_ENABLED === 'true'
+  enabled: process.env.DD_TRACE_ENABLED === 'true',
+  env: process.env.DD_ENV ?? 'test'
+})
+
+tracer.use('graphql', {
+  hooks: {
+    execute: (span, args) => {
+      span.setTag('viewerId', args?.contextValue?.authToken?.sub ?? "null")
+    }
+  }
 })
 export default tracer
