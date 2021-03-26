53632d73593d2553f02cdce949890b3f66b903d4
Bruce Tian <tianrunhe@gmail.com>
2021-03-25 17:04:02 -0700

    Remove the hook for GraphQL span

--- packages/server/tracer.js
+++ packages/server/tracer.js
@@ -1,12 +1,3 @@
 import tracer from 'dd-trace'
 tracer.init() // initialized in a different file to avoid hoisting.
-
-tracer.use('graphql', {
-  hooks: {
-    execute: (span, args) => {
-      span.setTag('viewerId', args?.contextValue?.authToken?.sub ?? "null")
-    }
-  }
-})
-
 export default tracer
