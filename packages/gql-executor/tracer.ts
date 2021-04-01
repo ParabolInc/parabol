2d8e870f87fa83ca86668d575f0edc0e5d2d0214
Bruce Tian <tianrunhe@gmail.com>
2021-03-31 21:52:32 -0700

    Remove duplicate dd tracer initialization parameters as all configuration is done via environment variables

--- packages/gql-executor/tracer.ts
+++ packages/gql-executor/tracer.ts
@@ -1,8 +1,5 @@
 import tracer from 'dd-trace'
-tracer.init({
-  enabled: process.env.DD_TRACE_ENABLED === 'true',
-  env: process.env.DD_ENV ?? 'test'
-})
+tracer.init()
 
 tracer.use('graphql', {
   hooks: {
