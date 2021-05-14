2b1bf0b9711b9d5dc3030533549c05db27994c0a
Bruce Tian <tianrunhe@gmail.com>
2021-05-14 15:15:37 +0800

    Update backfillSegmentId to be batch insertion queries into PG

--- packages/server/postgres/migrations/postDeploy/1620887961308_backfillSegmentId.ts
+++ packages/server/postgres/migrations/postDeploy/1620887961308_backfillSegmentId.ts
@@ -1,25 +1,50 @@
-import {ColumnDefinitions, MigrationBuilder} from 'node-pg-migrate'
+import {ColumnDefinitions} from 'node-pg-migrate'
 import getRethink from '../../../database/rethinkDriver'
-import updateUser from '../../queries/updateUser'
+import getPg from '../../getPg'
+import {backfillSegmentIdQuery} from '../../queries/generated/backfillSegmentIdQuery'
+import catchAndLog from '../../utils/catchAndLog'
 
 export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(): Promise<void> {
   const r = await getRethink()
+  const batchSize = 3000
   const doBackfill = async (usersAfterTs?: Date) => {
-    const rethinkUsers = await r
-      .table('User')
-      .between(usersAfterTs ?? r.minval, r.maxval, {index: 'updatedAt'})
-      .orderBy('updatedAt', {index: 'updatedAt'})
-      .filter((row) => row.hasFields('segmentId'))
-      .run()
+    let foundUsers = false
 
-    rethinkUsers.forEach(({id, segmentId}) => updateUser({segmentId}, id))
+    for (let i = 0; i < 1e5; i++) {
+      console.log('i:', i)
+      const offset = batchSize * i
+      const rethinkUsers = await r
+        .table('User')
+        .between(usersAfterTs ?? r.minval, r.maxval, {index: 'updatedAt'})
+        .orderBy('updatedAt', {index: 'updatedAt'})
+        .filter((row) => row.hasFields('segmentId'))
+        .pluck('id', 'segmentId')
+        .skip(offset)
+        .limit(batchSize)
+        .run()
+      if (!rethinkUsers.length) {
+        break
+      }
+      foundUsers = true
+      const pgUsers = rethinkUsers.map(({id, segmentId}) => ({id, segmentId}))
+      await catchAndLog(() => backfillSegmentIdQuery.run({users: pgUsers}, getPg()))
+    }
+    return foundUsers
   }
-  await doBackfill()
-  console.log('Finished backfilling segmentIds for users')
-}
 
-export async function down(pgm: MigrationBuilder): Promise<void> {
-  // noop
+  const doBackfillAccountingForRaceConditions = async (usersAfterTs?: Date) => {
+    for (let i = 0; i < 1e5; i++) {
+      const thisBackfillStartTs = new Date()
+      const backfillFoundUsers = await doBackfill(usersAfterTs)
+      if (!backfillFoundUsers) {
+        break
+      }
+      usersAfterTs = thisBackfillStartTs
+    }
+  }
+
+  await doBackfillAccountingForRaceConditions()
+  console.log('Finished backfilling segmentId.')
 }
