111900a733529adde20c73d7399301db28379608
Bruce Tian <tianrunhe@gmail.com>
2021-05-17 09:38:05 +0800

    Update setUserTierForUserIds method to also update PG

--- packages/server/utils/setUserTierForUserIds.ts
+++ packages/server/utils/setUserTierForUserIds.ts
@@ -1,5 +1,7 @@
 import getRethink from '../database/rethinkDriver'
 import db from '../db'
+import {TierEnum} from '../postgres/queries/generated/updateUserQuery'
+import updateUser from '../postgres/queries/updateUser'
 import segmentIo from './segmentIo'
 
 const setUserTierForUserIds = async (userIds: string[]) => {
@@ -35,6 +37,32 @@ const setUserTierForUserIds = async (userIds: string[]) => {
       {nonAtomic: true}
     )
     .run()
+
+  const userTiers = (await r
+    .table('OrganizationUser')
+    .getAll(r.args(userIds), {index: 'userId'})
+    .filter({removedAt: null})
+    .merge((orgUser) => ({
+      tier: r
+        .db('actionDevelopment')
+        .table('Organization')
+        .get(orgUser('orgId'))('tier')
+        .default('personal')
+    }))
+    .group('userId')('tier')
+    .ungroup()
+    .merge((row) => ({
+      tier: row('reduction')
+        .reduce((_left, right) => {
+          return r.branch(right.eq('enterprise'), 'enterprise', right.eq('pro'), 'pro', 'personal')
+        })
+        .default('personal')
+    }))
+    .run()) as {group: string; reduction: string[]; tier: TierEnum}[]
+  userTiers.forEach(({group: userId, tier}) => {
+    updateUser({tier}, userId)
+  })
+
   await Promise.all(userIds.map((userId) => db.clear('User', userId)))
   const users = await db.readMany('User', userIds)
   users.forEach((user) => {
