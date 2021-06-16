469fb0b1b3b55b5b6740d66bd6dee8bcd10df51d
nickoferrall <nickoferrall@gmail.com>
2021-06-16 15:01:20 -0500

    link to changelog

--- packages/client/hooks/useServiceWorkerUpdater.ts
+++ packages/client/hooks/useServiceWorkerUpdater.ts
@@ -21,7 +21,7 @@ const useServiceWorkerUpdater = () => {
         action: {
           label: `See what's changed`,
           callback: () => {
-            const url = `https://www.google.com/`
+            const url = 'https://github.com/ParabolInc/parabol/blob/master/CHANGELOG.md#parabol-change-log'
             window.open(url, '_blank', 'noopener')?.focus()
           }
         }
