78ce122f260c3d068136cf94aa1be764d1ca7cc2
Bruce Tian <tianrunhe@gmail.com>
2021-07-02 15:34:27 +0800

    Update the unique email constraint sql statement

--- packages/server/postgres/migrations/1625124096757_addUserEmailUniqueConstraint.ts
+++ packages/server/postgres/migrations/1625124096757_addUserEmailUniqueConstraint.ts
@@ -3,10 +3,12 @@ import {MigrationBuilder, ColumnDefinitions} from 'node-pg-migrate'
 export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.createIndex('User', ['email'], {
+    name: 'User_email_index',
+    unique: true
+  })
   pgm.sql(`
-     ALTER TABLE "User"
-       ADD CONSTRAINT "User_email_key"
-       UNIQUE ("email");
+      ALTER TABLE "User" ADD CONSTRAINT "User_email_key" UNIQUE USING INDEX "User_email_index";
    `)
 }
 
