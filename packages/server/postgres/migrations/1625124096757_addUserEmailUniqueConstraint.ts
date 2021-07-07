1190a579655ea956ebf9c158fd1b51a73c570cb8
Bruce Tian <tianrunhe@gmail.com>
2021-07-07 15:28:44 +0800

    make the User_email_index concurrent

--- packages/server/postgres/migrations/1625124096757_addUserEmailUniqueConstraint.ts
+++ packages/server/postgres/migrations/1625124096757_addUserEmailUniqueConstraint.ts
@@ -5,7 +5,8 @@ export const shorthands: ColumnDefinitions | undefined = undefined
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.createIndex('User', ['email'], {
     name: 'User_email_index',
-    unique: true
+    unique: true,
+    concurrently: true
   })
   pgm.sql(`
       ALTER TABLE "User" ADD CONSTRAINT "User_email_key" UNIQUE USING INDEX "User_email_index";
