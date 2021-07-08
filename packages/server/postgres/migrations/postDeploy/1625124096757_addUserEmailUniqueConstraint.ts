ce8f11d9eb71b55a710b674e16fe03a1ccea975c
Bruce Tian <tianrunhe@gmail.com>
2021-07-08 16:32:31 +0800

    Update to use pgm.createIndex & pgm.createConstraint

--- packages/server/postgres/migrations/postDeploy/1625124096757_addUserEmailUniqueConstraint.ts
+++ packages/server/postgres/migrations/postDeploy/1625124096757_addUserEmailUniqueConstraint.ts
@@ -3,19 +3,19 @@ import {MigrationBuilder, ColumnDefinitions} from 'node-pg-migrate'
 export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.createIndex('User', ['email'], {
+  pgm.createIndex('User', 'email', {
     name: 'User_email_index',
-    unique: true,
-    concurrently: true
+    unique: true
   })
-  pgm.sql(`
-      ALTER TABLE "User" ADD CONSTRAINT "User_email_key" UNIQUE USING INDEX "User_email_index";
-   `)
+  pgm.createConstraint('User', 'User_email_key', 'UNIQUE USING INDEX "User_email_index"')
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.sql(`
-     ALTER TABLE "User"
-       DROP CONSTRAINT "User_email_key";
-   `)
+  pgm.dropConstraint('User', 'User_email_key', {
+    ifExists: true
+  })
+  pgm.dropIndex('User', 'email', {
+    name: 'User_email_index',
+    ifExists: true
+  })
 }
