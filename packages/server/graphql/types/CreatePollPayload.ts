6325a5758e636b8ef8181d09775c0671dd48926e
Bartosz Jarocki <jarocki.bartek@gmail.com>
2021-09-14 16:42:25 +0200

    feat: added pollId to create poll payload

--- packages/server/graphql/types/CreatePollPayload.ts
+++ packages/server/graphql/types/CreatePollPayload.ts
@@ -1,11 +1,17 @@
-import {GraphQLNonNull, GraphQLObjectType} from 'graphql'
+import {GraphQLID, GraphQLNonNull, GraphQLObjectType} from 'graphql'
 import {GQLContext} from '../graphql'
 import makeMutationPayload from './makeMutationPayload'
 import Poll from './../types/Poll'
+import PollId from 'parabol-client/shared/gqlIds/PollId'
 
 export const CreatePollSuccess = new GraphQLObjectType<any, GQLContext>({
   name: 'CreatePollSuccess',
   fields: () => ({
+    pollId: {
+      type: GraphQLNonNull(GraphQLID),
+      description: 'Poll id in a format of `poll:idGeneratedByDatabase`',
+      resolve: ({pollId}) => PollId.join(pollId)
+    },
     poll: {
       type: GraphQLNonNull(Poll),
       description: 'the poll just created',
