4d099ab936d4ec2ce8416d36dde5a9fc64ab5c9b
Nick O'Ferrall <nickoferrall@gmail.com>
2021-09-15 12:48:53 +0100

    fix mailgun type error complaint

--- packages/server/email/MailManagerMailgun.ts
+++ packages/server/email/MailManagerMailgun.ts
@@ -4,17 +4,17 @@ import sendToSentry from '../utils/sendToSentry'
 import MailManager, {MailManagerOptions} from './MailManager'
 
 export default class MailManagerMailgun extends MailManager {
-  mailgun = new Mailgun(FormData);
+  mailgun = new Mailgun(FormData)
   mailgunClient = this.mailgun.client({
     username: 'api',
     key: process.env.MAILGUN_API_KEY || ''
-  });
+  })
   async sendEmail(options: MailManagerOptions) {
     const {subject, body, to, attachments, html, tags} = options
     const toArr = Array.isArray(to) ? to : [to]
     const toStr = toArr.join(',')
     try {
-      await this.mailgunClient.messages.create(process.env.MAILGUN_DOMAIN, {
+      await this.mailgunClient.messages.create(process.env.MAILGUN_DOMAIN!, {
         to: toStr,
         from: process.env.MAIL_FROM,
         subject,
