b497610383ba7fba8324b49738424957cd3a81d9
Bartosz Jarocki <jarocki.bartek@gmail.com>
2021-09-24 07:09:24 +0200

    feat: added poll option title counter

--- packages/client/components/Poll/PollOption.tsx
+++ packages/client/components/Poll/PollOption.tsx
@@ -2,14 +2,19 @@ import React, {Ref} from 'react'
 import styled from '@emotion/styled'
 import {usePollContext} from './PollContext'
 import {PALETTE} from '~/styles/paletteV3'
+import PollOptionInput from './PollOptionInput'
 
 interface Props {
   id: string
   title?: string
-  placeholder: string | null
+  placeholder?: string
 }
 
 const PollOptionRoot = styled('div')({
+  width: '100%'
+})
+
+const PollOptionTitle = styled('div')({
   width: '100%',
   height: '36px',
   fontSize: '14px',
@@ -20,48 +25,19 @@ const PollOptionRoot = styled('div')({
   alignItems: 'center'
 })
 
-const PollOptionInput = styled('input')({
-  padding: `10px 12px`,
-  fontSize: '14px',
-  color: PALETTE.SLATE_900,
-  borderRadius: '7px',
-  border: `1.5px solid ${PALETTE.SLATE_400}`,
-  ':hover, :focus, :active': {
-    outline: `none`,
-    border: `1.5px solid ${PALETTE.SKY_500}`
-  }
-})
-
-const PollOption = React.forwardRef((props: Props, ref: Ref<HTMLDivElement | HTMLInputElement>) => {
+const PollOption = React.forwardRef((props: Props, ref: Ref<HTMLDivElement>) => {
   const {id, title, placeholder} = props
-  const {onPollOptionSelected, pollState, updatePollOption} = usePollContext()
+  const {onPollOptionSelected, pollState} = usePollContext()
 
-  if (pollState === 'creating') {
-    const handlePollOptionUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
-      updatePollOption(id, event.target.value)
+  const renderPollOption = () => {
+    if (pollState === 'creating') {
+      return <PollOptionInput id={id} placeholder={placeholder} value={title} />
     }
 
-    return (
-      <PollOptionInput
-        key={id}
-        placeholder={placeholder ?? ''}
-        value={title}
-        onChange={handlePollOptionUpdate}
-      />
-    )
+    return <PollOptionTitle onClick={() => onPollOptionSelected(id)}>{title}</PollOptionTitle>
   }
 
-  return (
-    <PollOptionRoot
-      ref={ref}
-      key={id}
-      onClick={() => {
-        onPollOptionSelected(id)
-      }}
-    >
-      {title}
-    </PollOptionRoot>
-  )
+  return <PollOptionRoot ref={ref}>{renderPollOption()}</PollOptionRoot>
 })
 
 export default PollOption
---
+++ packages/client/components/Poll/PollOptionInput.tsx
@@ -0,0 +1,79 @@
+import React, {Ref} from 'react'
+import styled from '@emotion/styled'
+import {usePollContext} from './PollContext'
+import {PALETTE} from '../../styles/paletteV3'
+import {Polls} from '../../types/constEnums'
+
+interface Props {
+  id: string
+  value?: string
+  placeholder?: string
+}
+
+const PollOptionInputRoot = styled('div')({
+  position: 'relative',
+  width: '100%',
+  height: '36px',
+  display: 'flex',
+  alignItems: 'center'
+})
+
+const Input = styled('input')({
+  width: '100%',
+  padding: `10px 12px`,
+  fontSize: '14px',
+  color: PALETTE.SLATE_900,
+  borderRadius: '7px',
+  border: `1.5px solid ${PALETTE.SLATE_400}`,
+  ':hover, :focus, :active': {
+    outline: `none`,
+    border: `1.5px solid ${PALETTE.SKY_500}`
+  }
+})
+
+const Counter = styled('div')<{
+  isVisible: boolean
+  isMax: boolean
+}>(({isVisible, isMax}) => ({
+  display: isVisible ? 'block' : 'none',
+  position: 'absolute',
+  top: '0',
+  right: '0',
+  margin: '4px',
+  fontSize: '10px',
+  color: isMax ? PALETTE.TOMATO_500 : PALETTE.SLATE_600
+}))
+
+const PollOptionInput = React.forwardRef((props: Props, ref: Ref<HTMLDivElement>) => {
+  const {id, value, placeholder} = props
+  const {updatePollOption} = usePollContext()
+  const [isCounterVisible, setIsCounterVisible] = React.useState(false)
+  const handlePollOptionUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updatePollOption(id, event.target.value)
+  }
+  const inputValue = value ?? ''
+  const hasReachedMaxValue = inputValue.length >= Polls.MAX_OPTION_LENGTH
+
+  return (
+    <PollOptionInputRoot ref={ref}>
+      <Input
+        key={id}
+        placeholder={placeholder ?? ''}
+        value={value}
+        onChange={handlePollOptionUpdate}
+        maxLength={Polls.MAX_OPTION_LENGTH}
+        onFocus={() => {
+          setIsCounterVisible(true)
+        }}
+        onBlur={() => {
+          setIsCounterVisible(false)
+        }}
+      />
+      <Counter isVisible={isCounterVisible} isMax={hasReachedMaxValue}>
+        {inputValue.length}/{Polls.MAX_OPTION_LENGTH}
+      </Counter>
+    </PollOptionInputRoot>
+  )
+})
+
+export default PollOptionInput
