6c9319a7f2ecf637641f23711850833afb8e6f63
Georg Bremer <github@dschoordsch.de>
2021-09-28 20:47:56 +0200

    Add server tests (#5330)
    * Add server tests
    
    Add some tests running graphql queries against the dev server. These
    don't prime the database and thus are only meant to be run manually
    locally at the moment.
    Mutations updateWatchlist and deleteUser now clear the user from
    dataloader to return correct result on next query.

---
+++ packages/server/__tests__/common.ts
@@ -0,0 +1,138 @@
+require('../../../scripts/webpack/utils/dotenv')
+import fetch from 'node-fetch'
+import faker from 'faker'
+import ServerAuthToken from '../database/types/ServerAuthToken'
+import encodeAuthToken from '../utils/encodeAuthToken'
+
+const HOST = process.env.GRAPHQL_HOST || 'localhost:3000'
+const PROTOCOL = process.env.GRAPHQL_PROTOCOL || 'http'
+
+export async function sendIntranet(req: {
+  query: string
+  variables?: Record<string, any>
+  isPrivate?: boolean
+}) {
+  const authToken = encodeAuthToken(new ServerAuthToken())
+
+  const response = await fetch(`${PROTOCOL}://${HOST}/intranet-graphql`, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      'x-application-authorization': `Bearer ${authToken}`
+    },
+    body: JSON.stringify({
+      isPrivate: req.isPrivate,
+      query: req.query,
+      variables: req.variables
+    })
+  })
+  return response.json()
+}
+
+export async function sendPublic(req: {
+  query: string
+  variables?: Record<string, any>
+  authToken?: string
+}) {
+  const authToken = req.authToken ?? ''
+
+  const response = await fetch(`${PROTOCOL}://${HOST}/graphql`, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      'x-application-authorization': `Bearer ${authToken}`
+    },
+    body: JSON.stringify({
+      type: 'start',
+      payload: {
+        query: req.query,
+        variables: req.variables
+      }
+    })
+  })
+  const body = await response.json()
+  return body.payload
+}
+
+const SIGNUP_WITH_PASSWORD_MUTATION = `
+  mutation SignUpWithPasswordMutation(
+    $email: ID!
+    $password: String!
+    $invitationToken: ID
+    $segmentId: ID
+  ) {
+    signUpWithPassword(email: $email, password: $password, invitationToken: $invitationToken, segmentId: $segmentId) {
+      error {
+        message
+      }
+      authToken
+      user {
+        tms
+        id
+        email
+        picture
+        preferredName
+        createdAt
+      }
+    }
+    acceptTeamInvitation(invitationToken: $invitationToken) {
+      authToken
+      error {
+        message
+      }
+      meetingId
+      team {
+        id
+        activeMeetings {
+          __typename
+          id
+        }
+      }
+    }
+  }
+`
+
+export const signUpWithEmail = async (emailInput: string) => {
+  //FIXME #1402 email addresses are case sensitive
+  const email = emailInput.toLowerCase()
+
+  const password = faker.internet.password()
+  const signUp = await sendIntranet({
+    query: SIGNUP_WITH_PASSWORD_MUTATION,
+    variables: {
+      email,
+      password,
+      invitationToken: null,
+      segmentId: null
+    }
+  })
+
+  expect(signUp).toMatchObject({
+    data: {
+      signUpWithPassword: {
+        error: null,
+        authToken: expect.anything(),
+        user: {
+          id: expect.anything(),
+          email
+        }
+      }
+    }
+  })
+  const userId = signUp.data.signUpWithPassword.user.id
+  const authToken = signUp.data.signUpWithPassword.authToken
+
+  return {
+    userId,
+    email,
+    password,
+    authToken
+  }
+}
+
+export const signUp = async () => {
+  const email = faker.internet.email()
+  return signUpWithEmail(email)
+}
---
+++ packages/server/__tests__/updateEmail.test.ts
@@ -0,0 +1,72 @@
+import {sendIntranet, signUp} from './common'
+import faker from 'faker'
+
+test('Update user email with unknown email fails', async () => {
+  const email = faker.internet.email().toLowerCase()
+  const newEmail = faker.internet.email().toLowerCase()
+
+  const updateEmail = await sendIntranet({
+    query: `
+      mutation UpdateEmail($oldEmail: Email!, $newEmail: Email!) {
+        updateEmail(oldEmail: $oldEmail, newEmail: $newEmail)
+      }
+    `,
+    variables: {
+      oldEmail: email,
+      newEmail
+    },
+    isPrivate: true
+  })
+
+  expect(updateEmail).toMatchObject({
+    data: null
+  })
+})
+
+test('Update user email', async () => {
+  const {email, userId} = await signUp()
+  const newEmail = faker.internet.email().toLowerCase()
+
+  const updateEmail = await sendIntranet({
+    query: `
+      mutation UpdateEmail($oldEmail: Email!, $newEmail: Email!) {
+        updateEmail(oldEmail: $oldEmail, newEmail: $newEmail)
+      }
+    `,
+    variables: {
+      oldEmail: email,
+      newEmail
+    },
+    isPrivate: true
+  })
+
+  expect(updateEmail).toMatchObject({
+    data: {
+      updateEmail: true
+    }
+  })
+
+  const user = await sendIntranet({
+    query: `
+      query User($userId: ID!) {
+        user(userId: $userId) {
+          id
+          email
+        }
+      }
+    `,
+    variables: {
+      userId
+    },
+    isPrivate: true
+  })
+
+  expect(user).toMatchObject({
+    data: {
+      user: {
+        id: userId,
+        email: newEmail
+      }
+    }
+  })
+})
