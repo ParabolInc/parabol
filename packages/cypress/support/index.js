39bbd87ee8d23275fc7c8c992c2bd71607a6fae4
Bartosz Jarocki <jarocki.bartek@gmail.com>
2021-09-29 08:10:13 +0200

    dx: removed cypress

--- packages/cypress/support/index.js
+++
@@ -1,9 +0,0 @@
-import './commands'
-import 'cypress-pipe'
-
-// Use for debugging
-// Cypress.on('window:before:load', (win) => {
-//   cy.spy(win.console, 'log')
-//   cy.spy(win.console, 'warn')
-//   cy.spy(win.console, 'error')
-// })
