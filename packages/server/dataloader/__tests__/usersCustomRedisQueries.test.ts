35acdf01c44ecf9e55ee965a27a9e658f4962bb8
Georg Bremer <github@dschoordsch.de>
2021-10-15 17:40:30 +0200

    Add test for correct order in users dataloader

---
+++ packages/server/dataloader/__tests__/usersCustomRedisQueries.test.ts
@@ -0,0 +1,34 @@
+import '../../../../scripts/webpack/utils/dotenv'
+import getDataLoader from '../../graphql/getDataLoader'
+import getPg from '../../postgres/getPg'
+import faker from 'faker'
+
+afterAll(async () => {
+  const pg = getPg()
+  const dataloader = getDataLoader()
+
+  await pg.end()
+  dataloader.dispose(true)
+  // TODO shutdown redis to properly end test
+})
+
+test('Result is mapped to correct id', async () => {
+  const pg = getPg()
+  const dataloader = getDataLoader()
+
+  const expectedUsers = faker.helpers.shuffle(
+    (await pg.query('SELECT "id", "email" FROM "User" LIMIT 100')).rows
+  )
+  const userIds = expectedUsers.map(({id}) => id)
+
+  const actualUsers = (await (dataloader.get('users') as any).loadMany(userIds)).map(
+    ({id, email}) => ({
+      id,
+      email
+    })
+  )
+
+  console.log(actualUsers.length)
+
+  expect(actualUsers).toEqual(expectedUsers)
+})
