00ee358b396d9428b787bdd16a96bb040de7ce0b
Nick O'Ferrall <nickoferrall@gmail.com>
2021-10-18 13:06:39 -0500

    clean-up useGetRefVal var names

--- packages/client/hooks/useGetRefVal.ts
+++ packages/client/hooks/useGetRefVal.ts
@@ -2,17 +2,17 @@ import {RefObject, useLayoutEffect, useState} from 'react'
 import useResizeObserver from './useResizeObserver'
 
 const useGetRefVal = <T extends keyof HTMLDivElement>(ref: RefObject<HTMLDivElement>, key: T) => {
-  const [height, setHeight] = useState<null | HTMLDivElement[T]>(null)
+  const [val, setVal] = useState<null | HTMLDivElement[T]>(null)
   const getVal = () => {
     const {current: el} = ref
-    const height = el?.[key]
-    if (!height) return
-    setHeight(height)
+    const val = el?.[key]
+    if (!val) return
+    setVal(val)
   }
 
   useLayoutEffect(getVal, [ref])
   useResizeObserver(getVal, ref)
-  return height
+  return val
 }
 
 export default useGetRefVal
