a1d788f87a341a492bd112aa1b5bde32ae360640
Bartosz Jarocki <jarocki.bartek@gmail.com>
2021-11-09 16:02:05 +0100

    fix: filtering result of loadMany with errorFilter

--- packages/server/graphql/intranetSchema/mutations/updateOAuthRefreshTokens.ts
+++ packages/server/graphql/intranetSchema/mutations/updateOAuthRefreshTokens.ts
@@ -3,6 +3,7 @@ import {requireSU} from '../../../utils/authorization'
 import {GQLContext} from '../../graphql'
 import getAtlassianAuthsToUpdate from '../../../postgres/queries/getAtlassianAuthsToUpdate'
 import {Threshold} from '~/types/constEnums'
+import errorFilter from '../../errorFilter'
 
 const updateOAuthRefreshTokens = {
   type: GraphQLInt,
@@ -15,9 +16,9 @@ const updateOAuthRefreshTokens = {
     // RESOLUTION
     const olderThan14DaysThreshold = new Date(Date.now() - Threshold.OAUTH_REFRESH)
     const atlassianAuthsToUpdate = await getAtlassianAuthsToUpdate(olderThan14DaysThreshold)
-    const updatedAtlassianAuths = await dataLoader
-      .get('freshAtlassianAuth')
-      .loadMany(atlassianAuthsToUpdate)
+    const updatedAtlassianAuths = (
+      await dataLoader.get('freshAtlassianAuth').loadMany(atlassianAuthsToUpdate)
+    ).filter(errorFilter)
 
     return updatedAtlassianAuths.length
   }
