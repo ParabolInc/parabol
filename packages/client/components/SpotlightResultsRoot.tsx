7d48930376e7fabd68263bebf20886cb1da89055
Nick O'Ferrall <nickoferrall@gmail.com>
2021-11-11 18:34:00 +0000

    move prevent suspense logic to useQueryLoaderNow

--- packages/client/components/SpotlightResultsRoot.tsx
+++ packages/client/components/SpotlightResultsRoot.tsx
@@ -1,9 +1,9 @@
-import React, {RefObject, useCallback, useEffect, useRef} from 'react'
+import React, {RefObject, useRef} from 'react'
 import spotlightGroupsQuery, {
   SpotlightGroupsQuery
 } from '../__generated__/SpotlightGroupsQuery.graphql'
 import SpotlightGroups from './SpotlightGroups'
-import {useQueryLoader, fetchQuery, useRelayEnvironment, PreloadFetchPolicy} from 'react-relay'
+import useQueryLoaderNow from '~/hooks/useQueryLoaderNow'
 
 interface Props {
   spotlightGroupId?: string
@@ -19,29 +19,17 @@ const SpotlightResultsRoot = (props: Props) => {
   if (nextGroupId) {
     groupIdRef.current = nextGroupId
   }
-  const environment = useRelayEnvironment()
-  const [queryRef, loadQuery] = useQueryLoader<SpotlightGroupsQuery>(spotlightGroupsQuery)
-  const refresh = useCallback(() => {
-    const variables = {
-      reflectionGroupId: groupIdRef.current,
-      searchQuery: spotlightSearchQuery,
-      meetingId
-    }
-    // fetchQuery will fetch the query and write the data to the Relay store.
-    // This will ensure that when we re-render, the data is already cached
-    // and we don't suspend, which would cause the results to flash
-    fetchQuery(environment, spotlightGroupsQuery, variables).subscribe({
-      complete: () => {
-        // *After* the query has been fetched, we call loadQuery again to
-        // re-render with a new queryRef. At this point the data for the
-        // query should be cached, so we use the 'store-only' fetchPolicy
-        // to avoid suspending.
-        loadQuery(variables, {fetchPolicy: 'store-only' as PreloadFetchPolicy})
-      }
-    })
-  }, [groupIdRef.current, spotlightSearchQuery, meetingId])
-
-  useEffect(() => refresh(), [spotlightSearchQuery, groupIdRef.current])
+  const variables = {
+    reflectionGroupId: groupIdRef.current,
+    searchQuery: spotlightSearchQuery,
+    meetingId
+  }
+  const queryRef = useQueryLoaderNow<SpotlightGroupsQuery>(
+    spotlightGroupsQuery,
+    variables,
+    undefined,
+    true
+  )
 
   if (!queryRef) return null
   return <SpotlightGroups phaseRef={phaseRef} queryRef={queryRef} />
