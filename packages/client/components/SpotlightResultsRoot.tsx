530bc47f94915c55a92897ab9143cd67b6c32ba2
Nick O'Ferrall <nickoferrall@gmail.com>
2021-11-11 18:16:01 +0000

    avoid suspending spotlight results

--- packages/client/components/SpotlightResultsRoot.tsx
+++ packages/client/components/SpotlightResultsRoot.tsx
@@ -1,9 +1,9 @@
-import React, {RefObject, Suspense, useRef} from 'react'
+import React, {RefObject, useCallback, useEffect, useRef} from 'react'
 import spotlightGroupsQuery, {
   SpotlightGroupsQuery
 } from '../__generated__/SpotlightGroupsQuery.graphql'
-import useQueryLoaderNow from '../hooks/useQueryLoaderNow'
 import SpotlightGroups from './SpotlightGroups'
+import {useQueryLoader, fetchQuery, useRelayEnvironment, PreloadFetchPolicy} from 'react-relay'
 
 interface Props {
   spotlightGroupId?: string
@@ -19,20 +19,31 @@ const SpotlightResultsRoot = (props: Props) => {
   if (nextGroupId) {
     groupIdRef.current = nextGroupId
   }
-  const queryRef = useQueryLoaderNow<SpotlightGroupsQuery>(
-    spotlightGroupsQuery,
-    {
+  const environment = useRelayEnvironment()
+  const [queryRef, loadQuery] = useQueryLoader<SpotlightGroupsQuery>(spotlightGroupsQuery)
+  const refresh = useCallback(() => {
+    const variables = {
       reflectionGroupId: groupIdRef.current,
       searchQuery: spotlightSearchQuery,
       meetingId
-    },
-    // Results could be grouped or ungrouped since modal was last opened with identical variables
-    'network-only'
-  )
-  return (
-    <Suspense fallback={''}>
-      {queryRef && <SpotlightGroups phaseRef={phaseRef} queryRef={queryRef} />}
-    </Suspense>
-  )
+    }
+    // fetchQuery will fetch the query and write the data to the Relay store.
+    // This will ensure that when we re-render, the data is already cached
+    // and we don't suspend, which would cause the results to flash
+    fetchQuery(environment, spotlightGroupsQuery, variables).subscribe({
+      complete: () => {
+        // *After* the query has been fetched, we call loadQuery again to
+        // re-render with a new queryRef. At this point the data for the
+        // query should be cached, so we use the 'store-only' fetchPolicy
+        // to avoid suspending.
+        loadQuery(variables, {fetchPolicy: 'store-only' as PreloadFetchPolicy})
+      }
+    })
+  }, [groupIdRef.current, spotlightSearchQuery, meetingId])
+
+  useEffect(() => refresh(), [spotlightSearchQuery, groupIdRef.current])
+
+  if (!queryRef) return null
+  return <SpotlightGroups phaseRef={phaseRef} queryRef={queryRef} />
 }
 export default SpotlightResultsRoot
