f9c00c48cb8600bff2f8e3040a77580f006be45a
Matt Krick <matt.krick@gmail.com>
2021-11-12 13:16:49 -0800

    Use 2 GraphQL Executors (#5560)
    * use 2 gql executors
    
    Signed-off-by: Matt Krick <matt.krick@gmail.com>
    
    * use pubsub to connect directly with a gql instance
    
    Signed-off-by: Matt Krick <matt.krick@gmail.com>
    
    * test understanding of backpressure

--- scripts/gqlServers.js
+++ scripts/gqlServers.js
@@ -1,4 +1,7 @@
 const webpack = require('webpack')
+const cp = require('child_process')
+const {promisify} = require('util')
+const path = require('path')
 
 const gqlServers = async () => {
   await new Promise((resolve) => {
@@ -11,7 +14,20 @@ const gqlServers = async () => {
     })
   })
   require('../dev/web.js')
-  require('../dev/gqlExecutor.js')
+
+  // test a cluster in development
+  const executorPath = path.join(__dirname, '../dev/gqlExecutor.js')
+  const fork = promisify(cp.fork)
+  const {SERVER_ID, DEV_NUM_EXECUTORS} = process.env
+  // allow 1 - 8 executors to run in dev
+  const numExecutors = Math.min(Math.max(Number(DEV_NUM_EXECUTORS) || 1, 1), 8)
+  for (let i = 0; i < numExecutors; i++) {
+    const forkServerId = String(Number(SERVER_ID) + i + 1)
+    const env = {...process.env, SERVER_ID: forkServerId}
+    // fork all gql executors so they run in their own process
+    // so publishers (web.js) can't block consumers (gql executors)
+    fork(executorPath, {env})
+  }
 }
 
 gqlServers()
