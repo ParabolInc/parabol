cb3b599444b0f91bb0faab8febba7c4cbcf73bbc
Bartosz Jarocki <jarocki.bartek@gmail.com>
2021-11-12 07:59:52 +0100

    fix: return AtlassianAuth[] from getAtlassianAuthsToUpdate query

--- packages/server/postgres/queries/getAtlassianAuthsToUpdate.ts
+++ packages/server/postgres/queries/getAtlassianAuthsToUpdate.ts
@@ -1,8 +1,19 @@
 import getPg from '../getPg'
 import {getAtlassianAuthsToUpdateQuery} from './generated/getAtlassianAuthsToUpdateQuery'
+import {AtlassianAuth} from './getAtlassianAuthByUserIdTeamId'
 
 const getAtlassianAuthsToUpdate = async (updatedAtThreshold: Date) => {
-  return getAtlassianAuthsToUpdateQuery.run({updatedAtThreshold}, getPg())
+  const results = await getAtlassianAuthsToUpdateQuery.run({updatedAtThreshold}, getPg())
+  return results.map(
+    (atlassianAuth) =>
+      ({
+        ...atlassianAuth,
+        jiraSearchQueries: atlassianAuth.jiraSearchQueries.map((jsq: any) => ({
+          ...jsq,
+          lastUsedAt: new Date(jsq.lastUsedAt)
+        }))
+      } as AtlassianAuth)
+  )
 }
 
 export default getAtlassianAuthsToUpdate
