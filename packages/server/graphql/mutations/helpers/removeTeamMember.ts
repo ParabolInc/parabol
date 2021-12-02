ea8e29a20567b393449543d51c60f964987d92cd
Tiffany Han <tiffany@parabol.co>
2021-12-02 13:49:07 -0800

    add hardDeleteUser mutation (#5475)
    * add hardDeleteUser mutation
    
    * update to use new fns after merge
    
    * fix trying to find removed stage bug
    
    * reassign meeting created by user when deleted
    
    * log deleted user out
    
    * remove pinning minipass
    
    * moved some logic into softDeleteUser
    
    * set tombstone id for debugging, delete dangling 1-person meetings
    
    * eagerly fetch user data to update hubspot
    
    * remove unnecessary sleep
    
    * added comments
    
    * - fix reason arg destructuring
    - inline call to executeGraphQL
    - explicit types for eagerly passed parabolPayload
    - move deleting tasks belonging to 1-person teams to `removeTeamMember`
    - `softDeleteUser` returns user deleted email
    
    * cleanup
    
    * remove call to db.write
    
    Co-authored-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/mutations/helpers/removeTeamMember.ts
+++ packages/server/graphql/mutations/helpers/removeTeamMember.ts
@@ -4,6 +4,7 @@ import CheckInStage from '../../../database/types/CheckInStage'
 import NotificationKickedOut from '../../../database/types/NotificationKickedOut'
 import Task from '../../../database/types/Task'
 import UpdatesStage from '../../../database/types/UpdatesStage'
+import EstimateStage from '../../../database/types/EstimateStage'
 import removeUserTms from '../../../postgres/queries/removeUserTms'
 import updateTeamByTeamId from '../../../postgres/queries/updateTeamByTeamId'
 import archiveTasksForDB from '../../../safeMutations/archiveTasksForDB'
@@ -11,6 +12,7 @@ import {DataLoaderWorker} from '../../graphql'
 import removeSlackAuths from './removeSlackAuths'
 import removeStagesFromMeetings from './removeStagesFromMeetings'
 import removeUserFromMeetingStages from './removeUserFromMeetingStages'
+import AgendaItemsStage from '../../../database/types/AgendaItemsStage'
 
 interface Options {
   evictorUserId?: string
@@ -26,7 +28,10 @@ const removeTeamMember = async (
   const now = new Date()
   const {userId, teamId} = fromTeamMemberId(teamMemberId)
   // see if they were a leader, make a new guy leader so later we can reassign tasks
-  const activeTeamMembers = await r.table('TeamMember').getAll(teamId, {index: 'teamId'}).run()
+  const activeTeamMembers = await r
+    .table('TeamMember')
+    .getAll(teamId, {index: 'teamId'})
+    .run()
   const teamMember = activeTeamMembers.find((t) => t.id === teamMemberId)
   const {isLead, isNotRemoved} = teamMember ?? {}
   // if the guy being removed is the leader & not the last, pick a new one. else, use him
@@ -36,48 +41,77 @@ const removeTeamMember = async (
   }
 
   if (activeTeamMembers.length === 1) {
-    // archive single-person teams
     const updates = {
       isArchived: true,
       updatedAt: new Date()
     }
     await Promise.all([
-      r.table('Team').get(teamId).update(updates).run(),
-      updateTeamByTeamId(updates, teamId)
+      // archive single-person teams
+      r
+        .table('Team')
+        .get(teamId)
+        .update(updates)
+        .run(),
+      updateTeamByTeamId(updates, teamId),
+      // delete all tasks belonging to a 1-person team
+      r
+        .table('Task')
+        .getAll(teamId, {index: 'teamId'})
+        .delete()
     ])
   } else if (isLead) {
     // assign new leader, remove old leader flag
     await r({
-      newTeamLead: r.table('TeamMember').get(teamLeader.id).update({
-        isLead: true
-      }),
-      oldTeamLead: r.table('TeamMember').get(teamMemberId).update({isLead: false})
+      newTeamLead: r
+        .table('TeamMember')
+        .get(teamLeader.id)
+        .update({
+          isLead: true
+        }),
+      oldTeamLead: r
+        .table('TeamMember')
+        .get(teamMemberId)
+        .update({isLead: false})
     }).run()
   }
 
   // assign active tasks to the team lead
   const {integratedTasksToArchive, reassignedTasks} = await r({
-    teamMember: r.table('TeamMember').get(teamMemberId).update({
-      isNotRemoved: false,
-      updatedAt: now
-    }),
-    integratedTasksToArchive: r
+    teamMember: r
+      .table('TeamMember')
+      .get(teamMemberId)
+      .update({
+        isNotRemoved: false,
+        updatedAt: now
+      }),
+    integratedTasksToArchive: (r
       .table('Task')
       .getAll(userId, {index: 'userId'})
       .filter({teamId})
       .filter((task) => {
         return r.and(
-          task('tags').contains('archived').not(),
-          task('integrations').default(null).ne(null)
+          task('tags')
+            .contains('archived')
+            .not(),
+          task('integrations')
+            .default(null)
+            .ne(null)
         )
       })
-      .coerceTo('array') as unknown as Task[],
-    reassignedTasks: r
+      .coerceTo('array') as unknown) as Task[],
+    reassignedTasks: (r
       .table('Task')
       .getAll(userId, {index: 'userId'})
       .filter({teamId})
       .filter((task) =>
-        r.and(task('tags').contains('archived').not(), task('integrations').default(null).eq(null))
+        r.and(
+          task('tags')
+            .contains('archived')
+            .not(),
+          task('integrations')
+            .default(null)
+            .eq(null)
+        )
       )
       .update(
         {
@@ -85,7 +119,7 @@ const removeTeamMember = async (
         },
         {returnChanges: true}
       )('changes')('new_val')
-      .default([]) as unknown as Task[]
+      .default([]) as unknown) as Task[]
   }).run()
 
   await removeUserTms(teamId, userId)
@@ -95,16 +129,26 @@ const removeTeamMember = async (
   if (evictorUserId) {
     const notification = new NotificationKickedOut({teamId, userId, evictorUserId})
     notificationId = notification.id
-    await r.table('Notification').insert(notification).run()
+    await r
+      .table('Notification')
+      .insert(notification)
+      .run()
   }
 
   const archivedTasks = await archiveTasksForDB(integratedTasksToArchive)
   const archivedTaskIds = archivedTasks.map(({id}) => id)
+  const agendaItemIds = await r
+    .table('AgendaItem')
+    .getAll(teamId, {index: 'teamId'})
+    .filter((row) => row('teamMemberId').eq(teamMemberId))
+    .getField('id')
+    .run()
 
   // if a new meeting was currently running, remove them from it
-  const filterFn = (stage: CheckInStage | UpdatesStage) => {
-    return stage.teamMemberId === teamMemberId
-  }
+  const filterFn = (stage: CheckInStage | UpdatesStage | EstimateStage | AgendaItemsStage) =>
+    (stage as CheckInStage | UpdatesStage).teamMemberId === teamMemberId ||
+    (stage as EstimateStage).creatorUserId === userId ||
+    agendaItemIds.includes((stage as AgendaItemsStage).agendaItemId)
   removeSlackAuths(userId, teamId)
   await removeStagesFromMeetings(filterFn, teamId, dataLoader)
   await removeUserFromMeetingStages(userId, teamId, dataLoader)
