279ed4b3f0a307286ec39f0e0a978c743828dcbb
Igor <alan-desu@yandex.ru>
2021-12-02 16:49:04 +0400

    Remove thunk

--- packages/client/components/TaskEditor/decorators.ts
+++ packages/client/components/TaskEditor/decorators.ts
@@ -1,5 +1,5 @@
+import {CompositeDecorator, EditorState, ContentBlock} from 'draft-js'
 import EditorLink from './EditorLink'
-import {CompositeDecorator, EditorState} from 'draft-js'
 import Hashtag from './Hashtag'
 import Mention from './Mention'
 import SearchHighlight from './SearchHighlight'
@@ -11,26 +11,6 @@ const findEntity = (entityType) => (contentBlock, callback, contentState) => {
     const entityKey = character.getEntity()
     return entityKey !== null && contentState.getEntity(entityKey).getType() === entityType
   }, callback)
-}
-
-const findText = (searchQuery) => (contentBlock, callback) => {
-  if (!searchQuery) {
-    return
-  }
-  const textLower = contentBlock.getText().toLowerCase()
-  const searchQueryLower = searchQuery.toLowerCase()
-
-  let start = 0
-  let found = true
-  while (found) {
-    const index = textLower.indexOf(searchQueryLower, start)
-    if (index === -1) {
-      found = false
-    } else {
-      start = index + 1
-      callback(index, index + searchQueryLower.length)
-    }
-  }
 }
 
 const decorators = (
@@ -58,8 +38,31 @@ const decorators = (
   ]
 
   if (searchQuery) {
+    const findMatchingText = (
+      contentBlock: ContentBlock,
+      callback: (startIdx: number, endIdx: number) => void
+    ) => {
+      if (!searchQuery) {
+        return
+      }
+      const textLower = contentBlock.getText().toLowerCase()
+      const searchQueryLower = searchQuery.toLowerCase()
+
+      let start = 0
+      let found = true
+      while (found) {
+        const index = textLower.indexOf(searchQueryLower, start)
+        if (index === -1) {
+          found = false
+        } else {
+          start = index + 1
+          callback(index, index + searchQueryLower.length)
+        }
+      }
+    }
+
     compositeDecorator.push({
-      strategy: findText(searchQuery),
+      strategy: findMatchingText,
       component: SearchHighlight
     })
   }
