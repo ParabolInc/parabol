dba28c726cfbce3ef9ead22a73f748b63a7c5f22
Bartosz Jarocki <jarocki.bartek@gmail.com>
2021-12-20 07:31:20 +0100

    feat: aligned makeGlobalIntegrationProvidersFromEnv to use new data structure

--- packages/server/utils/makeGlobalIntegrationProvidersFromEnv.ts
+++ packages/server/utils/makeGlobalIntegrationProvidersFromEnv.ts
@@ -1,17 +1,23 @@
-const makeGlobalIntegrationProvidersFromEnv = (): any[] => {
-  const integrationProviders: any[] = []
-  if (process.env.GITLAB_CLIENT_ID && process.env.GITLAB_CLIENT_SECRET) {
-    integrationProviders.push({
-      type: 'gitlab',
-      tokenType: 'oauth2',
-      scope: 'global',
-      isActive: true,
-      name: 'GitLab.com',
-      serverBaseUri: 'https://gitlab.com',
-      oauthScopes: ['read_api'],
-      oauthClientId: process.env.GITLAB_CLIENT_ID,
-      oauthClientSecret: process.env.GITLAB_CLIENT_SECRET
-    })
+import {IUpsertGlobalIntegrationProviderQueryParams} from '../postgres/queries/generated/upsertGlobalIntegrationProviderQuery'
+
+const hasGitLabConfig = () => !!process.env.GITLAB_CLIENT_ID && !!process.env.GITLAB_CLIENT_SECRET
+const createGitLabIntegrationProviderUpsertParams =
+  (): IUpsertGlobalIntegrationProviderQueryParams => ({
+    type: 'gitlab',
+    name: 'GitLab.com',
+    providerMetadata: {
+      serverBaseUrl: 'https://gitlab.com',
+      scopes: ['read_api'],
+      clientId: process.env.GITLAB_CLIENT_ID!,
+      clientSecret: process.env.GITLAB_CLIENT_SECRET!
+    }
+  })
+
+const makeGlobalIntegrationProvidersFromEnv = () => {
+  const integrationProviders: IUpsertGlobalIntegrationProviderQueryParams[] = []
+
+  if (hasGitLabConfig()) {
+    integrationProviders.push(createGitLabIntegrationProviderUpsertParams())
   }
 
   return integrationProviders
