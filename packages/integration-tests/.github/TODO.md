d63a8ed91f71b4de84b08acf35859d80d400b809
Ben Limmer <ben@benlimmer.com>
2022-01-25 09:56:07 -0700

    feat(integration-tests): add playwright tests

---
+++ packages/integration-tests/.github/TODO.md
@@ -0,0 +1,3 @@
+# TODO
+
+the generator created this github workflow - consider translating to GH actions or circleci.
---
+++ packages/integration-tests/tests/config.ts
@@ -0,0 +1,21 @@
+import {Page} from '@playwright/test'
+
+type TestingMode = 'production' | 'local'
+
+export class Config {
+  readonly testingMode: TestingMode
+  readonly rootUrlPath: string
+
+  constructor(env = process.env) {
+    this.testingMode = env.INTEGRATION_TESTING_MODE === 'production' ? 'production' : 'local'
+    this.rootUrlPath =
+      this.testingMode === 'production' ? 'https://action.parabol.co' : 'http://localhost:3000'
+  }
+
+  public async goto(page: Page, path: string) {
+    return page.goto(`${this.rootUrlPath}/${path}`)
+  }
+}
+
+const config = new Config()
+export default config
