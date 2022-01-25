b9dc7e9bea46846403703e0e4dc820213733b2c2
Ben Limmer <ben@benlimmer.com>
2022-01-25 12:41:26 -0700

    add more sample tests

---
+++ packages/integration-tests/README.md
@@ -0,0 +1,5 @@
+# Integration Tests
+
+## TODO
+
+- [ ] Add eslint, especially for [no-floating-promises](https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/no-floating-promises.md) rule
--- packages/integration-tests/tests/retrospective-demo/smoke.test.ts
+++ packages/integration-tests/tests/retrospective-demo/smoke.test.ts
@@ -6,4 +6,13 @@ test.describe('retrospective-demo / smoke test', () => {
     await config.goto(page, '/retrospective-demo')
     await expect(page.locator('button', {hasText: 'Start Demo'})).toBeVisible()
   })
+
+  test('displays the reflect page first', async ({page}) => {
+    await config.goto(page, '/retrospective-demo')
+    await page.click('text=Start Demo')
+    await expect(page.locator('h1:has-text("Reflect")')).toBeVisible()
+    await expect(page.locator('[data-cy=reflection-column-Start]')).toBeVisible()
+    await expect(page.locator('[data-cy=reflection-column-Stop]')).toBeVisible()
+    await expect(page.locator('[data-cy=reflection-column-Continue]')).toBeVisible()
+  })
 })
