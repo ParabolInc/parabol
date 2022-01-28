a6d98512cdab4e7648b5030b9b37fcf423de17b2
Matt Krick <matt.krick@gmail.com>
2022-01-27 18:53:22 -0800

    fix retro template scrolling

--- packages/client/hooks/useScrollIntoVIew.ts
+++ packages/client/hooks/useScrollIntoVIew.ts
@@ -1,11 +1,23 @@
 import {RefObject, useEffect} from 'react'
 import {requestIdleCallback} from '../utils/requestIdleCallback'
+import useInitialRender from './useInitialRender'
 
-const useScrollIntoView = (ref: RefObject<HTMLElement>, isNeeded: boolean) => {
+const useScrollIntoView = (
+  ref: RefObject<HTMLElement>,
+  isNeeded: boolean,
+  onlyIfNotFullyVisible?: boolean
+) => {
+  const isInit = useInitialRender()
   useEffect(() => {
     if (isNeeded) {
       requestIdleCallback(() => {
-        ref.current && ref.current.scrollIntoView({behavior: 'smooth'})
+        if (!ref.current) return
+        if (onlyIfNotFullyVisible) {
+          // Be sure to put scroll-behavior: smooth on the overflowing div!
+          ref.current.scrollIntoViewIfNeeded()
+        } else {
+          ref.current.scrollIntoView({behavior: isInit ? undefined : 'smooth'})
+        }
       })
     }
   }, [isNeeded, ref])
