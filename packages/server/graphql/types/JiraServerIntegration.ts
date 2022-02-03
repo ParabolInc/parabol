c63c3849b19c163a09bcbf8b2eb6696a0f662157
Georg Bremer <github@dschoordsch.de>
2022-02-03 17:17:39 +0100

    Move source type to GraphQLObjectType instead of all resolvers

--- packages/server/graphql/types/JiraServerIntegration.ts
+++ packages/server/graphql/types/JiraServerIntegration.ts
@@ -4,7 +4,7 @@ import {GQLContext} from '../graphql'
 import IntegrationProviderOAuth1 from './IntegrationProviderOAuth1'
 import TeamMemberIntegrationAuthOAuth1 from './TeamMemberIntegrationAuthOAuth1'
 
-const JiraServerIntegration = new GraphQLObjectType<any, GQLContext>({
+const JiraServerIntegration = new GraphQLObjectType<{teamId: string; userId: string}, GQLContext>({
   name: 'JiraServerIntegration',
   description: 'Jira Server integration data for a given team member',
   fields: () => ({
@@ -20,9 +20,11 @@ const JiraServerIntegration = new GraphQLObjectType<any, GQLContext>({
     sharedProviders: {
       description: 'The non-global providers shared with the team or organization',
       type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(IntegrationProviderOAuth1))),
-      resolve: async ({teamId, userId}: {teamId: string, userId: string}, _args: unknown, {dataLoader}: GQLContext) => {
+      resolve: async ({teamId, userId}, _args, {dataLoader}) => {
         const teamMembers = await dataLoader.get('teamMembersByUserId').load(userId)
-        const teamMember = teamMembers.find((teamMember: TeamMember) => teamMember.teamId === teamId)
+        const teamMember = teamMembers.find(
+          (teamMember: TeamMember) => teamMember.teamId === teamId
+        )
         if (!teamMember) return []
 
         const team = await dataLoader.get('teams').loadNonNull(teamMember.teamId)
