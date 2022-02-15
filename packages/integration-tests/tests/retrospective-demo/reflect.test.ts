238bc140aec0b5d7f36d2b800c00808d76d9a6f7
Ben Limmer <ben@benlimmer.com>
2022-02-15 14:07:13 -0700

    add more reflect tests

--- packages/integration-tests/tests/retrospective-demo/reflect.test.ts
+++ packages/integration-tests/tests/retrospective-demo/reflect.test.ts
@@ -67,8 +67,9 @@ test.describe('restrospective-demo / reflect page', () => {
   })
 
   test('displays simulated users writing reflections in the start column', async ({page}) => {
-    await config.goto(page, '/retrospective-demo')
+    test.setTimeout(30_000)
 
+    await config.goto(page, '/retrospective-demo')
     await page.click('text=Start Demo')
 
     await expect(
@@ -100,6 +101,101 @@ test.describe('restrospective-demo / reflect page', () => {
     ).toBeVisible()
   })
 
-  // test('displays simulated users writing reflections in the stop column')
-  // test('displays simulated users writing reflections in the continue column')
+  test('displays simulated users writing reflections in the stop column', async ({page}) => {
+    test.setTimeout(60_000)
+
+    await config.goto(page, '/retrospective-demo')
+    await page.click('text=Start Demo')
+
+    await expect(
+      page.locator('[data-cy=reflection-column-Stop] :text("1 team member writing reflections...")')
+    ).toBeVisible({
+      timeout: 20_000 // first, the simulated users are only typing in the "Start" column, so this takes > 5 seconds
+    })
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Stop] :text("1 team member reflection + 1 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Stop] :text("1 team member reflection + 2 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Stop] :text("2 team member reflections + 2 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Stop] :text("3 team member reflections + 2 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Stop] :text("4 team member reflections + 2 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Stop] :text("5 team member reflections + 2 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Stop] :text("5 team member reflections + 1 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator('[data-cy=reflection-column-Stop] :text("5 team member reflections")')
+    ).toBeVisible()
+  })
+
+  test('displays simulated users writing reflections in the continue column', async ({page}) => {
+    test.setTimeout(80_000)
+
+    await config.goto(page, '/retrospective-demo')
+    await page.click('text=Start Demo')
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Continue] :text("1 team member writing reflections...")'
+      )
+    ).toBeVisible({
+      timeout: 40_000 // first, the simulated users are only typing in the "Start"/"Stop" columns, so this takes > 5 seconds
+    })
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Continue] :text("2 team members writing reflections...")'
+      )
+    ).toBeVisible({
+      timeout: 20_000 // this seems to be delayed from the server
+    })
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Continue] :text("1 team member reflection + 2 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator(
+        '[data-cy=reflection-column-Continue] :text("1 team member reflection + 1 in progress")'
+      )
+    ).toBeVisible()
+
+    await expect(
+      page.locator('[data-cy=reflection-column-Continue] :text("1 team member reflection")')
+    ).toBeVisible()
+  })
 })
