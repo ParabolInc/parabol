6162a53ee600cf90f0a93e7122bde2132479adb4
Ben Limmer <ben@benlimmer.com>
2022-02-15 17:11:20 -0700

    allow choosing a rate limiter

--- packages/server/graphql/getRateLimiter.ts
+++ packages/server/graphql/getRateLimiter.ts
@@ -1,11 +1,35 @@
-import RateLimiter from './RateLimiter'
+import {InMemoryRateLimiter, StubRateLimiter, RateLimiter} from '../utils/rateLimiters'
+
+const RATE_LIMITERS = Object.freeze({
+  'in-memory': InMemoryRateLimiter,
+  stub: StubRateLimiter
+})
+type RateLimiterType = keyof typeof RATE_LIMITERS
 
 let rateLimiter: RateLimiter
 const getRateLimiter = () => {
   if (!rateLimiter) {
-    rateLimiter = new RateLimiter()
+    rateLimiter = new RATE_LIMITERS[getRateLimiterFromEnv() || 'in-memory']()
   }
   return rateLimiter
+}
+
+function getRateLimiterFromEnv(): RateLimiterType | undefined {
+  const rateLimiterEnvVar = process.env.RATE_LIMITER
+  if (!rateLimiterEnvVar) {
+    return
+  }
+
+  const possibleRateLimiters = Object.keys(RATE_LIMITERS)
+  if (possibleRateLimiters.includes(rateLimiterEnvVar)) {
+    return rateLimiterEnvVar as RateLimiterType
+  } else {
+    throw new Error(
+      `Specified RateLimiter ${rateLimiterEnvVar} was not understood! Options are ${possibleRateLimiters.join(
+        ', '
+      )}`
+    )
+  }
 }
 
 export default getRateLimiter
--- packages/server/graphql/graphql.ts
+++ packages/server/graphql/graphql.ts
@@ -2,7 +2,7 @@ import {GraphQLFieldConfig} from 'graphql'
 import AuthToken from '../database/types/AuthToken'
 import RootDataLoader from '../dataloader/RootDataLoader'
 import {CacheWorker} from './DataLoaderCache'
-import RateLimiter from './RateLimiter'
+import {RateLimiter} from '../utils/rateLimiters'
 
 // Avoid needless parsing & validating for the 300 hottest operations
 export type DataLoaderWorker = CacheWorker<RootDataLoader>
