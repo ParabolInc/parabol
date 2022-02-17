87fb78983bde488262b2c2af229f2a5492fb338d
Bruce Tian <tianrunhe@gmail.com>
2022-02-17 14:55:27 -0800

    Fix linting problems

--- packages/client/utils/AtlassianManager.ts
+++ packages/client/utils/AtlassianManager.ts
@@ -600,35 +600,46 @@ export default abstract class AtlassianManager {
     }
   }
 
-  private readonly buildIssueKeyJQL = (queryString: string | null, filteredProjectKeys: string[]) => {
-    if (!queryString) return ""
+  private readonly buildIssueKeyJQL = (
+    queryString: string | null,
+    filteredProjectKeys: string[]
+  ) => {
+    if (!queryString) return ''
     const maybeIssueKeys = queryString.split(/[\s,]+/)
-    if (maybeIssueKeys.length === 0) return ""
+    if (maybeIssueKeys.length === 0) return ''
 
-    const validIssueKeys = maybeIssueKeys.map((rawIssueKey) => {
-      const maybeIssueKey = rawIssueKey.toUpperCase()
-      const match = maybeIssueKey.match(/(?<projectKey>[A-Za-z][A-Za-z_0-9]+)*-*(?<issueNumber>\d+)/)
-      if (!match || !match.groups) return ""
+    const validIssueKeys = maybeIssueKeys
+      .map((rawIssueKey) => {
+        const maybeIssueKey = rawIssueKey.toUpperCase()
+        const match = maybeIssueKey.match(
+          /(?<projectKey>[A-Za-z][A-Za-z_0-9]+)*-*(?<issueNumber>\d+)/
+        )
+        if (!match || !match.groups) return ''
 
-      const {projectKey: maybeProjectKey, issueNumber: maybeIssueNumber} = match.groups
-      if (maybeProjectKey && !maybeIssueNumber)
-        return ""
-      else if (maybeProjectKey && maybeIssueNumber) {
-        if (filteredProjectKeys.length == 0 || (filteredProjectKeys.length != 0 && filteredProjectKeys.includes(maybeProjectKey))) {
-          return `${maybeProjectKey}-${maybeIssueNumber}`
+        const {projectKey: maybeProjectKey, issueNumber: maybeIssueNumber} = match.groups
+        if (maybeProjectKey && !maybeIssueNumber) return ''
+        else if (maybeProjectKey && maybeIssueNumber) {
+          if (
+            filteredProjectKeys.length === 0 ||
+            (filteredProjectKeys.length !== 0 && filteredProjectKeys.includes(maybeProjectKey))
+          ) {
+            return `${maybeProjectKey}-${maybeIssueNumber}`
+          } else {
+            return ''
+          }
+        } else if (!maybeProjectKey && maybeIssueNumber) {
+          if (filteredProjectKeys.length === 0) {
+            return ''
+          } else {
+            return filteredProjectKeys.map((projectKey) => `${projectKey}-${maybeIssueNumber}`)
+          }
         } else {
-          return ""
+          return ''
         }
-      } else if (!maybeProjectKey && maybeIssueNumber) {
-        if (filteredProjectKeys.length == 0) {
-          return ""
-        } else {
-          return filteredProjectKeys.map((projectKey) => `${projectKey}-${maybeIssueNumber}`)
-        }
-      } else {
-        return ""
-      }
-    }).flat().filter(String).map((issueKey) => `\"${issueKey}\"`)
+      })
+      .flat()
+      .filter(String)
+      .map((issueKey) => `\"${issueKey}\"`)
     return validIssueKeys.length > 0 ? ` OR issueKey in (${validIssueKeys.join(', ')})` : ''
   }
 
