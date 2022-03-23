2a34532f99c257c1d495970f557f7eeb05b95d1d
Bruce Tian <tianrunhe@gmail.com>
2022-03-23 14:50:16 +0100

    Remove unused event handlers for PromptResponseEditor

--- packages/client/components/promptResponse/PromptResponseEditor.tsx
+++ packages/client/components/promptResponse/PromptResponseEditor.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import Placeholder from '@tiptap/extension-placeholder'
-import {EditorContent, EditorEvents, useEditor} from '@tiptap/react'
+import {EditorContent, EditorEvents, useEditor, JSONContent} from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
 interface Props {
   autoFocus?: boolean
-  initialDoc: string
+  initialDoc: JSONContent
   handleSubmit: (value: string) => void
   readOnly: boolean
   placeholder: string
@@ -27,54 +27,29 @@ const PromptResponseEditor = (props: Props) => {
   }
 
   const onUpdate = ({editor}: EditorEvents['update']) => {
-    const nextDoc = JSON.stringify(editor.getJSON())
+    const nextDoc = editor.getJSON()
     setEditing(true)
     setDoc(nextDoc)
   }
 
-  const onFocus = ({editor}: EditorEvents['focus']) => {
-    if (editor.getText().toLowerCase().startsWith(placeholder.toLowerCase())) {
-      editor.commands.selectAll()
-    }
-  }
-
   const onSubmit = async ({editor}: EditorEvents['blur']) => {
-    const nextDoc = JSON.stringify(editor.getJSON())
+    const nextDoc = editor.getJSON()
     setEditing(false)
     setDoc(nextDoc)
-    console.log(nextDoc)
-    handleSubmit(nextDoc)
+    handleSubmit(JSON.stringify(nextDoc))
   }
 
-  const reset = () => {
-    setEditing(false)
-    setDoc(initialDoc)
-    editor?.commands.blur()
-  }
-
-  const onKeyDown = (_view: any, event: KeyboardEvent) => {
-    if (event.key === 'Escape') {
-      setTimeout(reset)
-      return true
-    }
-    return false
-  }
-
-  const showPlaceholder = !doc && placeholder
+  const showPlaceholder = !doc.text && !!placeholder
   const editor = useEditor({
-    content: JSON.parse(doc),
+    content: doc,
     extensions: [
       StarterKit,
       Placeholder.configure({
         placeholder: showPlaceholder ? placeholder : ''
       })
     ],
-    editorProps: {
-      handleKeyDown: onKeyDown,
-    },
     autofocus: autoFocus,
     onUpdate,
-    onFocus,
     onBlur: onSubmit,
     editable: !readOnly,
   })
