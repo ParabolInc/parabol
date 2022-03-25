e896600fbd261d1fbc947835e51b505ba13e2e71
Matt Krick <matt.krick@gmail.com>
2022-03-25 14:17:33 -0700

    use raw-loader for typeDefs

--- packages/server/graphql/private/rootSchema.ts
+++ packages/server/graphql/private/rootSchema.ts
@@ -1,15 +1,13 @@
-import {loadFilesSync} from '@graphql-tools/load-files'
 import {makeExecutableSchema, mergeSchemas} from '@graphql-tools/schema'
-import path from 'path'
 import resolveTypesForMutationPayloads from '../resolveTypesForMutationPayloads'
 import publicSchema from '../rootSchema'
 import resolvers from './resolvers'
 
-const typeDefs = loadFilesSync(
-  path.join(__PROJECT_ROOT__, 'packages/server/graphql/private/typeDefs/*.graphql')
-)
+const importAllStrings = (context: __WebpackModuleApi.RequireContext) => {
+  return context.keys().map((id) => context(id).default)
+}
 
-console.log(typeDefs)
+const typeDefs = importAllStrings(require.context('./typeDefs', false, /.graphql$/))
 const privateSchema = resolveTypesForMutationPayloads(makeExecutableSchema({typeDefs, resolvers}))
 const fullSchema = mergeSchemas({schemas: [privateSchema, publicSchema]})
 export default fullSchema
