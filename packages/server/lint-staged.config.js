f235d5fa11b01870a7c4509bc82cece803eb04e8
Matt Krick <matt.krick@gmail.com>
2022-03-30 09:49:18 -0700

    fix lint-staged and vscode workspace settings

--- packages/server/lint-staged.config.js
+++ packages/server/lint-staged.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  '*.{js,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*': () => 'tsc --noEmit -p tsconfig.json'
+  '*.{ts,tsx}': ['eslint --fix', 'prettier --config ../../.prettierrc --ignore-path ./.eslintignore --write'],
+  '*.graphql': ['prettier --config ../../.prettierrc --ignore-path ./.eslintignore --write'],
+  '**/*.{ts,tsx}': () => 'tsc --noEmit -p tsconfig.json'
 }
