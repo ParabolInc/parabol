d0d66bdec7406d9fbdf61d9cf945a87e80290d7e
JimmyLv <jimmy.jinglv@gmail.com>
2022-03-30 12:22:54 +0800

    refactor: update QueryRenderer to usePreloadedQuery for  PokerTemplateListOrg #5297

--- packages/client/modules/meeting/components/PokerTemplateListOrgRoot.tsx
+++ packages/client/modules/meeting/components/PokerTemplateListOrgRoot.tsx
@@ -1,18 +1,10 @@
-import graphql from 'babel-plugin-relay/macro'
-import React from 'react'
-import {QueryRenderer} from 'react-relay'
-import useAtmosphere from '../../../hooks/useAtmosphere'
-import renderQuery from '../../../utils/relay/renderQuery'
+import React, {Suspense} from 'react'
 import MockTemplateList from './MockTemplateList'
 import PokerTemplateListOrg from './PokerTemplateListOrg'
-
-const query = graphql`
-  query PokerTemplateListOrgRootQuery($teamId: ID!) {
-    viewer {
-      ...PokerTemplateListOrg_viewer
-    }
-  }
-`
+import useQueryLoaderNow from '../../../hooks/useQueryLoaderNow'
+import pokerTemplateListOrgQuery, {
+  PokerTemplateListOrgQuery
+} from '../../../__generated__/PokerTemplateListOrgQuery.graphql'
 
 interface Props {
   isActive: boolean
@@ -21,16 +13,14 @@ interface Props {
 
 const PokerTemplateListOrgRoot = (props: Props) => {
   const {isActive, teamId} = props
-  const atmosphere = useAtmosphere()
+  const queryRef = useQueryLoaderNow<PokerTemplateListOrgQuery>(pokerTemplateListOrgQuery, {teamId})
+
   if (!isActive) return null
+
   return (
-    <QueryRenderer
-      environment={atmosphere}
-      query={query}
-      variables={{teamId}}
-      fetchPolicy={'store-or-network' as any}
-      render={renderQuery(PokerTemplateListOrg, {Loader: <MockTemplateList />})}
-    />
+    <Suspense fallback={<MockTemplateList />}>
+      {queryRef && <PokerTemplateListOrg queryRef={queryRef} />}
+    </Suspense>
   )
 }
 
