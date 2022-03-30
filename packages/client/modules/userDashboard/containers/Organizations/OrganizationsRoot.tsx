6fed357dbcf6ad9278084d00feb74e3301e514b0
JimmyLv <jimmy.jinglv@gmail.com>
2022-03-30 13:56:34 +0800

    refactor: remove renderQuery and add loader for OrganizationsRoot Suspense fallback #5297

--- packages/client/modules/userDashboard/containers/Organizations/OrganizationsRoot.tsx
+++ packages/client/modules/userDashboard/containers/Organizations/OrganizationsRoot.tsx
@@ -5,10 +5,16 @@ import useQueryLoaderNow from '../../../../hooks/useQueryLoaderNow'
 import organizationsQuery, {
   OrganizationsQuery
 } from '../../../../__generated__/OrganizationsQuery.graphql'
+import {renderLoader} from '../../../../utils/relay/renderFallback'
+import {LoaderSize} from '../../../../types/constEnums'
 
 const OrganizationsRoot = () => {
   const queryRef = useQueryLoaderNow<OrganizationsQuery>(organizationsQuery)
-  return <Suspense fallback={''}>{queryRef && <Organizations queryRef={queryRef} />}</Suspense>
+  return (
+    <Suspense fallback={renderLoader({size: LoaderSize.PANEL})}>
+      {queryRef && <Organizations queryRef={queryRef} />}
+    </Suspense>
+  )
 }
 
 export default withAtmosphere(OrganizationsRoot)
---
+++ packages/client/utils/relay/renderFallback.tsx
@@ -0,0 +1,38 @@
+import React, {ComponentType, ReactNode} from 'react'
+import ErrorComponent from '../../components/ErrorComponent/ErrorComponent'
+import LoadingComponent from '../../components/LoadingComponent/LoadingComponent'
+import * as Sentry from '@sentry/browser'
+
+interface LoaderOptions {
+  Loader?: ReactNode
+  props?: {[key: string]: any}
+  size?: number
+  loadingDelay?: number
+  menuLoadingWidth?: number
+}
+
+export const renderLoader = (options: LoaderOptions = {}) => {
+  console.log('========options========', options)
+  if (options.Loader) {
+    return options.Loader
+  }
+
+  const {menuLoadingWidth, loadingDelay, size} = options
+  return (
+    <LoadingComponent
+      delay={loadingDelay}
+      spinnerSize={size || 24}
+      height={menuLoadingWidth ? 24 : undefined}
+      width={menuLoadingWidth}
+      showAfter={menuLoadingWidth ? 0 : undefined}
+    />
+  )
+}
+
+type ErrorOptions = {Error?: ComponentType<{error: any}>}
+
+export const renderError = (error: Error, options: ErrorOptions = {}) => {
+  const Error = options.Error || ErrorComponent
+  const eventId = Sentry.captureException(error)
+  return <Error error={error} eventId={eventId} />
+}
