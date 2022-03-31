fa767a6f719cec03a966d41cc45d1a077a1857fb
Matt Krick <matt.krick@gmail.com>
2022-03-31 10:47:40 -0700

    feat(sdl): Add SDL to public schema (#6263)
    * feat(ping): try new typed graphql pattern
    
    * feat(ping) add internal context
    
    * fix(ping) add missing paren
    
    * feat(sdl): require.context whole directories
    
    * feat(sdl): move updateEmail to new pattern as example
    
    * feat(sdl): move logins to sdl to explore mappers
    
    * feat(sdl): rename source
    
    * feat(sdl): move typeDefs to sdl
    
    * feat(sdl) move update schema logic
    
    * feat(sdl) fix include patterns
    
    * add resolverTypes to gitignore
    
    * ignore resolverTypes
    
    * feat(ping): make ping more robust
    
    * feat(sdl): add extractResolver codemod
    
    * codegen all types in private schema
    
    * move non-operations from intranetSchema to private
    
    * first pass at query codemod
    
    * first pass at queries
    
    * handle resolve methods
    
    * it works on queries!
    
    * it works on mutations
    
    * remove intranetSchema
    
    * fixup draftEnterpriseInvoice
    
    * reset lint-staged
    
    * fix mapper types
    
    * add graphql-shield to private schema
    
    * first pass at sdl public schema
    
    * cherrypick connect fix
    
    * add ratelimit perm
    
    * test moving a mutation to sdl
    
    * update readme
    
    * generate schema first, then resolverTypes
    
    * fix prod.js build
    
    * use proper types for github extensions
    
    * default scalars to string
    
    * add demo query getDemoEntitites
    
    * comment out subscriptions for now
    
    * add more comments

--- packages/server/graphql/private/queries/company.ts
+++ packages/server/graphql/private/queries/company.ts
@@ -1,4 +1,3 @@
-import {requireSU} from '../../../utils/authorization'
 import getDomainFromEmail from '../../../utils/getDomainFromEmail'
 import isCompanyDomain from '../../../utils/isCompanyDomain'
 import {QueryResolvers} from '../resolverTypes'
@@ -7,12 +6,7 @@ export type CompanySource = {
   id: string
 }
 
-const company: QueryResolvers['company'] = async (
-  _source,
-  {domain, userId},
-  {authToken, dataLoader}
-) => {
-  requireSU(authToken)
+const company: QueryResolvers['company'] = async (_source, {domain, userId}, {dataLoader}) => {
   if (domain) return {id: domain}
   const user = userId && (await dataLoader.get('users').load(userId))
   if (!user) throw new Error('User not found')
