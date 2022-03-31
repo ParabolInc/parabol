8d37f4878eb2a93e9ad9bb38528f2964c9e9f6ad
Bob Jacobs <bob.jacobs@outlook.com>
2022-03-31 11:46:38 -0400

    resolving root mutation issue with startTeamPrompt mutation

--- packages/server/graphql/rootMutation.ts
+++ packages/server/graphql/rootMutation.ts
@@ -113,6 +113,7 @@ import startCheckIn from './mutations/startCheckIn'
 import startDraggingReflection from './mutations/startDraggingReflection'
 import startRetrospective from './mutations/startRetrospective'
 import startSprintPoker from './mutations/startSprintPoker'
+import startTeamPrompt from './mutations/startTeamPrompt'
 import toggleTeamDrawer from './mutations/toggleTeamDrawer'
 import updateAgendaItem from './mutations/updateAgendaItem'
 import updateCommentContent from './mutations/updateCommentContent'
@@ -245,6 +246,7 @@ export default new GraphQLObjectType<any, GQLContext>({
       startRetrospective,
       startSprintPoker,
       setTaskHighlight,
+      startTeamPrompt,
       updateAgendaItem,
       updateCommentContent,
       updateCreditCard,
