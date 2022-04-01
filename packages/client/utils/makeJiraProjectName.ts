a11f57c31e07a45bb070b4d3b84569b47d462f5b
Matt Krick <matt.krick@gmail.com>
2022-04-01 11:50:04 -0700

    remove stories, include tsconfig.eslint dirs

--- packages/client/utils/makeJiraProjectName.ts
+++
@@ -1,9 +0,0 @@
-import {AccessibleResource} from './AtlassianManager'
-
-const makeJiraProjectName = (projectName: string, sites: AccessibleResource[], cloudId: string) => {
-  const site = sites.find((site) => site.id === cloudId)
-  if (sites.length === 1 || !site) return projectName
-  return `${site.name}/${projectName}`
-}
-
-export default makeJiraProjectName
