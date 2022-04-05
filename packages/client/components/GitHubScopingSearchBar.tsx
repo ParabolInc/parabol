a13645806c05cb020d41e6b67738c6d5a19244ae
Nick O'Ferrall <nickoferrall@gmail.com>
2022-04-05 15:40:12 +0100

    implement new scope search bar ui in github

--- packages/client/components/GitHubScopingSearchBar.tsx
+++ packages/client/components/GitHubScopingSearchBar.tsx
@@ -2,16 +2,26 @@ import styled from '@emotion/styled'
 import graphql from 'babel-plugin-relay/macro'
 import React from 'react'
 import {useFragment} from 'react-relay'
+import {PALETTE} from '~/styles/paletteV3'
 import {GitHubScopingSearchBar_meeting$key} from '../__generated__/GitHubScopingSearchBar_meeting.graphql'
 import GitHubScopingSearchFilterToggle from './GitHubScopingSearchFilterToggle'
 import GitHubScopingSearchHistoryToggle from './GitHubScopingSearchHistoryToggle'
 import GitHubScopingSearchInput from './GitHubScopingSearchInput'
 
 const SearchBar = styled('div')({
-  alignItems: 'center',
-  display: 'flex',
   padding: 16
 })
+
+const SearchBarWrapper = styled('div')({
+  alignItems: 'center',
+  border: `1px solid ${PALETTE.SLATE_400}`,
+  borderRadius: '40px',
+  display: 'flex',
+  height: 44,
+  padding: '0 16px',
+  width: '100%'
+})
+
 interface Props {
   meetingRef: GitHubScopingSearchBar_meeting$key
 }
@@ -32,9 +42,11 @@ const GitHubScopingSearchBar = (props: Props) => {
 
   return (
     <SearchBar>
-      <GitHubScopingSearchHistoryToggle meetingRef={meeting} />
-      <GitHubScopingSearchInput meetingRef={meeting} />
-      <GitHubScopingSearchFilterToggle meetingRef={meeting} />
+      <SearchBarWrapper>
+        <GitHubScopingSearchHistoryToggle meetingRef={meeting} />
+        <GitHubScopingSearchInput meetingRef={meeting} />
+        <GitHubScopingSearchFilterToggle meetingRef={meeting} />
+      </SearchBarWrapper>
     </SearchBar>
   )
 }
