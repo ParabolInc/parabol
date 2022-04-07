05ec36cd13f43e38421dc0d16771d31872b26480
James Taber <jmtaber129@gmail.com>
2022-04-06 21:01:06 -0300

    feat(standups): Top Bar Component Stub (#6307)
    * feat(standups): Top Bar Component Stub
    
    * CR: different approach
    
    * CR: Fix header font 20->16px, make isMeetingSidebarCollapsed optional

--- packages/client/components/TeamPromptMeeting.tsx
+++ packages/client/components/TeamPromptMeeting.tsx
@@ -1,15 +1,13 @@
-import styled from '@emotion/styled'
-import React from 'react'
+import React, {Suspense} from 'react'
 import {Link} from 'react-router-dom'
 
-const Root = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: '100%'
-})
-
 import logoMarkPurple from '../styles/theme/images/brand/mark-color.svg'
+import MeetingArea from './MeetingArea'
+import MeetingContent from './MeetingContent'
+import MeetingHeaderAndPhase from './MeetingHeaderAndPhase'
+import MeetingStyles from './MeetingStyles'
+import TeamPromptTopBar from './TeamPrompt/TeamPromptTopBar'
+import PhaseWrapper from './PhaseWrapper'
 
 interface Props {
   meeting: any
@@ -17,11 +15,22 @@ interface Props {
 
 const TeamPromptMeeting = (_props: Props) => {
   return (
-    <Root>
-      <Link title='My Dashboard' to='/meetings'>
-        <img alt='Parabol' src={logoMarkPurple} />
-      </Link>
-    </Root>
+    <MeetingStyles>
+      <MeetingArea>
+        <Suspense fallback={''}>
+          <MeetingContent>
+            <MeetingHeaderAndPhase hideBottomBar={true}>
+              <TeamPromptTopBar />
+              <PhaseWrapper>
+                <Link title='My Dashboard' to='/meetings'>
+                  <img alt='Parabol' src={logoMarkPurple} />
+                </Link>
+              </PhaseWrapper>
+            </MeetingHeaderAndPhase>
+          </MeetingContent>
+        </Suspense>
+      </MeetingArea>
+    </MeetingStyles>
   )
 }
 
