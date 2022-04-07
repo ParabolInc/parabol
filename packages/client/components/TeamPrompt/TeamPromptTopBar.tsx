2079f78a2821dc3fdc623288c5d06d7229927126
James Taber <jmtaber129@gmail.com>
2022-04-07 10:30:21 -0300

    feat(standups): Top Bar Back Button (#6318)
    * feat(standups): Top Bar Back Button
    
    * CR: Make 'BackButton' use 'Link' component
    
    * feat(standups): Show meeting title (#6336)
    
    * feat(standups): Show meeting title
    
    * CR: meeting -> meetingRef

--- packages/client/components/TeamPrompt/TeamPromptTopBar.tsx
+++ packages/client/components/TeamPrompt/TeamPromptTopBar.tsx
@@ -1,5 +1,10 @@
+import graphql from 'babel-plugin-relay/macro'
 import styled from '@emotion/styled'
 import React from 'react'
+import {useFragment} from 'react-relay'
+
+import { TeamPromptTopBar_meeting$key } from '~/__generated__/TeamPromptTopBar_meeting.graphql'
+import BackButton from '../BackButton'
 import {HeadingBlock, MeetingTopBarStyles} from '../MeetingTopBar'
 
 const TeamPromptHeaderTitle = styled('h1')({
@@ -9,14 +14,40 @@ const TeamPromptHeaderTitle = styled('h1')({
   padding: 0
 })
 
-const TeamPromptTopBar = () => {
+const TeamPromptHeader = styled('div')({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'flex-start'
+})
+
+interface Props {
+  meetingRef: TeamPromptTopBar_meeting$key
+}
+
+const TeamPromptTopBar = (props: Props) => {
+  const {meetingRef} = props
+
+  const meeting = useFragment(
+    graphql`
+      fragment TeamPromptTopBar_meeting on TeamPromptMeeting {
+        name
+      }
+    `,
+    meetingRef
+  )
+
+  const { name: meetingName } = meeting
+
   return (
     <MeetingTopBarStyles>
       <HeadingBlock>
-        {/* :TODO: (jmtaber129): Add back button */}
-        <TeamPromptHeaderTitle>Hard-coded standup title</TeamPromptHeaderTitle>
-        {/* :TODO: (jmtaber129): Add avatars, overflow menu, etc. */}
+        <TeamPromptHeader>
+          <BackButton ariaLabel='Back to Meetings' to='/meetings' />
+          <TeamPromptHeaderTitle>{meetingName}</TeamPromptHeaderTitle>
+        </TeamPromptHeader>
       </HeadingBlock>
+      {/* :TODO: (jmtaber129): Add avatars, overflow menu, etc. */}
     </MeetingTopBarStyles>
   )
 }
