b9b43ec2a8546420119fb85ede14547f2e5e5e65
André Tannús <andre.tannus@gmail.com>
2022-04-26 16:56:49 -0300

    Implemented change suggestions:
    - Added type to inline styles.
    - Converted to function component and removed props.
    - Removed hocs.

--- packages/client/modules/email/components/SummaryEmail/ExportToPDF.tsx
+++ packages/client/modules/email/components/SummaryEmail/ExportToPDF.tsx
@@ -1,57 +1,50 @@
-import withAtmosphere, {WithAtmosphereProps} from 'parabol-client/decorators/withAtmosphere/withAtmosphere'
 import {PALETTE} from 'parabol-client/styles/paletteV3'
-import withMutationProps, {WithMutationProps} from 'parabol-client/utils/relay/withMutationProps'
-import React, {Component} from 'react'
+import React from 'react'
 import {ExternalLinks} from '../../../../types/constEnums'
-
-interface Props extends WithAtmosphereProps, WithMutationProps {}
 
 const label = 'Export to PDF'
 
-const tdStyle = {
+const tdStyle: React.CSSProperties = {
   color: PALETTE.SLATE_700,
   fontSize: '13px',
-  paddingTop: 32,
+  paddingTop: 32
 }
 
-const labelStyle = {
+const labelStyle: React.CSSProperties = {
   paddingLeft: 8
 }
-const imageStyle = {
+const imageStyle: React.CSSProperties = {
   verticalAlign: 'middle'
 }
 
-const linkStyle = {
+const linkStyle: React.CSSProperties = {
   height: '40px',
   padding: '12px 10px 0px 10px',
   cursor: 'pointer',
-  width: 'fit-content',
+  width: 'fit-content'
 }
 
-class ExportToPDF extends Component<Props> {
-
-  onClick() {
+function ExportToPDF() {
+  const onClick = () => {
     window.print()
   }
 
-  render() {
-    return (
-      <>
-        <tr className="hide-print" >
-          <td align='center' style={tdStyle} width='100%'>
-            <div onClick={this.onClick} style={linkStyle}>
-              <img
-                alt={label}
-                src={`${ExternalLinks.EMAIL_CDN}cloud_download.png`}
-                style={imageStyle}
-              />
-              <span style={labelStyle}>{label}</span>
-            </div>
-          </td>
-        </tr>
-      </>
-    )
-  }
+  return (
+    <>
+      <tr className='hide-print'>
+        <td align='center' style={tdStyle} width='100%'>
+          <div onClick={onClick} style={linkStyle}>
+            <img
+              alt={label}
+              src={`${ExternalLinks.EMAIL_CDN}cloud_download.png`}
+              style={imageStyle}
+            />
+            <span style={labelStyle}>{label}</span>
+          </div>
+        </td>
+      </tr>
+    </>
+  )
 }
 
-export default withAtmosphere(withMutationProps(ExportToPDF))
+export default ExportToPDF
