dcfd4a523d30b749796203188adb07256b4646fe
James Taber <jmtaber129@gmail.com>
2022-04-27 17:16:10 -0300

    Highlight card with open discussion

--- packages/client/components/TeamPrompt/TeamPromptResponseCard.tsx
+++ packages/client/components/TeamPrompt/TeamPromptResponseCard.tsx
@@ -21,19 +21,22 @@ const ResponseHeader = styled('div')({
   padding: '0 8px'
 })
 
-const ResponseCard = styled('div')<{isEmpty: boolean}>(({isEmpty = false}) => ({
-  background: isEmpty ? PALETTE.SLATE_300 : Card.BACKGROUND_COLOR,
-  borderRadius: Card.BORDER_RADIUS,
-  boxShadow: isEmpty ? undefined : Elevation.CARD_SHADOW,
-  flex: 1,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  color: isEmpty ? PALETTE.SLATE_600 : undefined,
-  padding: Card.PADDING,
-  minHeight: MIN_CARD_HEIGHT,
-  userSelect: 'none'
-}))
+const ResponseCard = styled('div')<{isEmpty: boolean; isHighlighted?: boolean}>(
+  ({isEmpty = false, isHighlighted = false}) => ({
+    background: isEmpty ? PALETTE.SLATE_300 : Card.BACKGROUND_COLOR,
+    borderRadius: Card.BORDER_RADIUS,
+    boxShadow: isEmpty ? undefined : Elevation.CARD_SHADOW,
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    color: isEmpty ? PALETTE.SLATE_600 : undefined,
+    padding: Card.PADDING,
+    minHeight: MIN_CARD_HEIGHT,
+    userSelect: 'none',
+    outline: isHighlighted ? `2px solid ${PALETTE.SKY_300}` : 'none'
+  })
+)
 
 export const TeamMemberName = styled('h3')({
   padding: '0 8px'
@@ -62,6 +65,12 @@ const TeamPromptResponseCard = (props: Props) => {
       fragment TeamPromptResponseCard_stage on TeamPromptResponseStage {
         id
         meetingId
+        meeting {
+          ... on TeamPromptMeeting {
+            localStageId
+            isRightDrawerOpen
+          }
+        }
         teamMember {
           userId
           picture
@@ -88,7 +97,7 @@ const TeamPromptResponseCard = (props: Props) => {
   const atmosphere = useAtmosphere()
   const {viewerId} = atmosphere
 
-  const {teamMember} = responseStage
+  const {teamMember, meeting} = responseStage
   const {picture, preferredName, userId} = teamMember
 
   const isCurrentViewer = userId === viewerId
@@ -101,7 +110,10 @@ const TeamPromptResponseCard = (props: Props) => {
         <TeamMemberName>{preferredName}</TeamMemberName>
         {/* :TODO: (jmtaber129): Show when response was last updated */}
       </ResponseHeader>
-      <ResponseCard isEmpty={isEmptyResponse}>
+      <ResponseCard
+        isEmpty={isEmptyResponse}
+        isHighlighted={meeting?.isRightDrawerOpen && meeting?.localStageId === responseStage.id}
+      >
         {isEmptyResponse ? (
           'No response, yet...'
         ) : (
