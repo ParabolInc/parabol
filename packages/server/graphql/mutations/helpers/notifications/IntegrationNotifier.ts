3927238e67fbf6205c2efbaf104630f78ade5796
Georg Bremer <github@dschoordsch.de>
2022-04-28 14:36:46 +0200

    Add EmptyNotifier to allow type safe iteration over methods

--- packages/server/graphql/mutations/helpers/notifications/IntegrationNotifier.ts
+++ packages/server/graphql/mutations/helpers/notifications/IntegrationNotifier.ts
@@ -1,20 +1,17 @@
 import {MattermostNotifier} from './MattermostNotifier'
-import {Notifier} from './Notifier'
+import {EmptyNotifier, Notifier} from './Notifier'
 import {SlackNotifier} from './SlackNotifier'
 
 const notifiers = [MattermostNotifier, SlackNotifier]
-//const methods: (keyof Notifier)[] = ['startMeeting', 'endMeeting', 'startTimeLimit', 'endTimeLimit', 'integrationUpdated']
-
-const f = notifiers[0]!
 
 export const IntegrationNotifier = {
   ...Object.fromEntries(
-    Object.entries(f).map(([key, method]) => [
-      key,
-      async (...args: Parameters<typeof method>) => {
+    Object.entries(EmptyNotifier).map(([name, fun]) => [
+      name,
+      async (...args: Parameters<typeof fun>) => {
         await Promise.allSettled(
           notifiers.map(async (notifier) => {
-            notifier[key](...args)
+            notifier[name](...args)
           })
         )
       }
