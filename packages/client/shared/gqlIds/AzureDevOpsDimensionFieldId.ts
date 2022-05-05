84d1bd0845bd3567d9aaeabeb5266c61798c63b8
Bob Jacobs <bob.jacobs@outlook.com>
2022-05-05 15:08:52 -0400

    feat:azuredevsop - code cleanup based off PR comments

--- packages/client/shared/gqlIds/AzureDevOpsDimensionFieldId.ts
+++ packages/client/shared/gqlIds/AzureDevOpsDimensionFieldId.ts
@@ -1,8 +1,10 @@
 const AzureDevOpsDimensionFieldId = {
   join: (instanceId: string, dimensionName: string, fieldId: string) =>
+    // replaces whitespace characters with '¶'
     `${instanceId}:${dimensionName.replace(/\s/g, '¶')}:${fieldId}`,
   split: (id: string) => {
     const [instanceId, dimensionName, fieldId] = id.split(':') as [string, string, string]
+    // replaces '¶' characters with whitespace
     return {instanceId, dimensionName: dimensionName.replace(/¶/g, ' '), fieldId}
   }
 }
