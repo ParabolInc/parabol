e7261902e815ea694988feea61ec3b43f83320e9
Jenny Chen <jenny0322@users.noreply.github.com>
2022-05-06 11:18:15 -0700

    remove excessive logging and unused method

--- packages/server/utils/MSTeamsServerManager.ts
+++ packages/server/utils/MSTeamsServerManager.ts
@@ -1,10 +1,9 @@
 import fetch from 'node-fetch'
 
-// Mattermost is a server-only integration for now, unlike the Slack integration
+// MS Teams is a server-only integration for now, unlike the Slack integration
 
 // We're following a similar manager pattern here should we wish to refactor the
-// Mattermost integration. We might want to do this if we decide to use Mattermost's
-// upcoming "App" framework
+// MS Teams integration.
 
 const MAX_REQUEST_TIME = 5000
 
@@ -40,8 +39,6 @@ abstract class MSTeamsManager {
     }
   }
 
-  // See
-
   async post(payload: any) {
     const res = await this.fetchWithTimeout(this.webhookUrl, {
       method: 'POST',
@@ -61,30 +58,6 @@ abstract class MSTeamsManager {
       }
     }
     return res
-  }
-
-  async postMessage(textOrAttachmentsArray: string | unknown[], notificationText?: string) {
-    const prop =
-      typeof textOrAttachmentsArray === 'string'
-        ? 'text'
-        : Array.isArray(textOrAttachmentsArray)
-        ? 'attachments'
-        : null
-    if (!prop) return new Error('Invalid MS Teams message')
-    const defaultPayload = {
-      [prop]: textOrAttachmentsArray
-    }
-    const payload =
-      prop === 'text'
-        ? defaultPayload
-        : {
-            ...defaultPayload,
-            text: notificationText
-          }
-    console.log('attachment type is ' + typeof textOrAttachmentsArray)
-    console.log('payload is ' + payload)
-
-    return await this.post(payload)
   }
 }
 
