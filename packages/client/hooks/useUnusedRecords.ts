1f64e48815f70ec1f3f43c6c92ef411d79fe7fea
Nick O'Ferrall <nickoferrall@gmail.com>
2022-05-06 10:06:43 +0100

    handle get issue error case

--- packages/client/hooks/useUnusedRecords.ts
+++ packages/client/hooks/useUnusedRecords.ts
@@ -4,16 +4,15 @@ import {Threshold} from '../types/constEnums'
 const useUnusedRecords = (
   serviceTaskIds: string[],
   usedServiceTaskIds: Set<string>,
-  thresh?: number
+  thresh = Threshold.MAX_POKER_STORIES
 ): [string[], boolean | null] => {
   return useMemo(() => {
     const unusedServiceTaskIds = [] as string[]
     serviceTaskIds.forEach((servieTaskId) => {
       if (!usedServiceTaskIds.has(servieTaskId)) unusedServiceTaskIds.push(servieTaskId)
     })
-    const maxStories = thresh ? thresh : Threshold.MAX_POKER_STORIES
     const allSelected =
-      unusedServiceTaskIds.length === 0 || usedServiceTaskIds.size === maxStories
+      unusedServiceTaskIds.length === 0 || usedServiceTaskIds.size === thresh
         ? true
         : unusedServiceTaskIds.length === serviceTaskIds.length
         ? false
