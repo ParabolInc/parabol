c0efc5e75d1f5f506c2d9c256227357537988d4f
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-05-09 17:43:53 +0200

    fix(email-summary): revert MeetingSummaryEmailRootSSR to use query renderer (#6523)
    * fix: revert MeetingSummaryEmailRootSSR to use query renderer
    
    * Remove environment option from useQueryLoaderNow
    
    The environment option was used for SSR which does not work with
    Suspense, thus it can be removed again.
    
    Co-authored-by: Georg Bremer <github@dschoordsch.de>

--- packages/client/modules/email/components/MeetingSummaryEmailRootSSR.tsx
+++ packages/client/modules/email/components/MeetingSummaryEmailRootSSR.tsx
@@ -1,10 +1,7 @@
 import graphql from 'babel-plugin-relay/macro'
-import {
-  MeetingSummaryEmailRootSSRQuery,
-  MeetingSummaryEmailRootSSRQueryResponse
-} from 'parabol-client/__generated__/MeetingSummaryEmailRootSSRQuery.graphql'
+import {MeetingSummaryEmailRootSSRQuery} from 'parabol-client/__generated__/MeetingSummaryEmailRootSSRQuery.graphql'
 import React from 'react'
-import {useLazyLoadQuery} from 'react-relay'
+import {QueryRenderer} from 'react-relay'
 import {Environment} from 'relay-runtime'
 import makeAppURL from '../../../utils/makeAppURL'
 import MeetingSummaryEmail from './SummaryEmail/MeetingSummaryEmail/MeetingSummaryEmail'
@@ -36,48 +33,38 @@ export const meetingSummaryUrlParams = {
 }
 
 const MeetingSummaryEmailRootSSR = (props: Props) => {
-  const {appOrigin, meetingId} = props
-  const data = useLazyLoadQuery<MeetingSummaryEmailRootSSRQuery>(
-    query,
-    {meetingId},
-    {UNSTABLE_renderPolicy: 'full'}
-  )
-  return (
-    <MeetingSummaryEmailContainer
-      viewer={data.viewer}
-      meetingId={meetingId}
-      appOrigin={appOrigin}
-    />
-  )
-}
+  const {appOrigin, environment, meetingId} = props
 
-interface MeetingSummaryEmailContainerProps {
-  viewer: MeetingSummaryEmailRootSSRQueryResponse['viewer']
-  appOrigin: string
-  meetingId: string
-}
-
-function MeetingSummaryEmailContainer(props: MeetingSummaryEmailContainerProps) {
-  const {viewer, meetingId, appOrigin} = props
-  if (!viewer) return null
-  const {newMeeting} = viewer
-  if (!newMeeting) return null
-  const {team} = newMeeting
-  const {id: teamId} = team
-  const options = {searchParams: meetingSummaryUrlParams}
-  const referrerUrl = makeAppURL(appOrigin, `new-summary/${meetingId}`, options)
-  const meetingUrl = makeAppURL(appOrigin, `meet/${meetingId}`, options)
-  const teamDashUrl = makeAppURL(appOrigin, `team/${teamId}`, options)
-  const emailCSVUrl = makeAppURL(appOrigin, `new-summary/${meetingId}/csv`, options)
   return (
-    <MeetingSummaryEmail
-      meeting={newMeeting}
-      referrer={'email'}
-      teamDashUrl={teamDashUrl}
-      meetingUrl={meetingUrl}
-      referrerUrl={referrerUrl}
-      emailCSVUrl={emailCSVUrl}
-      appOrigin={appOrigin}
+    <QueryRenderer<MeetingSummaryEmailRootSSRQuery>
+      environment={environment}
+      query={query}
+      variables={{meetingId}}
+      render={({props}) => {
+        if (!props) return null
+        const {viewer} = props
+        if (!viewer) return null
+        const {newMeeting} = viewer
+        if (!newMeeting) return null
+        const {team} = newMeeting
+        const {id: teamId} = team
+        const options = {searchParams: meetingSummaryUrlParams}
+        const referrerUrl = makeAppURL(appOrigin, `new-summary/${meetingId}`, options)
+        const meetingUrl = makeAppURL(appOrigin, `meet/${meetingId}`, options)
+        const teamDashUrl = makeAppURL(appOrigin, `team/${teamId}`, options)
+        const emailCSVUrl = makeAppURL(appOrigin, `new-summary/${meetingId}/csv`, options)
+        return (
+          <MeetingSummaryEmail
+            meeting={newMeeting}
+            referrer={'email'}
+            teamDashUrl={teamDashUrl}
+            meetingUrl={meetingUrl}
+            referrerUrl={referrerUrl}
+            emailCSVUrl={emailCSVUrl}
+            appOrigin={appOrigin}
+          />
+        )
+      }}
     />
   )
 }
