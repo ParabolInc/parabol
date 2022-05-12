543f25e5a0dfe3bd5e762ada262bc16166e8b6a0
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-05-12 18:02:10 +0200

    feat: added upsertTeamPromptResponseUpdater to the meeting subscription

--- packages/client/mutations/UpsertTeamPromptResponseMutation.ts
+++ packages/client/mutations/UpsertTeamPromptResponseMutation.ts
@@ -1,7 +1,8 @@
 import graphql from 'babel-plugin-relay/macro'
 import {commitMutation} from 'react-relay'
 import clientTempId from '~/utils/relay/clientTempId'
-import {LocalHandlers, StandardMutation} from '../types/relayMutations'
+import {UpsertTeamPromptResponseMutation_meeting} from '~/__generated__/UpsertTeamPromptResponseMutation_meeting.graphql'
+import {LocalHandlers, SharedUpdater, StandardMutation} from '../types/relayMutations'
 import {UpsertTeamPromptResponseMutation as TUpsertTeamPromptResponseMutation} from '../__generated__/UpsertTeamPromptResponseMutation.graphql'
 
 graphql`
@@ -36,6 +37,27 @@ const mutation = graphql`
     }
   }
 `
+
+export const upsertTeamPromptResponseUpdater: SharedUpdater<
+  UpsertTeamPromptResponseMutation_meeting
+> = (payload, {store}) => {
+  const newResponse = payload.getLinkedRecord('teamPromptResponse')
+  const newResponseCreatorId = newResponse.getValue('userId')
+  const meetingId = payload.getValue('meetingId')
+  const meeting = store.get(meetingId)
+  if (!meeting) return
+  const phases = meeting.getLinkedRecords('phases')
+  if (!phases) return
+  const [responsesPhase] = phases
+  if (!responsesPhase) return
+  const stages = responsesPhase.getLinkedRecords('stages')
+  if (!stages) return
+  const stageToUpdate = stages.find(
+    (stage) => stage.getLinkedRecord('teamMember')?.getValue('userId') === newResponseCreatorId
+  )
+  if (!stageToUpdate) return
+  stageToUpdate.setLinkedRecord(newResponse, 'response')
+}
 
 interface Handlers extends LocalHandlers {
   plaintextContent: string
