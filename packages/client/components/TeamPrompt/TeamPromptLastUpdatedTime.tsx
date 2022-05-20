d005a687416f2c42ad82ee7080c536f4d3b3f469
JimmyLv <jimmy.jinglv@gmail.com>
2022-05-20 16:29:00 +0800

    feat: show the createdTime as primary timestamp, updatedTime through a tooltip over Edited

--- packages/client/components/TeamPrompt/TeamPromptLastUpdatedTime.tsx
+++ packages/client/components/TeamPrompt/TeamPromptLastUpdatedTime.tsx
@@ -1,12 +1,18 @@
 import styled from '@emotion/styled'
 import React from 'react'
+import {MenuPosition} from '../../hooks/useCoords'
+import useTooltip from '../../hooks/useTooltip'
 import {PALETTE} from '../../styles/paletteV3'
+import absoluteDate from '../../utils/date/absoluteDate'
 import relativeDate from '../../utils/date/relativeDate'
 
 const Timestamp = styled('div')({
   color: PALETTE.SLATE_600,
   fontWeight: 600,
   fontSize: 12
+})
+const Hover = styled('span')({
+  cursor: 'pointer'
 })
 
 interface Props {
@@ -15,12 +21,32 @@ interface Props {
 }
 
 export default function LastUpdatedTime({updatedAt, createdAt}: Props) {
+  const {
+    tooltipPortal: createdTimePortal,
+    openTooltip: showCreatedTime,
+    closeTooltip: closeCreatedTime,
+    originRef: createdTimeRef
+  } = useTooltip<HTMLButtonElement>(MenuPosition.UPPER_CENTER)
+  const {
+    tooltipPortal: updatedTimePortal,
+    openTooltip: showUpdatedTime,
+    closeTooltip: closeUpdatedTime,
+    originRef: updatedTimeRef
+  } = useTooltip<HTMLButtonElement>(MenuPosition.UPPER_CENTER)
+
   const isEdited = createdAt !== updatedAt
-  const formattedTime = relativeDate(updatedAt)
   return (
     <Timestamp>
-      {formattedTime}
-      {isEdited && ' · Edited'}
+      <Hover onMouseEnter={showCreatedTime} onMouseLeave={closeCreatedTime} ref={createdTimeRef}>
+        {relativeDate(createdAt)}
+        {createdTimePortal(absoluteDate(updatedAt))}
+      </Hover>
+      {isEdited && (
+        <Hover onMouseEnter={showUpdatedTime} onMouseLeave={closeUpdatedTime} ref={updatedTimeRef}>
+          {' · Edited'}
+          {updatedTimePortal(absoluteDate(updatedAt))}
+        </Hover>
+      )}
     </Timestamp>
   )
 }
---
+++ packages/client/utils/date/absoluteDate.ts
@@ -0,0 +1,11 @@
+import formatTime from './formatTime'
+import formatWeekday from './formatWeekday'
+
+function absoluteDate(date: string | Date) {
+  const datetime = new Date(date)
+  const fallbackDate = formatWeekday(datetime)
+  const fallbackTime = formatTime(datetime)
+  return fallbackDate + ' ' + fallbackTime
+}
+
+export default absoluteDate
