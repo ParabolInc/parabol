e95e6fa1ebef67ade12a4fb962006a49a26714b7
Matt Krick <matt.krick@gmail.com>
2022-06-03 15:47:09 -0700

    revert debugging changes

--- scripts/prod.js
+++ scripts/prod.js
@@ -2,9 +2,6 @@ const webpack = require('webpack')
 const makeServersConfig = require('./webpack/prod.servers.config')
 const makeClientConfig = require('./webpack/prod.client.config')
 const generateGraphQLArtifacts = require('./generateGraphQLArtifacts')
-const cp = require('child_process')
-const {promisify} = require('util')
-const {Pool} = require('pg')
 
 const compile = (config, isSilent) => {
   return new Promise((resolve) => {
@@ -25,21 +22,6 @@ const compile = (config, isSilent) => {
 
 const prod = async (isDeploy) => {
   console.log('🙏🙏🙏      Building Production Server      🙏🙏🙏')
-  // console.log('PG BUILD START', Date.now())
-  // const exec = promisify(cp.exec)
-  // const pg = new Pool({
-  //   user: 'pgparaboladmin',
-  //   password: 'parabol',
-  //   database: 'parabol-saas',
-  //   host: 'localhost',
-  //   port: 5432
-  // })
-  // const res1 = await pg.query(`SELECT 1`)
-  // console.log({res1})
-  // await exec('yarn pg:build')
-  // console.log('PG BUILD COMPLETE', Date.now())
-  // const res = await exec('ls packages/server/postgres/queries/generated')
-  // console.log(res.stdout)
   await generateGraphQLArtifacts()
   const serversConfig = makeServersConfig({isDeploy})
   const clientConfig = makeClientConfig({isDeploy})
