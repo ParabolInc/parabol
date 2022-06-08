a3d01be24463e7cc40574c677eb8acfd29533609
Nick O'Ferrall <nickoferrall@gmail.com>
2022-06-08 12:58:26 +0100

    implement team member change

--- packages/server/graphql/private/mutations/changeEmailDomain.ts
+++ packages/server/graphql/private/mutations/changeEmailDomain.ts
@@ -14,14 +14,25 @@ const changeEmailDomain: MutationResolvers['changeEmailDomain'] = async (
   if (oldDomain === newDomain) {
     throw new Error('New domain is the same as the old one')
   }
+  const normalizedNewDomain = newDomain.toLowerCase()
+  const normalizedOldDomain = oldDomain.toLowerCase()
 
   // RESOLUTION
-  const results = await r
-    .table('Organization')
-    .filter((row) => row('activeDomain').eq(oldDomain))
-    .update({activeDomain: newDomain})
-    .run()
-  console.log('🚀  ~ results', results)
+  const [orgRes, teamMemberRes] = await Promise.all([
+    r
+      .table('Organization')
+      .filter((row) => row('activeDomain').eq(normalizedOldDomain))
+      .update({activeDomain: normalizedNewDomain})
+      .run(),
+    r
+      .table('TeamMember')
+      .filter((row) => r.expr(row('email')).split('@').nth(1).eq(normalizedOldDomain))
+      .update((row) => ({email: row('email').split('@').nth(0).add(`@${normalizedNewDomain}`)}), {
+        returnChanges: true
+      })
+      .run()
+  ])
+  console.log('🚀  ~ results', {orgRes, teamMemberRes})
 
   const data = {}
   return data
