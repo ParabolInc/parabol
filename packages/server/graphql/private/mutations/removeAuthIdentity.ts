790733d640ea5f2b1de867bf22783c1c1c52d434
Nick O'Ferrall <nickoferrall@gmail.com>
2022-06-09 17:46:14 +0100

    send password reset request to affected users

--- packages/server/graphql/private/mutations/removeAuthIdentity.ts
+++ packages/server/graphql/private/mutations/removeAuthIdentity.ts
@@ -1,42 +1,78 @@
+import base64url from 'base64url'
 import bcrypt from 'bcrypt'
+import crypto from 'crypto'
 import {AuthIdentityTypeEnum, Security} from 'parabol-client/types/constEnums'
+import {r} from 'rethinkdb-ts'
+import util from 'util'
 import AuthIdentityLocal from '../../../database/types/AuthIdentityLocal'
+import PasswordResetRequest from '../../../database/types/PasswordResetRequest'
+import getMailManager from '../../../email/getMailManager'
+import resetPasswordEmailCreator from '../../../email/resetPasswordEmailCreator'
+import generateRandomString from '../../../generateRandomString'
 import getUsersByDomain from '../../../postgres/queries/getUsersByDomain'
 import updateUser from '../../../postgres/queries/updateUser'
-import {getUserId} from '../../../utils/authorization'
-import {MutationResolvers} from '../../public/resolverTypes'
+import {MutationResolvers} from '../../private/resolverTypes'
+
+const randomBytes = util.promisify(crypto.randomBytes)
 
 const removeAuthIdentity: MutationResolvers['removeAuthIdentity'] = async (
   _source,
   {domain, identityType, addLocal},
-  {authToken}
+  {ip}
 ) => {
-  const viewerId = getUserId(authToken)
-  const now = new Date()
-
   // VALIDATION
+  const users = await getUsersByDomain(domain)
+  if (!users.length) {
+    return {error: {message: 'No user emails match that domain'}}
+  }
 
   // RESOLUTION
-  const users = await getUsersByDomain(domain)
-  const dummyPassword = 'dummy'
-  const dummyHashedPassword = await bcrypt.hash(dummyPassword, Security.SALT_ROUNDS)
-  const updateUsersPromises = users.map((user) => {
+  const updateUsersPromises = users.map(async (user) => {
     const {id: userId, identities} = user
     const filteredIdentities = identities.filter((identity) => identity.type !== identityType)
     if (!filteredIdentities.length || addLocal) {
       const identityId = `${userId}:${AuthIdentityTypeEnum.LOCAL}`
+      const dummyPassword = generateRandomString(Security.SALT_ROUNDS)
+      const dummyHashedPassword = await bcrypt.hash(dummyPassword, Security.SALT_ROUNDS)
       const newIdentity = new AuthIdentityLocal({
         hashedPassword: dummyHashedPassword,
         id: identityId,
         isEmailVerified: false
       })
       const newIdentities = [...filteredIdentities, newIdentity]
+      user.identities = newIdentities
       return updateUser({identities: newIdentities}, userId)
     } else {
+      user.identities = filteredIdentities
       return updateUser({identities: filteredIdentities}, userId)
     }
   })
-  const result = await Promise.all(updateUsersPromises)
+  await Promise.all(updateUsersPromises)
+
+  // send forgot password email
+  users.forEach(async ({email}) => {
+    const tokenBuffer = await randomBytes(48)
+    const resetPasswordToken = base64url.encode(tokenBuffer)
+    // invalidate all other tokens for this email
+    await r
+      .table('PasswordResetRequest')
+      .getAll(email, {index: 'email'})
+      .filter({isValid: true})
+      .update({isValid: false})
+      .run()
+    await r
+      .table('PasswordResetRequest')
+      .insert(new PasswordResetRequest({ip, email, token: resetPasswordToken}))
+      .run()
+    const {subject, body, html} = resetPasswordEmailCreator({resetPasswordToken})
+    await getMailManager().sendEmail({
+      to: email,
+      subject,
+      body,
+      html,
+      tags: ['type:resetPassword']
+    })
+  })
 
   const updatedUserIds = users.map(({id}) => id)
   const data = {userIds: updatedUserIds}
