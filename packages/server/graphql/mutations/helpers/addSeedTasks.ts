fcfb98197a4c326293f9476be4bf7e19648b2e58
JimmyLv <jimmy.jinglv@gmail.com>
2022-06-09 11:49:20 +0800

    feat: add integrations task for first-time users, use jsdom to attach the /integration link #5943

--- packages/server/graphql/mutations/helpers/addSeedTasks.ts
+++ packages/server/graphql/mutations/helpers/addSeedTasks.ts
@@ -1,5 +1,8 @@
 import convertToTaskContent from 'parabol-client/utils/draftjs/convertToTaskContent'
 import getTagsFromEntityMap from 'parabol-client/utils/draftjs/getTagsFromEntityMap'
+import makeAppURL from 'parabol-client/utils/makeAppURL'
+import {convertHtmlToTaskContent} from '~/utils/draftjs/convertHtmlToTaskContent'
+import appOrigin from '../../../appOrigin'
 import getRethink from '../../../database/rethinkDriver'
 import {RValue} from '../../../database/stricterR'
 import {TaskStatusEnum} from '../../../database/types/Task'
@@ -8,20 +11,35 @@ import generateUID from '../../../generateUID'
 const CONTENT_STRING = `
   This is a task card. They can be created here, in a meeting, or via an integration`
 
-const SEED_TASKS = [
-  {
-    status: 'active' as TaskStatusEnum,
-    sortOrder: 0,
-    content: convertToTaskContent(CONTENT_STRING),
-    plaintextContent: CONTENT_STRING
-  }
-]
+function getSeedTasks(teamId: string) {
+  const integrationURL = makeAppURL(appOrigin, `team/${teamId}/settings`)
+
+  const taskContent = `Parabol supports integrations for Jira, GitHub, GitLab, Slack and Mattermost. Connect your tools on Settings > Integrations.`
+  const taskContentHTML = `Parabol supports integrations for Jira, GitHub, GitLab, Slack and Mattermost. Connect your tools on <a href="${integrationURL}">Settings > Integrations</a>.`
+
+  const content = convertHtmlToTaskContent(taskContentHTML)
+
+  return [
+    {
+      status: 'active' as TaskStatusEnum,
+      sortOrder: 0,
+      content: convertToTaskContent(CONTENT_STRING),
+      plaintextContent: CONTENT_STRING
+    },
+    {
+      status: 'active' as TaskStatusEnum,
+      sortOrder: 1,
+      content: content,
+      plaintextContent: taskContent
+    }
+  ]
+}
 
 export default async (userId: string, teamId: string) => {
   const r = await getRethink()
   const now = new Date()
 
-  const seedTasks = SEED_TASKS.map((proj) => ({
+  const seedTasks = getSeedTasks(teamId).map((proj) => ({
     ...proj,
     id: `${teamId}::${generateUID()}`,
     createdAt: now,
