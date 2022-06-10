0be6d00281457f68d3319c3421797546a4c90c68
Igor <alan-desu@yandex.ru>
2022-06-10 16:51:08 +0400

    Parabol scoping search bar reusable components

--- packages/client/components/ParabolScopingSearchBar.tsx
+++ packages/client/components/ParabolScopingSearchBar.tsx
@@ -1,35 +1,11 @@
-import styled from '@emotion/styled'
 import graphql from 'babel-plugin-relay/macro'
 import React from 'react'
 import {useFragment} from 'react-relay'
-import {PALETTE} from '../styles/paletteV3'
-import {ICON_SIZE} from '../styles/typographyV2'
 import {ParabolScopingSearchBar_meeting$key} from '../__generated__/ParabolScopingSearchBar_meeting.graphql'
-import Icon from './Icon'
-import ParabolScopingSearchCurrentFilters from './ParabolScopingSearchCurrentFilters'
 import ParabolScopingSearchFilterToggle from './ParabolScopingSearchFilterToggle'
-import ParabolScopingSearchInput from './ParabolScopingSearchInput'
-
-const SearchIcon = styled(Icon)({
-  color: PALETTE.SLATE_600,
-  fontSize: ICON_SIZE.MD24,
-  padding: 3,
-  marginRight: 12
-})
-
-const SearchBar = styled('div')({
-  padding: 16
-})
-
-const SearchBarWrapper = styled('div')({
-  alignItems: 'center',
-  border: `1px solid ${PALETTE.SLATE_400}`,
-  borderRadius: '40px',
-  display: 'flex',
-  height: 44,
-  padding: '0 16px',
-  width: '100%'
-})
+import ScopingSearchBar from './ScopingSearchBar'
+import ScopingSearchHistoryToggle from './ScopingSearchHistoryToggle'
+import ScopingSearchInput from './ScopingSearchInput'
 
 interface Props {
   meetingRef: ParabolScopingSearchBar_meeting$key
@@ -41,23 +17,32 @@ const ParabolScopingSearchBar = (props: Props) => {
   const meeting = useFragment(
     graphql`
       fragment ParabolScopingSearchBar_meeting on PokerMeeting {
-        ...ParabolScopingSearchInput_meeting
+        id
+        parabolSearchQuery {
+          queryString
+          statusFilters
+        }
         ...ParabolScopingSearchFilterToggle_meeting
-        ...ParabolScopingSearchCurrentFilters_meeting
       }
     `,
     meetingRef
   )
 
+  const {id: meetingId, parabolSearchQuery} = meeting
+  const {queryString, statusFilters} = parabolSearchQuery
+  const currentFilters = statusFilters?.length ? statusFilters.join(', ') : 'None'
+
   return (
-    <SearchBar>
-      <SearchBarWrapper>
-        <SearchIcon>search</SearchIcon>
-        <ParabolScopingSearchInput meeting={meeting} />
-        <ParabolScopingSearchFilterToggle meeting={meeting} />
-      </SearchBarWrapper>
-      <ParabolScopingSearchCurrentFilters meetingRef={meeting} />
-    </SearchBar>
+    <ScopingSearchBar currentFilters={currentFilters}>
+      <ScopingSearchHistoryToggle />
+      <ScopingSearchInput
+        placeholder={'Search Parabol tasks'}
+        queryString={queryString ?? ''}
+        meetingId={meetingId}
+        linkedRecordName={'parabolSearchQuery'}
+      />
+      <ParabolScopingSearchFilterToggle meeting={meeting} />
+    </ScopingSearchBar>
   )
 }
 
