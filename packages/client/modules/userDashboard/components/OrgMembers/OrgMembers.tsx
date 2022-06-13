b1fdefffc4553bea52525820a57f148adfa7d533
Nick O'Ferrall <nickoferrall@gmail.com>
2022-06-13 19:25:51 +0100

    refactor orgMembers to useFragment hook

--- packages/client/modules/userDashboard/components/OrgMembers/OrgMembers.tsx
+++ packages/client/modules/userDashboard/components/OrgMembers/OrgMembers.tsx
@@ -2,20 +2,68 @@ import graphql from 'babel-plugin-relay/macro'
 import type {Parser as JSON2CSVParser} from 'json2csv'
 import Parser from 'json2csv/lib/JSON2CSVParser' // only grab the sync parser
 import React from 'react'
-import {createPaginationContainer} from 'react-relay'
+import {PreloadedQuery, usePaginationFragment, usePreloadedQuery} from 'react-relay'
+import {OrgMembersPaginationQuery} from '~/__generated__/OrgMembersPaginationQuery.graphql'
+import {OrgMembersQuery} from '~/__generated__/OrgMembersQuery.graphql'
+import {OrgMembers_viewer$key} from '~/__generated__/OrgMembers_viewer.graphql'
 import ExportToCSVButton from '../../../../components/ExportToCSVButton'
 import Panel from '../../../../components/Panel/Panel'
-import {OrgMembers_viewer} from '../../../../__generated__/OrgMembers_viewer.graphql'
 import OrgMemberRow from '../OrgUserRow/OrgMemberRow'
 
 interface Props {
-  viewer: OrgMembers_viewer
+  queryRef: PreloadedQuery<OrgMembersQuery>
 }
 
 const OrgMembers = (props: Props) => {
-  const {
-    viewer: {organization}
-  } = props
+  const {queryRef} = props
+  const query = usePreloadedQuery<OrgMembersQuery>(
+    graphql`
+      query OrgMembersQuery($orgId: ID!, $first: Int!, $after: String) {
+        ...OrgMembers_viewer
+      }
+    `,
+    queryRef,
+    {
+      UNSTABLE_renderPolicy: 'full'
+    }
+  )
+  const paginationRes = usePaginationFragment<OrgMembersPaginationQuery, OrgMembers_viewer$key>(
+    graphql`
+      fragment OrgMembers_viewer on Query @refetchable(queryName: "OrgMembersPaginationQuery") {
+        viewer {
+          organization(orgId: $orgId) {
+            ...OrgMemberRow_organization
+            name
+            isBillingLeader
+            organizationUsers(first: $first, after: $after)
+              @connection(key: "OrgMembers_organizationUsers") {
+              edges {
+                cursor
+                node {
+                  id
+                  inactive
+                  role
+                  user {
+                    preferredName
+                    email
+                  }
+                  ...OrgMemberRow_organizationUser
+                }
+              }
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+            }
+          }
+        }
+      }
+    `,
+    query
+  )
+  const {data} = paginationRes
+  const {viewer} = data
+  const {organization} = viewer
   if (!organization) return null
   const {organizationUsers, name: orgName, isBillingLeader} = organization
   const billingLeaderCount = organizationUsers.edges.reduce(
@@ -69,64 +117,4 @@ const OrgMembers = (props: Props) => {
   )
 }
 
-export default createPaginationContainer<Props>(
-  OrgMembers,
-  {
-    viewer: graphql`
-      fragment OrgMembers_viewer on User {
-        organization(orgId: $orgId) {
-          ...OrgMemberRow_organization
-          name
-          isBillingLeader
-          organizationUsers(first: $first, after: $after)
-            @connection(key: "OrgMembers_organizationUsers") {
-            edges {
-              cursor
-              node {
-                id
-                inactive
-                role
-                user {
-                  preferredName
-                  email
-                }
-                ...OrgMemberRow_organizationUser
-              }
-            }
-            pageInfo {
-              hasNextPage
-              endCursor
-            }
-          }
-        }
-      }
-    `
-  },
-  {
-    direction: 'forward',
-    getConnectionFromProps(props) {
-      const {viewer} = props
-      return viewer && viewer.organization && viewer.organization.organizationUsers
-    },
-    getFragmentVariables(prevVars, totalCount) {
-      return {
-        ...prevVars,
-        first: totalCount
-      }
-    },
-    getVariables(_props, {count, cursor}, fragmentVariables) {
-      return {
-        ...fragmentVariables,
-        first: count,
-        after: cursor
-      }
-    },
-    query: graphql`
-      query OrgMembersPaginationQuery($first: Int!, $after: String, $orgId: ID!) {
-        viewer {
-          ...OrgMembers_viewer
-        }
-      }
-    `
-  }
-)
+export default OrgMembers
