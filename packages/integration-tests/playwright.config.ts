18d0a832eebe9de7130da7c15b480af1835a25f5
JimmyLv <jimmy.jinglv@gmail.com>
2022-06-14 10:41:00 +0800

    test: try to use mobile chrome as mobile viewport testing

--- packages/integration-tests/playwright.config.ts
+++ packages/integration-tests/playwright.config.ts
@@ -67,18 +67,18 @@ const config: PlaywrightTestConfig = {
     // },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5']
-    //   }
-    // },
     {
-      name: 'Mobile Safari',
+      name: 'Mobile Chrome',
       use: {
-        ...devices['iPhone 12']
+        ...devices['Pixel 5']
       }
-    }
+    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: {
+    //     ...devices['iPhone 12']
+    //   }
+    // }
 
     /* Test against branded browsers. */
     // {
