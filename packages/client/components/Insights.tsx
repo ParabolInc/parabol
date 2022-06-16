9d02d3869e0c0329cae78f53599b84fd3b1a2abc
Matt Krick <matt.krick@gmail.com>
2022-06-15 18:37:15 -0700

    add empty state

--- packages/client/components/Insights.tsx
+++ packages/client/components/Insights.tsx
@@ -2,9 +2,11 @@ import styled from '@emotion/styled'
 import graphql from 'babel-plugin-relay/macro'
 import React from 'react'
 import {PreloadedQuery, usePreloadedQuery} from 'react-relay'
+import {Elevation} from '../styles/elevation'
 import {PALETTE} from '../styles/paletteV3'
 import {InsightsQuery} from '../__generated__/InsightsQuery.graphql'
 import InsightsDomainPanel from './InsightsDomainPanel'
+import Panel from './Panel/Panel'
 
 interface Props {
   queryRef: PreloadedQuery<InsightsQuery>
@@ -15,6 +17,13 @@ const DashSectionHeader = styled('h1')({
   fontSize: 20,
   lineHeight: '28px',
   paddingLeft: 32
+})
+
+const StatsPanel = styled(Panel)({
+  boxShadow: Elevation.Z3,
+  maxWidth: 520,
+  marginLeft: 16,
+  padding: 16
 })
 
 const Insights = (props: Props) => {
@@ -39,6 +48,9 @@ const Insights = (props: Props) => {
   return (
     <div>
       <DashSectionHeader>Usage</DashSectionHeader>
+      {domains.length === 0 && (
+        <StatsPanel>Usage stats are only available for qualified customers</StatsPanel>
+      )}
       {domains.map((domain) => {
         return <InsightsDomainPanel key={domain.id} domainRef={domain} />
       })}
