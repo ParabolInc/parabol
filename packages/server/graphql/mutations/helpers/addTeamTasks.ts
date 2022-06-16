c36183e15da9357716e385163ac891b4a4fd2362
JimmyLv <jimmy.jinglv@gmail.com>
2022-06-16 16:41:11 +0800

    feat: add team tasks when creating a new team

---
+++ packages/server/graphql/mutations/helpers/addTeamTasks.ts
@@ -0,0 +1,35 @@
+import makeAppURL from 'parabol-client/utils/makeAppURL'
+import appOrigin from '../../../appOrigin'
+import {TaskStatusEnum} from '../../../database/types/Task'
+import {convertHtmlToTaskContent} from '../../../utils/draftjs/convertHtmlToTaskContent'
+import {addTasks} from './addTasks'
+
+const INVITE_TASK_STRING = `Congratulations on your new team! You can now start inviting your teammates`
+const NEW_MEETING_TASK_STRING = `Start a meeting with your team`
+
+const addTeamTasks = async (userId: string, teamId: string) => {
+  const inviteURL = makeAppURL(appOrigin, `team/${teamId}?invite=true`)
+  const inviteTeamTaskHTML = `Congratulations on your new team! You can now start <a href="${inviteURL}">inviting your teammates</a>`
+
+  const newMeetingURL = makeAppURL(appOrigin, `/new-meeting/${teamId}?source=TeamTask`)
+  const newMeetingTaskHTML = `<a href="${newMeetingURL}">Start a meeting</a> with your team`
+
+  const teamTasks = [
+    {
+      status: 'active' as TaskStatusEnum,
+      sortOrder: 1,
+      content: convertHtmlToTaskContent(inviteTeamTaskHTML),
+      plaintextContent: INVITE_TASK_STRING
+    },
+    {
+      status: 'active' as TaskStatusEnum,
+      sortOrder: 0,
+      content: convertHtmlToTaskContent(newMeetingTaskHTML),
+      plaintextContent: NEW_MEETING_TASK_STRING
+    }
+  ]
+
+  return await addTasks(teamTasks, teamId, userId)
+}
+
+export default addTeamTasks
--- packages/server/graphql/mutations/helpers/createTeamAndLeader.ts
+++ packages/server/graphql/mutations/helpers/createTeamAndLeader.ts
@@ -10,6 +10,7 @@ import IUser from '../../../postgres/types/IUser'
 import catchAndLog from '../../../postgres/utils/catchAndLog'
 import addTeamIdToTMS from '../../../safeMutations/addTeamIdToTMS'
 import insertNewTeamMember from '../../../safeMutations/insertNewTeamMember'
+import addTeamTasks from './addTeamTasks'
 
 interface ValidNewTeam {
   id: string
@@ -45,6 +46,7 @@ export default async function createTeamAndLeader(user: IUser, newTeam: ValidNew
     // denormalize common fields to team member
     insertNewTeamMember(user, teamId),
     r.table('TimelineEvent').insert(timelineEvent).run(),
-    addTeamIdToTMS(userId, teamId)
+    addTeamIdToTMS(userId, teamId),
+    addTeamTasks(userId, teamId)
   ])
 }
