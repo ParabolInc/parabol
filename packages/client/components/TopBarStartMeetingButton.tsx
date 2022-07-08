edd440ed03b4b547597b087b399adcda7e21c3b9
Georg Bremer <github@dschoordsch.de>
2022-07-08 18:19:39 +0200

    Make top bar start meeting button flat

--- packages/client/components/TopBarStartMeetingButton.tsx
+++ packages/client/components/TopBarStartMeetingButton.tsx
@@ -2,13 +2,10 @@ import styled from '@emotion/styled'
 import React from 'react'
 import {useLocation} from 'react-router'
 import useRouter from '~/hooks/useRouter'
-import {PALETTE} from '~/styles/paletteV3'
 import getTeamIdFromPathname from '~/utils/getTeamIdFromPathname'
-import FloatingActionButton from './FloatingActionButton'
+import FlatPrimaryButton from './FlatPrimaryButton'
 
-const Button = styled(FloatingActionButton)({
-  color: '#fff',
-  backgroundImage: PALETTE.GRADIENT_TOMATO_600_ROSE_500,
+const Button = styled(FlatPrimaryButton)({
   height: 40,
   overflow: 'hidden',
   paddingLeft: 24,
