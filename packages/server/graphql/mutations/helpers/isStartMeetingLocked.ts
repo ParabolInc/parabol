6856c6530a55106c2a4d088893682da48c64229c
Nick O'Ferrall <nickoferrall@gmail.com>
2022-07-19 17:51:17 +0100

    lock team if anyOrgTeamIsLocked

--- packages/server/graphql/mutations/helpers/isStartMeetingLocked.ts
+++ packages/server/graphql/mutations/helpers/isStartMeetingLocked.ts
@@ -1,17 +1,18 @@
 import isValid from '../../../graphql/isValid'
+import getTeamsByOrgIds from '../../../postgres/queries/getTeamsByOrgIds'
 import {DataLoaderWorker} from '../../graphql'
 
 const isStartMeetingLocked = async (teamId: string, dataLoader: DataLoaderWorker) => {
   const team = await dataLoader.get('teams').loadNonNull(teamId)
-  const {isPaid, lockMessageHTML, tier} = team
-  if (tier !== 'personal') {
-    return isPaid
-      ? null
-      : 'Sorry! We are unable to start your meeting because your team has an overdue payment'
+  const {lockMessageHTML, orgId, tier} = team
+  const orgTeams = await getTeamsByOrgIds([orgId])
+  const anyOrgTeamIsLocked = orgTeams.some((team) => !team.isPaid)
+  if (tier !== 'personal' && anyOrgTeamIsLocked) {
+    // if this team was manually locked, be mean because they called this by hiding the modal
+    return lockMessageHTML
+      ? 'Wow, you’re determined to use Parabol! That’s awesome! Do you want to keep sneaking over the gate, or walk through the door with our Sales team?'
+      : 'Sorry! We are unable to start your meeting because one or more of the teams in your org has an overdue payment'
   }
-  // if this team was manually locked, be mean because they called this by hiding the modal
-  if (lockMessageHTML)
-    return 'Wow, you’re determined to use Parabol! That’s awesome! Do you want to keep sneaking over the gate, or walk through the door with our Sales team?'
   // if this team wasn't manually locked, see if any of its members are on locked teams
   const teamMembers = await dataLoader.get('teamMembersByTeamId').load(teamId)
   const userIds = teamMembers.map(({userId}) => userId)
@@ -20,11 +21,9 @@ const isStartMeetingLocked = async (teamId: string, dataLoader: DataLoaderWorker
     .flat()
   const teamIds = [...new Set(allTeamMembers.map(({teamId}) => teamId))]
   const allRelatedTeams = (await dataLoader.get('teams').loadMany(teamIds)).filter(isValid)
-  const anyTeamIsLocked = allRelatedTeams.some(
-    (team) => team.tier !== 'personal' && team.isPaid === false
-  )
+  const anyTeamIsLocked = allRelatedTeams.some((team) => team.isPaid === false)
   return anyTeamIsLocked
-    ? 'One or more of your team members is over the usage limit. Please contact sales.'
+    ? 'One or more of your team members is over the usage limit. Please contact sales'
     : null
 }
 
