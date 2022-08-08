75dfa17a99abd714c4fe552e72d82c069f46a054
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-08-08 09:10:41 +0200

    feat: added stop recurrence mutation

--- packages/client/components/TeamPrompt/TeamPromptOptionsMenu.tsx
+++ packages/client/components/TeamPrompt/TeamPromptOptionsMenu.tsx
@@ -69,7 +69,7 @@ const TeamPromptOptionsMenu = (props: Props) => {
       {recurrence && (
         <MenuItem
           key='copy'
-          isDisabled={!!endedAt || !!meetingSeries?.cancelledAt}
+          isDisabled={!!endedAt}
           label={
             <OptionMenuItem>
               <StyledIcon>replay</StyledIcon>
@@ -88,7 +88,7 @@ const TeamPromptOptionsMenu = (props: Props) => {
         />
       )}
       <MenuItem
-        key='end'
+        key='copy'
         isDisabled={!!endedAt}
         label={
           <OptionMenuItem>
