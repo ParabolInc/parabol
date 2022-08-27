b38c167d7160203834675b2dd4f502ab881c94b2
James Taber <jmtaber129@gmail.com>
2022-08-27 16:01:17 -0500

    Add comments

--- scripts/codeshift/convertToUseFragment.ts
+++ scripts/codeshift/convertToUseFragment.ts
@@ -12,6 +12,13 @@ const transform: Transform = (fileInfo, api, options) => {
 
   const root = j(source)
 
+  if (root.find(j.Identifier, {name: 'createFragmentContainer'}).size() > 2) {
+    // We only expect two refs - the import and the call to wrap the component.
+    throw new Error('more than two refs to createFragmentContainer')
+  }
+
+  // Find where we're wrapping our component with 'createFragmentContainer':
+  //   createFragmentContainer(MyComponent, {myProp: graphql`<fragment>`})
   const functionExpressions = root.find(j.CallExpression, {
     callee: {
       type: 'Identifier',
@@ -26,26 +33,40 @@ const transform: Transform = (fileInfo, api, options) => {
   const functionExpression = functionExpressions.get().value
 
   const replacementExport = functionExpression.arguments[0]
-  const fragmentProps = functionExpression.arguments[1]
 
+  // Replace the call to 'createFragmentContainer' with just a reference to the component we're
+  // wrapping:
+  //   createFragmentContainer(MyComponent, {myProp: graphql``})
   functionExpressions.replaceWith(replacementExport)
+
+  if (replacementExport.type !== 'Identifier') {
+    // We need to get the component idetifier from the HOC call site, so if the export looks like:
+    //   createFragmentContainer(withOtherHoc(Component), {myProp: <fragment>})
+    // It's less trivial to get the identifier.
+    // :TODO: (jmtaber129): Support files that look like this, either by traversing down the tree of
+    // call expressions until we hit the identifier, or by taking in the component name from the
+    // command line or file name.
+    throw new Error('trying to convert component with non-trivial HOC wrapping')
+  }
 
   const componentName = replacementExport.name
 
-  console.log(fragmentProps)
+  // Build a map of propName -> graphql fragment.
+  const fragmentProps = functionExpression.arguments[1]
   const propMap = {}
   fragmentProps.properties.forEach((prop) => {
     propMap[prop.key.name] = prop.value
   })
 
-  // Find the component we're changing
-  const component = root.find(j.VariableDeclarator, {id: {name: componentName}})
-  if (component.size() !== 1) {
+  // Find the declaration of the component we're changing
+  const componentDeclaration = root.find(j.VariableDeclarator, {id: {name: componentName}})
+  if (componentDeclaration.size() !== 1) {
     throw new Error("can't find component!")
   }
 
-  // Find its destructuring of props
-  const propDeclaration = component.find(j.VariableDeclaration, {
+  // Find where the component destructures its props:
+  //  const {myProp} = props
+  const propDestructuring = componentDeclaration.find(j.VariableDeclaration, {
     declarations: [
       {
         id: {type: 'ObjectPattern'},
@@ -54,12 +75,14 @@ const transform: Transform = (fileInfo, api, options) => {
     ]
   })
 
-  if (propDeclaration.size() !== 1) {
+  if (propDestructuring.size() !== 1) {
     throw new Error('props destructured never or more than once')
   }
 
+  // Check whether this is renaming any props when destructuring:
+  //  const {myProp: otherNameForProp} = props
   const hasRenamedProps =
-    propDeclaration
+    propDestructuring
       .find(j.ObjectProperty)
       .filter(
         (p) =>
@@ -71,10 +94,15 @@ const transform: Transform = (fileInfo, api, options) => {
       .size() > 0
 
   if (hasRenamedProps) {
-    throw new Error('props already renaming destructured prop')
+    // :TODO: (jmtaber129): Support this.
+    throw new Error('prop names renamed during prop destructuring')
   }
 
-  propDeclaration
+  // Rename the destructured props to '<propName>Ref'. From
+  //   const {myProp} = props
+  // to
+  //   const {myProp: myPropRef} = props
+  propDestructuring
     .find(j.ObjectProperty)
     .filter(
       (p) =>
@@ -93,7 +121,10 @@ const transform: Transform = (fileInfo, api, options) => {
       }
     })
 
-  propDeclaration.insertAfter((p) => {
+  // Insert the 'useFragment' call right after the prop destructuring:
+  //   const {myProp: myPropRef} = props
+  //   const myProp = useFragment(graphql`<fragment>`, myPropRef)
+  propDestructuring.insertAfter(() => {
     return Object.keys(propMap).map((prop) => {
       console.log(prop)
       return statement`const ${prop} = useFragment(${propMap[prop]}, ${j.identifier(
@@ -102,26 +133,46 @@ const transform: Transform = (fileInfo, api, options) => {
     })
   })
 
-  root
-    .find(j.TSInterfaceDeclaration, {id: {name: 'Props'}})
-    .find(j.TSPropertySignature, {key: {type: 'Identifier'}})
-    .filter((p) => Object.keys(propMap).includes((p.value.key as any).name))
-    .forEach((p) => {
-      if (
-        p.value.typeAnnotation?.type !== 'TSTypeAnnotation' ||
-        p.value.typeAnnotation.typeAnnotation.type !== 'TSTypeReference' ||
-        p.value.typeAnnotation.typeAnnotation.typeName.type !== 'Identifier'
-      ) {
-        throw new Error(
-          `invalid type for prop: ${(p.value.key as any).name}, ${p.value.initializer?.type}`
-        )
-      }
+  // Find the interface declaration for the Props type:
+  //   interface Props {
+  //     nonFragmentProp: string
+  //     myProp: MyFragmentName_type
+  //   }
+  // and update the types to be a graphql key:
+  //   interface Props {
+  //     nonFragmentProp: string
+  //     myProp: MyFragmentName_type$key
+  //   }
+  const didUpdateTypes =
+    root
+      .find(j.TSInterfaceDeclaration, {id: {name: 'Props'}})
+      .find(j.TSPropertySignature, {key: {type: 'Identifier'}})
+      .filter((p) => Object.keys(propMap).includes((p.value.key as any).name))
+      .forEach((p) => {
+        if (
+          p.value.typeAnnotation?.type !== 'TSTypeAnnotation' ||
+          p.value.typeAnnotation.typeAnnotation.type !== 'TSTypeReference' ||
+          p.value.typeAnnotation.typeAnnotation.typeName.type !== 'Identifier'
+        ) {
+          throw new Error(
+            `invalid type for prop: ${(p.value.key as any).name}, ${p.value.initializer?.type}`
+          )
+        }
 
-      const name = p.value.typeAnnotation.typeAnnotation.typeName.name
+        const name = p.value.typeAnnotation.typeAnnotation.typeName.name
 
-      root.find(j.Identifier, {name}).replaceWith(j.identifier(name + '$key'))
-    })
+        root.find(j.Identifier, {name}).replaceWith(j.identifier(name + '$key'))
+      })
+      .size() === Object.keys(propMap).length
 
+  if (!didUpdateTypes) {
+    throw new Error('Could not update types in interface')
+  }
+
+  // Update the import:
+  //   import {createFragmentContainer} from 'react-relay'
+  // to
+  //   import {useFragment} from 'react-relay'
   root
     .find(j.Identifier, {name: 'createFragmentContainer'})
     .replaceWith(j.identifier('useFragment'))
