a05de20b964c51bfc0fb1d5e4e11d311e104d464
Georg Bremer <github@dschoordsch.de>
2022-09-08 10:18:02 +0200

    Revert "feat(devops): Bundle used node_modules in the server compilation (#6965)"
    This reverts commit 37b53ffa05e096a93a753cbca2f3d459566d5531.
    
    Deployment failed because a shared library cannot be resolved although
    present, see #7161.

--- packages/server/postgres/utils/dropUndefined.ts
+++ packages/server/postgres/utils/dropUndefined.ts
@@ -1,10 +1,8 @@
 // Removed keys are deleted in rethink while they are undefined in postgres
 export function dropUndefined(object: unknown) {
-  if (typeof object === 'object' && object !== null) {
-    Object.keys(object).forEach((key) => {
-      if (object[key] === undefined) {
-        delete object[key]
-      }
-    })
-  }
+  Object.keys(object).forEach((key) => {
+    if (object[key] === undefined) {
+      delete object[key]
+    }
+  })
 }
