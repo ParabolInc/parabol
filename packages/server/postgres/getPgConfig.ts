c41758dd495b7bd96c8ebbcc24dd75788380ad28
Matt Krick <matt.krick@gmail.com>
2022-09-13 11:27:02 -0700

    feat(pg): support SSL certs
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/postgres/getPgConfig.ts
+++ packages/server/postgres/getPgConfig.ts
@@ -1,4 +1,15 @@
+import {readFileSync} from 'fs'
+import path from 'path'
 import PROD from '../PROD'
+
+const getSSL = () => {
+  if (process.env.PGSSLMODE !== 'require') return undefined
+  const PG_ROOT = path.join(__PROJECT_ROOT__, 'packages/server/postgres')
+  const ca = readFileSync(path.join(PG_ROOT, 'root.crt'))
+  const key = readFileSync(path.join(PG_ROOT, 'postgresql.key'))
+  const cert = readFileSync(path.join(PG_ROOT, 'postgresql.crt'))
+  return {ca, key, cert, rejectUnauthorized: true}
+}
 
 const getPgConfig = () => ({
   user: process.env.POSTGRES_USER,
@@ -6,7 +17,8 @@ const getPgConfig = () => ({
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT),
-  max: Number(process.env.POSTGRES_POOL_SIZE) || PROD ? 20 : 5
+  max: Number(process.env.POSTGRES_POOL_SIZE) || PROD ? 20 : 5,
+  ssl: getSSL()
 })
 
 export default getPgConfig
