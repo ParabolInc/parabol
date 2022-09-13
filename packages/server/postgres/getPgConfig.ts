a580d0d2b3b54b0f1e892df9f823c9b1c1c9343b
Matt Krick <matt.krick@gmail.com>
2022-09-13 11:39:52 -0700

    feat(pg): graceful support for defaults
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/postgres/getPgConfig.ts
+++ packages/server/postgres/getPgConfig.ts
@@ -14,14 +14,19 @@ const getSSL = () => {
   }
 }
 
-const getPgConfig = () => ({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  max: Number(process.env.POSTGRES_POOL_SIZE) || PROD ? 20 : 5,
-  ssl: getSSL()
-})
+const getPgConfig = () => {
+  const config = {
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    max: Number(process.env.POSTGRES_POOL_SIZE) || PROD ? 20 : 5
+  }
+  const ssl = getSSL()
+  // ssl value defaults to process.env.PGSSLMODE, so only set the variable here if CA is found
+  if (!ssl) return config
+  return {...config, ssl}
+}
 
 export default getPgConfig
