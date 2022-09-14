8846f437dd77e4a88e5dd72e5f58c3b2ba56adaa
Georg Bremer <github@dschoordsch.de>
2022-09-14 18:28:44 +0200

    Add more verbose output with instructions

--- scripts/create-jira-keys.sh
+++ scripts/create-jira-keys.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 openssl genrsa -out jira_privatekey.pem 1024
-openssl req -newkey rsa:1024 -x509 -key jira_privatekey.pem -out jira_publickey.cer -days 365
+openssl req -newkey rsa:1024 -x509 -key jira_privatekey.pem -out jira_publickey.cer -days 365 -batch
 openssl pkcs8 -topk8 -nocrypt -in jira_privatekey.pem -out jira_privatekey.pcks8
 openssl x509 -pubkey -noout -in jira_publickey.cer  > jira_publickey.pem
 openssl rand -base64 32 >consumerKey
+
+echo
+echo -----------------------------------
+echo SEND TO CLIENT
+echo Consumer Key:
+cat consumerKey
+echo Consumer Name:
+echo Parabol integration
+echo Public Key:
+cat jira_publickey.pem
+echo
+echo "Ask for their Jira Server URL, (ours is 'jira.parabol.co' for example)"
+echo -----------------------------------
+echo 
+echo to run the mutation you need
+echo "serverBaseUrl: <ASK CLIENT>"
+echo "consumerKey: <see file consumerKey>"
+echo "consumerSecret: <see file jira_privatekey.pem>"
 
