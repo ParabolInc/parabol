8290f4d5a5c8e938abfc91e8c93c69b60fb47423
Georg Bremer <github@dschoordsch.de>
2022-09-22 12:33:35 +0200

    chore: Add jira server key generation script (#7180)
    * chore: Add Jira Server key generation script
    
    Add a script to simplify generating the necessary private/public keys
    for a Jira Server integration.
    
    * Add more verbose output with instructions
    
    * Make send to client block clearer
    
    * Specify which mutation needs to be run to add Jira

---
+++ scripts/create-jira-keys.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+openssl genrsa -out jira_privatekey.pem 1024
+openssl req -newkey rsa:1024 -x509 -key jira_privatekey.pem -out jira_publickey.cer -days 365 -batch
+openssl pkcs8 -topk8 -nocrypt -in jira_privatekey.pem -out jira_privatekey.pcks8
+openssl x509 -pubkey -noout -in jira_publickey.cer  > jira_publickey.pem
+openssl rand -base64 32 >consumerKey
+
+echo
+echo -----------------------------------
+echo BEGIN SEND TO CLIENT
+echo Consumer Key:
+cat consumerKey
+echo Consumer Name:
+echo Parabol integration
+echo Public Key:
+cat jira_publickey.pem
+echo
+echo "Ask for their Jira Server URL, (ours is 'jira.parabol.co' for example)"
+echo END SEND TO CLIENT
+echo -----------------------------------
+echo 
+echo to run the addIntegrationProvider mutation later you need
+echo "serverBaseUrl: <ASK CLIENT>"
+echo "consumerKey: <see file consumerKey>"
+echo "consumerSecret: <see file jira_privatekey.pem>"
+
