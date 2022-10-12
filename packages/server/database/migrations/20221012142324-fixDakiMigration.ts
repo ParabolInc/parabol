1e1ff1ddb02738ac3b6e648a5fe83ef872a89b1e
Nick O'Ferrall <nickoferrall@gmail.com>
2022-10-12 14:42:49 +0100

    add missing daki prompt

---
+++ packages/server/database/migrations/20221012142324-fixDakiMigration.ts
@@ -0,0 +1,24 @@
+import {R} from 'rethinkdb-ts'
+
+export const up = async function (r: R) {
+  const now = new Date()
+  const missingKeepPrompt = {
+    createdAt: now,
+    description: 'What should we continue doing?',
+    groupColor: '#D9D916',
+    id: 'keepPrompt',
+    isActive: true,
+    question: 'Keep',
+    sortOrder: 2,
+    teamId: 'aGhostTeam',
+    templateId: 'dropAddKeepImproveDAKITemplate',
+    title: 'Keep',
+    updatedAt: now
+  }
+
+  await r.db('actionDevelopment').table('ReflectPrompt').insert(missingKeepPrompt).run()
+}
+
+export const down = async function (r: R) {
+  await r.db('actionDevelopment').table('ReflectPrompt').get('keepPrompt').delete().run()
+}
