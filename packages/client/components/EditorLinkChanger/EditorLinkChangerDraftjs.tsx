74989a935c089f05c79cdf08eea69c424870def0
Prashant Kumar <87Kumarprashant@gmail.com>
2022-10-17 19:04:29 +0530

    fix: Hyperlink removes whitespace (#7089)
    * fix: Hyperlink removes whitespace
    
    * resolve spaces underline issue
    
    * resolve issues and added whitespace only links
    
    * resolve issues
    
    * resolve logic error
    
    * resolve issue
    
    * resolve issue

--- packages/client/components/EditorLinkChanger/EditorLinkChangerDraftjs.tsx
+++ packages/client/components/EditorLinkChanger/EditorLinkChangerDraftjs.tsx
@@ -1,4 +1,4 @@
-import {EditorState, SelectionState} from 'draft-js'
+import {ContentState, EditorState, Modifier, SelectionState} from 'draft-js'
 import React, {RefObject} from 'react'
 import {UseTaskChild} from '../../hooks/useTaskChildFocus'
 import {BBox} from '../../types/animations'
@@ -23,6 +23,20 @@ interface Props {
   useTaskChild: UseTaskChild
 }
 
+const contentStateWithFocusAtEnd = (givenContentState, givenSelectionState) => {
+  const endKey = givenContentState.getSelectionAfter().getEndKey()
+  const endOffset = givenContentState.getSelectionAfter().getEndOffset()
+  const collapsedSelectionState = givenSelectionState.merge({
+    anchorKey: endKey,
+    anchorOffset: endOffset,
+    focusKey: endKey,
+    focusOffset: endOffset
+  })
+  return givenContentState.merge({
+    selectionAfter: collapsedSelectionState
+  }) as ContentState
+}
+
 const EditorLinkChangerDraftjs = (props: Props) => {
   const {
     editorState,
@@ -37,11 +51,47 @@ const EditorLinkChangerDraftjs = (props: Props) => {
   } = props
   useTaskChild('editor-link-changer')
   const handleSubmit = ({text, href}: {text: string; href: string}) => {
+    // don't include leading and trailing whitespace in the link text
+    const nonWhitespaceFromStart = text.search(/\S/)
+    const nonWhitespaceFromEnd = text.search(/\s*$/)
+    const startStr = nonWhitespaceFromStart === -1 ? '' : text.slice(0, nonWhitespaceFromStart)
+    const endStr = nonWhitespaceFromEnd === -1 ? '' : text.slice(nonWhitespaceFromEnd)
+    const hasTextTitle = !!text.trim()
+    // trim link text if it contains any non-whitespace characters, otherwise keep it verbatim
+    const trimmedText = hasTextTitle ? text.trim() : text
     const focusedEditorState = EditorState.forceSelection(editorState, selectionState)
-    const nextEditorState = completeEntity(focusedEditorState, 'LINK', {href}, text, {
+    let newEditorState = focusedEditorState
+    if (hasTextTitle) {
+      const contentState = focusedEditorState.getCurrentContent()
+      const expandedSelectionState = focusedEditorState.getSelection()
+      const contentStateAfterStartStr: ContentState = expandedSelectionState.isCollapsed()
+        ? Modifier.insertText(contentState, expandedSelectionState, startStr)
+        : Modifier.replaceText(contentState, expandedSelectionState, startStr)
+      newEditorState = EditorState.push(
+        focusedEditorState,
+        contentStateWithFocusAtEnd(contentStateAfterStartStr, expandedSelectionState),
+        'insert-characters'
+      )
+    }
+    newEditorState = completeEntity(newEditorState, 'LINK', {href}, trimmedText, {
       keepSelection: true
     })
-    setEditorState(nextEditorState)
+    const selectionStateAfterTrimmedText = newEditorState.getSelection()
+    let contentStateAfterEndStr = newEditorState.getCurrentContent()
+    if (hasTextTitle) {
+      const contentWithEntity = newEditorState.getCurrentContent()
+      contentStateAfterEndStr = Modifier.insertText(
+        contentWithEntity,
+        selectionStateAfterTrimmedText,
+        endStr
+      )
+    }
+    newEditorState = EditorState.push(
+      editorState,
+      contentStateWithFocusAtEnd(contentStateAfterEndStr, selectionStateAfterTrimmedText),
+      'apply-entity'
+    )
+    setEditorState(newEditorState)
     setTimeout(() => editorRef.current && editorRef.current.focus(), 0)
   }
 
