95479068aacc605c5ae235e5f2d2d6601e6964a3
Nick O'Ferrall <nickoferrall@gmail.com>
2022-10-21 16:39:58 +0100

    implement rename christmas template

---
+++ packages/server/database/migrations/20221021163318-rename-christmas-template.ts
@@ -0,0 +1,18 @@
+import {R} from 'rethinkdb-ts'
+
+export const up = async function (r: R) {
+  await r
+    .table('MeetingTemplate')
+    .get('aChristmasCarolRetrospectiveTemplate')
+    .update({name: 'Christmas Retrospective'})
+    .run()
+}
+
+export const down = async function (r: R) {
+  //
+  await r
+    .table('MeetingTemplate')
+    .get('aChristmasCarolRetrospectiveTemplate')
+    .update({name: 'A Christmas Carol Retrospective'})
+    .run()
+}
