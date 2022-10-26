05f2a4d86c1f9aaa4e0dff5f217391a3eb09124e
Nick O'Ferrall <nickoferrall@gmail.com>
2022-10-26 18:30:44 +0100

    remove default null

--- packages/server/database/migrations/20221026164434-accessiblePremiumTemplateId.ts
+++ packages/server/database/migrations/20221026164434-accessiblePremiumTemplateId.ts
@@ -3,7 +3,7 @@ import {R} from 'rethinkdb-ts'
 export const up = async function (r: R) {
   await r
     .table('MeetingSettings')
-    .update((row) => ({accessiblePremiumTemplateId: row('selectedTemplateId').default(null)}))
+    .update((row) => ({accessiblePremiumTemplateId: row('selectedTemplateId')}))
     .run()
 }
 
