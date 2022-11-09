efac7cb447cc431fa9691ec26a32b5bedd022040
Nick O'Ferrall <nickoferrall@gmail.com>
2022-11-09 19:04:34 +0000

    show org copy

--- packages/server/graphql/types/UpgradeCTALocationEnum.ts
+++ packages/server/graphql/types/UpgradeCTALocationEnum.ts
@@ -1,13 +1,14 @@
 import {GraphQLEnumType} from 'graphql'
 
-export type UpgradeCTALocationEnumType = 'publicTemplates' | 'teamTemplate'
+export type UpgradeCTALocationEnumType = 'publicTemplates' | 'teamTemplate' | 'orgTemplate'
 
 const UpgradeCTALocationEnum = new GraphQLEnumType({
   name: 'UpgradeCTALocationEnum',
   description: 'Where the upgrade to pro CTA button was clicked',
   values: {
     publicTemplate: {},
-    teamTemplate: {}
+    teamTemplate: {},
+    orgTemplate: {}
   }
 })
 
