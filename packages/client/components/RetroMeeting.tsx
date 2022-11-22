6b952ca67650fbd3800d9d187f361b70a86c5394
Georg Bremer <github@dschoordsch.de>
2022-11-22 13:14:42 +0100

    Direct access lock message designed

--- packages/client/components/RetroMeeting.tsx
+++ packages/client/components/RetroMeeting.tsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled'
+import {Lock} from '@mui/icons-material'
 import graphql from 'babel-plugin-relay/macro'
 import React, {ReactElement, Suspense} from 'react'
 import {useFragment} from 'react-relay'
@@ -9,16 +11,82 @@ import useAtmosphere from '../hooks/useAtmosphere'
 import useMeeting from '../hooks/useMeeting'
 import LocalAtmosphere from '../modules/demo/LocalAtmosphere'
 import NewMeetingAvatarGroup from '../modules/meeting/components/MeetingAvatarGroup/NewMeetingAvatarGroup'
-import {RetroDemo} from '../types/constEnums'
+import {modalShadow} from '../styles/elevation'
+import {PALETTE} from '../styles/paletteV3'
+import {Radius, RetroDemo} from '../types/constEnums'
 import lazyPreload, {LazyExoticPreload} from '../utils/lazyPreload'
 import MeetingControlBar from './MeetingControlBar'
 import MeetingStyles from './MeetingStyles'
+import PrimaryButton from './PrimaryButton'
 import ResponsiveDashSidebar from './ResponsiveDashSidebar'
 import RetroMeetingSidebar from './RetroMeetingSidebar'
 
 interface Props {
   meeting: RetroMeeting_meeting$key
 }
+
+const DialogOverlay = styled('div')({
+  position: 'fixed',
+  width: '100%',
+  height: '100%',
+  //backgroundColor: '#aaaaaaaa',
+  backdropFilter: 'blur(3px)',
+  zIndex: 100,
+  paddingTop: '20%'
+})
+
+const DialogContainer = styled('div')({
+  display: 'flex',
+  backgroundColor: 'white',
+  borderRadius: Radius.DIALOG,
+  boxShadow: modalShadow,
+  flexDirection: 'column',
+  // overflow: 'auto', removed because react-beautiful-dnd only supports 1 scrolling parent
+  margin: '0 auto',
+  maxHeight: '90vh',
+  maxWidth: 'calc(100vw - 48px)',
+  minWidth: 280,
+  width: 512,
+  alignItems: 'center',
+  padding: 24
+})
+
+const TimelineEventTitle = styled('span')({
+  color: PALETTE.SLATE_700,
+  fontSize: 14,
+  fontWeight: 600,
+  lineHeight: '20px'
+})
+
+const TimelineEventBody = styled('div')({
+  padding: '0 16px 16px 16px',
+  fontSize: 14,
+  lineHeight: '20px',
+  textAlign: 'center'
+})
+
+const EventIcon = styled('div')({
+  borderRadius: '100%',
+  color: PALETTE.GRAPE_500,
+  display: 'block',
+  userSelect: 'none',
+  height: 40,
+  width: 40,
+  svg: {
+    height: 40,
+    width: 40
+  }
+})
+
+const HeaderText = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  fontSize: 14,
+  justifyContent: 'space-around',
+  lineHeight: '20px',
+  margin: '16px 16px 8px',
+  paddingTop: 2
+})
 
 const phaseLookup = {
   checkin: lazyPreload(
@@ -56,6 +124,7 @@ const RetroMeeting = (props: Props) => {
         ...RetroDiscussPhase_meeting
         ...NewMeetingAvatarGroup_meeting
         id
+        locked
         showSidebar
         localPhase {
           phaseType
@@ -106,6 +175,20 @@ const RetroMeeting = (props: Props) => {
         handleGotoNext={handleGotoNext}
         gotoStageId={gotoStageId}
       />
+      <DialogOverlay>
+        <DialogContainer>
+          <EventIcon>
+            <Lock />
+          </EventIcon>
+          <HeaderText>
+            <TimelineEventTitle>Past Meetings Locked</TimelineEventTitle>
+          </HeaderText>
+          <TimelineEventBody>
+            Your plan includes 30 days of meeting history. Unlock more by upgrading.
+          </TimelineEventBody>
+          <PrimaryButton>Unlock past meetings</PrimaryButton>
+        </DialogContainer>
+      </DialogOverlay>
     </MeetingStyles>
   )
 }
