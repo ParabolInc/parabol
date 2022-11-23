a8320a21ad8fb2b7fcf6f3331c09d668519cd19a
Nick O'Ferrall <nickoferrall@gmail.com>
2022-11-23 18:37:39 +0000

    add updateTier migration with org, orgUser, invoice, and user

--- packages/server/database/migrations/20221123163542-updateTier.ts
+++ packages/server/database/migrations/20221123163542-updateTier.ts
@@ -1,59 +1,107 @@
 import {R} from 'rethinkdb-ts'
 
 export const up = async function (r: R) {
-  await r
-    .table('Organization')
-    .update((row) => ({
-      tier: r.branch(
-        row('tier').eq('personal'),
-        'starter',
-        r.branch(row('tier').eq('pro'), 'team', row('tier'))
-      )
-    }))
-    .run()
-
-  await r
-    .table('OrganizationUser')
-    .update((row) => ({
-      tier: r.branch(
-        row('tier').eq('personal'),
-        'starter',
-        r.branch(row('tier').eq('pro'), 'team', row('tier'))
-      ),
-      suggestedTier: r.branch(
-        row('suggestedTier').eq('personal'),
-        'starter',
-        r.branch(row('suggestedTier').eq('pro'), 'team', row('suggestedTier'))
-      )
-    }))
-    .run()
+  const testa = await Promise.all([
+    r
+      .table('Organization')
+      .update((row) => ({
+        tier: r.branch(
+          row('tier').eq('personal'),
+          'starter',
+          r.branch(row('tier').eq('pro'), 'team', row('tier'))
+        )
+      }))
+      .run(),
+    r
+      .table('OrganizationUser')
+      .update((row) => ({
+        tier: r.branch(
+          row('tier').default(null).eq('personal'),
+          'starter',
+          r.branch(row('tier').default(null).eq('pro'), 'team', row('tier').default(null))
+        ),
+        suggestedTier: r.branch(
+          row('suggestedTier').default(null).eq('personal'),
+          'starter',
+          r.branch(
+            row('suggestedTier').default(null).eq('pro'),
+            'team',
+            row('suggestedTier').default(null)
+          )
+        )
+      }))
+      .run(),
+    r
+      .table('Invoice')
+      .update((row) => ({
+        tier: r.branch(
+          row('tier').eq('personal'),
+          'starter',
+          r.branch(row('tier').eq('pro'), 'team', row('tier'))
+        )
+      }))
+      .run(),
+    r
+      .table('User')
+      .update((row) => ({
+        tier: r.branch(
+          row('tier').eq('personal'),
+          'starter',
+          r.branch(row('tier').eq('pro'), 'team', row('tier'))
+        )
+      }))
+      .run()
+  ])
+  console.log('🚀 ~ testa', testa)
 }
 
 export const down = async function (r: R) {
-  await r
-    .table('Organization')
-    .update((row) => ({
-      tier: r.branch(
-        row('tier').eq('starter'),
-        'personal',
-        r.branch(row('tier').eq('team'), 'pro', row('tier'))
-      )
-    }))
-    .run()
-
-  await r
-    .table('OrganizationUser')
-    .update((row) => ({
-      tier: r.branch(
-        row('tier').eq('starter'),
-        'personal',
-        r.branch(row('tier').eq('team'), 'pro', row('tier'))
-      ),
-      suggestedTier: r.branch(
-        row('suggestedTier').eq('starter'),
-        'personal',
-        r.branch(row('suggestedTier').eq('team'), 'pro', row('suggestedTier'))
-      )
-    }))
-    .run()
+  const testDown = await Promise.all([
+    r
+      .table('Organization')
+      .update((row) => ({
+        tier: r.branch(
+          row('tier').eq('starter'),
+          'personal',
+          r.branch(row('tier').eq('team'), 'pro', row('tier'))
+        )
+      }))
+      .run(),
+    r
+      .table('OrganizationUser')
+      .update((row) => ({
+        tier: r.branch(
+          row('tier').eq('starter'),
+          'personal',
+          r.branch(row('tier').eq('team'), 'pro', row('tier'))
+        ),
+        suggestedTier: r.branch(
+          row('suggestedTier').eq('starter'),
+          'personal',
+          r.branch(row('suggestedTier').eq('team'), 'pro', row('suggestedTier').default(null))
+        )
+      }))
+      .run(),
+    r
+      .table('Invoice')
+      .update((row) => ({
+        tier: r.branch(
+          row('tier').eq('starter'),
+          'personal',
+          r.branch(row('tier').eq('team'), 'pro', row('tier'))
+        )
+      }))
+      .run(),
+    r
+      .table('User')
+      .update((row) => ({
+        tier: r.branch(
+          row('tier').eq('starter'),
+          'personal',
+          r.branch(row('tier').eq('team'), 'pro', row('tier'))
+        )
+      }))
+      .run()
+  ])
+  console.log('🚀 ~ testDown', testDown)
 }
