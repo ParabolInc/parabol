bb4ad250a048967ec65690db2de8611e35df896b
Matt Krick <matt.krick@gmail.com>
2022-11-28 11:20:51 -0800

    feat:  oauth2 redirect to cloudflare (#7446)
    * feat:  oauth2 redirect to cloudflare
    
    Signed-off-by: Matt Krick <matt.krick@gmail.com>
    
    * feat: support github, slack
    
    Signed-off-by: Matt Krick <matt.krick@gmail.com>
    
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/types/modules.d.ts
+++ packages/client/types/modules.d.ts
@@ -34,6 +34,7 @@ interface Window {
     sentry: string
     slack: string
     stripe: string
+    oauth2Redirect: string
     prblIn: string
     AUTH_INTERNAL_ENABLED: boolean
     AUTH_GOOGLE_ENABLED: boolean
--- packages/client/utils/SlackClientManager.ts
+++ packages/client/utils/SlackClientManager.ts
@@ -2,14 +2,16 @@ import Atmosphere from '../Atmosphere'
 import {MenuMutationProps} from '../hooks/useMutationProps'
 import AddSlackAuthMutation from '../mutations/AddSlackAuthMutation'
 import getOAuthPopupFeatures from './getOAuthPopupFeatures'
-import makeHref from './makeHref'
 import SlackManager from './SlackManager'
 class SlackClientManager extends SlackManager {
   fetch = window.fetch.bind(window)
   static openOAuth(atmosphere: Atmosphere, teamId: string, mutationProps: MenuMutationProps) {
     const {submitting, onError, onCompleted, submitMutation} = mutationProps
-    const providerState = Math.random().toString(36).substring(5)
-    const redirect = makeHref('/auth/slack')
+    const hash = Math.random().toString(36).substring(5)
+    const providerState = btoa(
+      JSON.stringify({hash, origin: window.location.origin, service: 'slack'})
+    )
+    const redirect = window.__ACTION__.oauth2Redirect
     const uri = `https://slack.com/oauth/v2/authorize?client_id=${window.__ACTION__.slack}&scope=${SlackClientManager.SCOPE}&state=${providerState}&redirect_uri=${redirect}`
     const popup = window.open(
       uri,
