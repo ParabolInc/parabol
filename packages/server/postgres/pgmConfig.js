8cda4eed45776946f71132dfac0fd1ebe47161a2
Matt Krick <matt.krick@gmail.com>
2022-12-02 13:39:18 -0800

    fix: no spread pgm ssl params
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/postgres/pgmConfig.js
+++ packages/server/postgres/pgmConfig.js
@@ -6,11 +6,11 @@ const pgmConfig = {
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   database: process.env.POSTGRES_DB,
-  ...(ssl ?? {}),
+  ssl: ssl ?? undefined,
   tsconfig: 'packages/server/tsconfig.json',
   'migrations-dir': 'packages/server/postgres/migrations',
   'migrations-table': 'PgMigrations',
-  'template-file-name':'packages/server/postgres/migrationTemplate.ts'
+  'template-file-name': 'packages/server/postgres/migrationTemplate.ts'
 }
 
 module.exports = pgmConfig
