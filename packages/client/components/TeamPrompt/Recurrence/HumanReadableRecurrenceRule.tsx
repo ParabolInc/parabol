289eee26e29a24c950db000c0b10083ff74852cc
Bartosz Jarocki <jarocki.bartek@gmail.com>
2022-12-06 17:20:21 +0100

    feat: added human readable recurring meeting indicator

---
+++ packages/client/components/TeamPrompt/Recurrence/HumanReadableRecurrenceRule.tsx
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+import React, {useMemo} from 'react'
+import {RRule} from 'rrule'
+import {PALETTE} from '../../../styles/paletteV3'
+
+const HumanReadableRecurrenceRuleHeading = styled('h2')({
+  fontSize: 14,
+  fontWeight: 400,
+  lineHeight: '14px',
+  margin: `4px 0px 0px 0px`,
+  color: PALETTE.SLATE_700
+})
+
+interface Props {
+  recurrenceRule: string
+}
+
+export const HumanReadableRecurrenceRule = ({recurrenceRule}: Props) => {
+  const humanReadableText = useMemo(() => {
+    return RRule.fromString(recurrenceRule).toText()
+  }, [recurrenceRule])
+
+  return (
+    <HumanReadableRecurrenceRuleHeading>
+      Recurrs {humanReadableText}
+    </HumanReadableRecurrenceRuleHeading>
+  )
+}
