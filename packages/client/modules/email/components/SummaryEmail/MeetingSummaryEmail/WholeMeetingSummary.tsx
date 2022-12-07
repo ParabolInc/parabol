6b36319627e66c7f7812fa7d4a32230c4983b4f4
Nick O'Ferrall <nickoferrall@gmail.com>
2022-12-07 12:46:23 +0000

    add whole meeting ui to meeting summary

---
+++ packages/client/modules/email/components/SummaryEmail/MeetingSummaryEmail/WholeMeetingSummary.tsx
@@ -0,0 +1,65 @@
+import graphql from 'babel-plugin-relay/macro'
+import {PALETTE} from 'parabol-client/styles/paletteV3'
+import {FONT_FAMILY} from 'parabol-client/styles/typographyV2'
+import {WholeMeetingSummary_meeting$key} from 'parabol-client/__generated__/WholeMeetingSummary_meeting.graphql'
+import React from 'react'
+import {useFragment} from 'react-relay'
+import EmailBorderBottom from './EmailBorderBottom'
+
+const topicTitleStyle = {
+  color: PALETTE.SLATE_700,
+  fontFamily: FONT_FAMILY.SANS_SERIF,
+  fontWeight: 600,
+  lineHeight: '22px',
+  fontSize: 14,
+  padding: '8px 48px'
+}
+
+const textStyle = {
+  color: PALETTE.SLATE_700,
+  fontFamily: FONT_FAMILY.SANS_SERIF,
+  padding: '0px 48px 8px 48px',
+  fontSize: 14
+}
+
+interface Props {
+  meetingRef: WholeMeetingSummary_meeting$key
+}
+
+const WholeMeetingSummary = (props: Props) => {
+  const {meetingRef} = props
+  const meeting = useFragment(
+    graphql`
+      fragment WholeMeetingSummary_meeting on RetrospectiveMeeting {
+        facilitator {
+          user {
+            featureFlags {
+              aiSummary
+            }
+          }
+        }
+        summary
+      }
+    `,
+    meetingRef
+  )
+  const {summary, facilitator} = meeting
+  if (!summary || !facilitator.user.featureFlags.aiSummary) return null
+  return (
+    <>
+      <tr>
+        <td align='center' style={{paddingTop: 20}}>
+          <tr>
+            <td style={topicTitleStyle}>{'Meeting Summary'}</td>
+          </tr>
+          <tr>
+            <td style={textStyle}>{summary}</td>
+          </tr>
+        </td>
+      </tr>
+      <EmailBorderBottom />
+    </>
+  )
+}
+
+export default WholeMeetingSummary
