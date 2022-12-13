604ec0c4836993db11d46ce5f715921550d7ada5
Georg Bremer <github@dschoordsch.de>
2022-12-13 10:04:54 +0100

    PR cleanup

--- packages/client/utils/meetings/isPhaseComplete.ts
+++ packages/client/utils/meetings/isPhaseComplete.ts
@@ -4,7 +4,7 @@ const isPhaseComplete = (
   phaseType: NewMeetingPhaseTypeEnum,
   phases: ReadonlyArray<{phaseType: string; stages: ReadonlyArray<{isComplete: boolean}>}>
 ) => {
-  const phase = phases.find((p) => p.phaseType === phaseType)!
+  const phase = phases.find((p) => p.phaseType === phaseType)
   return phase ? phase.stages.every((stage) => stage && stage.isComplete === true) : true
 }
 
