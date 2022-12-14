81514ce07b3792bd111565a3db2d301af4ca358a
Igor Lesnenko <igor.lesnenko@gmail.com>
2022-12-14 18:18:59 +0400

    Move to private

--- packages/server/graphql/private/mutations/addFeatureFlagToOrg.ts
+++ packages/server/graphql/private/mutations/addFeatureFlagToOrg.ts
@@ -1,23 +1,11 @@
 import getRethink from '../../../database/rethinkDriver'
-import {getUserId, isSuperUser} from '../../../utils/authorization'
-import standardError from '../../../utils/standardError'
 import {MutationResolvers} from '../resolverTypes'
 
 const addFeatureFlagToOrg: MutationResolvers['addFeatureFlagToOrg'] = async (
   _source,
-  {orgIds, flag},
-  {authToken}
+  {orgIds, flag}
 ) => {
   const r = await getRethink()
-
-  // AUTH
-  const viewerId = getUserId(authToken)
-
-  if (!isSuperUser(authToken)) {
-    return standardError(new Error('Not authorised to add feature flag'), {
-      userId: viewerId
-    })
-  }
 
   // RESOLUTION
   const updatedOrgIds = (await r
