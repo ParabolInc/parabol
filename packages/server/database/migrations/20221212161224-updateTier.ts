876598e7f02edd783ddf6b75770874733ecd30df
Nick O'Ferrall <nickoferrall@gmail.com>
2022-12-15 11:47:12 +0000

    remove Team from rethink migration

--- packages/server/database/migrations/20221212161224-updateTier.ts
+++ packages/server/database/migrations/20221212161224-updateTier.ts
@@ -52,18 +52,6 @@ export const up = async function (r: R) {
         }))
         .run(),
       r
-        .table('Team')
-        .skip(skip)
-        .limit(BATCH_SIZE)
-        .update((row) => ({
-          tier: r.branch(
-            row('tier').eq('personal'),
-            'starter',
-            r.branch(row('tier').eq('pro'), 'team', row('tier'))
-          )
-        }))
-        .run(),
-      r
         .table('User')
         .skip(skip)
         .limit(BATCH_SIZE)
@@ -130,18 +118,6 @@ export const down = async function (r: R) {
         .run(),
       r
         .table('Invoice')
-        .skip(skip)
-        .limit(BATCH_SIZE)
-        .update((row) => ({
-          tier: r.branch(
-            row('tier').eq('starter'),
-            'personal',
-            r.branch(row('tier').eq('team'), 'pro', row('tier'))
-          )
-        }))
-        .run(),
-      r
-        .table('Team')
         .skip(skip)
         .limit(BATCH_SIZE)
         .update((row) => ({
