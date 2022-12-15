16bb2ec3a586c671459b3d6fc52abe391f20534e
James Taber <jmtaber129@gmail.com>
2022-12-15 16:55:58 -0600

    Email notification

---
+++ packages/client/modules/email/components/EmailNotifications/EmailDiscussionMentioned.tsx
@@ -0,0 +1,95 @@
+import graphql from 'babel-plugin-relay/macro'
+import {convertFromRaw, Editor, EditorState} from 'draft-js'
+import editorDecorators from 'parabol-client/components/TaskEditor/decorators'
+import {EmailDiscussionMentioned_notification$key} from 'parabol-client/__generated__/EmailDiscussionMentioned_notification.graphql'
+import React, {useMemo, useRef} from 'react'
+import {useFragment} from 'react-relay'
+import {cardShadow} from '../../../../styles/elevation'
+import {PALETTE} from '../../../../styles/paletteV3'
+import {FONT_FAMILY} from '../../../../styles/typographyV2'
+import makeAppURL from '../../../../utils/makeAppURL'
+import {notificationSummaryUrlParams} from '../NotificationSummaryEmail'
+import EmailNotificationTemplate from './EmailNotificationTemplate'
+
+const editorStyles = {
+  backgroundColor: '#FFFFFF',
+  borderColor: PALETTE.SLATE_400,
+  borderRadius: '4px',
+  borderStyle: 'solid',
+  borderWidth: '1px',
+  color: PALETTE.SLATE_700,
+  fontFamily: FONT_FAMILY.SANS_SERIF,
+  fontSize: '14px',
+  boxShadow: cardShadow,
+  lineHeight: '20px',
+  margin: '4px 0 0',
+  padding: 8
+}
+
+interface Props {
+  notificationRef: EmailDiscussionMentioned_notification$key
+  appOrigin: string
+}
+
+const EmailDiscussionMentioned = (props: Props) => {
+  const {notificationRef, appOrigin} = props
+  const notification = useFragment(
+    graphql`
+      fragment EmailDiscussionMentioned_notification on NotifyDiscussionMentioned {
+        ...EmailNotificationTemplate_notification
+        author {
+          rasterPicture
+          preferredName
+        }
+        meeting {
+          id
+          name
+        }
+        comment {
+          content
+        }
+      }
+    `,
+    notificationRef
+  )
+  const {meeting, author, comment} = notification
+  const {rasterPicture: authorPicture, preferredName: authorName} = author
+
+  const {id: meetingId, name: meetingName} = meeting
+
+  const linkUrl = makeAppURL(appOrigin, `/meet/${meetingId}`, {
+    searchParams: notificationSummaryUrlParams
+  })
+
+  const contentState = useMemo(() => convertFromRaw(JSON.parse(comment.content)), [comment.content])
+  const editorStateRef = useRef<EditorState>()
+  const getEditorState = () => {
+    return editorStateRef.current
+  }
+  editorStateRef.current = EditorState.createWithContent(
+    contentState,
+    editorDecorators(getEditorState)
+  )
+
+  return (
+    <EmailNotificationTemplate
+      avatar={authorPicture}
+      message={`${authorName} mentioned you in a discussion in ${meetingName}.`}
+      notificationRef={notification}
+      linkLabel={'See the discussion'}
+      linkUrl={linkUrl}
+    >
+      <div style={editorStyles}>
+        <Editor
+          readOnly
+          editorState={editorStateRef.current}
+          onChange={() => {
+            /**/
+          }}
+        />
+      </div>
+    </EmailNotificationTemplate>
+  )
+}
+
+export default EmailDiscussionMentioned
