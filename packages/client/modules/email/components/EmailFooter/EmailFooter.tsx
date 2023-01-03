d4195d0190b419739fcbcf6b2bfda4c59defa5fb
Matt Krick <matt.krick@gmail.com>
2023-01-03 12:36:09 -0800

    fix: emails landing in spam
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/modules/email/components/EmailFooter/EmailFooter.tsx
+++ packages/client/modules/email/components/EmailFooter/EmailFooter.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import {emailCopyStyle, emailTableBase, emailTextColorLight} from '../../styles'
+import {ContactInfo} from '../../../../types/constEnums'
+import {emailCopyStyle, emailTableBase, emailTextColor} from '../../styles'
 import EmptySpace from '../EmptySpace/EmptySpace'
 
 const copyStyle = {
   ...emailCopyStyle,
-  color: emailTextColorLight,
+  color: emailTextColor,
   fontSize: '13px',
   lineHeight: '20px',
   margin: 0,
   textAlign: 'left'
-} as const
-
-const boldCopyStyle = {
-  ...copyStyle,
-  fontWeight: 600
 } as const
 
 const linkStyle = {
@@ -23,23 +19,38 @@ const linkStyle = {
 
 const year = new Date().getFullYear()
 
+const finePrintStyle = {
+  fontSize: 12,
+  lineHeight: '1.5',
+  margin: '16px auto 0'
+}
+
 const EmailFooter = () => {
   return (
     <table align='left' width='100%' style={emailTableBase}>
       <tbody>
         <tr>
           <td>
-            <EmptySpace height={24} />
-            <div style={copyStyle}>
-              {`©${year} Parabol, Inc.`}
-              <br />
-              <span style={boldCopyStyle}>{'Get in touch'}</span>
-              {': '}
-              <a href='mailto:love@parabol.co' title='Get in touch' style={linkStyle}>
-                {'love@parabol.co'}
-              </a>
-            </div>
             <EmptySpace height={8} />
+            <div style={finePrintStyle}>
+              {'Parabol, Inc.'}
+              <br />
+              {'8605 Santa Monica Blvd'}
+              <br />
+              {'West Hollywood, CA 90069-4109'}
+              <br />
+              {'United States'}
+              <br />
+              <a
+                href={`tel:${ContactInfo.TELEPHONE.replace('-', '')}`}
+                title={`Call us: ${ContactInfo.TELEPHONE}`}
+                style={linkStyle}
+              >
+                {ContactInfo.TELEPHONE}
+              </a>
+              <br />
+              {`©${year} Parabol, Inc.`}
+            </div>
           </td>
         </tr>
       </tbody>
