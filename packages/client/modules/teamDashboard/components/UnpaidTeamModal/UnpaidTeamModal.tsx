3b5528c9d8e22a7141d9bc92612441beb8e5090e
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-01-11 18:45:20 +0900

    feat(teams-limit): lock teams if organization is locked

--- packages/client/modules/teamDashboard/components/UnpaidTeamModal/UnpaidTeamModal.tsx
+++ packages/client/modules/teamDashboard/components/UnpaidTeamModal/UnpaidTeamModal.tsx
@@ -9,10 +9,21 @@ import IconLabel from '../../../../components/IconLabel'
 import PrimaryButton from '../../../../components/PrimaryButton'
 import useAtmosphere from '../../../../hooks/useAtmosphere'
 import useRouter from '../../../../hooks/useRouter'
+import {PALETTE} from '../../../../styles/paletteV3'
+import {ExternalLinks, Threshold} from '../../../../types/constEnums'
 import {UnpaidTeamModalQuery} from '../../../../__generated__/UnpaidTeamModalQuery.graphql'
 
 const StyledButton = styled(PrimaryButton)({
   margin: '1.5rem auto 0'
+})
+
+const LockedAtContent = styled(DialogContent)({
+  textAlign: 'left'
+})
+
+const ContactUsLink = styled('a')({
+  color: PALETTE.SKY_500,
+  textDecoration: 'underline'
 })
 
 interface Props {
@@ -26,6 +37,8 @@ const query = graphql`
         lockMessageHTML
         organization {
           id
+          lockedAt
+          name
           billingLeaders {
             id
             preferredName
@@ -54,6 +67,44 @@ const UnpaidTeamModal = (props: Props) => {
   const {team} = viewer
   if (!team) return null
   const {name: teamName, organization, lockMessageHTML} = team
+
+  const {name: organizationName} = organization
+
+  const {id: orgId, billingLeaders, name: orgName} = organization
+  const [firstBillingLeader] = billingLeaders
+  const billingLeaderName = firstBillingLeader?.preferredName ?? 'Unknown'
+  const email = firstBillingLeader?.email ?? 'Unknown'
+  const isALeader = billingLeaders.findIndex((leader) => leader.id === viewerId) !== -1
+  const handleClick = () => history.push(`/me/organizations/${orgId}`)
+
+  if (organization.lockedAt) {
+    return (
+      <DashModal>
+        <DialogTitle>{'Organization Locked'}</DialogTitle>
+        <LockedAtContent>
+          Unfortunately, <strong>{organizationName}</strong> has exceeded the{' '}
+          {Threshold.MAX_PERSONAL_TIER_TEAMS} teams limit on the Starter Plan for more than{' '}
+          {Threshold.PERSONAL_TIER_LOCK_AFTER_DAYS} days, and your account has been locked.
+          <br />
+          <br />
+          You can re-activate your teams by upgrading your account.
+          <br />
+          <br />
+          If you’d like to keep using Parabol with Starter Plan, please{' '}
+          <ContactUsLink href={ExternalLinks.CONTACT} target='_blank' rel='noopener noreferrer'>
+            contact us
+          </ContactUsLink>{' '}
+          to let us know which teams you’d like to delete to fit within the two-team limit.
+          {isALeader && (
+            <StyledButton size='medium' onClick={handleClick}>
+              <IconLabel icon='arrow_forward' iconAfter label='Upgrade' />
+            </StyledButton>
+          )}
+        </LockedAtContent>
+      </DashModal>
+    )
+  }
+
   if (lockMessageHTML) {
     return (
       <DashModal>
@@ -62,12 +113,6 @@ const UnpaidTeamModal = (props: Props) => {
     )
   }
 
-  const {id: orgId, billingLeaders, name: orgName} = organization
-  const [firstBillingLeader] = billingLeaders
-  const billingLeaderName = firstBillingLeader?.preferredName ?? 'Unknown'
-  const email = firstBillingLeader?.email ?? 'Unknown'
-  const isALeader = billingLeaders.findIndex((leader) => leader.id === viewerId) !== -1
-  const handleClick = () => history.push(`/me/organizations/${orgId}`)
   const problem = `There in an unpaid invoice for ${teamName}.`
   const solution = isALeader
     ? `Head over to ${orgName} Settings to add a payment method`
