7871acf5e031fe0e5130f1484e5ebb0cb047c5b6
Nick O'Ferrall <nickoferrall@gmail.com>
2023-01-12 12:54:36 +0000

    check disable anonymity

--- packages/client/components/RetroReflectPhase/RetroReflectPhase.tsx
+++ packages/client/components/RetroReflectPhase/RetroReflectPhase.tsx
@@ -28,8 +28,8 @@ const RetroReflectPhase = (props: Props) => {
   const [activeIdx, setActiveIdx] = useState(0)
   const isDesktop = useBreakpoint(Breakpoint.SINGLE_REFLECTION_COLUMN)
   const {localPhase, endedAt, showSidebar, settings} = meeting
-  const {disableAnonymity} = settings
   if (!localPhase || !localPhase.reflectPrompts) return null
+  const {disableAnonymity} = settings
   const reflectPrompts = localPhase!.reflectPrompts
   const focusedPromptId = localPhase!.focusedPromptId
   const ColumnWrapper = isDesktop ? ReflectWrapperDesktop : ReflectWrapperMobile
