5b7b2b00c0b058bd007a8545f6a14546a75696b7
Nick O'Ferrall <nickoferrall@gmail.com>
2023-01-13 15:06:20 +0000

    add migration to update parabol ai icon

---
+++ packages/server/postgres/migrations/1673613407246_updateAIUserPic.ts
@@ -0,0 +1,24 @@
+import {Client} from 'pg'
+import getPgConfig from '../getPgConfig'
+
+export async function up() {
+  const client = new Client(getPgConfig())
+  await client.connect()
+
+  const picture = 'https://action-files.parabol.co/static/favicon-with-more-padding.jpeg'
+  await client.query(`
+    UPDATE "User" SET "picture" = '${picture}' WHERE "id" = 'parabolAIUser';
+  `)
+  await client.end()
+}
+
+export async function down() {
+  const client = new Client(getPgConfig())
+  await client.connect()
+  const oldPicture = 'https://action-files.parabol.co/static/favicon.ico'
+
+  await client.query(`
+    UPDATE "User" SET "picture" = '${oldPicture}' WHERE "id" = 'parabolAIUser';
+  `)
+  await client.end()
+}
