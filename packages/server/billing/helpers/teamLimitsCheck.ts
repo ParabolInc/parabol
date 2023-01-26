f7668eeca61132f6161d93d819a804d94d48eff4
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-01-27 01:20:55 +0900

    feat(teams-limit): lock organization if limit the is exceeded for 30 days (#7606)
    * feat(teams-limit): Add snackbar to usage stats enabled notification
    
    * feat(teams-limit): lock organization if limit the is exceeded for 30 days
    
    * Add variables for easier testing
    
    * Tweak test data
    
    * Move job processor to teamsLimit file
    
    * Fix
    
    * pass dataloader directly
    
    * Don't  use snack nag if the limit exceeded
    
    * pinned snackbar notifications
    
    * notification type filter
    
    * Invalidate organization dataloader
    
    * console.error
    
    * fix merge

--- packages/server/billing/helpers/teamLimitsCheck.ts
+++ packages/server/billing/helpers/teamLimitsCheck.ts
@@ -3,6 +3,7 @@ import {Threshold} from 'parabol-client/types/constEnums'
 import {r} from 'rethinkdb-ts'
 import {RDatum, RValue} from '../../database/stricterR'
 import NotificationTeamsLimitExceeded from '../../database/types/NotificationTeamsLimitExceeded'
+import ScheduledJobOrganizationLock from '../../database/types/ScheduledJobOrganizationLock'
 import {DataLoaderWorker} from '../../graphql/graphql'
 import isValid from '../../graphql/isValid'
 import publishNotification from '../../graphql/public/mutations/helpers/publishNotification'
@@ -14,7 +15,7 @@ import {appendUserFeatureFlagsQuery} from '../../postgres/queries/generated/appe
 //   MAX_PERSONAL_TIER_TEAMS = 0,
 //   MIN_STICKY_TEAM_MEETING_ATTENDEES = 1,
 //   MIN_STICKY_TEAM_MEETINGS = 1,
-//   PERSONAL_TIER_LOCK_AFTER_DAYS = 1
+//   PERSONAL_TIER_LOCK_AFTER_DAYS = 0
 // }
 
 const getBillingLeaders = async (orgId: string, dataLoader: DataLoaderWorker) => {
@@ -37,6 +38,19 @@ const enableUsageStats = async (userIds: string[], orgId: string) => {
     .run()
 
   await appendUserFeatureFlagsQuery.run({ids: userIds, flag: 'insights'}, getPg())
+}
+
+const scheduleJobs = async (scheduledLockAt: Date, orgId: string) => {
+  const scheduledLock = r
+    .table('ScheduledJob')
+    .insert(new ScheduledJobOrganizationLock(scheduledLockAt, orgId))
+    .run()
+
+  // TODO: implement additional reminders
+  // const scheduleFirstReminder
+  // const scheduleSecondReminder
+
+  await Promise.all([scheduledLock])
 }
 
 const sendWebsiteNotifications = async (
@@ -119,6 +133,7 @@ export const maybeRemoveRestrictions = async (orgId: string, dataLoader: DataLoa
         updatedAt: new Date()
       })
       .run()
+    dataLoader.get('organizations').clear(orgId)
   }
 }
 
@@ -140,16 +155,20 @@ export const checkTeamsLimit = async (orgId: string, dataLoader: DataLoaderWorke
 
   const now = new Date()
 
-  // Schedule lock
+  const scheduledLockAt = new Date(
+    now.getTime() + ms(`${Threshold.PERSONAL_TIER_LOCK_AFTER_DAYS}d`)
+  )
+
   await r
     .table('Organization')
     .get(orgId)
     .update({
       tierLimitExceededAt: now,
-      scheduledLockAt: new Date(now.getTime() + ms(`${Threshold.PERSONAL_TIER_LOCK_AFTER_DAYS}d`)),
+      scheduledLockAt,
       updatedAt: now
     })
     .run()
+  dataLoader.get('organizations').clear(orgId)
 
   const billingLeaders = await getBillingLeaders(orgId, dataLoader)
   const billingLeadersIds = billingLeaders.map((billingLeader) => billingLeader.id)
@@ -158,4 +177,34 @@ export const checkTeamsLimit = async (orgId: string, dataLoader: DataLoaderWorke
     await enableUsageStats(billingLeadersIds, orgId)
     await sendWebsiteNotifications(orgId, billingLeadersIds, dataLoader)
   }
+
+  await scheduleJobs(scheduledLockAt, orgId)
+}
+
+export const processLockOrganizationJob = async (
+  job: ScheduledJobOrganizationLock,
+  dataLoader: DataLoaderWorker
+) => {
+  const {orgId, runAt} = job
+
+  const organization = await dataLoader.get('organizations').load(orgId)
+
+  // Skip the job if unlocked or already locked or scheduled lock date changed
+  if (
+    !organization.scheduledLockAt ||
+    organization.lockedAt ||
+    organization.scheduledLockAt.getTime() !== runAt.getTime()
+  ) {
+    return
+  }
+
+  const now = new Date()
+
+  return r
+    .table('Organization')
+    .get(orgId)
+    .update({
+      lockedAt: now
+    })
+    .run()
 }
