df8c7c29126eba4fe7dd3ca4b2ff84e549a6bd45
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-01-27 14:27:29 +0900

    Remove args type

--- packages/server/graphql/public/types/NotifyTeamsLimitExceeded.ts
+++ packages/server/graphql/public/types/NotifyTeamsLimitExceeded.ts
@@ -2,7 +2,7 @@ import {NotifyTeamsLimitExceededResolvers} from '../resolverTypes'
 
 const NotifyTeamsLimitExceeded: NotifyTeamsLimitExceededResolvers = {
   __isTypeOf: ({type}) => type === 'TEAMS_LIMIT_EXCEEDED',
-  organization: ({orgId}, _args: unknown, {dataLoader}) => {
+  organization: ({orgId}, _args, {dataLoader}) => {
     return dataLoader.get('organizations').load(orgId)
   }
 }
--- packages/server/graphql/public/types/NotifyTeamsLimitReminder.ts
+++ packages/server/graphql/public/types/NotifyTeamsLimitReminder.ts
@@ -2,7 +2,7 @@ import {NotifyTeamsLimitReminderResolvers} from '../resolverTypes'
 
 const NotifyTeamsLimitReminder: NotifyTeamsLimitReminderResolvers = {
   __isTypeOf: ({type}) => type === 'TEAMS_LIMIT_REMINDER',
-  organization: ({orgId}, _args: unknown, {dataLoader}) => {
+  organization: ({orgId}, _args, {dataLoader}) => {
     return dataLoader.get('organizations').load(orgId)
   }
 }
