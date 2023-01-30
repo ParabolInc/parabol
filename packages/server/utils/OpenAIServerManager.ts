7885bcb897d86503110a764002d1ca94fb8ec1d1
Nick O'Ferrall <nickoferrall@gmail.com>
2023-01-30 22:49:58 +0000

    feat(ai-summary): add summary to end meeting notification  (#7659)
    * add ai summary to slack blocks
    
    * remove line breaks from ai summary in slack notification
    
    * add to mattermost and msTeams
    
    * use trim
    
    * trim summary in OpenAIServerManager

--- packages/server/utils/OpenAIServerManager.ts
+++ packages/server/utils/OpenAIServerManager.ts
@@ -32,7 +32,7 @@ class OpenAIServerManager {
         frequency_penalty: 0,
         presence_penalty: 0
       })
-      return (response.data.choices[0]?.text as string) ?? null
+      return (response.data.choices[0]?.text?.trim() as string) ?? null
     } catch (e) {
       const error = e instanceof Error ? e : new Error('OpenAI failed to getSummary')
       sendToSentry(error)
