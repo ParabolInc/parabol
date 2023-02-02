34b7c0d788ac8433c6da53ee5c0328014ccb7034
Nick O'Ferrall <nickoferrall@gmail.com>
2023-02-02 12:12:29 -0600

    second commit

--- packages/server/graphql/types/UserFlagEnum.ts
+++ packages/server/graphql/types/UserFlagEnum.ts
@@ -1,6 +1,7 @@
 import {GraphQLEnumType} from 'graphql'
 
 export type UserFeatureFlagEnum = 'azureDevOps' | 'msTeams'
+
 const UserFlagEnum = new GraphQLEnumType({
   name: 'UserFlagEnum',
   description: 'A flag to give an individual user super powers',
