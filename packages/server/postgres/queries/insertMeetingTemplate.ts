f63bad88a2ac768647d00abd8e848667ae0016c3
Matt Krick <matt.krick@gmail.com>
2023-02-07 14:15:07 -0800

    fix: log rejected pg insert
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/postgres/queries/insertMeetingTemplate.ts
+++ packages/server/postgres/queries/insertMeetingTemplate.ts
@@ -6,13 +6,14 @@ const insertMeetingTemplate = async (meetingTemplate: MeetingTemplate) => {
   const r = await getRethink()
   const pg = getPg()
   const {id, name, teamId, orgId, parentTemplateId, type} = meetingTemplate
-  const [rRes] = await Promise.allSettled([
+  const [rRes, pgRes] = await Promise.allSettled([
     r.table('MeetingTemplate').insert(meetingTemplate).run(),
     pg.query(
       `INSERT INTO "MeetingTemplate" (id, name, "teamId", "orgId", "parentTemplateId", type) VALUES ($1, $2, $3, $4, $5, $6)`,
       [id, name, teamId, orgId, parentTemplateId, type]
     )
   ])
+  if (pgRes.status === 'rejected') console.log(pgRes.reason)
   if (rRes.status === 'rejected') throw rRes.reason
 }
 
