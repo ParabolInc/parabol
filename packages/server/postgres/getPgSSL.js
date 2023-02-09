b717f1b14e2427ac874663b04f37db003af4bbb8
Bayu <bayu.prasetyo@pinhome.id>
2023-02-09 18:29:07 +0700

    chore: if there is no SSL env then ignore postgre ssl

--- packages/server/postgres/getPgSSL.js
+++ packages/server/postgres/getPgSSL.js
@@ -5,6 +5,8 @@ const getSSL = () => {
   try {
     // optional env var, likely outside the app dir
     const POSTGRES_SSL_DIR = process.env.POSTGRES_SSL_DIR
+    if (!POSTGRES_SSL_DIR) return undefined
+
     const ca = readFileSync(path.join(POSTGRES_SSL_DIR, 'root.crt'))
     const key = readFileSync(path.join(POSTGRES_SSL_DIR, 'postgresql.key'))
     const cert = readFileSync(path.join(POSTGRES_SSL_DIR, 'postgresql.crt'))
