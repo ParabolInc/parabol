768a8b8f61a8d11ce8eef3f49bcbd4ff1af2a6c6
Bayu <bayu.prasetyo@pinhome.id>
2023-02-09 21:15:41 +0700

    chore: try to using db url instead

--- packages/server/postgres/pgtypedConfig.js
+++ packages/server/postgres/pgtypedConfig.js
@@ -3,6 +3,8 @@ const ssl = require('./getPgSSL')()
 
 const emitTemplateTarget = 'packages/server/postgres/queries/generated/{{name}}.ts'
 
+console.log('===process.env.POSTGRES_URL', process.env.POSTGRES_URL)
+
 const pgtypedConfig = {
   transforms: [
     {
@@ -18,15 +20,7 @@ const pgtypedConfig = {
   ],
   srcDir: 'packages/server/postgres/queries/src',
   camelCaseColumnNames: false,
-  db: {
-    dbName: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    ssl: ssl ?? {
-      rejectUnauthorized: false
-    }
-  }
+  dbUrl: process.env.POSTGRES_URL
 }
+
 module.exports = pgtypedConfig
