53350285258c1f2fc49f4357b8e287e9254a12f0
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-02-10 16:02:09 +0700

    use connectRethinkDB in migration

--- packages/server/postgres/migrations/20230210004337-ScheduledJobOrgIdIndex.ts
+++ packages/server/postgres/migrations/20230210004337-ScheduledJobOrgIdIndex.ts
@@ -1,8 +1,24 @@
-import {R} from 'rethinkdb-ts'
+import {r, R} from 'rethinkdb-ts'
+import {ParabolR} from '../../database/rethinkDriver'
+
+const connectRethinkDB = async () => {
+  const {hostname: host, port, pathname} = new URL(process.env.RETHINKDB_URL!)
+  await r.connectPool({
+    host,
+    port: parseInt(port, 10),
+    db: pathname.split('/')[1]
+  })
+  return r as any as ParabolR
+}
+
 export const up = async function (r: R) {
+  await connectRethinkDB()
   await r.table('ScheduledJob').indexCreate('orgId').run()
+  await r.getPoolMaster()?.drain()
 }
 
 export const down = async function (r: R) {
+  await connectRethinkDB()
   await r.table('ScheduledJob').indexDrop('orgId').run()
+  await r.getPoolMaster()?.drain()
 }
