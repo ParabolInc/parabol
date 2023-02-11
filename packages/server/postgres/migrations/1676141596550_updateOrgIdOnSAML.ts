8b98b7bbd0b0d24270d03eb1057e633d532d8beb
gitstart-parabol <parabol@gitstart.com>
2023-02-11 21:10:38 +0000

    update: resolve failing gitactions

--- packages/server/postgres/migrations/1676141596550_updateOrgIdOnSAML.ts
+++ packages/server/postgres/migrations/1676141596550_updateOrgIdOnSAML.ts
@@ -1,4 +1,5 @@
 import {r} from 'rethinkdb-ts'
+import {ParabolR} from '../../database/rethinkDriver'
 
 const connectRethinkDB = async () => {
   const {hostname: host, port, pathname} = new URL(process.env.RETHINKDB_URL!)
@@ -7,18 +8,20 @@ const connectRethinkDB = async () => {
     port: parseInt(port, 10),
     db: pathname.split('/')[1]
   })
+  return r as unknown as ParabolR
 }
 
 export async function up() {
   await connectRethinkDB()
   await r
     .table('SAML')
-    .update((row) => ({
+    .update((row: any) => ({
       orgId: row('orgId').default(null)
     }))
     .run()
 
   await r.table('SAML').indexCreate('orgId').run()
+  await r.getPoolMaster()?.drain()
 }
 
 export async function down() {
