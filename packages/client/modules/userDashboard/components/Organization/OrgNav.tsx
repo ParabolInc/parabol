6f3897cf4c1d5745913b39aad6770a7d6404e8e6
Nick O'Ferrall <nickoferrall@gmail.com>
2023-02-15 15:55:38 +0000

    make nav clickable

--- packages/client/modules/userDashboard/components/Organization/OrgNav.tsx
+++ packages/client/modules/userDashboard/components/Organization/OrgNav.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 import {NavigateNext} from '@mui/icons-material'
 import React from 'react'
+import {useHistory} from 'react-router'
 import {PALETTE} from '../../../../styles/paletteV3'
 import {ICON_SIZE} from '../../../../styles/typographyV2'
 
@@ -9,35 +10,41 @@ const Wrapper = styled('div')({
   display: 'flex',
   flexShrink: 0,
   fontSize: 13,
-  // justifyContent: 'center',
   lineHeight: '20px',
-  border: '2px solid red',
+  padding: '16px 0px',
   width: '100%'
 })
 
 const StyledIcon = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
   height: ICON_SIZE.MD18,
   color: PALETTE.SLATE_700,
   opacity: 0.5
 })
 
-const NavItemLabel = styled('span')({
+const NavItemLabel = styled('span')<{isBold?: boolean}>(({isBold}) => ({
   display: 'inline-block',
-  verticalAlign: 'middle'
-})
+  verticalAlign: 'middle',
+  fontWeight: isBold ? 600 : 400,
+  '&:hover': {
+    cursor: 'pointer'
+  }
+}))
 
 const OrgNav = () => {
+  const history = useHistory()
   return (
     <Wrapper>
-      <NavItemLabel>Dashboard</NavItemLabel>
+      <NavItemLabel onClick={() => history.push('/meetings')}>Dashboard</NavItemLabel>
       <StyledIcon>
         <NavigateNext />
       </StyledIcon>
-      <NavItemLabel>Organization</NavItemLabel>
+      <NavItemLabel onClick={() => history.push('/me/organizations')}>Organization</NavItemLabel>
       <StyledIcon>
         <NavigateNext />
       </StyledIcon>
-      <NavItemLabel>{`Hardcoded's Org`}</NavItemLabel>
+      <NavItemLabel isBold>{`Hardcoded's Org`}</NavItemLabel>
     </Wrapper>
   )
 }
