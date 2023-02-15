d5fa5337dcf5d08fa02c035c0cfed7f92448c066
Nick O'Ferrall <nickoferrall@gmail.com>
2023-02-15 18:18:08 +0000

    add plan details icon

--- packages/client/modules/userDashboard/components/OrgBilling/OrgPlansAndBillingHeading.tsx
+++ packages/client/modules/userDashboard/components/OrgBilling/OrgPlansAndBillingHeading.tsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled'
+import {ExpandMore} from '@mui/icons-material'
 import graphql from 'babel-plugin-relay/macro'
 import React from 'react'
 import {useFragment} from 'react-relay'
+import PlainButton from '../../../../components/PlainButton/PlainButton'
 import {PALETTE} from '../../../../styles/paletteV3'
 import {upperFirst} from '../../../../utils/upperFirst'
 import {OrgPlansAndBillingHeading_organization$key} from '../../../../__generated__/OrgPlansAndBillingHeading_organization.graphql'
@@ -29,7 +31,9 @@ const Wrapper = styled('div')({
   fontWeight: 600,
   lineHeight: '24px',
   padding: '16px 0px',
-  flexWrap: 'wrap'
+  flexWrap: 'wrap',
+  maxWidth: 800,
+  position: 'relative'
 })
 
 const SubtitleBlock = styled('div')({
@@ -38,6 +42,38 @@ const SubtitleBlock = styled('div')({
   borderRadius: 2,
   lineHeight: '26px',
   fontWeight: 500
+})
+
+const StyledIcon = styled(ExpandMore)({
+  color: PALETTE.SKY_500,
+  height: 20,
+  width: 20,
+  padding: 0,
+  alignContent: 'center'
+})
+
+const StyledButton = styled(PlainButton)({
+  alignItems: 'center',
+  backgroundColor: 'transparent',
+  display: 'flex',
+  flexDirection: 'column',
+  height: 'fit-content',
+  justifyContent: 'center',
+  margin: 0,
+  padding: 0,
+  position: 'absolute',
+  right: 0,
+  opacity: 1,
+  width: 'fit-content'
+})
+
+const Label = styled.span({
+  display: 'block',
+  fontWeight: 600,
+  fontSize: 12,
+  lineHeight: '20px',
+  textDecoration: 'none',
+  textAlign: 'center'
 })
 
 type Props = {
@@ -65,6 +101,10 @@ const OrgPlansAndBillingHeading = (props: Props) => {
         <Subtitle>{`is currently on the `}</Subtitle>
         <Subtitle isBold>{`${tierName} Plan.`}</Subtitle>
       </SubtitleBlock>
+      <StyledButton>
+        <StyledIcon />
+        <Label>{'Plan Details'}</Label>
+      </StyledButton>
     </Wrapper>
   )
 }
