60ed3c93c5f1cb7a12824953fe7126fb8631f05c
Nick O'Ferrall <nickoferrall@gmail.com>
2023-02-28 18:04:36 +0000

    feat(checkout-flow): plans UI (#7793)
    * set-up org billing and plans skeleton
    
    * start org nav
    
    * make nav clickable
    
    * pass org ref down and make name dynamic
    
    * add types
    
    * create plans and billing heading
    
    * add hardcoded stats
    
    * add plan details icon
    
    * add starter panel
    
    * add team and enterprise
    
    * fix left panel padding and button positioning
    
    * highlight plans
    
    * add highlighting colors on hover
    
    * add info icon
    
    * improve plans ui
    
    * fix up button styles
    
    * improve hovering ui
    
    * remove unnecessary css in heading and nav
    
    * remove css in plans
    
    * map through plans and stats
    
    * update list styles
    
    * update nav icon styles
    
    * update feature flags following merge conflict
    
    * add tooltip to org stats and move it to its own file
    
    * fix plans underline width
    
    * reduce font size
    
    * increase stats block font
    
    * add active user tooltip
    
    * move ElementWidth.PANEL_WIDTH to constEnums
    
    * change isBold to isCurrent and remove org from title
    
    * on wide screens, center everything
    
    * add routing and fix width of members page
    
    * move everything back to the left to the left

---
+++ packages/client/modules/userDashboard/components/OrgBilling/OrgStats.tsx
@@ -0,0 +1,117 @@
+import styled from '@emotion/styled'
+import {Info} from '@mui/icons-material'
+import React from 'react'
+import {tooltipTextLookup} from '../../../../components/InsightsDomainPanel'
+import {MenuPosition} from '../../../../hooks/useCoords'
+import useTooltip from '../../../../hooks/useTooltip'
+import {PALETTE} from '../../../../styles/paletteV3'
+
+const StatBlocks = styled('div')({
+  display: 'flex',
+  width: '100%',
+  padding: '8px 0px'
+})
+
+const StatBlock = styled('div')({
+  borderLeft: `1px solid ${PALETTE.SLATE_400}`,
+  ':first-of-type': {
+    borderLeft: 'none'
+  },
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '33.33%',
+  padding: '14px 0px'
+})
+
+const StatBlockNumber = styled('div')({
+  color: PALETTE.SLATE_600,
+  fontSize: 48,
+  lineHeight: '72px'
+})
+
+const StatBlockLabel = styled('div')({
+  color: PALETTE.SLATE_800,
+  fontSize: 14,
+  fontWeight: 600,
+  lineHeight: '16px',
+  textTransform: 'capitalize',
+  display: 'flex',
+  alignItems: 'center'
+})
+
+const StyledIcon = styled('div')({
+  width: 18,
+  height: 18,
+  color: PALETTE.SLATE_600,
+  paddingLeft: 8,
+  display: 'flex',
+  alignItems: 'center',
+  '&:hover': {
+    cursor: 'pointer'
+  }
+})
+
+const IconBlock = styled('div')({
+  display: 'flex'
+})
+
+const OrgStats = () => {
+  const {
+    tooltipPortal: teamsPortal,
+    openTooltip: teamsOpenTooltip,
+    closeTooltip: teamsCloseTooltip,
+    originRef: teamsRef
+  } = useTooltip<HTMLDivElement>(MenuPosition.LOWER_CENTER)
+
+  const {
+    tooltipPortal: membersPortal,
+    openTooltip: membersOpenTooltip,
+    closeTooltip: membersCloseTooltip,
+    originRef: membersRef
+  } = useTooltip<HTMLDivElement>(MenuPosition.LOWER_CENTER)
+
+  return (
+    <StatBlocks>
+      <StatBlock>
+        <StatBlockNumber>
+          {'18'}
+          <IconBlock ref={teamsRef}></IconBlock>
+        </StatBlockNumber>
+        <StatBlockLabel>
+          {'Active Teams'}
+          <StyledIcon ref={teamsRef} onMouseOver={teamsOpenTooltip} onMouseOut={teamsCloseTooltip}>
+            {<Info />}
+          </StyledIcon>
+        </StatBlockLabel>
+        {teamsPortal(tooltipTextLookup.team)}
+      </StatBlock>
+      <StatBlock>
+        <StatBlockNumber>
+          {'18'}
+          <IconBlock></IconBlock>
+        </StatBlockNumber>
+        <StatBlockLabel>
+          {'Active Members'}
+          <StyledIcon
+            ref={membersRef}
+            onMouseOver={membersOpenTooltip}
+            onMouseOut={membersCloseTooltip}
+          >
+            {<Info />}
+          </StyledIcon>
+        </StatBlockLabel>
+        {membersPortal(tooltipTextLookup.member)}
+      </StatBlock>
+      <StatBlock>
+        <StatBlockNumber>
+          {'18'}
+          <IconBlock></IconBlock>
+        </StatBlockNumber>
+        <StatBlockLabel>{'Total Meetings'}</StatBlockLabel>
+      </StatBlock>
+    </StatBlocks>
+  )
+}
+
+export default OrgStats
