f891af0d0499e93fbb3cbf134e4c4f5addd4a394
James Taber <jmtaber129@gmail.com>
2023-03-01 17:20:52 +0100

    chore(relay): Fix convertToUseFragment bug

--- scripts/codeshift/convertToUseFragment.ts
+++ scripts/codeshift/convertToUseFragment.ts
@@ -52,7 +52,10 @@ const transform: Transform = (fileInfo, api, options) => {
   const fragmentProps = functionExpression.arguments[1]
   const propMap = {}
   fragmentProps.properties.forEach((prop) => {
-    if (root.find(j.Identifier, {name: `${prop.key.name}Ref`}).length > 0) {
+    const conflicts = root
+      .find(j.Identifier, {name: `${prop.key.name}Ref`})
+      .filter((path) => j.JSXAttribute.check(path.parent))
+    if (conflicts.length > 0) {
       throw new Error(`Naming conflict with ${prop.key.name}Ref`)
     }
     propMap[prop.key.name] = prop.value
