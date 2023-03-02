67f5ff0dfb0f673dc883ea8af95115ca7a3321bd
Nick O'Ferrall <nickoferrall@gmail.com>
2023-03-02 13:31:13 +0000

    customise styles

--- packages/client/modules/userDashboard/components/OrgBilling/Billing.tsx
+++ packages/client/modules/userDashboard/components/OrgBilling/Billing.tsx
@@ -1,12 +1,8 @@
 import styled from '@emotion/styled'
-import {Error as ErrorIcon} from '@mui/icons-material'
-import useScript from '../../../../hooks/useScript'
 import {Divider} from '@mui/material'
-import Cleave from 'cleave.js/react'
 import React, {useEffect, useState} from 'react'
 import BillingForm from './BillingForm'
 import Panel from '../../../../components/Panel/Panel'
-import PrimaryButton from '../../../../components/PrimaryButton'
 import Row from '../../../../components/Row/Row'
 import useForm from '../../../../hooks/useForm'
 import {PALETTE} from '../../../../styles/paletteV3'
@@ -52,25 +48,6 @@ const HeadingBlock = styled('div')({
   padding: '16px 16px 0px 16px'
 })
 
-const LineIcon = styled('div')({
-  svg: {
-    fontSize: 19
-  },
-  display: 'flex'
-})
-
-const UpgradeButton = styled(PrimaryButton)<{isDisabled: boolean}>(({isDisabled}) => ({
-  background: isDisabled ? PALETTE.SLATE_200 : PALETTE.SKY_500,
-  color: isDisabled ? PALETTE.SLATE_600 : PALETTE.WHITE,
-  boxShadow: 'none',
-  marginTop: 16,
-  elevation: 0,
-  '&:hover': {
-    boxShadow: 'none',
-    background: isDisabled ? PALETTE.SLATE_200 : PALETTE.SKY_600
-  }
-}))
-
 const Title = styled('div')({
   color: PALETTE.SLATE_800,
   fontSize: 22,
@@ -96,40 +73,6 @@ const Content = styled('div')({
   width: '100%'
 })
 
-const Form = styled('form')({
-  display: 'flex',
-  flexDirection: 'column',
-  width: '100%'
-})
-
-const StyledInput = styled('input')({
-  background: PALETTE.SLATE_200,
-  border: 'none',
-  borderBottom: `1px solid ${PALETTE.SLATE_400}`,
-  color: PALETTE.SLATE_800,
-  fontSize: 16,
-  marginBottom: 16,
-  padding: '12px 16px',
-  outline: 0,
-  '::placeholder': {
-    color: PALETTE.SLATE_600
-  }
-})
-
-const CleaveInput = styled(Cleave)({
-  background: PALETTE.SLATE_200,
-  border: 'none',
-  borderBottom: `1px solid ${PALETTE.SLATE_400}`,
-  color: PALETTE.SLATE_800,
-  fontSize: 16,
-  marginBottom: 16,
-  padding: '12px 16px',
-  outline: 0,
-  '::placeholder': {
-    color: PALETTE.SLATE_600
-  }
-})
-
 const InputLabel = styled('span')({
   display: 'block',
   fontSize: 12,
@@ -150,19 +93,6 @@ const InfoText = styled('span')({
   textTransform: 'none'
 })
 
-const InputWrapper = styled('div')({
-  display: 'flex',
-  width: '100%',
-  flexWrap: 'nowrap',
-  justifyContent: 'space-between'
-})
-
-const InputBlock = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  maxWidth: '47%'
-})
-
 const TotalBlock = styled('div')({
   display: 'flex',
   justifyContent: 'space-between',
@@ -171,18 +101,6 @@ const TotalBlock = styled('div')({
 
 const ActiveUserBlock = styled('div')({
   paddingTop: 16
-})
-
-const Error = styled('div')<{isError: boolean}>(({isError}) => ({
-  alignItems: 'center',
-  color: isError ? PALETTE.TOMATO_500 : PALETTE.SLATE_600,
-  display: isError ? 'flex' : 'none',
-  lineHeight: '24px'
-}))
-
-const Message = styled('div')({
-  fontSize: 15,
-  paddingLeft: 4
 })
 
 const stripePromise = loadStripe('pk_test_MNoKbCzQX0lhktuxxI7M14wd')
@@ -216,8 +134,6 @@ const Billing = () => {
   const atmosphere = useAtmosphere()
   const {onError, onCompleted} = useMutationProps()
 
-  const isStripeLoaded = useScript('https://js.stripe.com/v2/')
-
   useEffect(() => {
     const myOnComplete = (res, errors) => {
       if (errors) {
@@ -232,10 +148,20 @@ const Billing = () => {
   }, [])
 
   const appearance = {
-    theme: 'stripe'
+    theme: 'stripe',
+
+    variables: {
+      colorBackground: PALETTE.SLATE_200,
+      border: 'none',
+      borderBottom: `1px solid ${PALETTE.SLATE_400}`,
+      color: PALETTE.SLATE_800,
+      fontSize: 16,
+      marginBottom: 16,
+      padding: '12px 16px',
+      outline: 0
+    }
   } as const
   const options = {
-    // TODO: add this
     clientSecret,
     appearance,
     fonts: [
@@ -285,77 +211,6 @@ const Billing = () => {
             <Elements options={options} stripe={stripePromise}>
               <BillingForm />
             </Elements>
-
-            {/* <Form>
-              <InputLabel>{'Cardholder Name'}</InputLabel>
-              <StyledInput
-                autoFocus
-                maxLength={100}
-                placeholder='Full Name'
-                onBlur={() => setDirtyField('cardName')}
-                onChange={onChange}
-                id='cardholder-name'
-                name='cardName'
-                type='text'
-                autoComplete='cc-name'
-              />
-              <InputLabel>{'Card Number'}</InputLabel>
-              <CleaveInput
-                options={{creditCard: true}}
-                autoComplete='cc-number'
-                id='card-number'
-                placeholder='1234 5678 8765 4321'
-                name={'cardNumber'}
-                onBlur={() => setDirtyField('cardNumber')}
-                onChange={onChange}
-                type='text'
-              />
-              <InputWrapper>
-                <InputBlock>
-                  <InputLabel>{'Expiry Date'}</InputLabel>
-                  <CleaveInput
-                    maxLength={5}
-                    placeholder='MM/YY'
-                    options={{date: true, datePattern: ['m', 'd']}}
-                    onChange={onChange}
-                    onBlur={() => setDirtyField('expiry')}
-                    id='expiry'
-                    name={'expiry'}
-                    type='text'
-                    autoComplete='cc-exp'
-                  />
-                </InputBlock>
-                <InputBlock>
-                  <InputLabel>{'CVV'}</InputLabel>
-                  <CleaveInput
-                    maxLength={5}
-                    placeholder='123'
-                    onBlur={() => setDirtyField('cvc')}
-                    onChange={onChange}
-                    id='cvv'
-                    options={{
-                      blocks: [4],
-                      numericOnly: true
-                    }}
-                    name={'cvc'}
-                    type='text'
-                    autoComplete='cc-csc'
-                  />
-                </InputBlock>
-              </InputWrapper>
-              <Error isError={!!error}>
-                <LineIcon>{error && <ErrorIcon />}</LineIcon>
-                <Message>{error}</Message>
-              </Error>
-              <UpgradeButton
-                size='medium'
-                // onClick={handleSubmit}
-                isDisabled={!canSubmit}
-                type={'submit'}
-              >
-                {'Upgrade'}
-              </UpgradeButton>
-            </Form> */}
           </Content>
         </Plan>
         <Plan>
--- packages/client/modules/userDashboard/components/OrgBilling/BillingForm.tsx
+++ packages/client/modules/userDashboard/components/OrgBilling/BillingForm.tsx
@@ -1,11 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import styled from '@emotion/styled'
-import {
-  PaymentElement,
-  LinkAuthenticationElement,
-  useStripe,
-  useElements
-} from '@stripe/react-stripe-js'
+import {PaymentElement, useStripe, useElements} from '@stripe/react-stripe-js'
 import PrimaryButton from '../../../../components/PrimaryButton'
 import {PALETTE} from '../../../../styles/paletteV3'
 
@@ -28,93 +23,22 @@ const UpgradeButton = styled(PrimaryButton)<{isDisabled: boolean}>(({isDisabled}
   }
 }))
 
-type Props = {}
-
-export default function CheckoutForm(props: Props) {
+export default function BillingForm() {
   const stripe = useStripe()
   const elements = useElements()
 
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState(null)
+  // const [email, setEmail] = useState('')
+  // const [message, setMessage] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
 
-  // useEffect(() => {
-  //   if (!stripe) {
-  //     return
-  //   }
-
-  //   if (!clientSecret) {
-  //     return
-  //   }
-
-  //   const checkIntent = async () => {
-  //     const testa = await stripe.retrievePaymentIntent(clientSecret)
-  //     console.log('🚀 ~ testa:', testa)
-  //   }
-
-  //   checkIntent()
-
-  //   stripe.retrievePaymentIntent(clientSecret).then(({paymentIntent}) => {
-  //     switch (paymentIntent?.status) {
-  //       case 'succeeded':
-  //         setMessage('Payment succeeded!')
-  //         break
-  //       case 'processing':
-  //         setMessage('Your payment is processing.')
-  //         break
-  //       case 'requires_payment_method':
-  //         setMessage('Your payment was not successful, please try again.')
-  //         break
-  //       default:
-  //         setMessage('Something went wrong.')
-  //         break
-  //     }
-  //   })
-  // }, [stripe])
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return
-    }
-
-    setIsLoading(true)
-
-    const {error} = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        // Make sure to change this to your payment completion page
-        return_url: 'http://localhost:3000'
-      }
-    })
-
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
-    if (error.type === 'card_error' || error.type === 'validation_error') {
-      // setMessage(error.message)
-    } else {
-      // setMessage('An unexpected error occurred.')
-    }
-
+  // TODO: implement in: https://github.com/ParabolInc/parabol/issues/7693
+  // look at: https://stripe.com/docs/payments/quickstart
+  const handleSubmit = async () => {
     setIsLoading(false)
-  }
-
-  const paymentElementOptions = {
-    layout: 'tabs'
   }
 
   return (
     <form id='payment-form' onSubmit={handleSubmit}>
-      {/* <LinkAuthenticationElement
-        id='link-authentication-element'
-        // onChange={(e) => setEmail(e.target.value)}
-      /> */}
       <PaymentElement
         id='payment-element'
         options={{
@@ -126,23 +50,11 @@ export default function CheckoutForm(props: Props) {
           }
         }}
       />
-      {/* <button disabled={isLoading || !stripe || !elements} id='submit'>
-        <span id='button-text'>
-          {isLoading ? <div className='spinner' id='spinner'></div> : 'Pay now'}
-        </span>
-      </button> */}
       <ButtonBlock>
-        <UpgradeButton
-          size='medium'
-          // onClick={handleSubmit}
-          isDisabled={isLoading || !stripe || !elements}
-          type={'submit'}
-        >
+        <UpgradeButton size='medium' isDisabled={isLoading || !stripe || !elements} type={'submit'}>
           {'Upgrade'}
         </UpgradeButton>
       </ButtonBlock>
-      {/* Show any error or success messages */}
-      {message && <div id='payment-message'>{message}</div>}
     </form>
   )
 }
