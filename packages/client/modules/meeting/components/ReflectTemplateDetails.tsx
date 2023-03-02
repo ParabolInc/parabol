9efb2862fe1d4f6d6dbce4eba9a53bc2fcb505b6
Georg Bremer <github@dschoordsch.de>
2023-03-02 11:33:35 +0100

    feat: Add additional retrospective template illustrations

--- packages/client/modules/meeting/components/ReflectTemplateDetails.tsx
+++ packages/client/modules/meeting/components/ReflectTemplateDetails.tsx
@@ -16,6 +16,38 @@ import threeLittlePigsTemplate from '../../../../../static/images/illustrations/
 import whatWentWellTemplate from '../../../../../static/images/illustrations/whatWentWellTemplate.png'
 import winningStreakTemplate from '../../../../../static/images/illustrations/winningStreakTemplate.png'
 import workingStuckTemplate from '../../../../../static/images/illustrations/workingStuckTemplate.png'
+import easterRetrospectiveTemplate from '../../../../../static/images/illustrations/easterRetrospectiveTemplate.png'
+import heardSeenRespectedHSRTemplate from '../../../../../static/images/illustrations/heardSeenRespectedHSRTemplate.png'
+import holiRetrospectiveTemplate from '../../../../../static/images/illustrations/holiRetrospectiveTemplate.png'
+import lunarNewYearRetrospectiveTemplate from '../../../../../static/images/illustrations/lunarNewYearRetrospectiveTemplate.png'
+import midsummerRetrospectiveTemplate from '../../../../../static/images/illustrations/midsummerRetrospectiveTemplate.png'
+import aChristmasCarolRetrospectiveTemplate from '../../../../../static/images/illustrations/aChristmasCarolRetrospectiveTemplate.png'
+import alwaysBeLearningRetrospectiveTemplate from '../../../../../static/images/illustrations/alwaysBeLearningRetrospectiveTemplate.png'
+import diwaliRetrospectiveTemplate from '../../../../../static/images/illustrations/diwaliRetrospectiveTemplate.png'
+import dreamTeamRetrospectiveTemplate from '../../../../../static/images/illustrations/dreamTeamRetrospectiveTemplate.png'
+import halloweenRetrospectiveTemplate from '../../../../../static/images/illustrations/halloweenRetrospectiveTemplate.png'
+import handsOnDeckActivityTemplate from '../../../../../static/images/illustrations/handsOnDeckActivityTemplate.png'
+import herosJourneyTemplate from '../../../../../static/images/illustrations/herosJourneyTemplate.png'
+import highlightsLowlightsTemplate from '../../../../../static/images/illustrations/highlightsLowlightsTemplate.png'
+import hopesAndFearsTemplate from '../../../../../static/images/illustrations/hopesAndFearsTemplate.png'
+import hotAirBalloonTemplate from '../../../../../static/images/illustrations/hotAirBalloonTemplate.png'
+import keepProblemTryTemplate from '../../../../../static/images/illustrations/keepProblemTryTemplate.png'
+import marieKondoRetrospectiveTemplate from '../../../../../static/images/illustrations/marieKondoRetrospectiveTemplate.png'
+import original4Template from '../../../../../static/images/illustrations/original4Template.png'
+import questionsCommentsConcernsTemplate from '../../../../../static/images/illustrations/questionsCommentsConcernsTemplate.png'
+import roseThornBudTemplate from '../../../../../static/images/illustrations/roseThornBudTemplate.png'
+import sWOTAnalysisTemplate from '../../../../../static/images/illustrations/sWOTAnalysisTemplate.png'
+import saMoLoTemplate from '../../../../../static/images/illustrations/saMoLoTemplate.png'
+import scrumValuesRetrospectiveTemplate from '../../../../../static/images/illustrations/scrumValuesRetrospectiveTemplate.png'
+import sixThinkingHatsTemplate from '../../../../../static/images/illustrations/sixThinkingHatsTemplate.png'
+import speedCarTemplate from '../../../../../static/images/illustrations/speedCarTemplate.png'
+import superheroRetrospectiveTemplate from '../../../../../static/images/illustrations/superheroRetrospectiveTemplate.png'
+import surprisedWorriedInspiredTemplate from '../../../../../static/images/illustrations/surprisedWorriedInspiredTemplate.png'
+import teamCharterTemplate from '../../../../../static/images/illustrations/teamCharterTemplate.png'
+import teamRetreatPlanningTemplate from '../../../../../static/images/illustrations/teamRetreatPlanningTemplate.png'
+import thanksgivingRetrospectiveTemplate from '../../../../../static/images/illustrations/thanksgivingRetrospectiveTemplate.png'
+import wRAPTemplate from '../../../../../static/images/illustrations/wRAPTemplate.png'
+import wsjfTemplate from '../../../../../static/images/illustrations/wsjfTemplate.png'
 import useAtmosphere from '../../../hooks/useAtmosphere'
 import useMutationProps from '../../../hooks/useMutationProps'
 import AddReflectTemplateMutation from '../../../mutations/AddReflectTemplateMutation'
@@ -116,19 +148,53 @@ const ReflectTemplateDetails = (props: Props) => {
     gotoTeamTemplates()
   }
   const defaultIllustrations = {
-    sailboatTemplate: sailboatTemplate,
-    startStopContinueTemplate: startStopContinueTemplate,
-    workingStuckTemplate: workingStuckTemplate,
-    whatWentWellTemplate: whatWentWellTemplate,
+    aChristmasCarolRetrospectiveTemplate: aChristmasCarolRetrospectiveTemplate,
+    alwaysBeLearningRetrospectiveTemplate: alwaysBeLearningRetrospectiveTemplate,
+    diwaliRetrospectiveTemplate: diwaliRetrospectiveTemplate,
+    dreamTeamRetrospectiveTemplate: dreamTeamRetrospectiveTemplate,
     dropAddKeepImproveDAKITemplate: dropAddKeepImproveDAKITemplate,
-    leanCoffeeTemplate: leanCoffeeTemplate,
-    starfishTemplate: starfishTemplate,
+    easterRetrospectiveTemplate: easterRetrospectiveTemplate,
+    energyLevelsTemplate: energyLevelsTemplate,
     fourLsTemplate: fourLsTemplate,
     gladSadMadTemplate: gladSadMadTemplate,
-    energyLevelsTemplate: energyLevelsTemplate,
+    halloweenRetrospectiveTemplate: halloweenRetrospectiveTemplate,
+    handsOnDeckActivityTemplate: handsOnDeckActivityTemplate,
+    heardSeenRespectedHSRTemplate: heardSeenRespectedHSRTemplate,
+    herosJourneyTemplate: herosJourneyTemplate,
+    highlightsLowlightsTemplate: highlightsLowlightsTemplate,
+    holiRetrospectiveTemplate: holiRetrospectiveTemplate,
+    hopesAndFearsTemplate: hopesAndFearsTemplate,
+    hotAirBalloonTemplate: hotAirBalloonTemplate,
+    keepProblemTryTemplate: keepProblemTryTemplate,
+    leanCoffeeTemplate: leanCoffeeTemplate,
+    lunarNewYearRetrospectiveTemplate: lunarNewYearRetrospectiveTemplate,
+    marieKondoRetrospectiveTemplate: marieKondoRetrospectiveTemplate,
+    midsummerRetrospectiveTemplate: midsummerRetrospectiveTemplate,
     mountainClimberTemplate: mountainClimberTemplate,
+    original4Template: original4Template,
+    questionsCommentsConcernsTemplate: questionsCommentsConcernsTemplate,
+    roseThornBudTemplate: roseThornBudTemplate,
+    sWOTAnalysisTemplate: sWOTAnalysisTemplate,
+    saMoLoTemplate: saMoLoTemplate,
+    sailboatTemplate: sailboatTemplate,
+    scrumValuesRetrospectiveTemplate: scrumValuesRetrospectiveTemplate,
+    sixThinkingHatsTemplate: sixThinkingHatsTemplate,
+    speedCarTemplate: speedCarTemplate,
+    starfishTemplate: starfishTemplate,
+    startStopContinueTemplate: startStopContinueTemplate,
+    superheroRetrospectiveTemplate: superheroRetrospectiveTemplate,
+    surprisedWorriedInspiredTemplate: surprisedWorriedInspiredTemplate,
+    teamCharterTemplate: teamCharterTemplate,
+    teamRetreatPlanningTemplate: teamRetreatPlanningTemplate,
+    thanksgivingRetrospectiveTemplate: thanksgivingRetrospectiveTemplate,
     threeLittlePigsTemplate: threeLittlePigsTemplate,
-    winningStreakTemplate: winningStreakTemplate
+    wRAPTemplate: wRAPTemplate,
+    whatWentWellTemplate: whatWentWellTemplate,
+    winningStreakTemplate: winningStreakTemplate,
+    workingStuckTemplate: workingStuckTemplate,
+    wsjfTemplate: wsjfTemplate
+    //cannot find
+    //newYearRetrospectiveTemplate: newYearRetrospectiveTemplate,
   } as const
   const headerImg = defaultIllustrations[templateId as keyof typeof defaultIllustrations]
     ? defaultIllustrations[templateId as keyof typeof defaultIllustrations]
