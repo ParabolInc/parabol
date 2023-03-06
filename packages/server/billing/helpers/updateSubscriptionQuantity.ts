a90ef16924427fc2d1d3fe5c28358d2b956f311e
Georg Bremer <github@dschoordsch.de>
2023-03-06 15:42:45 +0100

    Redis lock updating the stripe quantity

--- packages/server/billing/helpers/updateSubscriptionQuantity.ts
+++ packages/server/billing/helpers/updateSubscriptionQuantity.ts
@@ -3,6 +3,7 @@ import {DataLoaderWorker} from '../../graphql/graphql'
 import {getStripeManager} from '../../utils/stripe'
 import insertStripeQuantityMismatchLogging from '../../postgres/queries/insertStripeQuantityMismatchLogging'
 import sendToSentry from '../../utils/sendToSentry'
+import RedisLockQueue from '../../utils/RedisLockQueue'
 
 /**
  * Check and update if necessary the subscription quantity
@@ -16,46 +17,51 @@ const updateSubscriptionQuantity = async (
   const r = await getRethink()
   const manager = getStripeManager()
 
-  const [org, orgUserCount] = await Promise.all([
-    dataLoader.get('organizations').load(orgId),
-    r
-      .table('OrganizationUser')
-      .getAll(orgId, {index: 'orgId'})
-      .filter({removedAt: null, inactive: false})
-      .count()
-      .run()
-  ])
+  const org = await dataLoader.get('organizations').load(orgId)
   if (!org) throw new Error(`org not found for invoice`)
   const {stripeSubscriptionId} = org
   if (!stripeSubscriptionId) return
 
-  const subscription = await manager.retrieveSubscription(stripeSubscriptionId)
-  const {id: subscriptionId} = subscription
-  const teamSubscription = await manager.getSubscriptionItem(subscriptionId)
-  if (
-    teamSubscription &&
-    teamSubscription.quantity !== undefined &&
-    teamSubscription.quantity !== orgUserCount
-  ) {
-    await manager.updateSubscriptionItemQuantity(teamSubscription.id, orgUserCount)
-    if (logMismatch) {
-      insertStripeQuantityMismatchLogging(
-        orgId,
-        null,
-        new Date(),
-        'invoice.created',
-        teamSubscription.quantity,
-        orgUserCount,
-        []
-      )
-      sendToSentry(new Error('Stripe Quantity Mismatch'), {
-        tags: {
-          quantity: orgUserCount,
-          subscriptionQuantity: teamSubscription.quantity,
-          stripeSubscriptionId
-        }
-      })
+  const redisLock = new RedisLockQueue(`updateSubscriptionQuantity:${orgId}`, 5000)
+  try {
+    await redisLock.lock(10000)
+
+    const [orgUserCount, teamSubscription] = await Promise.all([
+      r
+        .table('OrganizationUser')
+        .getAll(orgId, {index: 'orgId'})
+        .filter({removedAt: null, inactive: false})
+        .count()
+        .run(),
+      await manager.getSubscriptionItem(stripeSubscriptionId)
+    ])
+    if (
+      teamSubscription &&
+      teamSubscription.quantity !== undefined &&
+      teamSubscription.quantity !== orgUserCount
+    ) {
+      await manager.updateSubscriptionItemQuantity(teamSubscription.id, orgUserCount)
+      if (logMismatch) {
+        insertStripeQuantityMismatchLogging(
+          orgId,
+          null,
+          new Date(),
+          'invoice.created',
+          teamSubscription.quantity,
+          orgUserCount,
+          []
+        )
+        sendToSentry(new Error('Stripe Quantity Mismatch'), {
+          tags: {
+            quantity: orgUserCount,
+            subscriptionQuantity: teamSubscription.quantity,
+            stripeSubscriptionId
+          }
+        })
+      }
     }
+  } finally {
+    await redisLock.unlock()
   }
 }
 
