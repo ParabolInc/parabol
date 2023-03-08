4e69309ad95be3a34b936c8802ef6c86f5f54076
James Taber <jmtaber129@gmail.com>
2023-03-08 17:56:43 +0000

    feat(rid): Basic activity library search

--- packages/client/components/ActivityLibrary/ActivityLibrary.tsx
+++ packages/client/components/ActivityLibrary/ActivityLibrary.tsx
@@ -1,10 +1,12 @@
 import graphql from 'babel-plugin-relay/macro'
-import React from 'react'
+import React, {useMemo} from 'react'
 import {PreloadedQuery, usePreloadedQuery} from 'react-relay'
 import {Redirect} from 'react-router'
 import {ActivityLibraryQuery} from '~/__generated__/ActivityLibraryQuery.graphql'
 import ActivityLibrarySideBar from './ActivityLibrarySideBar'
 import ActivityLibraryCard from './ActivityLibraryCard'
+import SearchBar from './SearchBar'
+import useSearchFilter from '../../hooks/useSearchFilter'
 
 graphql`
   fragment ActivityLibrary_template on MeetingTemplate {
@@ -39,19 +41,28 @@ interface Props {
   queryRef: PreloadedQuery<ActivityLibraryQuery>
 }
 
+const getTemplateValue = (template: {name: string}) => template.name
+
 export const ActivityLibrary = (props: Props) => {
   const {queryRef} = props
-  const data = usePreloadedQuery<ActivityLibraryQuery>(query, queryRef, {
-    UNSTABLE_renderPolicy: 'full'
-  })
+  const data = usePreloadedQuery<ActivityLibraryQuery>(query, queryRef)
   const {viewer} = data
   const {featureFlags, availableTemplates} = viewer
 
-  const templates = [
-    {id: 'action', type: 'action', name: 'Check-in', team: {name: 'Parabol'}},
-    {id: 'teamPrompt', type: 'teamPrompt', name: 'Standup', team: {name: 'Parabol'}},
-    ...availableTemplates.edges.map((edge) => edge.node)
-  ]
+  const templates = useMemo(
+    () => [
+      {id: 'action', type: 'action', name: 'Check-in', team: {name: 'Parabol'}},
+      {id: 'teamPrompt', type: 'teamPrompt', name: 'Standup', team: {name: 'Parabol'}},
+      ...availableTemplates.edges.map((edge) => edge.node)
+    ],
+    [availableTemplates]
+  )
+
+  const {
+    query: searchQuery,
+    filteredItems: filteredTemplates,
+    onQueryChange
+  } = useSearchFilter(templates, getTemplateValue)
 
   if (!featureFlags.retrosInDisguise) {
     return <Redirect to='/404' />
@@ -60,15 +71,18 @@ export const ActivityLibrary = (props: Props) => {
   return (
     <div className='flex'>
       <ActivityLibrarySideBar />
-      <div className='flex flex-wrap'>
-        {templates.map((template) => (
-          <ActivityLibraryCard
-            key={template.id}
-            type={template.type}
-            name={template.name}
-            teamName={template.team.name}
-          />
-        ))}
+      <div>
+        <SearchBar searchQuery={searchQuery} onChange={onQueryChange} />
+        <div className='flex flex-wrap'>
+          {filteredTemplates.map((template) => (
+            <ActivityLibraryCard
+              key={template.id}
+              type={template.type}
+              name={template.name}
+              teamName={template.team.name}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
