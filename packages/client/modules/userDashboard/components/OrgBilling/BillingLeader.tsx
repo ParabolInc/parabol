2848b27a3ff83b29e429ce660dcf165107b54ac5
Nick O'Ferrall <nickoferrall@gmail.com>
2023-03-10 16:47:46 +0000

    clean up

--- packages/client/modules/userDashboard/components/OrgBilling/BillingLeader.tsx
+++ packages/client/modules/userDashboard/components/OrgBilling/BillingLeader.tsx
@@ -1,35 +1,17 @@
 import styled from '@emotion/styled'
-import defaultUserAvatar from '../../../../styles/theme/images/avatar-user.svg'
 import graphql from 'babel-plugin-relay/macro'
-import {Divider} from '@mui/material'
 import Avatar from '../../../../components/Avatar/Avatar'
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import {BillingLeader_user$key} from '../../../../__generated__/BillingLeader_user.graphql'
-import BillingForm from './BillingForm'
-import Panel from '../../../../components/Panel/Panel'
 import Row from '../../../../components/Row/Row'
-import {PALETTE} from '../../../../styles/paletteV3'
-import {loadStripe} from '@stripe/stripe-js'
-import {Elements} from '@stripe/react-stripe-js'
-import CreatePaymentIntentMutation from '../../../../mutations/CreatePaymentIntentMutation'
-import useAtmosphere from '../../../../hooks/useAtmosphere'
-import useMutationProps from '../../../../hooks/useMutationProps'
-import {CreatePaymentIntentMutationResponse} from '../../../../__generated__/CreatePaymentIntentMutation.graphql'
-import {CompletedHandler} from '../../../../types/relayMutations'
-import {Breakpoint, ElementWidth} from '../../../../types/constEnums'
+import {ElementWidth} from '../../../../types/constEnums'
 import RowInfoHeading from '../../../../components/Row/RowInfoHeading'
 import RowInfoHeader from '../../../../components/Row/RowInfoHeader'
-import RoleTag from '../../../../components/Tag/RoleTag'
-import RowInfoLink from '../../../../components/Row/RowInfoLink'
 import RowActions from '../../../../components/Row/RowActions'
 import FlatButton from '../../../../components/FlatButton'
 import RowInfo from '../../../../components/Row/RowInfo'
 import {useFragment} from 'react-relay'
 import IconLabel from '../../../../components/IconLabel'
-
-const StyledPanel = styled(Panel)({
-  maxWidth: ElementWidth.PANEL_WIDTH
-})
 
 const StyledRow = styled(Row)<{isFirstRow: boolean}>(({isFirstRow}) => ({
   padding: '12px 16px',
@@ -37,78 +19,6 @@ const StyledRow = styled(Row)<{isFirstRow: boolean}>(({isFirstRow}) => ({
   alignItems: 'center',
   border: isFirstRow ? 'none' : undefined
 }))
-
-const Plan = styled('div')({
-  lineHeight: '16px',
-  textTransform: 'capitalize',
-  textAlign: 'center',
-  display: 'flex',
-  padding: '0px 16px 16px 16px',
-  flexWrap: 'wrap',
-  width: '50%',
-  overflow: 'hidden'
-})
-
-const Title = styled('div')({
-  color: PALETTE.SLATE_800,
-  fontSize: 22,
-  fontWeight: 600,
-  lineHeight: '30px',
-  textTransform: 'capitalize',
-  display: 'flex',
-  width: '100%',
-  padding: '8px 0px 16px 0px'
-})
-
-const Subtitle = styled('div')({
-  color: PALETTE.SLATE_800,
-  fontSize: 16,
-  fontWeight: 600,
-  lineHeight: '30px',
-  textTransform: 'capitalize',
-  display: 'flex',
-  paddingBottom: 8
-})
-
-const Content = styled('div')({
-  width: '100%'
-})
-
-const InputLabel = styled('span')({
-  display: 'block',
-  fontSize: 12,
-  fontWeight: 600,
-  textAlign: 'left',
-  paddingBottom: 4,
-  color: PALETTE.SLATE_600,
-  textTransform: 'uppercase'
-})
-
-const InfoText = styled('span')({
-  display: 'block',
-  fontSize: 12,
-  fontWeight: 600,
-  textAlign: 'left',
-  paddingBottom: 8,
-  color: PALETTE.SLATE_600,
-  textTransform: 'none'
-})
-
-const TotalBlock = styled('div')({
-  display: 'flex',
-  justifyContent: 'space-between',
-  paddingTop: 16
-})
-
-const ActiveUserBlock = styled('div')({
-  paddingTop: 16
-})
-
-const AvatarBlock = styled('div')({})
-
-const StyledRowInfo = styled(RowInfo)({
-  paddingLeft: 0
-})
 
 const ActionsBlock = styled('div')({
   alignItems: 'center',
@@ -119,21 +29,6 @@ const ActionsBlock = styled('div')({
 const MenuToggleBlock = styled('div')({
   marginLeft: 8,
   width: '2rem'
-})
-
-// interface Props extends WithMutationProps {
-//   billingLeaderCount: number
-//   organizationUser: OrgMemberRow_organizationUser
-//   organization: OrgMemberRow_organization
-// }
-
-const StyledHeading = styled(RowInfoHeading)({
-  // paddingLeft: 16
-})
-
-const StyledFlatButton = styled(FlatButton)({
-  paddingLeft: 16,
-  paddingRight: 16
 })
 
 const StyledButton = styled(FlatButton)({
@@ -158,75 +53,23 @@ const BillingLeader = (props: Props) => {
     `,
     billingLeaderRef
   )
-  console.log('🚀 ~ child.........:', {billingLeader, isFirstRow})
-
   const {preferredName, picture} = billingLeader
-  // const {billingLeaders} = organization
-  const [clientSecret, setClientSecret] = useState('')
-  const atmosphere = useAtmosphere()
-  const {onError} = useMutationProps()
 
   return (
     <StyledRow isFirstRow={isFirstRow}>
       <Avatar hasBadge={false} picture={picture} size={ElementWidth.BILLING_AVATAR} />
       <RowInfo>
         <RowInfoHeader>
-          <StyledHeading>{preferredName}</StyledHeading>
-          {/* {isBillingLeader && <RoleTag>{'Billing Leader'}</RoleTag>} */}
-          {/* <RoleTag>{'Billing Leader'}</RoleTag> */}
-          {/* {inactive && !isBillingLeader && <InactiveTag>{'Inactive'}</InactiveTag>} */}
-          {/* {new Date(newUserUntil) > new Date() && <EmphasisTag>{'New'}</EmphasisTag>} */}
+          <RowInfoHeading>{preferredName}</RowInfoHeading>
         </RowInfoHeader>
-        {/* <RowInfoLink href={`mailto:${email}`} title='Send an email'> */}
-        {/* <RowInfoLink title='Send an email'>{preferredName}</RowInfoLink> */}
       </RowInfo>
       <RowActions>
         <ActionsBlock>
-          {/* {!isBillingLeader && viewerId === userId && ( */}
-          {/* // <StyledFlatButton onClick={toggleLeave} onMouseEnter={LeaveOrgModal.preload}> */}
-          {/* <StyledFlatButton>Leave Organization</StyledFlatButton> */}
-          {/* )} */}
-          {/* {isViewerLastBillingLeader && userId === viewerId && (
-            <MenuToggleBlock
-              onClick={closeTooltip}
-              onMouseOver={openTooltip}
-              onMouseOut={closeTooltip}
-              ref={tooltipRef}
-            >
-              {tooltipPortal(
-                <div>
-                  {'You need to promote another Billing Leader'}
-                  <br />
-                  {'before you can leave this role or Organization.'}
-                </div>
-              )}
-              <MenuButton disabled />
-            </MenuToggleBlock>
-          )} */}
           <MenuToggleBlock>
-            {/* <MenuButton
-              onClick={togglePortal}
-              onMouseEnter={BillingLeaderActionMenu.preload}
-              ref={originRef}
-            /> */}
             <StyledButton>
               <IconLabel icon='more_vert' />
             </StyledButton>
           </MenuToggleBlock>
-          {/* {menuPortal(
-            <BillingLeaderActionMenu
-              menuProps={menuProps}
-              isViewerLastBillingLeader={isViewerLastBillingLeader}
-              organizationUser={organizationUser}
-              organization={organization}
-              toggleLeave={toggleLeave}
-              toggleRemove={toggleRemove}
-            />
-          )}
-          {leaveModal(<LeaveOrgModal orgId={orgId} />)}
-          {removeModal(
-            <RemoveFromOrgModal orgId={orgId} userId={userId} preferredName={preferredName} />
-          )} */}
         </ActionsBlock>
       </RowActions>
     </StyledRow>
