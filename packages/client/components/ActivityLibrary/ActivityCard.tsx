c77a97ac90579984c43c51649ffd8c3be358d129
Bartosz Jarocki <jarocki.bartek@gmail.com>
2023-03-10 08:16:22 +0100

    feat: added activity library card

---
+++ packages/client/components/ActivityLibrary/ActivityCard.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx'
+import React from 'react'
+import {MeetingTypeEnum} from '../../__generated__/ActivityLibraryQuery.graphql'
+
+interface CardTheme {
+  primary: string
+  secondary: string
+}
+
+export const MeetingThemes: Record<MeetingTypeEnum, CardTheme> = {
+  action: {primary: 'bg-aqua-400', secondary: 'bg-[#CBECF0]'},
+  poker: {primary: 'bg-tomato-500', secondary: 'bg-[#FFE2E0]'},
+  retrospective: {primary: 'bg-grape-500', secondary: 'bg-[#F2E1F7]'},
+  teamPrompt: {primary: 'bg-aqua-400', secondary: 'bg-[#CBECF0]'}
+}
+
+export interface Props {
+  className?: string
+  type: MeetingTypeEnum
+  children: React.ReactNode
+}
+
+export const ActivityCard = (props: Props) => {
+  const {className, type, children} = props
+  return (
+    <div
+      className={clsx(
+        'relative flex aspect-[156/120] max-h-[160px] w-auto flex-col overflow-hidden rounded-lg p-3 md:aspect-[256/160]',
+        MeetingThemes[type].secondary,
+        className
+      )}
+    >
+      <div
+        className={clsx(
+          'absolute top-0 right-0 h-8 w-8 rounded-bl-full',
+          MeetingThemes[type].primary
+        )}
+      />
+      <div
+        className={clsx(
+          'absolute bottom-0 left-0 h-8 w-8 rounded-tr-full',
+          MeetingThemes[type].primary
+        )}
+      />
+      {children}
+    </div>
+  )
+}
--- packages/client/components/ActivityLibrary/ActivityLibraryCard.tsx
+++ packages/client/components/ActivityLibrary/ActivityLibraryCard.tsx
@@ -1,21 +1,88 @@
-import React from 'react'
+import clsx from 'clsx'
+import React, {PropsWithChildren, ComponentPropsWithoutRef, ElementType, ReactNode} from 'react'
+import {MeetingTypeEnum} from '../../__generated__/ActivityLibraryQuery.graphql'
+import {ActivityCard} from './ActivityCard'
 
-interface Props {
-  name: string
-  type: string
-  teamName: string
-}
+const Image = (props: PropsWithChildren<React.ImgHTMLAttributes<HTMLImageElement>>) => {
+  const {className, src} = props
 
-const ActivityLibraryCard = (props: Props) => {
-  const {type, name, teamName} = props
   return (
-    <div className='m-2 border-solid p-2'>
-      <div>
-        {type} on team {teamName}
-      </div>
-      <div className='text-lg font-bold'>{name}</div>
+    <div className='absolute inset-0 top-5 flex'>
+      <img className={clsx('m-auto h-[86px] w-auto object-cover', className)} src={src} />
     </div>
   )
 }
 
-export default ActivityLibraryCard
+type TitleProps<T extends ElementType> = {
+  as?: T
+  children: ReactNode
+}
+
+const Title = <T extends ElementType = 'div'>(
+  props: TitleProps<T> & ComponentPropsWithoutRef<T>
+) => {
+  const {as, children, className, ...rest} = props
+  const Component = as || 'div'
+
+  return (
+    <Component
+      className={clsx(
+        'block pr-8 text-base font-semibold leading-5 text-slate-800 focus:outline-none',
+        className
+      )}
+      {...rest}
+    >
+      {/* Extend touch target to entire activity card if it's a link */}
+      {'to' in props && <span className='absolute inset-0' aria-hidden='true' />}
+      {children}
+    </Component>
+  )
+}
+
+interface BadgeProps {
+  className?: string
+  children?: React.ReactNode
+}
+
+const Badge = (props: BadgeProps) => {
+  const {className, children} = props
+
+  return (
+    <div
+      className={clsx(
+        'absolute bottom-0 right-0 m-2 rounded-full bg-gold-300 px-2 py-[1px] text-xs font-semibold text-grape-700',
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+interface CardProps {
+  className?: string
+  type: MeetingTypeEnum
+  children: React.ReactNode
+}
+
+const Card = (props: CardProps) => {
+  const {className, type, children} = props
+
+  return (
+    <ActivityCard
+      className={clsx(
+        'group transition-shadow focus-within:ring-2 focus-within:ring-primary hover:shadow-xl motion-reduce:transition-none',
+        className
+      )}
+      type={type}
+    >
+      {children}
+    </ActivityCard>
+  )
+}
+
+export const ActivityLibraryCard = Object.assign(Card, {
+  Image,
+  Title,
+  Badge
+})
