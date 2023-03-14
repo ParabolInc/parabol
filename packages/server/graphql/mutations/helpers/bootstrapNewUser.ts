e16c8f9e4c5de948ede073638e4c901eb839b62c
Nick O'Ferrall <nickoferrall@gmail.com>
2023-03-14 17:40:39 +0000

    add templateLimit flag to p0 and users that belong to domain in experiment

--- packages/server/graphql/mutations/helpers/bootstrapNewUser.ts
+++ packages/server/graphql/mutations/helpers/bootstrapNewUser.ts
@@ -10,23 +10,37 @@ import insertUser from '../../../postgres/queries/insertUser'
 import IUser from '../../../postgres/types/IUser'
 import {analytics} from '../../../utils/analytics/analytics'
 import segmentIo from '../../../utils/segmentIo'
+import {UserFeatureFlagsResolvers} from '../../private/resolverTypes'
+import {UserFeatureFlags, UserFlagEnum} from '../../public/resolverTypes'
 import addSeedTasks from './addSeedTasks'
 import createNewOrg from './createNewOrg'
 import createTeamAndLeader from './createTeamAndLeader'
 import isPatientZero from './isPatientZero'
+import getUsersbyDomain from '../../../postgres/queries/getUsersByDomain'
 
 const bootstrapNewUser = async (newUser: User, isOrganic: boolean) => {
   const {id: userId, createdAt, preferredName, email, featureFlags, tier, segmentId} = newUser
   const domain = email.split('@')[1]
-  const isPatient0 = await isPatientZero(domain)
+  const [isPatient0, usersWithDomain] = await Promise.all([
+    isPatientZero(domain),
+    getUsersbyDomain(domain)
+  ])
   const r = await getRethink()
   const joinEvent = new TimelineEventJoinedParabol({userId})
+
+  // remove the following after templateLimit experiment is complete: https://github.com/ParabolInc/parabol/issues/7712
+  const hideTemplateLimitsP0Experiment = process.env.hideTemplateLimitsP0Experiment
+  const domainUserHasFlag = usersWithDomain.some((user) =>
+    user.featureFlags.includes('templateLimit')
+  )
+  const addTemplateFlag = !hideTemplateLimitsP0Experiment && (isPatient0 || domainUserHasFlag)
+  const experimentalFlags = addTemplateFlag ? [...featureFlags, 'templateLimit'] : featureFlags
 
   await Promise.all([
     r({
       event: r.table('TimelineEvent').insert(joinEvent)
     }).run(),
-    insertUser({...newUser, isPatient0})
+    insertUser({...newUser, isPatient0, featureFlags: experimentalFlags})
   ])
 
   // Identify the user so user properties are set before any events are sent
@@ -37,7 +51,7 @@ const bootstrapNewUser = async (newUser: User, isOrganic: boolean) => {
       email,
       name: preferredName,
       isActive: true,
-      featureFlags,
+      featureFlags: experimentalFlags,
       highestTier: tier,
       isPatient0
     },
