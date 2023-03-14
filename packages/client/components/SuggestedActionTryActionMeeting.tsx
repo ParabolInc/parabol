a39694d878b6bff9469829f7048a1ab35f546a07
James Taber <jmtaber129@gmail.com>
2023-03-14 16:54:17 +0000

    chore(relay): createFragmentContainer -> useFragment 7/7 (#7875)

--- packages/client/components/SuggestedActionTryActionMeeting.tsx
+++ packages/client/components/SuggestedActionTryActionMeeting.tsx
@@ -1,28 +1,41 @@
 import graphql from 'babel-plugin-relay/macro'
 import React from 'react'
-import {createFragmentContainer} from 'react-relay'
+import {useFragment} from 'react-relay'
 import {RouteComponentProps, withRouter} from 'react-router'
 import {PALETTE} from '../styles/paletteV3'
-import {SuggestedActionTryActionMeeting_suggestedAction} from '../__generated__/SuggestedActionTryActionMeeting_suggestedAction.graphql'
+import {SuggestedActionTryActionMeeting_suggestedAction$key} from '../__generated__/SuggestedActionTryActionMeeting_suggestedAction.graphql'
 import SuggestedActionButton from './SuggestedActionButton'
 import SuggestedActionCard from './SuggestedActionCard'
 import SuggestedActionCopy from './SuggestedActionCopy'
 
 interface Props extends RouteComponentProps<{[x: string]: string | undefined}> {
-  suggestedAction: SuggestedActionTryActionMeeting_suggestedAction
+  suggestedAction: SuggestedActionTryActionMeeting_suggestedAction$key
 }
 
 const SuggestedActionTryActionMeeting = (props: Props) => {
+  const {suggestedAction: suggestedActionRef} = props
+  const suggestedAction = useFragment(
+    graphql`
+      fragment SuggestedActionTryActionMeeting_suggestedAction on SuggestedActionTryActionMeeting {
+        id
+        team {
+          id
+          name
+        }
+      }
+    `,
+    suggestedActionRef
+  )
+  const {id: suggestedActionId, team} = suggestedAction
+  const {name: teamName} = team
+
   const onClick = () => {
-    const {history, suggestedAction} = props
+    const {history} = props
     const {team} = suggestedAction
     const {id: teamId} = team
     history.push(`/new-meeting/${teamId}`)
   }
 
-  const {suggestedAction} = props
-  const {id: suggestedActionId, team} = suggestedAction
-  const {name: teamName} = team
   return (
     <SuggestedActionCard
       backgroundColor={PALETTE.AQUA_400}
@@ -35,14 +48,4 @@ const SuggestedActionTryActionMeeting = (props: Props) => {
   )
 }
 
-export default createFragmentContainer(withRouter(SuggestedActionTryActionMeeting), {
-  suggestedAction: graphql`
-    fragment SuggestedActionTryActionMeeting_suggestedAction on SuggestedActionTryActionMeeting {
-      id
-      team {
-        id
-        name
-      }
-    }
-  `
-})
+export default withRouter(SuggestedActionTryActionMeeting)
