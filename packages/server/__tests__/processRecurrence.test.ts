a3db24f6a7ecf2e2df5b25a1ac430b47a0d442b2
Bartosz Jarocki <bartosz.jarocki@hey.com>
2023-03-28 08:45:16 +0200

    chore: added process recurrence infinite loop test

--- packages/server/__tests__/processRecurrence.test.ts
+++ packages/server/__tests__/processRecurrence.test.ts
@@ -381,3 +381,79 @@ test('Should not start a new meeting if the rrule has not started', async () => 
   const actualMeeting = await r.table('NewMeeting').get(meetingId).run()
   expect(actualMeeting.endedAt).toBeTruthy()
 })
+
+test('Should not hang if the rrule interval is invalid', async () => {
+  const r = await getRethink()
+  const {userId} = await signUp()
+  const {id: teamId} = (await getUserTeams(userId))[0]
+
+  const now = new Date()
+
+  // Create a meeting series that's been going on for a few days, and happens daily at 9a UTC.
+  const startDate = new Date(
+    Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - 2, 9)
+  )
+  const recurrenceRule = new RRule({
+    freq: RRule.DAILY,
+    tzid: 'America/Los_Angeles',
+    interval: NaN,
+    dtstart: startDate
+  })
+
+  const newMeetingSeriesId = await insertMeetingSeriesQuery({
+    meetingType: 'teamPrompt',
+    title: 'Daily Test Standup',
+    recurrenceRule: recurrenceRule.toString(),
+    duration: 24 * 60, // 24 hours
+    teamId,
+    facilitatorId: userId
+  })
+
+  const meetingId = generateUID()
+  const meeting = new MeetingTeamPrompt({
+    id: meetingId,
+    teamId,
+    meetingCount: 0,
+    phases: [new TeamPromptResponsesPhase(['foobar'])],
+    facilitatorUserId: userId,
+    meetingPrompt: 'What are you working on today? Stuck on anything?',
+    scheduledEndTime: new Date(Date.now() - ms('5m')),
+    meetingSeriesId: newMeetingSeriesId
+  })
+
+  // The last meeting in the series was created just over 24h ago, so the next one should start
+  // soon.
+  meeting.createdAt = new Date(meeting.createdAt.getTime() - ms('25h'))
+
+  await r.table('NewMeeting').insert(meeting).run()
+
+  const update = await sendIntranet({
+    query: PROCESS_RECURRENCE,
+    isPrivate: true
+  })
+
+  expect(update).toEqual({
+    data: {
+      processRecurrence: {
+        meetingsStarted: 1,
+        meetingsEnded: 1
+      }
+    }
+  })
+
+  await assertIdempotency()
+
+  const actualMeeting = await r.table('NewMeeting').get(meetingId).run()
+  expect(actualMeeting.endedAt).toBeTruthy()
+
+  const lastMeeting = await r
+    .table('NewMeeting')
+    .filter({meetingType: 'teamPrompt', meetingSeriesId: newMeetingSeriesId})
+    .orderBy(r.desc('createdAt'))
+    .nth(0)
+    .run()
+
+  expect(lastMeeting).toMatchObject({
+    name: expect.stringMatching(/Daily Test Standup.*/)
+  })
+})
