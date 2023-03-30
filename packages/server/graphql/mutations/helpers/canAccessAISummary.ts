69ed4ecc712ea627eee2d78c7111ae36c76a7459
Nick O'Ferrall <nickoferrall@gmail.com>
2023-03-30 12:21:45 +0100

    second commit for pr rules

--- packages/server/graphql/mutations/helpers/canAccessAISummary.ts
+++ packages/server/graphql/mutations/helpers/canAccessAISummary.ts
@@ -3,6 +3,7 @@ import {Team} from '../../../postgres/queries/getTeamsByIds'
 
 const canAccessAISummary = (team: Team, featureFlags: string[]) => {
   if (featureFlags.includes('noAISummary') || !team) return false
+
   const {qualAIMeetingsCount, tier} = team
   if (tier !== 'starter') return true
   return qualAIMeetingsCount < Threshold.MAX_QUAL_AI_MEETINGS
