c435a6e679e2bc47f1d794ed22443e8261833f76
Nick O'Ferrall <nickoferrall@gmail.com>
2023-03-30 12:14:12 +0100

    change aiSummary flag to noAISummary

--- packages/server/graphql/mutations/helpers/canAccessAISummary.ts
+++ packages/server/graphql/mutations/helpers/canAccessAISummary.ts
@@ -2,7 +2,7 @@ import {Threshold} from 'parabol-client/types/constEnums'
 import {Team} from '../../../postgres/queries/getTeamsByIds'
 
 const canAccessAISummary = (team: Team, featureFlags: string[]) => {
-  if (!featureFlags.includes('aiSummary') || !team) return false
+  if (featureFlags.includes('noAISummary') || !team) return false
   const {qualAIMeetingsCount, tier} = team
   if (tier !== 'starter') return true
   return qualAIMeetingsCount < Threshold.MAX_QUAL_AI_MEETINGS
