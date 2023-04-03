df770cb9713429e3870a6c283ec79f2df7a20e17
Georg Bremer <github@dschoordsch.de>
2023-04-03 15:48:24 +0200

    Fix User.tier edge case when they're not in any org
    If a user left the last organization, they would not get their tier
    updated.

--- packages/server/utils/setUserTierForUserIds.ts
+++ packages/server/utils/setUserTierForUserIds.ts
@@ -4,7 +4,7 @@ import OrganizationUser from '../database/types/OrganizationUser'
 import User from '../database/types/User'
 import {TierEnum} from '../postgres/queries/generated/updateUserQuery'
 import {getUsersByIds} from '../postgres/queries/getUsersByIds'
-import updateUserTiers from '../postgres//queries/updateUserTiers'
+import updateUserTiers from '../postgres/queries/updateUserTiers'
 import segmentIo from './segmentIo'
 
 const setUserTierForUserIds = async (userIds: string[]) => {
@@ -57,7 +57,14 @@ const setUserTierForUserIds = async (userIds: string[]) => {
       )
     }))
     .run()) as {id: string; tier: TierEnum}[]
-  await updateUserTiers({users: userTiers})
+  const newUserTiers = userIds.map((userId) => {
+    const userTier = userTiers.find((userTier) => userTier.id === userId)
+    return {
+      id: userId,
+      tier: userTier ? userTier.tier : 'starter'
+    }
+  })
+  await updateUserTiers({users: newUserTiers})
 
   const users = await getUsersByIds(userIds)
   users.forEach((user) => {
