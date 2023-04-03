880ace48e2ceae63fafd3667c45fba7ad6eeefb6
Bruce Tian <tianrunhe@gmail.com>
2023-04-03 09:39:46 -0700

    feat(ai-summary): replace GPT-3 with ChatGPT (#7958)

--- packages/server/utils/OpenAIServerManager.ts
+++ packages/server/utils/OpenAIServerManager.ts
@@ -20,20 +20,28 @@ class OpenAIServerManager {
     if (!this.openAIApi) return null
     try {
       const location = summaryLocation ?? 'retro meeting'
-      const response = await this.openAIApi.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `Below is a comma-separated list of text from a ${location}. Summarize the text for a second-grade student in one or two sentences.
+      const response = await this.openAIApi.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            content: `Below is a comma-separated list of text from a ${location}.
+            Summarize the text for a second-grade student in one or two sentences.
+            If you can't provide a summary, simply say the word "No".
 
-        Text: """
-        ${text}
-        """`,
+            Text: """
+            ${text}
+            """`
+          }
+        ],
         temperature: 0.7,
         max_tokens: 256,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0
       })
-      return (response.data.choices[0]?.text?.trim() as string) ?? null
+      const answer = (response.data.choices[0]?.message?.content?.trim() as string) ?? null
+      return /^No\.*$/i.test(answer) ? null : answer
     } catch (e) {
       const error = e instanceof Error ? e : new Error('OpenAI failed to getSummary')
       sendToSentry(error)
