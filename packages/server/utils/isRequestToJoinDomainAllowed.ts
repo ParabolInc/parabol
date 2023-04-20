b2704ce3a7df21d150e96eaee9521e44961a892a
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-04-20 16:12:41 +0400

    Remove debug logs

--- packages/server/utils/isRequestToJoinDomainAllowed.ts
+++ packages/server/utils/isRequestToJoinDomainAllowed.ts
@@ -4,32 +4,6 @@ import getRethink from '../database/rethinkDriver'
 const hasEligibleOrg = async (activeDomain: string) => {
   const r = await getRethink()
 
-  const orgsOnDomain = await r
-    .table('Organization')
-    .getAll(activeDomain, {index: 'activeDomain'})
-    .filter((org) => org('featureFlags').contains('promptToJoinOrg'))
-    .filter((org) => org('tier').ne('enterprise'))
-    .run()
-
-  console.log('orgsOnDomain', orgsOnDomain)
-
-  const orgsOnDomainWithMoreThanOneUser = await r
-    .table('Organization')
-    .getAll(activeDomain, {index: 'activeDomain'})
-    .filter((org) => org('featureFlags').contains('promptToJoinOrg'))
-    .filter((org) => org('tier').ne('enterprise'))
-    .filter((org) =>
-      r
-        .table('OrganizationUser')
-        .getAll(org('id'), {index: 'orgId'})
-        .filter({inactive: false, removedAt: null})
-        .count()
-        .gt(1)
-    )
-    .run()
-
-  console.log('orgsOnDomainWithMoreThanOneUser', orgsOnDomainWithMoreThanOneUser)
-
   return r
     .table('Organization')
     .getAll(activeDomain, {index: 'activeDomain'})
@@ -50,18 +24,13 @@ const hasEligibleOrg = async (activeDomain: string) => {
 }
 
 const isRequestToJoinDomainAllowed = async (domain: string) => {
-  console.log('isRequestToJoinDomainAllowed domain:', domain)
   if (!isCompanyDomain(domain)) {
-    console.log('Not a company domain, skip')
     return false
   }
 
   if (!(await hasEligibleOrg(domain))) {
-    console.log('No matching org, skip')
     return false
   }
-
-  console.log('All good sending notification')
 
   return true
 }
--- packages/server/utils/sendPromptToJoinOrg.ts
+++ packages/server/utils/sendPromptToJoinOrg.ts
@@ -4,13 +4,11 @@ import isRequestToJoinDomainAllowed from './isRequestToJoinDomainAllowed'
 import getDomainFromEmail from './getDomainFromEmail'
 
 const sendPromptToJoinOrg = async (email, userId) => {
-  console.log('sendPromptToJoinOrg', email, userId)
   const r = await getRethink()
 
   const activeDomain = getDomainFromEmail(email)
 
   if (!(await isRequestToJoinDomainAllowed(activeDomain))) {
-    console.log('Request not allowed')
     return
   }
 
@@ -18,8 +16,6 @@ const sendPromptToJoinOrg = async (email, userId) => {
     userId,
     activeDomain
   })
-
-  console.log('NotificationToInsert', notificationToInsert)
 
   await r.table('Notification').insert(notificationToInsert).run()
 }
