c3224e79bfd9d45f37ea2e3741893ce38d88c424
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-04-25 16:10:27 +0400

    Fix type

--- packages/server/database/types/NotificationRequestToJoinOrg.ts
+++ packages/server/database/types/NotificationRequestToJoinOrg.ts
@@ -1,7 +1,7 @@
 import Notification from './Notification'
 
 interface Input {
-  domainJoinRequestId: number
+  domainJoinRequestId: string
   email: string
   name: string
   picture: string
@@ -9,7 +9,7 @@ interface Input {
 }
 
 export default class NotificationRequestToJoinOrg extends Notification {
-  domainJoinRequestId: number
+  domainJoinRequestId: string
   email: string
   name: string
   picture: string
