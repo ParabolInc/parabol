0238746a7090d6fa7a98cc89292cd73d81ca4385
Nick O'Ferrall <nickoferrall@gmail.com>
2023-04-27 20:32:45 +0100

    add suggestGroups org flag

--- packages/server/graphql/public/types/OrganizationFeatureFlags.ts
+++ packages/server/graphql/public/types/OrganizationFeatureFlags.ts
@@ -3,7 +3,8 @@ import {OrganizationFeatureFlagsResolvers} from '../resolverTypes'
 const OrganizationFeatureFlags: OrganizationFeatureFlagsResolvers = {
   SAMLUI: ({SAMLUI}) => !!SAMLUI,
   teamsLimit: ({teamsLimit}) => !!teamsLimit,
-  promptToJoinOrg: ({promptToJoinOrg}) => !!promptToJoinOrg
+  promptToJoinOrg: ({promptToJoinOrg}) => !!promptToJoinOrg,
+  suggestGroups: ({suggestGroups}) => !!suggestGroups
 }
 
 export default OrganizationFeatureFlags
