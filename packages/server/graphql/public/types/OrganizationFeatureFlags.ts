38706cd1d34425bbbb66cb50f7e912bb51009e6e
Nick O'Ferrall <nickoferrall@gmail.com>
2023-05-02 16:47:27 +0100

    second commit for pr rules

--- packages/server/graphql/public/types/OrganizationFeatureFlags.ts
+++ packages/server/graphql/public/types/OrganizationFeatureFlags.ts
@@ -3,8 +3,8 @@ import {OrganizationFeatureFlagsResolvers} from '../resolverTypes'
 const OrganizationFeatureFlags: OrganizationFeatureFlagsResolvers = {
   SAMLUI: ({SAMLUI}) => !!SAMLUI,
   teamsLimit: ({teamsLimit}) => !!teamsLimit,
-  promptToJoinOrg: ({promptToJoinOrg}) => !!promptToJoinOrg,
-  suggestGroups: ({suggestGroups}) => !!suggestGroups
+  suggestGroups: ({suggestGroups}) => !!suggestGroups,
+  promptToJoinOrg: ({promptToJoinOrg}) => !!promptToJoinOrg
 }
 
 export default OrganizationFeatureFlags
