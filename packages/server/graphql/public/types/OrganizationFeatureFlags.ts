373e9a6b1f2769dec83ea9cdb532ac45926bcf3b
Nick O'Ferrall <nickoferrall@gmail.com>
2023-05-04 09:21:23 +0100

    feat(suggest-groups): Add suggestGroups org feature flag (#8130)
    * add suggestGroups org flag
    
    * second commit for pr rules

--- packages/server/graphql/public/types/OrganizationFeatureFlags.ts
+++ packages/server/graphql/public/types/OrganizationFeatureFlags.ts
@@ -3,6 +3,7 @@ import {OrganizationFeatureFlagsResolvers} from '../resolverTypes'
 const OrganizationFeatureFlags: OrganizationFeatureFlagsResolvers = {
   SAMLUI: ({SAMLUI}) => !!SAMLUI,
   teamsLimit: ({teamsLimit}) => !!teamsLimit,
+  suggestGroups: ({suggestGroups}) => !!suggestGroups,
   promptToJoinOrg: ({promptToJoinOrg}) => !!promptToJoinOrg
 }
 
