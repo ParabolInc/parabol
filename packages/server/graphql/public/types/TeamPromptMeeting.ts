9cbba3bdc798df9ba15387eb93caeffbcd1f4ebc
James Taber <jmtaber129@gmail.com>
2023-05-04 15:49:38 -0400

    feat(recurrence): Prev/next meeting navigation

--- packages/server/graphql/public/types/TeamPromptMeeting.ts
+++ packages/server/graphql/public/types/TeamPromptMeeting.ts
@@ -1,5 +1,7 @@
 import MeetingSeriesId from 'parabol-client/shared/gqlIds/MeetingSeriesId'
 import {TeamPromptMeetingResolvers} from '../resolverTypes'
+import getRethink from '../../../database/rethinkDriver'
+import MeetingTeamPrompt from '../../../database/types/MeetingTeamPrompt'
 
 const TeamPromptMeeting: TeamPromptMeetingResolvers = {
   meetingSeriesId: ({meetingSeriesId}, _args, _context) => {
@@ -17,6 +19,48 @@ const TeamPromptMeeting: TeamPromptMeetingResolvers = {
       }
 
       return series
+    }
+    return null
+  },
+  prevMeeting: async ({meetingSeriesId, createdAt}, _args, {dataLoader}) => {
+    if (meetingSeriesId) {
+      const series = await dataLoader.get('meetingSeries').load(meetingSeriesId)
+      if (!series || series.cancelledAt) {
+        return null
+      }
+
+      const r = await getRethink()
+      const meetings = await r
+        .table('NewMeeting')
+        .getAll(meetingSeriesId, {index: 'meetingSeriesId'})
+        .filter({meetingType: 'teamPrompt'})
+        .filter((row) => row('createdAt').lt(createdAt))
+        .orderBy(r.desc('createdAt'))
+        .limit(1)
+        .run()
+
+      return meetings[0] as MeetingTeamPrompt
+    }
+    return null
+  },
+  nextMeeting: async ({meetingSeriesId, createdAt}, _args, {dataLoader}) => {
+    if (meetingSeriesId) {
+      const series = await dataLoader.get('meetingSeries').load(meetingSeriesId)
+      if (!series || series.cancelledAt) {
+        return null
+      }
+
+      const r = await getRethink()
+      const meetings = await r
+        .table('NewMeeting')
+        .getAll(meetingSeriesId, {index: 'meetingSeriesId'})
+        .filter({meetingType: 'teamPrompt'})
+        .filter((doc) => doc('createdAt').gt(createdAt))
+        .orderBy(r.asc('createdAt'))
+        .limit(1)
+        .run()
+
+      return meetings[0] as MeetingTeamPrompt
     }
     return null
   }
