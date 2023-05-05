bf55352e913855928de2d3040be4828adfacd48e
Nick O'Ferrall <nickoferrall@gmail.com>
2023-05-05 15:48:22 +0200

    calculate amount

--- packages/server/graphql/public/mutations/createSetupIntent.ts
+++ packages/server/graphql/public/mutations/createSetupIntent.ts
@@ -1,3 +1,4 @@
+import {MONTHLY_PRICE} from 'parabol-client/utils/constants'
 import {getUserId} from '../../../utils/authorization'
 import {getStripeManager} from '../../../utils/stripe'
 import {MutationResolvers} from '../resolverTypes'
@@ -8,11 +9,17 @@ const createSetupIntent: MutationResolvers['createSetupIntent'] = async (
   {authToken, dataLoader}
 ) => {
   const viewerId = getUserId(authToken)
-  const viewer = await dataLoader.get('users').loadNonNull(viewerId)
+  const [viewer, organizationUsers] = await Promise.all([
+    dataLoader.get('users').loadNonNull(viewerId),
+    dataLoader.get('organizationUsersByOrgId').load(orgId)
+  ])
+  const inactiveUserCount = organizationUsers.filter(({inactive}) => inactive).length
+  const activeUserCount = organizationUsers.length - inactiveUserCount
   const {email} = viewer
   const manager = getStripeManager()
   const customer = await manager.createCustomer(orgId, email)
-  const amount = 1000
+
+  const amount = MONTHLY_PRICE * activeUserCount
   const paymentIntent = await manager.createPaymentIntent(customer.id, amount)
 
   const {client_secret: clientSecret} = paymentIntent
