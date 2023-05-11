98723cfad4cb914080b12b4d1a4c8923ac9d1cae
Nick O'Ferrall <nickoferrall@gmail.com>
2023-05-11 14:19:16 +0200

    clean up unused code

--- packages/client/components/GcalConfigMenu.tsx
+++ packages/client/components/GcalConfigMenu.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import useAtmosphere from '../hooks/useAtmosphere'
 import {MenuProps} from '../hooks/useMenu'
 import {MenuMutationProps} from '../hooks/useMutationProps'
 import Menu from './Menu'
@@ -8,24 +7,18 @@ import MenuItem from './MenuItem'
 interface Props {
   menuProps: MenuProps
   mutationProps: MenuMutationProps
-  // teamId: string
 }
 
 const GcalConfigMenu = (props: Props) => {
   const {menuProps, mutationProps} = props
-  const {onError, onCompleted, submitMutation, submitting} = mutationProps
-  const atmosphere = useAtmosphere()
+  const {submitMutation, submitting} = mutationProps
   const openOAuth = () => {
-    // GitHubClientManager.openOAuth(atmosphere, teamId, mutationProps)
+    // TODO: implement
   }
 
   const removeGcal = () => {
     if (submitting) return
     submitMutation()
-    // wait for the portal to animate closed before removing, otherwise it'll stick around forever
-    // setTimeout(() => {
-    //   RemoveGcalAuthMutation(atmosphere, {teamId}, {onCompleted, onError})
-    // }, Duration.PORTAL_CLOSE)
   }
   return (
     <Menu ariaLabel={'Configure your Google Calendar integration'} {...menuProps}>
--- packages/client/modules/teamDashboard/components/ProviderRow/GcalProviderRow.tsx
+++ packages/client/modules/teamDashboard/components/ProviderRow/GcalProviderRow.tsx
@@ -1,33 +1,18 @@
-import graphql from 'babel-plugin-relay/macro'
 import React from 'react'
-import {useFragment} from 'react-relay'
-import GitHubConfigMenu from '../../../../components/GitHubConfigMenu'
 import GcalProviderLogo from '../../../../components/GcalProviderLogo'
-import useAtmosphere from '../../../../hooks/useAtmosphere'
 import {MenuPosition} from '../../../../hooks/useCoords'
 import useMenu from '../../../../hooks/useMenu'
 import useMutationProps, {MenuMutationProps} from '../../../../hooks/useMutationProps'
 import {Providers} from '../../../../types/constEnums'
-import GitHubClientManager from '../../../../utils/GitHubClientManager'
-import {GcalProviderRow_viewer$key} from '../../../../__generated__/GcalProviderRow_viewer.graphql'
 import ProviderRow from './ProviderRow'
 import GcalConfigMenu from '../../../../components/GcalConfigMenu'
 
-interface Props {
-  // teamId: string
-  // viewer: GcalProviderRow_viewer$key
-}
-
-const GcalProviderRow = (props: Props) => {
+const GcalProviderRow = () => {
   const {submitting, submitMutation, onError, onCompleted} = useMutationProps()
-  const atmosphere = useAtmosphere()
   const mutationProps = {submitting, submitMutation, onError, onCompleted} as MenuMutationProps
-  // const {teamMember} = viewer
-  // const {integrations} = teamMember!
-  // const {github} = integrations
   const gcalAuth = false
   const openOAuth = () => {
-    // GitHubClientManager.openOAuth(atmosphere, teamId, mutationProps)
+    // TODO: implement
   }
   const {togglePortal, originRef, menuPortal, menuProps} = useMenu(MenuPosition.UPPER_RIGHT)
 
