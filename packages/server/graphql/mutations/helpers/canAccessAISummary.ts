a282f062172690ea357f5ba518f72f248569e22e
Nick O'Ferrall <nickoferrall@gmail.com>
2023-05-15 14:44:35 +0100

    chore: AI opt out at org level (#8163)
    * add noAISummary as org feature flag
    
    * check org feature flag in canAccessAISummary

--- packages/server/graphql/mutations/helpers/canAccessAISummary.ts
+++ packages/server/graphql/mutations/helpers/canAccessAISummary.ts
@@ -1,10 +1,16 @@
 import {Threshold} from 'parabol-client/types/constEnums'
 import {Team} from '../../../postgres/queries/getTeamsByIds'
+import {DataLoaderWorker} from '../../graphql'
 
-const canAccessAISummary = (team: Team, featureFlags: string[]) => {
+const canAccessAISummary = async (
+  team: Team,
+  featureFlags: string[],
+  dataLoader: DataLoaderWorker
+) => {
   if (featureFlags.includes('noAISummary') || !team) return false
-
-  const {qualAIMeetingsCount, tier} = team
+  const {qualAIMeetingsCount, tier, orgId} = team
+  const organization = await dataLoader.get('organizations').load(orgId)
+  if (organization.featureFlags?.includes('noAISummary')) return false
   if (tier !== 'starter') return true
   return qualAIMeetingsCount < Threshold.MAX_QUAL_AI_MEETINGS
 }
