9c7f585eb8d12def858c49f17f3f5a1874b41e2f
Georg Bremer <github@dschoordsch.de>
2023-05-16 17:18:25 +0200

    Bring the migration into the future
    or present

--- packages/server/postgres/migrations/1684250022646_teamHealthPhase.ts
+++ packages/server/postgres/migrations/1684250022646_teamHealthPhase.ts
@@ -1,5 +1,15 @@
-import {Client} from 'pg'
-import getPgConfig from '../getPgConfig'
+import {r} from 'rethinkdb-ts'
+import {ParabolR} from '../../database/rethinkDriver'
+
+const connectRethinkDB = async () => {
+  const {hostname: host, port, pathname} = new URL(process.env.RETHINKDB_URL!)
+  await r.connectPool({
+    host,
+    port: parseInt(port, 10),
+    db: pathname.split('/')[1]
+  })
+  return r as any as ParabolR
+}
 
 export async function up() {
   // Noop, using the feature will however create data which is not backwards compatible, thus only a down migration
