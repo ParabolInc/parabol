37f0b96ebe5dd6a4a2232efda7bfafa5fbaa41e5
Nick O'Ferrall <nickoferrall@gmail.com>
2023-05-17 18:55:09 +0100

    revert pg.d.ts change

--- packages/server/postgres/pg.d.ts
+++ packages/server/postgres/pg.d.ts
@@ -181,10 +181,10 @@ export interface MeetingTemplate {
   type: 'action' | 'poker' | 'retrospective' | 'teamPrompt'
   isStarter: Generated<boolean>
   isFree: Generated<boolean>
-  illustrationUrl: string | null
+  illustrationUrl: string
   hideStartingAt: Timestamp | null
   hideEndingAt: Timestamp | null
-  mainCategory: string | null
+  mainCategory: string
 }
 
 export interface MeetingTemplateUserFavorite {
