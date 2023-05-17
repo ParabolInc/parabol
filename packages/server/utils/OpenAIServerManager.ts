1c3e5290cf74888a19ec8e82c953c2b298d8a389
Bruce Tian <tianrunhe@gmail.com>
2023-05-17 16:26:47 -0700

    Increase max_tokens to 4096

--- packages/server/utils/OpenAIServerManager.ts
+++ packages/server/utils/OpenAIServerManager.ts
@@ -35,7 +35,7 @@ class OpenAIServerManager {
           }
         ],
         temperature: 0.7,
-        max_tokens: 256,
+        max_tokens: 4096,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0
