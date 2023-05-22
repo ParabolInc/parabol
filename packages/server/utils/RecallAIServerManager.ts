95cc7ca992d1b78493ffcf03109e54fc4eef98c0
Nick O'Ferrall <nickoferrall@gmail.com>
2023-05-22 15:36:53 +0100

    fix conflict get base64Image

--- packages/server/utils/RecallAIServerManager.ts
+++ packages/server/utils/RecallAIServerManager.ts
@@ -1,4 +1,6 @@
 import api from 'api'
+import axios from 'axios'
+import {ExternalLinks} from '../../client/types/constEnums'
 
 const sdk = api('@recallai/v1.6#536jnqlf7d6blh')
 
@@ -11,6 +13,23 @@ type TranscriptWord = {
   text: string
 }
 
+const getBase64Image = async () => {
+  try {
+    const imageUrl = ExternalLinks.LOGO
+    const response = await axios({
+      method: 'get',
+      url: imageUrl,
+      responseType: 'arraybuffer'
+    })
+    const buffer = Buffer.from(response.data, 'binary')
+    const base64Image = buffer.toString('base64')
+    return base64Image
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
 class RecallAIServerManager {
   constructor() {
     sdk.auth(`Token ${process.env.RECALL_AI_KEY}`)
@@ -18,6 +37,11 @@ class RecallAIServerManager {
 
   async createBot(videoMeetingURL: string) {
     try {
+      const base64Image = await getBase64Image()
+      if (!base64Image) return null
+
+      const PROTOCOL = process.env.GRAPHQL_PROTOCOL || 'http'
+      const HOST = process.env.GRAPHQL_HOST || 'localhost:3000'
       const {data} = await sdk.bot_create({
         bot_name: 'Parabol Notetaker',
         real_time_transcription: {
@@ -25,8 +49,17 @@ class RecallAIServerManager {
           destination_url: 'http://localhost:3000' // this is required by the API but it's not doing anything and can be any URL. TODO: speak with recall.ai about this & fix
         },
         transcription_options: {provider: 'assembly_ai'},
-        automatic_video_output: {in_call_recording: {kind: 'jpeg', b64_data: 'SGVsbG8gV29ybGQh'}}, // b64 data is for the bot img. It's also a required field, but can be any string I believe. TODO: speak with recall.ai about removing/changing this requirement
+        chat: {
+          on_bot_join: {send_to: 'everyone', message: 'Parabol Notetaker has joined the call'}
+        },
+        automatic_leave: {
+          waiting_room_timeout: 1200,
+          noone_joined_timeout: 1200,
+          everyone_left_timeout: 2
+        },
+        automatic_video_output: {in_call_recording: {kind: 'jpeg', b64_data: base64Image}},
         recording_mode: 'speaker_view',
+        recording_mode_options: {participant_video_when_screenshare: 'hide'},
         meeting_url: videoMeetingURL
       })
       const {id: botId} = data
@@ -48,6 +81,7 @@ class RecallAIServerManager {
         const text = words.map((word) => word.text).join(' ')
         return `${speaker}: ${text}`
       })
+      console.log('🚀 ~ transcript:', transcript)
       return transcript.join('\n') as string
     } catch (err) {
       console.error(err)
