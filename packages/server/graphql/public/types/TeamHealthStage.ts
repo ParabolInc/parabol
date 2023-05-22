cd883ba2fb499fe497561d24e2ed66e4fe776af9
Georg Bremer <github@dschoordsch.de>
2023-05-22 13:09:07 +0200

    Cleanup

--- packages/server/graphql/public/types/TeamHealthStage.ts
+++ packages/server/graphql/public/types/TeamHealthStage.ts
@@ -8,25 +8,25 @@ export type TeamHealthStageSource = TeamHealthStageDB & {
 
 const TeamHealthStage: TeamHealthStageResolvers = {
   __isTypeOf: ({phaseType}) => phaseType === 'TEAM_HEALTH',
-  meeting: ({meetingId}, _args: any, {dataLoader}) => dataLoader.get('newMeetings').load(meetingId),
-  phase: async ({meetingId, phaseType}, _args: any, {dataLoader}) => {
+  meeting: ({meetingId}, _args, {dataLoader}) => dataLoader.get('newMeetings').load(meetingId),
+  phase: async ({meetingId, phaseType}, _args, {dataLoader}) => {
     const meeting = await dataLoader.get('newMeetings').load(meetingId)
     const {phases} = meeting
     return phases.find((phase) => phase.phaseType === phaseType)
   },
-  isViewerReady: ({readyToAdvance}: {readyToAdvance?: string[]}, _args: any, {authToken}) => {
+  isViewerReady: ({readyToAdvance}, _args, {authToken}) => {
     const viewerId = getUserId(authToken)
     return readyToAdvance?.includes(viewerId) ?? false
   },
-  readyCount: async ({meetingId, readyToAdvance}, _args: any, {dataLoader}, ref: any) => {
+  readyCount: async ({meetingId, readyToAdvance}, _args, {dataLoader}, ref) => {
     if (!readyToAdvance) return 0
     if (!meetingId) console.log('no meetingid', ref)
     const meeting = await dataLoader.get('newMeetings').load(meetingId)
     const {facilitatorUserId} = meeting
-    return readyToAdvance.filter((userId: string) => userId !== facilitatorUserId).length
+    return readyToAdvance.filter((userId) => userId !== facilitatorUserId).length
   },
-  timeRemaining: ({scheduledEndTime}: {scheduledEndTime?: Date | null}) => {
-    return scheduledEndTime ? (scheduledEndTime as any) - Date.now() : null
+  timeRemaining: ({scheduledEndTime}) => {
+    return scheduledEndTime ? scheduledEndTime.valueOf() - Date.now() : null
   },
   scores: ({id: stageId, scores}) => {
     return (
