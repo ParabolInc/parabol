30b5b7f580f147fbbb7f491347fe21f56e7d48c7
Nick O'Ferrall <nickoferrall@gmail.com>
2023-06-06 17:55:03 +0200

    feat(checkout-flow): credit card UI (#8318)
    * get credit card ui looking pretty
    
    * get card number instead of cardelememt
    
    * fix jumpiness
    
    * clean up
    
    * check tier on client and server
    
    * update client tier check
    
    * show error message after each cc input
    
    * disable upgrade button if all cc info isnt complete and valid
    
    * make handleChange more concise

--- packages/client/modules/userDashboard/components/OrgBilling/BillingForm.tsx
+++ packages/client/modules/userDashboard/components/OrgBilling/BillingForm.tsx
@@ -1,6 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, FormEvent} from 'react'
 import styled from '@emotion/styled'
-import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js'
+import {
+  CardNumberElement,
+  CardExpiryElement,
+  CardCvcElement,
+  useStripe,
+  useElements
+} from '@stripe/react-stripe-js'
+
 import PrimaryButton from '../../../../components/PrimaryButton'
 import {PALETTE} from '../../../../styles/paletteV3'
 import Confetti from '../../../../components/Confetti'
@@ -10,7 +17,7 @@ import useMutationProps from '../../../../hooks/useMutationProps'
 import StyledError from '../../../../components/StyledError'
 import {UpgradeToTeamTierMutation$data} from '../../../../__generated__/UpgradeToTeamTierMutation.graphql'
 import SendClientSegmentEventMutation from '../../../../mutations/SendClientSegmentEventMutation'
-import {StripeCardElementChangeEvent} from '@stripe/stripe-js'
+import {StripeElementChangeEvent} from '@stripe/stripe-js'
 
 const ButtonBlock = styled('div')({
   display: 'flex',
@@ -19,15 +26,6 @@ const ButtonBlock = styled('div')({
   wrap: 'nowrap',
   flexDirection: 'column',
   width: '100%'
-})
-
-const StyledForm = styled('form')({
-  display: 'flex',
-  height: '100%',
-  width: '100%',
-  flexWrap: 'nowrap',
-  flexDirection: 'column',
-  alignItems: 'space-between'
 })
 
 const UpgradeButton = styled(PrimaryButton)<{isDisabled: boolean}>(({isDisabled}) => ({
@@ -55,8 +53,7 @@ const ErrorMsg = styled(StyledError)({
   textTransform: 'none'
 })
 
-const CARD_OPTIONS = {
-  hidePostalCode: true,
+const CARD_ELEMENT_OPTIONS = {
   style: {
     base: {
       color: PALETTE.SLATE_800,
@@ -64,14 +61,8 @@ const CARD_OPTIONS = {
       fontSmoothing: 'antialiased',
       fontSize: '16px',
       '::placeholder': {
-        color: PALETTE.SLATE_800
-      },
-      marginBottom: '16px',
-      padding: '12px 16px'
-    },
-    invalid: {
-      color: PALETTE.TOMATO_500,
-      iconColor: PALETTE.TOMATO_500
+        color: PALETTE.SLATE_600
+      }
     }
   }
 }
@@ -90,14 +81,37 @@ const BillingForm = (props: Props) => {
   const {onError} = useMutationProps()
   const [errorMsg, setErrorMsg] = useState<null | string>()
   const [hasStarted, setHasStarted] = useState(false)
+  const [cardNumberError, setCardNumberError] = useState<null | string>()
+  const [expiryDateError, setExpiryDateError] = useState<null | string>()
+  const [cvcError, setCvcError] = useState<null | string>()
+  const [cardNumberComplete, setCardNumberComplete] = useState(false)
+  const [expiryDateComplete, setExpiryDateComplete] = useState(false)
+  const [cvcComplete, setCvcComplete] = useState(false)
+  const hasValidCCDetails =
+    cardNumberComplete &&
+    expiryDateComplete &&
+    cvcComplete &&
+    !cardNumberError &&
+    !expiryDateError &&
+    !cvcError
+  const isUpgradeDisabled = isLoading || !stripe || !elements || !hasValidCCDetails
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (!stripe || !elements) return
     setIsLoading(true)
-    if (errorMsg) setErrorMsg(null)
-    const cardElement = elements.getElement(CardElement)
-    if (!cardElement) return
+    if (errorMsg) {
+      setIsLoading(false)
+      setErrorMsg(null)
+      return
+    }
+    const cardElement = elements.getElement(CardNumberElement)
+    if (!cardElement) {
+      setIsLoading(false)
+      const newErrorMsg = 'Something went wrong. Please try again.'
+      setErrorMsg(newErrorMsg)
+      return
+    }
     const {paymentMethod, error} = await stripe.createPaymentMethod({
       type: 'card',
       card: cardElement
@@ -105,6 +119,7 @@ const BillingForm = (props: Props) => {
 
     if (error) {
       setErrorMsg(error.message)
+      setIsLoading(false)
       return
     }
 
@@ -112,16 +127,16 @@ const BillingForm = (props: Props) => {
       const {upgradeToTeamTier} = res
       const stripeSubscriptionClientSecret = upgradeToTeamTier?.stripeSubscriptionClientSecret
       if (!stripeSubscriptionClientSecret) {
+        setErrorMsg('Something went wrong. Please try again.')
         setIsLoading(false)
         return
       }
       const {error} = await stripe.confirmCardPayment(stripeSubscriptionClientSecret)
+      setIsLoading(false)
       if (error) {
         setErrorMsg(error.message)
-        setIsLoading(false)
         return
       }
-      setIsLoading(false)
       setIsPaymentSuccessful(true)
     }
 
@@ -132,29 +147,99 @@ const BillingForm = (props: Props) => {
     )
   }
 
-  const handleChange = (event: StripeCardElementChangeEvent) => {
-    if (!hasStarted && !event.empty) {
-      SendClientSegmentEventMutation(atmosphere, 'Payment Details Started', {orgId})
-      setHasStarted(true)
-    }
-    if (event.complete) {
-      SendClientSegmentEventMutation(atmosphere, 'Payment Details Complete', {orgId})
-    }
-  }
+  const handleChange =
+    (type: 'CardNumber' | 'ExpiryDate' | 'CVC') => (event: StripeElementChangeEvent) => {
+      if (errorMsg) setErrorMsg(null)
+      if (!hasStarted && !event.empty) {
+        SendClientSegmentEventMutation(atmosphere, 'Payment Details Started', {orgId})
+        setHasStarted(true)
+      }
+      if (event.complete) {
+        SendClientSegmentEventMutation(atmosphere, 'Payment Details Complete', {orgId})
+      }
 
+      const errorSetters = {
+        CardNumber: setCardNumberError,
+        ExpiryDate: setExpiryDateError,
+        CVC: setCvcError
+      }
+
+      const completionSetters = {
+        CardNumber: setCardNumberComplete,
+        ExpiryDate: setExpiryDateComplete,
+        CVC: setCvcComplete
+      }
+
+      if (event.error) {
+        errorSetters[type](event.error.message)
+      } else {
+        errorSetters[type](null)
+      }
+
+      completionSetters[type](event.complete)
+    }
+
+  if (!stripe || !elements) return null
   return (
-    <StyledForm id='payment-form' onSubmit={handleSubmit}>
-      <CardElement onChange={handleChange} options={CARD_OPTIONS} />
+    <form onSubmit={handleSubmit}>
+      <div className='mb-4'>
+        <label className='block text-left text-xs font-semibold uppercase text-slate-600'>
+          Card number
+        </label>
+
+        <div className='mt-1'>
+          <CardNumberElement
+            className='focus:ring-indigo-500 focus:border-indigo-500 block w-full border-b border-slate-400 bg-slate-200 px-4 py-3 shadow-sm outline-none sm:text-sm'
+            options={CARD_ELEMENT_OPTIONS}
+            onChange={handleChange('CardNumber')}
+          />
+          {cardNumberError && <ErrorMsg>{cardNumberError}</ErrorMsg>}
+        </div>
+      </div>
+
+      <div className='flex space-x-5'>
+        <div className='w-1/2'>
+          <label className='block text-left text-xs font-semibold uppercase text-slate-600'>
+            Expiry date
+          </label>
+          <div className='mt-1'>
+            <CardExpiryElement
+              className='focus:ring-indigo-500 focus:border-indigo-500 block w-full border-b border-slate-400 bg-slate-200 px-4 py-3 shadow-sm outline-none sm:text-sm'
+              options={CARD_ELEMENT_OPTIONS}
+              onChange={handleChange('ExpiryDate')}
+            />
+            {expiryDateError && <ErrorMsg>{expiryDateError}</ErrorMsg>}
+          </div>
+        </div>
+        <div className='w-1/2'>
+          <label className='block text-left text-xs font-semibold uppercase text-slate-600'>
+            CVC
+          </label>
+          <div className='mt-1'>
+            <CardCvcElement
+              className='focus:ring-indigo-500 focus:border-indigo-500 block w-full border-b border-slate-400 bg-slate-200 px-4 py-3 shadow-sm outline-none sm:text-sm'
+              options={CARD_ELEMENT_OPTIONS}
+              onChange={handleChange('CVC')}
+            />
+            {cvcError && <ErrorMsg>{cvcError}</ErrorMsg>}
+          </div>
+        </div>
+      </div>
       <ButtonBlock>
         {errorMsg && <ErrorMsg>{errorMsg}</ErrorMsg>}
-        <UpgradeButton size='medium' isDisabled={isLoading || !stripe || !elements} type={'submit'}>
+        <UpgradeButton
+          size='medium'
+          disabled={isUpgradeDisabled}
+          isDisabled={isUpgradeDisabled}
+          type={'submit'}
+        >
           {'Upgrade'}
         </UpgradeButton>
       </ButtonBlock>
       <ConfettiWrapper>
         <Confetti active={isPaymentSuccessful} />
       </ConfettiWrapper>
-    </StyledForm>
+    </form>
   )
 }
 
