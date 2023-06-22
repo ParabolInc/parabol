84a5b744f6aaac0ec19fd3d42db1e0884af5f4a6
Matt Krick <matt.krick@gmail.com>
2023-06-22 12:02:25 -0700

    fix: hide seasonal templates, private templates from same-org teams (#8426)
    * fix: hide same-org scope TEAM templates, show seasonal
    
    Signed-off-by: Matt Krick <matt.krick@gmail.com>
    
    * bump changelog
    
    Signed-off-by: Matt Krick <matt.krick@gmail.com>
    
    * v6.109.1
    
    ---------
    
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/dataloader/customLoaderMakers.ts
+++ packages/server/dataloader/customLoaderMakers.ts
@@ -1,4 +1,5 @@
 import DataLoader from 'dataloader'
+import {sql} from 'kysely'
 import {PARABOL_AI_USER_ID} from '../../client/utils/constants'
 import getRethink, {RethinkSchema} from '../database/rethinkDriver'
 import {RDatum} from '../database/stricterR'
@@ -438,13 +439,13 @@ export const meetingTemplatesByOrgId = (parent: RootDataLoader) => {
         .selectAll()
         .where('orgId', 'in', orgIds)
         .where('isActive', '=', true)
-        // .where(({or, cmpr}) =>
-        //   or([
-        //     cmpr('hideStartingAt', 'is', null),
-        //     sql`make_date(2020 , extract(month from current_date)::integer, extract(day from current_date)::integer) between "hideEndingAt" and "hideStartingAt"`,
-        //     sql`make_date(2019 , extract(month from current_date)::integer, extract(day from current_date)::integer) between "hideEndingAt" and "hideStartingAt"`
-        //   ])
-        // )
+        .where(({or, cmpr}) =>
+          or([
+            cmpr('hideStartingAt', 'is', null),
+            sql`make_date(2020 , extract(month from current_date)::integer, extract(day from current_date)::integer) between "hideEndingAt" and "hideStartingAt"`,
+            sql`make_date(2019 , extract(month from current_date)::integer, extract(day from current_date)::integer) between "hideEndingAt" and "hideStartingAt"`
+          ])
+        )
         .orderBy('createdAt', 'desc')
         .execute()
       return orgIds.map((orgId) => docs.filter((doc) => doc.orgId === orgId))
