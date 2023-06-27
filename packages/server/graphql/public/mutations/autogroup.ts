0e0f6940955255c993c6732b6de0fcf5069e23df
Nick O'Ferrall <nickoferrall@gmail.com>
2023-06-27 14:21:29 +0100

    store resetReflectionGroups in retro meeting

--- packages/server/graphql/public/mutations/autogroup.ts
+++ packages/server/graphql/public/mutations/autogroup.ts
@@ -1,4 +1,5 @@
 import {SubscriptionChannel} from '../../../../client/types/constEnums'
+import getRethink from '../../../database/rethinkDriver'
 import MeetingRetrospective from '../../../database/types/MeetingRetrospective'
 import {getUserId, isTeamMember} from '../../../utils/authorization'
 import publish from '../../../utils/publish'
@@ -12,14 +13,17 @@ const autogroup: MutationResolvers['autogroup'] = async (
   {meetingId}: {meetingId: string},
   context: GQLContext
 ) => {
+  const r = await getRethink()
   const {authToken, dataLoader, socketId: mutatorId} = context
   const viewerId = getUserId(authToken)
   const operationId = dataLoader.share()
   const subOptions = {operationId, mutatorId}
-  const [meeting, reflections] = await Promise.all([
+  const [meeting, reflections, reflectionGroups] = await Promise.all([
     dataLoader.get('newMeetings').load(meetingId),
-    dataLoader.get('retroReflectionsByMeetingId').load(meetingId)
+    dataLoader.get('retroReflectionsByMeetingId').load(meetingId),
+    dataLoader.get('retroReflectionGroupsByMeetingId').load(meetingId)
   ])
+
   if (!meeting) {
     return standardError(new Error('Meeting not found'), {userId: viewerId})
   }
@@ -36,6 +40,18 @@ const autogroup: MutationResolvers['autogroup'] = async (
   if (!isTeamMember(authToken, teamId)) {
     return standardError(new Error('Team not found'), {userId: viewerId})
   }
+
+  const resetReflectionGroups = reflectionGroups.map((group) => {
+    const {id, title} = group
+    const reflectionIds = reflections
+      .filter(({reflectionGroupId}) => reflectionGroupId === id)
+      .map(({id}) => id)
+    return {
+      groupTitle: title ?? '',
+      reflectionIds
+    }
+  })
+  await r.table('NewMeeting').get(meetingId).update({resetReflectionGroups}).run()
 
   await Promise.all(
     autogroupReflectionGroups.flatMap((group) => {
