5886b7485144c42216a702a1f298b623b729c8e2
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-06-27 18:15:36 +0700

    feat(share-summary): Add share topic button to meeting summary (#8331)
    * feat(share-summary): Add share topic button to meeting summary
    
    * renderLoader
    
    * Fix email render
    
    * Fix modal position
    
    * Fix scroll issue
    
    * Fix modal scroll

---
+++ packages/client/components/ShareTopicModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import DialogContainer from './DialogContainer'
+import DialogContent from './DialogContent'
+import DialogTitle from './DialogTitle'
+import PrimaryButton from './PrimaryButton'
+import SecondaryButton from './SecondaryButton'
+
+interface Props {
+  closePortal: () => void
+  stageId: string
+}
+
+// TODO: Create generic dialog components using tailwind https://github.com/ParabolInc/parabol/issues/8107
+const ShareTopicModal = (props: Props) => {
+  const {closePortal} = props
+
+  const onShare = () => {
+    /* TODO */
+  }
+
+  return (
+    <DialogContainer>
+      <DialogTitle>{'Share topic'}</DialogTitle>
+      <DialogContent>
+        <div className={'mb-4 text-base'}>Where would you like to share the topic to?</div>
+
+        <div className={'mt-6 flex w-full justify-end'}>
+          <div className={'mr-2'}>
+            <SecondaryButton onClick={closePortal} size='small'>
+              Cancel
+            </SecondaryButton>
+          </div>
+          <PrimaryButton size='small' onClick={onShare}>
+            Share
+          </PrimaryButton>
+        </div>
+      </DialogContent>
+    </DialogContainer>
+  )
+}
+
+export default ShareTopicModal
---
+++ packages/client/components/ShareTopicRoot.tsx
@@ -0,0 +1,41 @@
+import React, {Suspense, useCallback, useEffect} from 'react'
+import {useHistory, useLocation} from 'react-router'
+import useRouter from '../hooks/useRouter'
+import useModal from '../hooks/useModal'
+import ShareTopicModal from '~/components/ShareTopicModal'
+import {renderLoader} from '../utils/relay/renderLoader'
+
+const ShareTopicRoot = () => {
+  const {match} = useRouter<{stageId: string; meetingId: string}>()
+  const {params} = match
+
+  const {meetingId, stageId} = params
+
+  const location = useLocation<{backgroundLocation?: Location}>()
+  const history = useHistory()
+
+  const onClose = useCallback(() => {
+    const state = location.state
+    history.replace(state?.backgroundLocation ?? `/new-summary/${meetingId}`)
+  }, [location])
+
+  const {openPortal, closePortal, modalPortal} = useModal({
+    id: 'shareTopicModal',
+    onClose
+  })
+
+  useEffect(() => {
+    openPortal()
+    return () => {
+      closePortal()
+    }
+  }, [])
+
+  return (
+    <Suspense fallback={renderLoader()}>
+      {modalPortal(<ShareTopicModal closePortal={closePortal} stageId={stageId} />)}
+    </Suspense>
+  )
+}
+
+export default ShareTopicRoot
