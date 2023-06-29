0a9aea4c04f89ae6282d523db0ff05502b1b24bc
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-06-29 17:54:20 +0400

    chore: add radix select

--- packages/client/components/ShareTopicModal.tsx
+++ packages/client/components/ShareTopicModal.tsx
@@ -8,6 +8,15 @@ import {DialogDescription} from '../ui/Dialog/DialogDescription'
 import {DialogActions} from '../ui/Dialog/DialogActions'
 import {DialogClose} from '../ui/Dialog/DialogClose'
 
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectGroup,
+  SelectValue,
+  SelectContent
+} from '../ui/Select'
+
 interface Props {
   isOpen: boolean
   onClose: () => void
@@ -21,11 +30,31 @@ const ShareTopicModal = (props: Props) => {
     /* TODO */
   }
 
+  const labelStyles = `w-[90px] text-left text-sm font-semibold`
+
   return (
     <SimpleModalDialog isOpen={isOpen} onClose={onClose}>
       <DialogContent>
         <DialogTitle>Share topic</DialogTitle>
         <DialogDescription>Where would you like to share the topic to?</DialogDescription>
+
+        <fieldset className='mx-0 mb-[15px] mb-2 flex items-center gap-5 p-0'>
+          <label className={labelStyles} htmlFor='name'>
+            Integration
+          </label>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder='Select one' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value='slack'>Slack</SelectItem>
+                <SelectItem value='teams'>Teams</SelectItem>
+                <SelectItem value='mattermost'>Mattermost</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </fieldset>
 
         <DialogActions>
           <SecondaryButton onClick={onClose} size='small'>
