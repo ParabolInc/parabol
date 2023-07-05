76b86c0e2e80d04d3e8bc715edf50abd800143ed
James Taber <jmtaber129@gmail.com>
2023-07-05 11:38:00 -0500

    fix: Limit number of teams that can be moved in one moveTeamToOrg call

--- packages/server/graphql/mutations/moveTeamToOrg.ts
+++ packages/server/graphql/mutations/moveTeamToOrg.ts
@@ -14,6 +14,8 @@ import {DataLoaderWorker, GQLContext} from '../graphql'
 import isValid from '../isValid'
 import getKysely from '../../postgres/getKysely'
 
+const MAX_NUM_TEAMS = 40
+
 const moveToOrg = async (
   teamId: string,
   orgId: string,
@@ -149,6 +151,13 @@ export default {
     {teamIds, orgId}: {teamIds: string[]; orgId: string},
     {authToken, dataLoader}: GQLContext
   ) {
+    if (teamIds.length > MAX_NUM_TEAMS) {
+      return standardError(
+        new Error(
+          `Can only move ${MAX_NUM_TEAMS} teams at once. Please split team IDs into batches.`
+        )
+      )
+    }
     const results = [] as (string | any)[]
     for (let i = 0; i < teamIds.length; i++) {
       const teamId = teamIds[i]!
