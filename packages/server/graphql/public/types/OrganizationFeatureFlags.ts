058f36d0ce94031f30c4686febfa970ee2327eb3
Bruce Tian <tianrunhe@gmail.com>
2023-07-08 14:31:39 -0600

    Add AIGeneratedDiscussionPrompt feature flag

--- packages/server/graphql/public/types/OrganizationFeatureFlags.ts
+++ packages/server/graphql/public/types/OrganizationFeatureFlags.ts
@@ -3,6 +3,7 @@ import {OrganizationFeatureFlagsResolvers} from '../resolverTypes'
 const OrganizationFeatureFlags: OrganizationFeatureFlagsResolvers = {
   SAMLUI: ({SAMLUI}) => !!SAMLUI,
   noAISummary: ({noAISummary}) => !!noAISummary,
+  AIGeneratedDiscussionPrompt: ({AIGeneratedDiscussionPrompt}) => !!AIGeneratedDiscussionPrompt,
   promptToJoinOrg: ({promptToJoinOrg}) => !!promptToJoinOrg,
   zoomTranscription: ({zoomTranscription}) => !!zoomTranscription,
   shareSummary: ({shareSummary}) => !!shareSummary,
