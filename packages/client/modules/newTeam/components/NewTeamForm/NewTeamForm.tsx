5732cee7626b237cba0b81f9067c9d077c8cc5b1
Nick O'Ferrall <nickoferrall@gmail.com>
2023-07-11 15:36:55 +0100

    change checkbox to an Invite team members button

--- packages/client/modules/newTeam/components/NewTeamForm/NewTeamForm.tsx
+++ packages/client/modules/newTeam/components/NewTeamForm/NewTeamForm.tsx
@@ -2,7 +2,6 @@ import styled from '@emotion/styled'
 import graphql from 'babel-plugin-relay/macro'
 import React, {ChangeEvent, FormEvent, useState} from 'react'
 import {useFragment} from 'react-relay'
-import Checkbox from '../../../../components/Checkbox'
 import DashHeaderTitle from '../../../../components/DashHeaderTitle'
 import FieldLabel from '../../../../components/FieldLabel/FieldLabel'
 import BasicTextArea from '../../../../components/InputField/BasicTextArea'
@@ -125,7 +124,6 @@ const NewTeamForm = (props: Props) => {
   const [invitees, setInvitees] = useState([] as string[])
   const [isSubmitted, setIsSubmitted] = useState(false)
   const lockedSelectedOrg = organizations.find((org) => org.id === orgId && org.lockedAt)
-  const [inviteAll, setInviteAll] = useState(false)
   const disableFields = !!lockedSelectedOrg && !isNewOrg
   const selectedOrg = organizations.find((org) => org.id === orgId)
   const selectedOrgTeamMemberEmails = selectedOrg?.teams.flatMap(({teamMembers}) =>
@@ -231,35 +229,23 @@ const NewTeamForm = (props: Props) => {
     setInvitees(uniqueInvitees)
   }
 
-  const handleToggleInviteAll = () => {
-    if (!inviteAll) {
-      const {parsedInvitees} = parseEmailAddressList(rawInvitees)
-      const currentInvitees = parsedInvitees
-        ? (parsedInvitees.map((invitee: any) => invitee.address) as string[])
-        : []
-      const emailsToAdd = uniqueEmailsFromSelectedOrg.filter(
-        (email) => !currentInvitees.includes(email)
-      )
-      const lastInvitee = currentInvitees[currentInvitees.length - 1]
-      const formattedCurrentInvitees =
-        currentInvitees.length && lastInvitee && !lastInvitee.endsWith(',')
-          ? `${currentInvitees.join(', ')}, `
-          : currentInvitees.join(', ')
-      setRawInvitees(`${formattedCurrentInvitees}${emailsToAdd.join(', ')}`)
-      setInvitees([...currentInvitees, ...emailsToAdd])
-    } else {
-      const {parsedInvitees} = parseEmailAddressList(rawInvitees)
-      const currentInvitees = parsedInvitees
-        ? (parsedInvitees.map((invitee: any) => invitee.address) as string[])
-        : []
-      const remainingInvitees = currentInvitees.filter(
-        (email) => !uniqueEmailsFromSelectedOrg.includes(email)
-      )
-      setRawInvitees(remainingInvitees.join(', '))
-      setInvitees(remainingInvitees)
-    }
+  const addAllTeamMembers = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation()
+    const {parsedInvitees} = parseEmailAddressList(rawInvitees)
+    const currentInvitees = parsedInvitees
+      ? (parsedInvitees.map((invitee: any) => invitee.address) as string[])
+      : []
+    const emailsToAdd = uniqueEmailsFromSelectedOrg.filter(
+      (email) => !currentInvitees.includes(email)
+    )
+    const lastInvitee = currentInvitees[currentInvitees.length - 1]
+    const formattedCurrentInvitees =
+      currentInvitees.length && lastInvitee && !lastInvitee.endsWith(',')
+        ? `${currentInvitees.join(', ')}, `
+        : currentInvitees.join(', ')
+    setRawInvitees(`${formattedCurrentInvitees}${emailsToAdd.join(', ')}`)
+    setInvitees([...currentInvitees, ...emailsToAdd])
     onCompleted()
-    setInviteAll((inviteAll) => !inviteAll)
   }
 
   return (
@@ -323,12 +309,15 @@ const NewTeamForm = (props: Props) => {
             value={rawInvitees}
           />
           {showInviteAll && (
-            <div className='flex cursor-pointer items-center pt-2' onClick={handleToggleInviteAll}>
-              <Checkbox active={inviteAll} />
-              <label htmlFor='checkbox' className='text-gray-700 ml-2 cursor-pointer'>
-                {`Invite team members from ${selectedOrg.name}`}
-              </label>
-            </div>
+            <button
+              className={
+                'mt-2 flex w-max cursor-pointer items-center rounded-full border border-solid border-slate-400 bg-white px-3 py-2 text-center font-sans text-sm font-semibold text-slate-700 hover:bg-slate-100'
+              }
+              onClick={addAllTeamMembers}
+              type='button'
+            >
+              {`Invite team members`}
+            </button>
           )}
           <StyledButton disabled={disableFields} size='large' waiting={submitting}>
             {isNewOrg ? 'Create Team & Org' : 'Create Team'}
