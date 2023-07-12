032dab76a54a3f16da416ce6dd94de5c374507a4
Matt Krick <matt.krick@gmail.com>
2023-07-12 09:56:07 -0700

    chore: drop other unused tables
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/postgres/migrations/1689031398651_del-team-meetingTemplate.ts
+++ packages/server/postgres/migrations/1689031398651_del-team-meetingTemplate.ts
@@ -13,7 +13,13 @@ export async function up() {
   await connectRethinkDB()
   try {
     // 20220329101759-dropTeams never ran in prod, deleting the Team table here
-    await Promise.allSettled([r.tableDrop('Team').run(), r.tableDrop('MeetingTemplate').run()])
+    await Promise.allSettled([
+      r.tableDrop('Team').run(),
+      r.tableDrop('MeetingTemplate').run(),
+      r.tableDrop('GQLRequest').run(),
+      r.tableDrop('DuplicateEmails').run(),
+      r.tableDrop('SecureDomain').run()
+    ])
   } catch {
     // noop
   }
