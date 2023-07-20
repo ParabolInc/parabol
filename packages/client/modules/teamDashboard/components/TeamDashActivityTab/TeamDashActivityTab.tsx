4f80fc0fc8b16826e968f79837b21d57a10ec1b5
Georg Bremer <github@dschoordsch.de>
2023-07-20 17:24:18 +0200

    Add empty meeting state

--- packages/client/modules/teamDashboard/components/TeamDashActivityTab/TeamDashActivityTab.tsx
+++ packages/client/modules/teamDashboard/components/TeamDashActivityTab/TeamDashActivityTab.tsx
@@ -4,6 +4,8 @@ import {useFragment} from 'react-relay'
 import {TeamDashActivityTab_team$key} from '~/__generated__/TeamDashActivityTab_team.graphql'
 import useTransition from '../../../../hooks/useTransition'
 import MeetingCard from '../../../../components/MeetingCard'
+import DemoMeetingCard from '../../../../components/DemoMeetingCard'
+import TutorialMeetingCard from '../../../../components/TutorialMeetingCard'
 
 interface Props {
   teamRef: TeamDashActivityTab_team$key
@@ -33,20 +35,35 @@ const TeamDashActivityTab = (props: Props) => {
   )
 
   return (
-    <div className='flex h-full w-full'>
-      {transitioningMeetings.map((meeting) => {
-        const {child} = meeting
-        const {id, displayIdx} = child
-        return (
-          <MeetingCard
-            key={id}
-            displayIdx={displayIdx}
-            meeting={meeting.child}
-            onTransitionEnd={meeting.onTransitionEnd}
-            status={meeting.status}
-          />
-        )
-      })}
+    <div className='flex h-full w-full flex-1 flex-col overflow-auto pl-4'>
+      <div className='flex flex-col pl-2'>
+        <h3 className='mb-0 text-base font-semibold'>Open Meetings</h3>
+        {transitioningMeetings.length === 0 && (
+          <p className='my-2'>No meetings yet? You've come to the right place!</p>
+        )}
+      </div>
+      <div className='flex w-full flex-wrap'>
+        {transitioningMeetings.length > 0 ? (
+          transitioningMeetings.map((meeting) => {
+            const {child} = meeting
+            const {id, displayIdx} = child
+            return (
+              <MeetingCard
+                key={id}
+                displayIdx={displayIdx}
+                meeting={meeting.child}
+                onTransitionEnd={meeting.onTransitionEnd}
+                status={meeting.status}
+              />
+            )
+          })
+        ) : (
+          <>
+            <DemoMeetingCard />
+            <TutorialMeetingCard />
+          </>
+        )}
+      </div>
     </div>
   )
 }
