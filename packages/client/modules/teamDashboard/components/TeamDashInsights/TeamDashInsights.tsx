52b76888803489ecb56eac2383c33c65247a425d
Georg Bremer <github@dschoordsch.de>
2023-07-26 12:13:45 +0200

    Add tooltip

--- packages/client/modules/teamDashboard/components/TeamDashInsights/TeamDashInsights.tsx
+++ packages/client/modules/teamDashboard/components/TeamDashInsights/TeamDashInsights.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import {useFragment} from 'react-relay'
 import {TeamDashInsights_insights$key} from '~/__generated__/TeamDashInsights_insights.graphql'
 import emojis from '../../../../utils/emojis'
+import Tooltip from '../../../../components/Tooltip'
+import {Info as InfoIcon} from '@mui/icons-material'
 
 interface Props {
   teamInsightsRef: TeamDashInsights_insights$key
@@ -31,7 +33,12 @@ const TeamDashInsights = (props: Props) => {
       <div className='flex w-full flex-wrap'>
         {mostUsedEmojis && mostUsedEmojis.length > 0 && (
           <div className='relative m-2 flex w-[320px] flex-col rounded bg-white drop-shadow'>
-            <div className='p-4 text-sm font-semibold text-slate-600'>Favorite Emojis</div>
+            <div className='flex items-center justify-between'>
+              <div className='p-4 text-sm font-semibold text-slate-600'>Favorite Reactions</div>
+              <Tooltip text='Your team’s most used emoji reactions' className='pr-3 text-slate-600'>
+                <InfoIcon />
+              </Tooltip>
+            </div>
             <div className='flex flex-row justify-center'>
               {mostUsedEmojis.map((emoji) => (
                 <div
