e7fceedb0996ccc9a09bbb9999080f736261f3d3
Taylor Hanken <taylor@parabol.co>
2023-08-07 08:30:12 -0700

    Move migration to postgres folder

--- packages/server/database/migrations/20230801200811-fixOrganizationTier.ts
+++
@@ -1,9 +0,0 @@
-import {R} from 'rethinkdb-ts'
-
-export const up = async function (r: R) {
-  r.table('Organization').filter(r.row('tier').eq('personal')).update({tier: 'starter'}).run()
-}
-
-export const down = async function (r: R) {
-  // noop
-}
