a45b9afbeddc720ba64ae03993d7029b24c450af
James Taber <jmtaber129@gmail.com>
2023-08-08 15:45:31 -0400

    feat(standups): Standup AI Summaries (#8569)
    * feat(standups): Standup AI Summaries
    
    * Add feature flag
    
    * Minor cleanup
    
    * CR: Change model + make standup summaries free
    
    * Use dashes over asterisks for bullets
    
    * CR: Fix renaming
    
    * CR: Refactor 'canAccessAISummary' and remove copy-pasta

---
+++ packages/server/graphql/mutations/helpers/generateStandupMeetingSummary.ts
@@ -0,0 +1,34 @@
+import OpenAIServerManager from '../../../utils/OpenAIServerManager'
+import {DataLoaderWorker} from '../../graphql'
+import canAccessAISummary from './canAccessAISummary'
+import {getTeamPromptResponsesByMeetingId} from '../../../postgres/queries/getTeamPromptResponsesByMeetingIds'
+import MeetingTeamPrompt from '../../../database/types/MeetingTeamPrompt'
+
+const generateStandupMeetingSummary = async (
+  meeting: MeetingTeamPrompt,
+  dataLoader: DataLoaderWorker
+) => {
+  const [facilitator, team] = await Promise.all([
+    dataLoader.get('users').loadNonNull(meeting.facilitatorUserId),
+    dataLoader.get('teams').loadNonNull(meeting.teamId)
+  ])
+  const isAISummaryAccessible = await canAccessAISummary(
+    team,
+    facilitator.featureFlags,
+    dataLoader,
+    'standup'
+  )
+
+  if (!isAISummaryAccessible) return
+  const responses = await getTeamPromptResponsesByMeetingId(meeting.id)
+
+  const contentToSummarize = responses.map((response) => response.plaintextContent)
+  if (contentToSummarize.length === 0) return
+
+  const manager = new OpenAIServerManager()
+  const summary = await manager.getStandupSummary(contentToSummarize, meeting.meetingPrompt)
+  if (!summary) return
+  return summary
+}
+
+export default generateStandupMeetingSummary
