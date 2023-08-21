f4dabc56ac28e25f80e1c79aed478965e0cc32f4
Nick O'Ferrall <nickoferrall@gmail.com>
2023-08-21 19:49:06 +0100

    update valid email test

--- packages/server/__tests__/inviteToTeam.test.ts
+++ packages/server/__tests__/inviteToTeam.test.ts
@@ -1,8 +1,7 @@
 import {getUserTeams, sendPublic, signUp} from './common'
 
-test('Invite to team, works for ordinary email domain', async () => {
+test('Invite to team, works for valid email', async () => {
   const {userId, authToken} = await signUp()
-  const {email: inviteeEmail} = await signUp()
 
   const teamId = (await getUserTeams(userId))[0].id
   const inviteToTeam = await sendPublic({
@@ -19,7 +18,7 @@ test('Invite to team, works for ordinary email domain', async () => {
     `,
     variables: {
       teamId,
-      invitees: ['foo@example.com', inviteeEmail]
+      invitees: ['nick@parabol.co']
     },
     authToken
   })
@@ -28,43 +27,7 @@ test('Invite to team, works for ordinary email domain', async () => {
     data: {
       inviteToTeam: {
         error: null,
-        invitees: ['foo@example.com', inviteeEmail]
-      }
-    }
-  })
-})
-
-test('Invite to team, deny for untrusted domain', async () => {
-  const {userId, authToken} = await signUp()
-  const {email: inviteeEmail} = await signUp()
-
-  const teamId = (await getUserTeams(userId))[0].id
-  const inviteToTeam = await sendPublic({
-    query: `
-      mutation InviteToTeam($teamId: ID!, $invitees: [Email!]!) {
-        inviteToTeam(teamId: $teamId, invitees: $invitees) {
-          error {
-            title
-            message
-          }
-          invitees
-        }
-      }
-    `,
-    variables: {
-      teamId,
-      invitees: ['foo@qq.com', inviteeEmail]
-    },
-    authToken
-  })
-
-  expect(inviteToTeam).toMatchObject({
-    data: {
-      inviteToTeam: {
-        error: {
-          message: 'Cannot invite by email. Try using invite link',
-          title: null
-        }
+        invitees: ['nick@parabol.co']
       }
     }
   })
