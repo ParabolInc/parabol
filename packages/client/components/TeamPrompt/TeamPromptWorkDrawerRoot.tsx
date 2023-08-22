41f8a3ee243387f57fcca430229240e74bdb1cb5
James Taber <jmtaber129@gmail.com>
2023-08-22 14:59:59 -0500

    Do mutation

--- packages/client/components/TeamPrompt/TeamPromptWorkDrawerRoot.tsx
+++ packages/client/components/TeamPrompt/TeamPromptWorkDrawerRoot.tsx
@@ -4,16 +4,18 @@ import useQueryLoaderNow from '../../hooks/useQueryLoaderNow'
 import teamPromptWorkDrawerQuery, {
   TeamPromptWorkDrawerQuery
 } from '../../__generated__/TeamPromptWorkDrawerQuery.graphql'
+import {TeamPromptWorkDrawer_meeting$key} from '../../__generated__/TeamPromptWorkDrawer_meeting.graphql'
 import ErrorBoundary from '../ErrorBoundary'
 import TeamPromptWorkDrawer from './TeamPromptWorkDrawer'
 import {renderLoader} from '~/utils/relay/renderLoader'
 
 interface Props {
   onToggleDrawer: () => void
+  meetingRef: TeamPromptWorkDrawer_meeting$key
 }
 
 const TeamPromptWorkDrawerRoot = (props: Props) => {
-  const {onToggleDrawer} = props
+  const {meetingRef, onToggleDrawer} = props
   const atmosphere = useAtmosphere()
   const queryRef = useQueryLoaderNow<TeamPromptWorkDrawerQuery>(teamPromptWorkDrawerQuery, {
     userIds: [atmosphere.viewerId]
@@ -21,7 +23,13 @@ const TeamPromptWorkDrawerRoot = (props: Props) => {
   return (
     <ErrorBoundary>
       <Suspense fallback={renderLoader()}>
-        {queryRef && <TeamPromptWorkDrawer queryRef={queryRef} onToggleDrawer={onToggleDrawer} />}
+        {queryRef && (
+          <TeamPromptWorkDrawer
+            queryRef={queryRef}
+            meetingRef={meetingRef}
+            onToggleDrawer={onToggleDrawer}
+          />
+        )}
       </Suspense>
     </ErrorBoundary>
   )
