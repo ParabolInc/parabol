b8ec2c8facbdb3b5c3acd6ccd763453800de24d1
James Taber <jmtaber129@gmail.com>
2023-08-23 18:02:02 -0500

    feat(standups): 'Your work' GitHub integration

--- packages/client/components/TeamPrompt/TeamPromptWorkDrawer.tsx
+++ packages/client/components/TeamPrompt/TeamPromptWorkDrawer.tsx
@@ -17,6 +17,11 @@ import AddTaskButton from '../AddTaskButton'
 import CreateTaskMutation from '../../mutations/CreateTaskMutation'
 import useAtmosphere from '../../hooks/useAtmosphere'
 import dndNoise from '../../utils/dndNoise'
+import Tabs from '../Tabs/Tabs'
+import Tab from '../Tab/Tab'
+import ParabolLogoSVG from '../ParabolLogoSVG'
+import GitHubSVG from '../GitHubSVG'
+import GitHubObjectCard from './WorkDrawer/GitHubObjectCard'
 
 interface Props {
   queryRef: PreloadedQuery<TeamPromptWorkDrawerQuery>
@@ -28,7 +33,7 @@ const TeamPromptWorkDrawer = (props: Props) => {
   const {queryRef, meetingRef, onToggleDrawer} = props
   const data = usePreloadedQuery<TeamPromptWorkDrawerQuery>(
     graphql`
-      query TeamPromptWorkDrawerQuery($after: DateTime, $userIds: [ID!]) {
+      query TeamPromptWorkDrawerQuery($after: DateTime, $teamId: ID!, $userIds: [ID!]) {
         viewer {
           id
           tasks(first: 1000, after: $after, userIds: $userIds)
@@ -41,13 +46,82 @@ const TeamPromptWorkDrawer = (props: Props) => {
               }
             }
           }
+          teamMember(teamId: $teamId) {
+            integrations {
+              github {
+                isActive
+                api {
+                  errors {
+                    message
+                    locations {
+                      line
+                      column
+                    }
+                    path
+                  }
+                  query {
+                    issues: search(
+                      first: 15
+                      type: ISSUE
+                      query: "is:issue sort:updated assignee:@me"
+                    ) @connection(key: "TeamPromptMeeting_issues") {
+                      edges {
+                        node {
+                          __typename
+                          ... on _xGitHubIssue {
+                            id
+                            title
+                            number
+                            repository {
+                              nameWithOwner
+                            }
+                            url
+                            state
+                            updatedAt
+                            lastEvent: timelineItems(last: 1) {
+                              updatedAt
+                            }
+                          }
+                        }
+                      }
+                    }
+                    pullRequests: search(
+                      first: 15
+                      type: ISSUE
+                      query: "is:pr sort:updated author:@me"
+                    ) @connection(key: "TeamPromptMeeting_pullRequests") {
+                      edges {
+                        node {
+                          __typename
+                          ... on _xGitHubPullRequest {
+                            id
+                            title
+                            number
+                            repository {
+                              nameWithOwner
+                            }
+                            url
+                            state
+                            updatedAt
+                            lastEvent: timelineItems(last: 1) {
+                              updatedAt
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
         }
       }
     `,
     queryRef
   )
   const {viewer} = data
-  const {tasks, id: viewerId} = viewer
+  const {tasks, id: viewerId, teamMember} = viewer
 
   const meeting = useFragment(
     graphql`
@@ -66,6 +140,15 @@ const TeamPromptWorkDrawer = (props: Props) => {
     .map((edge) => edge.node)
     .filter((task) => task.status === selectedStatus)
 
+  const github = teamMember?.integrations.github
+
+  const githubIssues = teamMember?.integrations.github?.api?.query?.issues.edges?.map(
+    (edge) => edge?.node
+  )
+  const githubPullRequests = teamMember?.integrations.github?.api?.query?.pullRequests.edges?.map(
+    (edge) => edge?.node
+  )
+
   const handleAddTask = () => {
     CreateTaskMutation(
       atmosphere,
@@ -82,57 +165,150 @@ const TeamPromptWorkDrawer = (props: Props) => {
     )
   }
 
+  const [activeIdx, setActiveIdx] = useState(0)
+  const [githubType, setGithubType] = useState<'issue' | 'pr'>('issue')
+  const githubObjects = githubType === 'issue' ? githubIssues : githubPullRequests
+
   return (
     <>
-      <div className='px-4 pt-4'>
-        <div className='flex justify-between'>
-          <div className='text-base font-semibold'>Your Tasks</div>
-          <div className='cursor-pointer text-slate-600 hover:opacity-50' onClick={onToggleDrawer}>
-            <Close />
-          </div>
-        </div>
-        <div className='my-4 flex gap-2'>
-          {meetingColumnArray.map((status) => (
+      <div className='pt-4'>
+        <div className='border-b border-solid border-slate-300'>
+          <div className='flex justify-between px-4'>
+            <div className='text-base font-semibold'>Your Work</div>
             <div
-              key={status}
-              className={clsx(
-                'flex-shrink-0 cursor-pointer rounded-full py-2 px-4 text-sm leading-3 text-slate-800',
-                status === selectedStatus
-                  ? 'bg-grape-700 font-semibold text-white focus:text-white'
-                  : 'border border-slate-300 bg-white'
-              )}
-              onClick={() => setSelectedStatus(status)}
+              className='cursor-pointer text-slate-600 hover:opacity-50'
+              onClick={onToggleDrawer}
             >
-              {taskStatusLabels[status]}
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className='flex h-full flex-col items-center gap-y-2 overflow-auto px-4 pt-1 pb-4'>
-        {selectedTasks.length > 0 ? (
-          selectedTasks.map((task) => (
-            <NullableTask
-              className='w-full rounded border border-solid border-slate-100'
-              key={task.id}
-              dataCy='foo'
-              area={'userDash'}
-              task={task}
-            />
-          ))
-        ) : (
-          <div className='-mt-14 flex h-full flex-col items-center justify-center'>
-            <img className='w-20' src={halloweenRetrospectiveTemplate} />
-            <div className='mt-7'>
-              You don’t have any <b>{taskStatusLabels[selectedStatus]}</b> tasks.
-              <br />
-              Try adding new tasks below.
+              <Close />
             </div>
           </div>
-        )}
+          <Tabs activeIdx={activeIdx}>
+            <Tab
+              key='parabol'
+              onClick={() => setActiveIdx(0)}
+              label={
+                <div className='flex items-center justify-center'>
+                  <ParabolLogoSVG />
+                </div>
+              }
+            />
+            <Tab
+              key='github'
+              onClick={() => setActiveIdx(1)}
+              label={
+                <div className='flex items-center justify-center'>
+                  <GitHubSVG />
+                </div>
+              }
+            />
+          </Tabs>
+        </div>
       </div>
-      <div className='flex items-center justify-center border-t border-solid border-slate-200 p-2'>
-        <AddTaskButton dataCy={`your-work-task`} onClick={handleAddTask} />
-      </div>
+      {activeIdx === 0 ? (
+        <>
+          <div>
+            <div className='my-4 flex gap-2 px-4'>
+              {meetingColumnArray.map((status) => (
+                <div
+                  key={status}
+                  className={clsx(
+                    'flex-shrink-0 cursor-pointer rounded-full py-2 px-4 text-sm leading-3 text-slate-800',
+                    status === selectedStatus
+                      ? 'bg-grape-700 font-semibold text-white focus:text-white'
+                      : 'border border-slate-300 bg-white'
+                  )}
+                  onClick={() => setSelectedStatus(status)}
+                >
+                  {taskStatusLabels[status]}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='flex h-full flex-col items-center gap-y-2 overflow-auto px-4 pt-1 pb-4'>
+            {selectedTasks.length > 0 ? (
+              selectedTasks.map((task) => (
+                <NullableTask
+                  className='w-full rounded border border-solid border-slate-100'
+                  key={task.id}
+                  dataCy='foo'
+                  area={'userDash'}
+                  task={task}
+                />
+              ))
+            ) : (
+              <div className='-mt-14 flex h-full flex-col items-center justify-center'>
+                <img className='w-20' src={halloweenRetrospectiveTemplate} />
+                <div className='mt-7'>
+                  You don’t have any <b>{taskStatusLabels[selectedStatus]}</b> tasks.
+                  <br />
+                  Try adding new tasks below.
+                </div>
+              </div>
+            )}
+          </div>
+          <div className='flex items-center justify-center border-t border-solid border-slate-200 p-2'>
+            <AddTaskButton dataCy={`your-work-task`} onClick={handleAddTask} />
+          </div>
+        </>
+      ) : (
+        <>
+          {github?.isActive ? (
+            <>
+              <div className='my-4 flex w-full gap-2 px-4'>
+                <div
+                  key={'issue'}
+                  className={clsx(
+                    'grow cursor-pointer rounded-full py-2 px-4 text-center text-sm leading-3 text-slate-800',
+                    'issue' === githubType
+                      ? 'bg-grape-700 font-semibold text-white focus:text-white'
+                      : 'border border-slate-300 bg-white'
+                  )}
+                  onClick={() => setGithubType('issue')}
+                >
+                  Your issues
+                </div>
+                <div
+                  key={'pr'}
+                  className={clsx(
+                    'grow cursor-pointer rounded-full py-2 px-4 text-center text-sm leading-3 text-slate-800',
+                    'pr' === githubType
+                      ? 'bg-grape-700 font-semibold text-white focus:text-white'
+                      : 'border border-slate-300 bg-white'
+                  )}
+                  onClick={() => setGithubType('pr')}
+                >
+                  Your pull requests
+                </div>
+              </div>
+              <div className='flex flex h-full flex-col gap-y-2 overflow-auto px-4'>
+                {githubObjects?.map((object, idx) => {
+                  if (
+                    object?.__typename === '_xGitHubIssue' ||
+                    object?.__typename === '_xGitHubPullRequest'
+                  ) {
+                    return (
+                      <GitHubObjectCard
+                        type={githubType}
+                        key={idx}
+                        title={object.title}
+                        status={object.state}
+                        number={object.number}
+                        repoName={object.repository.nameWithOwner}
+                        url={object.url}
+                        updatedAt={object.lastEvent.updatedAt}
+                      />
+                    )
+                  } else {
+                    return null
+                  }
+                })}
+              </div>
+            </>
+          ) : (
+            <div>needs auth</div>
+          )}
+        </>
+      )}
     </>
   )
 }
---
+++ packages/client/components/TeamPrompt/WorkDrawer/GitHubObjectCard.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import gitHubSVG from '../../../styles/theme/images/graphics/github-circle.svg'
+import gitHubMerged from '../../../styles/theme/images/graphics/github-merged.svg'
+import githubIssueClosed from '../../../styles/theme/images/graphics/github-issue-closed.svg'
+import githubIssueOpen from '../../../styles/theme/images/graphics/github-issue-open.svg'
+import githubPROpen from '../../../styles/theme/images/graphics/github-pr-open.svg'
+import relativeDate from '../../../utils/date/relativeDate'
+
+const ISSUE_STATUS_MAP: Record<string, any> = {
+  OPEN: githubIssueOpen,
+  CLOSED: githubIssueClosed
+}
+
+const PR_STATUS_MAP: Record<string, any> = {
+  OPEN: githubPROpen,
+  MERGED: gitHubMerged
+}
+
+interface Props {
+  type: 'issue' | 'pr'
+  title: string
+  status: string
+  number: number
+  repoName: string
+  url: string
+  updatedAt: string
+}
+
+const GitHubObjectCard = (props: Props) => {
+  const {title, status, number, repoName, url, type, updatedAt} = props
+  const statusImg = type === 'issue' ? ISSUE_STATUS_MAP[status] : PR_STATUS_MAP[status]
+  return (
+    <div className='rounded border border-solid border-slate-300 p-4'>
+      <div className='flex gap-2 text-xs text-slate-600'>
+        {statusImg && <img src={statusImg} />}
+        <div className='font-medium'>#{number}</div>
+        <div>Updated {relativeDate(updatedAt)}</div>
+      </div>
+      <div className='my-2 text-sm'>{title}</div>
+      <div className='flex items-center gap-2'>
+        <div className='h-4 w-4'>
+          <img src={gitHubSVG} />
+        </div>
+        <div className='text-xs text-slate-600'>{repoName}</div>
+      </div>
+    </div>
+  )
+}
+
+export default GitHubObjectCard
