cdf1f7dd505266f43576760fc869ab665452bc41
James Taber <jmtaber129@gmail.com>
2023-08-24 12:00:02 -0500

    Copy link to clipboard

--- packages/client/components/TeamPrompt/WorkDrawer/GitHubObjectCard.tsx
+++ packages/client/components/TeamPrompt/WorkDrawer/GitHubObjectCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import CopyToClipboard from 'react-copy-to-clipboard'
 import gitHubSVG from '../../../styles/theme/images/graphics/github-circle.svg'
 import gitHubMerged from '../../../styles/theme/images/graphics/github-merged.svg'
 import githubIssueClosed from '../../../styles/theme/images/graphics/github-issue-closed.svg'
 import githubIssueOpen from '../../../styles/theme/images/graphics/github-issue-open.svg'
 import githubPROpen from '../../../styles/theme/images/graphics/github-pr-open.svg'
 import relativeDate from '../../../utils/date/relativeDate'
+import {Link} from '@mui/icons-material'
+import useTooltip from '../../../hooks/useTooltip'
+import {MenuPosition} from '../../../hooks/useCoords'
 
 const ISSUE_STATUS_MAP: Record<string, any> = {
   OPEN: githubIssueOpen,
@@ -33,6 +37,11 @@ const GitHubObjectCard = (props: Props) => {
   const modifiedStatus = prIsDraft && status === 'OPEN' ? 'DRAFT' : status
   const statusImg =
     type === 'issue' ? ISSUE_STATUS_MAP[modifiedStatus] : PR_STATUS_MAP[modifiedStatus]
+
+  const {tooltipPortal, openTooltip, closeTooltip, originRef} = useTooltip<HTMLDivElement>(
+    MenuPosition.UPPER_CENTER
+  )
+
   return (
     <div className='rounded border border-solid border-slate-300 p-4'>
       <div className='flex gap-2 text-xs text-slate-600'>
@@ -47,15 +56,31 @@ const GitHubObjectCard = (props: Props) => {
           {title}
         </a>
       </div>
-      <div className='flex items-center gap-2'>
-        <div className='h-4 w-4'>
-          <img src={gitHubSVG} />
-        </div>
-        <div className='text-xs text-slate-600'>
-          <a href={repoUrl} target='_blank' className='hover:underline' rel='noreferrer'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center gap-2'>
+          <div className='h-4 w-4'>
+            <img src={gitHubSVG} />
+          </div>
+          <a
+            href={repoUrl}
+            target='_blank'
+            className='text-xs text-slate-600 hover:underline'
+            rel='noreferrer'
+          >
             {repoName}
           </a>
         </div>
+        <CopyToClipboard text={url}>
+          <div
+            className='h-6 rounded-full bg-transparent p-0 text-slate-500 hover:bg-slate-200'
+            onMouseEnter={openTooltip}
+            onMouseLeave={closeTooltip}
+            ref={originRef}
+          >
+            <Link className='h-6 w-6 cursor-pointer p-0.5' />
+          </div>
+        </CopyToClipboard>
+        {tooltipPortal('Copy link')}
       </div>
     </div>
   )
