bd474a856345eccc3812a856183b7146aa19dd3a
Bartosz Jarocki <bartosz.jarocki@hey.com>
2023-08-24 10:15:26 +0200

    code cleanup

--- packages/server/utils/tiptap/convertToMarkdown.tsx
+++ packages/server/utils/tiptap/convertToMarkdown.tsx
@@ -3,8 +3,8 @@ import {JSONContent} from '@tiptap/core'
 const LIST_SPACING = '   '
 
 const tiptapNodeHandlersLookup: {[type: string]: (node: JSONContent, depth?: number) => string} = {
-  doc: (node) => (node.content || []).map((node) => convertToMarkdown(node)).join('\n'),
-  paragraph: (node) => (node.content || []).map((node) => convertToMarkdown(node)).join(''),
+  doc: ({content = []}) => content.map((node) => convertToMarkdown(node)).join('\n'),
+  paragraph: ({content = []}) => content.map((node) => convertToMarkdown(node)).join(''),
   text: (node) => {
     let textContent = node.text || ''
     if (node.marks) {
@@ -16,22 +16,23 @@ const tiptapNodeHandlersLookup: {[type: string]: (node: JSONContent, depth?: num
     return textContent
   },
   // for now lets just make headings bold
-  heading: (node) => `*${(node.content || []).map((node) => convertToMarkdown(node)).join('')}*`,
-  bulletList: (node, depth = 0) =>
-    (node.content || [])
-      .map((n) => `${LIST_SPACING.repeat(depth)}- ${convertToMarkdown(n, depth + 1)}`)
+  heading: ({content = []}) => `*${content.map((node) => convertToMarkdown(node)).join('')}*`,
+  bulletList: ({content = []}, depth = 0) =>
+    content
+      .map((node) => `${LIST_SPACING.repeat(depth)}- ${convertToMarkdown(node, depth + 1)}`)
       .join('\n'),
-  orderedList: (node, depth = 0) =>
-    (node.content || [])
+  orderedList: ({content = []}, depth = 0) =>
+    content
       .map(
-        (n, idx) => `${LIST_SPACING.repeat(depth)}${idx + 1}. ${convertToMarkdown(n, depth + 1)}`
+        (node, idx) =>
+          `${LIST_SPACING.repeat(depth)}${idx + 1}. ${convertToMarkdown(node, depth + 1)}`
       )
       .join('\n'),
-  listItem: (node, depth = 0) =>
-    (node.content || []).map((n) => convertToMarkdown(n, depth)).join('\n'),
-  codeBlock: (node) => {
-    const content = (node.content || []).map((node) => convertToMarkdown(node)).join('')
-    return `\`\`\`${content}\`\`\`` // Triple backticks to denote a code block in Slack
+  listItem: ({content = []}, depth = 0) =>
+    content.map((node) => convertToMarkdown(node, depth)).join('\n'),
+  codeBlock: ({content = []}) => {
+    const markdownContent = content.map((node) => convertToMarkdown(node)).join('')
+    return `\`\`\`${markdownContent}\`\`\`` // Triple backticks to denote a code block in Slack
   }
 }
 
@@ -48,8 +49,7 @@ export const convertToMarkdown = (node: JSONContent, depth?: number): string => 
   const handler = tiptapNodeHandlersLookup[node.type!]
   if (handler) {
     return handler(node, depth)
-  } else {
-    // If the node type isn't specifically handled (like an unsupported custom type), just iterate through its content if any
-    return (node.content || []).map((n) => convertToMarkdown(n)).join('')
   }
+  // If the node type isn't specifically handled (like an unsupported custom type), just iterate through its content if any
+  return (node.content || []).map((n) => convertToMarkdown(n)).join('')
 }
