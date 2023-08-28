ef77c58ddb3038e02ab414dca863f824e5dda2a5
James Taber <jmtaber129@gmail.com>
2023-08-28 15:20:55 -0500

    Refactor GH query tabs

--- packages/client/components/TeamPrompt/WorkDrawer/GitHubIntegrationPanel.tsx
+++ packages/client/components/TeamPrompt/WorkDrawer/GitHubIntegrationPanel.tsx
@@ -9,6 +9,17 @@ import useAtmosphere from '../../../hooks/useAtmosphere'
 import useMutationProps from '../../../hooks/useMutationProps'
 import GitHubIntegrationResultsRoot from './GitHubIntegrationResultsRoot'
 
+const GITHUB_QUERY_TABS: {key: 'issue' | 'pullRequest'; label: string}[] = [
+  {
+    key: 'issue',
+    label: 'Issues'
+  },
+  {
+    key: 'pullRequest',
+    label: 'Pull Requests'
+  }
+]
+
 interface Props {
   meetingRef: GitHubIntegrationPanel_meeting$key
 }
@@ -49,30 +60,20 @@ const GitHubIntegrationPanel = (props: Props) => {
       {teamMember?.integrations.github?.isActive ? (
         <>
           <div className='my-4 flex w-full gap-2 px-4'>
-            <div
-              key={'issue'}
-              className={clsx(
-                'w-1/2 cursor-pointer rounded-full py-2 px-4 text-center text-sm leading-3 text-slate-800',
-                'issue' === githubType
-                  ? 'bg-grape-700 font-semibold text-white focus:text-white'
-                  : 'border border-slate-300 bg-white'
-              )}
-              onClick={() => setGithubType('issue')}
-            >
-              Issues
-            </div>
-            <div
-              key={'pullRequest'}
-              className={clsx(
-                'w-1/2 cursor-pointer rounded-full py-2 px-4 text-center text-sm leading-3 text-slate-800',
-                'pullRequest' === githubType
-                  ? 'bg-grape-700 font-semibold text-white focus:text-white'
-                  : 'border border-slate-300 bg-white'
-              )}
-              onClick={() => setGithubType('pullRequest')}
-            >
-              Pull requests
-            </div>
+            {GITHUB_QUERY_TABS.map((tab) => (
+              <div
+                key={tab.key}
+                className={clsx(
+                  'w-1/2 cursor-pointer rounded-full py-2 px-4 text-center text-sm leading-3 text-slate-800',
+                  tab.key === githubType
+                    ? 'bg-grape-700 font-semibold text-white focus:text-white'
+                    : 'border border-slate-300 bg-white'
+                )}
+                onClick={() => setGithubType(tab.key)}
+              >
+                {tab.label}
+              </div>
+            ))}
           </div>
           <GitHubIntegrationResultsRoot teamId={teamMember.teamId} queryType={githubType} />
         </>
