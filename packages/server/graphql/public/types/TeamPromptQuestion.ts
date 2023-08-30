2207c71afc74ff2f45241015e21d312a42bf084e
Matt Krick <matt.krick@gmail.com>
2023-08-30 10:59:24 -0700

    fix: team prompt q types (#8755)

--- packages/server/graphql/public/types/TeamPromptQuestion.ts
+++ packages/server/graphql/public/types/TeamPromptQuestion.ts
@@ -1,4 +1,8 @@
+import {Selectable} from 'kysely'
 import {TeamPromptQuestionResolvers} from '../resolverTypes'
+import {TeamPromptQuestion} from '../../../postgres/pg.d'
+
+export type TTeamPromptQuestion = Selectable<TeamPromptQuestion>
 
 const TeamPromptQuestion: TeamPromptQuestionResolvers = {
   id: ({id}) => `teamPromptQuestion:${id}`
