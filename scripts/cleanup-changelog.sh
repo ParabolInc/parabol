31cd2675e3e14f55b5829e414cc2908f10293ed7
Georg Bremer <github@dschoordsch.de>
2023-08-30 09:30:10 +0200

    Make `sed -i` cross platform compatible
    In-place without a backup file does not work in a compatible way between
    gnu-sed and bsd-sed.

--- scripts/cleanup-changelog.sh
+++ scripts/cleanup-changelog.sh
@@ -2,8 +2,8 @@
 # replace uppercase words: with bold to preserve these
 # replace lowercase words: with nothing as these are the conventional commit categories
 # replace lowercase words (with parens): with bold to preserve the title
-sed -i '' -E '
+sed -i.bak -E '
   s/^- ([A-Z][[:lower:]]*): /- **\1**: /
   s/^- ([[:lower:]]*): ?/- /
   s/^- ([[:lower:]]*)\((.*)\): ?/- **\2**: /
-' CHANGELOG.md
+' CHANGELOG.md && rm CHANGELOG.md.bak
