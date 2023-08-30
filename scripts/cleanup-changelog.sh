32efb22286e89e11d16889b612da034943bb3fd3
Georg Bremer <github@dschoordsch.de>
2023-08-30 09:44:31 +0200

    Use named character classes to better represent intend

--- scripts/cleanup-changelog.sh
+++ scripts/cleanup-changelog.sh
@@ -3,7 +3,7 @@
 # replace lowercase words: with nothing as these are the conventional commit categories
 # replace lowercase words (with parens): with bold to preserve the title
 sed -i.bak -E '
-  s/^- ([A-Z][[:lower:]]*): /- **\1**: /
-  s/^- ([[:lower:]]*): ?/- /
-  s/^- ([[:lower:]]*)\((.*)\): ?/- **\2**: /
+  s/^- ([[:upper:]][[:alnum:]]*): /- **\1**: /
+  s/^- ([[:alnum:]]*): ?/- /
+  s/^- ([[:alnum:]]*)\((.*)\): ?/- **\2**: /
 ' CHANGELOG.md && rm CHANGELOG.md.bak
