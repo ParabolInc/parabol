91554c98f5e8929ebb42880a0a2a8cfc17b3bf2f
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-08-31 10:01:19 +0300

    chore: fix cleanup-changelog.sh on mac os (#8685)
    * chore: fix cleanup-changelog.sh on mac os
    
    * tweak
    
    * Make `sed -i` cross platform compatible
    
    In-place without a backup file does not work in a compatible way between
    gnu-sed and bsd-sed.
    
    * Use named character classes to better represent intend
    
    ---------
    
    Co-authored-by: Georg Bremer <github@dschoordsch.de>

--- scripts/cleanup-changelog.sh
+++ scripts/cleanup-changelog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# replace uppercase words: with bold to preserver these
+# replace uppercase words: with bold to preserve these
 # replace lowercase words: with nothing as these are the conventional commit categories
 # replace lowercase words (with parens): with bold to preserve the title
-sed -i -E '
-  s/^- ([A-Z]\w*): /- **\1**: /
-  s/^- \w*: ?/- /
-  s/^- \w*\((.*)\): ?/- **\1**: /
-' CHANGELOG.md
+sed -i.bak -E '
+  s/^- ([[:upper:]][[:alnum:]]*): /- **\1**: /
+  s/^- ([[:alnum:]]*): ?/- /
+  s/^- ([[:alnum:]]*)\((.*)\): ?/- **\2**: /
+' CHANGELOG.md && rm CHANGELOG.md.bak
