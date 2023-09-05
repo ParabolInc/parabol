150ae74b20cb6912bb5b3a18fd2645a729dd5d30
Bruce Tian <tianrunhe@gmail.com>
2023-09-05 10:25:15 -0700

    Wire up front-end mutation

--- packages/client/mutations/BatchArchiveTasksMutation.ts
+++ packages/client/mutations/BatchArchiveTasksMutation.ts
@@ -1,12 +1,18 @@
 import graphql from 'babel-plugin-relay/macro'
 import {commitMutation} from 'react-relay'
 import {StandardMutation} from '../types/relayMutations'
+import ITask from '../../server/database/types/Task'
 import {BatchArchiveTasksMutation as TBatchArchiveTasksMutation} from '../__generated__/BatchArchiveTasksMutation.graphql'
+import getTagsFromEntityMap from '../utils/draftjs/getTagsFromEntityMap'
+import handleUpsertTasks from './handlers/handleUpsertTasks'
+import handleRemoveTasks from './handlers/handleRemoveTasks'
 
 graphql`
-  fragment BatchArchiveTasksMutation_task on BatchArchiveTasksSuccess {
+  fragment BatchArchiveTasksMutation_tasks on BatchArchiveTasksSuccess {
     tasks {
       id
+      content
+      tags
     }
   }
 `
@@ -19,26 +25,52 @@ const mutation = graphql`
           message
         }
       }
-      ...BatchArchiveTasksMutation_task @relay(mask: false)
+      ...BatchArchiveTasksMutation_tasks @relay(mask: false)
     }
   }
 `
 
 const BatchArchiveTasksMutation: StandardMutation<TBatchArchiveTasksMutation> = (
   atmosphere,
-  variables,
+  {taskIds},
   {onError, onCompleted}
 ) => {
   return commitMutation<TBatchArchiveTasksMutation>(atmosphere, {
     mutation,
-    variables,
+    variables: {
+      taskIds
+    },
     updater: (store) => {
       const payload = store.getRootField('batchArchiveTasks')
       if (!payload) return
+      const error = payload.getLinkedRecord('error')
+      if (error) {
+        taskIds.forEach((taskId) => {
+          const task = store.get<ITask>(taskId)
+          if (task) {
+            const message = error.getValue('message')
+            task.setValue(message, 'error')
+          }
+        })
+      }
+      const tasks = payload.getLinkedRecords('tasks')!
+      tasks.forEach((task) => {
+        handleUpsertTasks(task as any, store)
+        handleRemoveTasks(task as any, store)
+      })
     },
     optimisticUpdater: (store) => {
       const payload = store.getRootField('batchArchiveTasks')
       if (!payload) return
+      const archivedTasks = payload.getLinkedRecords('tasks')
+      archivedTasks.forEach((archivedTask) => {
+        const content = archivedTask.getValue('content')
+        const {entityMap} = JSON.parse(content)
+        const nextTags = getTagsFromEntityMap(entityMap)
+        archivedTask.setValue(nextTags, 'tags')
+        handleUpsertTasks(archivedTask as any, store)
+        handleRemoveTasks(archivedTask as any, store)
+      })
     },
     onCompleted,
     onError
