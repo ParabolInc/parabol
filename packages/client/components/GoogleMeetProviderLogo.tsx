245795a34291dcbce12750d97a867e30d9bb3586
Nick O'Ferrall <nickoferrall@gmail.com>
2023-09-11 17:31:22 +0100

    add video conferencing options to gcal modal

---
+++ packages/client/components/GoogleMeetProviderLogo.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled'
+import logo from '../styles/theme/images/graphics/google-meet-icon.svg'
+
+const GoogleMeetProviderLogo = styled('div')({
+  background: `url("${logo}")`,
+  height: 24,
+  width: 24,
+  backgroundSize: 'contain',
+  backgroundRepeat: 'no-repeat'
+})
+
+export default GoogleMeetProviderLogo
--- packages/client/modules/userDashboard/components/GcalModal/GcalModal.tsx
+++ packages/client/modules/userDashboard/components/GcalModal/GcalModal.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import graphql from 'babel-plugin-relay/macro'
 import dayjs from 'dayjs'
 import React, {useState} from 'react'
@@ -20,6 +21,13 @@ import StyledError from '../../../../components/StyledError'
 import DialogContainer from '../../../../components/DialogContainer'
 import {Close} from '@mui/icons-material'
 import PlainButton from '../../../../components/PlainButton/PlainButton'
+import Menu from '../../../../components/Menu'
+import MenuItem from '../../../../components/MenuItem'
+import useMenu from '../../../../hooks/useMenu'
+import {MenuPosition} from '../../../../hooks/useCoords'
+import RaisedButton from '../../../../components/RaisedButton'
+import {Elevation} from '../../../../styles/elevation'
+import GoogleMeetProviderLogo from '../../../../components/GoogleMeetProviderLogo'
 
 const Wrapper = styled('div')({
   display: 'flex',
@@ -64,6 +72,11 @@ const ErrorMessage = styled(StyledError)({
   paddingBottom: 8
 })
 
+const StyledButton = styled(RaisedButton)({
+  borderRadius: 4,
+  padding: '6px 12px'
+})
+
 const validateTitle = (title: string) => {
   return new Legitity(title).trim().min(2, `C’mon, you call that a title?`)
 }
@@ -84,6 +97,7 @@ const GcalModal = (props: Props) => {
   const [inviteError, setInviteError] = useState<null | string>(null)
   const [rawInvitees, setRawInvitees] = useState('')
   const [invitees, setInvitees] = useState([] as string[])
+  const {togglePortal, originRef, menuPortal, menuProps} = useMenu(MenuPosition.UPPER_CENTER)
 
   const team = useFragment(
     graphql`
@@ -215,6 +229,26 @@ const GcalModal = (props: Props) => {
               setEnd={setEnd}
             />
           </div>
+          <div className='pt-2'>
+            <StyledButton onClick={togglePortal} ref={originRef} elevationHovered={Elevation.Z3}>
+              {'Add Video Conferencing'} <ArrowDropDownIcon />
+            </StyledButton>
+          </div>
+          {menuPortal(
+            <Menu ariaLabel={'Select a video conferencing option'} {...menuProps}>
+              <MenuItem
+                label={
+                  <div className='align-center flex p-3 hover:cursor-pointer'>
+                    <GoogleMeetProviderLogo />
+                    <label className='text-gray-500 pl-2 text-sm font-normal hover:cursor-pointer'>
+                      Google Meet
+                    </label>
+                  </div>
+                }
+              ></MenuItem>
+              <MenuItem isDisabled label={'Zoom (Coming Soon!)'}></MenuItem>
+            </Menu>
+          )}
           <p className='pt-3 text-xs leading-4'>{'Invite others to your Google Calendar event'}</p>
           <BasicTextArea
             name='rawInvitees'
