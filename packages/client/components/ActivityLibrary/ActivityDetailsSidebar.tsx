14814d5de102d339b8eaaac2279f80cec2d7e715
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-09-21 17:11:06 +0200

    feat(ad-hoc): allow to create team from teams dropdown (#8831)
    * chore(one-on-one): add oneOnOne org feature flag
    
    * chore(one-on-one): add one-on-one meeting template
    
    * Change color
    
    * Put one on one in standup
    
    * feat(one-on-one): allow oneOnOne input in startCheckIn mutation
    
    * fix checkin
    
    * revert duplicate meeting check
    
    * Remove userId option
    
    * Publish team payload
    
    * chore(one-on-one): Add user picker
    
    * Submit mutation
    
    * remove ui/input
    
    * Code cleanup
    
    * chore(one-on-one): add user picker styles
    
    * args validation
    
    * prettier
    
    * generateOneOnOneTeamName
    
    * isOneOnOneTeam
    
    * chore(one-on-one): show organiation picker if can't determine org automatically
    
    * Enable multiple false
    
    * chore(one-on-one): Add team exists warning
    
    * use isOneOnOne team to find teams
    
    * Add analytics
    
    * Show only mutual orgs
    
    * Show name in warning
    
    * Exclude self
    
    * Add a validation
    
    * Avatar
    
    * fix meeting name
    
    * Separate files
    
    * simplify
    
    * Remove useEffect
    
    * autoselect
    
    * Hide schedule button
    
    * Move team exists check to organization
    
    * feat(ad-hoc): allow to create team from teams dropdown
    
    * Rename component
    
    * use organization(id)
    
    * handleAddTeamClick
    
    * add Teammate Selected event

--- packages/client/components/ActivityLibrary/ActivityDetailsSidebar.tsx
+++ packages/client/components/ActivityLibrary/ActivityDetailsSidebar.tsx
@@ -71,6 +71,7 @@ const ActivityDetailsSidebar = (props: Props) => {
       fragment ActivityDetailsSidebar_viewer on User {
         featureFlags {
           gcal
+          adHocTeams
         }
         ...AdhocTeamMultiSelect_viewer
         organizations {
@@ -328,6 +329,7 @@ const ActivityDetailsSidebar = (props: Props) => {
               selectedTeamRef={selectedTeam}
               teamsRef={availableTeams}
               customPortal={teamScopePopover}
+              allowAddTeam={viewer.featureFlags.adHocTeams}
             />
           )}
 
