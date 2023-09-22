a06bb7ae58eb1b185daeb7c8aaef12957c912ae6
Georg Bremer <github@dschoordsch.de>
2023-09-22 12:09:49 +0200

    Design tweaks

--- packages/client/modules/teamDashboard/components/ProviderRow/ProviderRow.tsx
+++ packages/client/modules/teamDashboard/components/ProviderRow/ProviderRow.tsx
@@ -11,19 +11,11 @@ interface Props extends Omit<ProviderRowBaseProps, 'connectButton'> {
 }
 
 const ProviderRow = (props: Props) => {
-  const {onConnectClick, submitting, error, ...other} = props
+  const {onConnectClick, submitting, ...other} = props
 
   const connectButton = <ConnectButton onConnectClick={onConnectClick} submitting={submitting} />
 
-  return (
-    <ProviderRowBase {...other} connectButton={connectButton}>
-      {!!error && (
-        <div className='px-4 pb-2 text-tomato-500 [&_a]:font-semibold [&_a]:text-tomato-500 [&_a]:underline'>
-          {error}
-        </div>
-      )}
-    </ProviderRowBase>
-  )
+  return <ProviderRowBase {...other} connectButton={connectButton} />
 }
 
 export default ProviderRow
