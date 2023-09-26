7f528fbf4d9bcfc232afaba86ba7ba95b4fed0a5
Georg Bremer <github@dschoordsch.de>
2023-09-26 15:38:37 +0200

    Cleanup

--- packages/server/graphql/mutations/helpers/gatherInsights.ts
+++ packages/server/graphql/mutations/helpers/gatherInsights.ts
@@ -8,7 +8,6 @@ const gatherInsights = async (meeting: Meeting, dataLoader: DataLoaderWorker) =>
     collectReactjis(meeting, dataLoader),
     calculateEngagement(meeting, dataLoader)
   ])
-  console.log('GEORG engagement', engagement)
   return {usedReactjis, engagement}
 }
 
