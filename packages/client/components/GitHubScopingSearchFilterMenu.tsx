9700613f32c90bf448f353e0100da94a98d362e1
James Taber <jmtaber129@gmail.com>
2023-09-27 16:50:08 -0500

    Refactor poker github repo filter

--- packages/client/components/GitHubScopingSearchFilterMenu.tsx
+++ packages/client/components/GitHubScopingSearchFilterMenu.tsx
@@ -1,31 +1,12 @@
-import styled from '@emotion/styled'
 import graphql from 'babel-plugin-relay/macro'
-import React, {useMemo} from 'react'
+import React from 'react'
 import {commitLocalUpdate, PreloadedQuery, usePreloadedQuery} from 'react-relay'
-import useSearchFilter from '~/hooks/useSearchFilter'
 import useAtmosphere from '../hooks/useAtmosphere'
-import useGetRepoContributions from '../hooks/useGetRepoContributions'
 import {MenuProps} from '../hooks/useMenu'
 import SearchQueryId from '../shared/gqlIds/SearchQueryId'
 import getReposFromQueryStr from '../utils/getReposFromQueryStr'
 import {GitHubScopingSearchFilterMenuQuery} from '../__generated__/GitHubScopingSearchFilterMenuQuery.graphql'
-import Checkbox from './Checkbox'
-import {EmptyDropdownMenuItemLabel} from './EmptyDropdownMenuItemLabel'
-import Menu from './Menu'
-import MenuItem from './MenuItem'
-import MenuItemLabel from './MenuItemLabel'
-import {SearchMenuItem} from './SearchMenuItem'
-import TypeAheadLabel from './TypeAheadLabel'
-
-const StyledCheckBox = styled(Checkbox)({
-  marginLeft: -8,
-  marginRight: 8
-})
-const StyledMenuItemLabel = styled(MenuItemLabel)({})
-
-const StyledMenu = styled(Menu)({
-  maxWidth: '100%'
-})
+import GitHubRepoSearchFilterMenu from './GitHubRepoSearchFilterMenu'
 
 interface Props {
   menuProps: MenuProps
@@ -37,12 +18,6 @@ type GitHubSearchQuery = NonNullable<
     GitHubScopingSearchFilterMenuQuery['response']['viewer']['meeting']
   >['githubSearchQuery']
 >
-
-const MAX_REPOS = 10
-
-const getValue = (item: {nameWithOwner?: string}) => {
-  return item.nameWithOwner || 'Unknown Repo'
-}
 
 const GitHubScopingSearchFilterMenu = (props: Props) => {
   const {menuProps, queryRef} = props
@@ -59,7 +34,7 @@ const GitHubScopingSearchFilterMenu = (props: Props) => {
             }
           }
           teamMember(teamId: $teamId) {
-            ...useGetRepoContributions_teamMember
+            ...GitHubRepoSearchFilterMenu_teamMember
           }
         }
       }
@@ -67,78 +42,36 @@ const GitHubScopingSearchFilterMenu = (props: Props) => {
     queryRef
   )
 
-  const repoContributions = useGetRepoContributions(query.viewer.teamMember!)
   const meeting = query?.viewer?.meeting
   const meetingId = meeting?.id ?? ''
   const githubSearchQuery = meeting?.githubSearchQuery
   const queryString = githubSearchQuery?.queryString ?? null
   const atmosphere = useAtmosphere()
-
-  const {
-    query: searchQuery,
-    filteredItems: filteredRepoContributions,
-    onQueryChange
-  } = useSearchFilter(repoContributions, getValue)
-
-  // TODO parse the query string & extract out the repositories
   const selectedRepos = getReposFromQueryStr(queryString)
-  const selectedAndFilteredRepos = useMemo(() => {
-    const adjustedMax = selectedRepos.length >= MAX_REPOS ? selectedRepos.length + 1 : MAX_REPOS
-    const repos = filteredRepoContributions.map(({nameWithOwner}) =>
-      nameWithOwner.toLowerCase().trim()
-    )
-    return Array.from(new Set([...selectedRepos, ...repos])).slice(0, adjustedMax)
-  }, [filteredRepoContributions])
 
-  const {portalStatus, isDropdown} = menuProps
   return (
-    <StyledMenu
-      keepParentFocus
-      ariaLabel='Define the GitHub search query'
-      portalStatus={portalStatus}
-      isDropdown={isDropdown}
-    >
-      <SearchMenuItem
-        placeholder='Search your GitHub repos'
-        onChange={onQueryChange}
-        value={searchQuery}
-      />
-      {repoContributions.length === 0 && (
-        <EmptyDropdownMenuItemLabel key='no-results'>No repos found!</EmptyDropdownMenuItemLabel>
-      )}
-      {selectedAndFilteredRepos.map((repo) => {
-        const isSelected = selectedRepos.includes(repo)
-        const handleClick = () => {
-          commitLocalUpdate(atmosphere, (store) => {
-            const searchQueryId = SearchQueryId.join('github', meetingId)
-            const githubSearchQuery = store.get<GitHubSearchQuery>(searchQueryId)!
-            const newFilters = isSelected
-              ? selectedRepos.filter((name) => name !== repo)
-              : selectedRepos.concat(repo)
-            const queryString = githubSearchQuery.getValue('queryString')
-            const queryWithoutRepos = queryString
-              .trim()
-              .split(' ')
-              .filter((str) => !str.includes('repo:'))
-            const newRepos = newFilters.map((name) => `repo:${name}`)
-            const newQueryStr = queryWithoutRepos.concat(newRepos).join(' ')
-            githubSearchQuery.setValue(newQueryStr, 'queryString')
-          })
-        }
-        return (
-          <MenuItem
-            key={repo}
-            label={
-              <StyledMenuItemLabel>
-                <StyledCheckBox active={isSelected} />
-                <TypeAheadLabel query={searchQuery} label={repo} />
-              </StyledMenuItemLabel>
-            }
-            onClick={handleClick}
-          />
-        )
-      })}
-    </StyledMenu>
+    <GitHubRepoSearchFilterMenu
+      selectedRepos={selectedRepos}
+      onToggleRepo={(repo, isSelected) => {
+        commitLocalUpdate(atmosphere, (store) => {
+          const searchQueryId = SearchQueryId.join('github', meetingId)
+          const githubSearchQuery = store.get<GitHubSearchQuery>(searchQueryId)!
+          const newFilters = isSelected
+            ? selectedRepos.filter((name) => name !== repo)
+            : selectedRepos.concat(repo)
+          const queryString = githubSearchQuery.getValue('queryString')
+          const queryWithoutRepos = queryString
+            .trim()
+            .split(' ')
+            .filter((str) => !str.includes('repo:'))
+          const newRepos = newFilters.map((name) => `repo:${name}`)
+          const newQueryStr = queryWithoutRepos.concat(newRepos).join(' ')
+          githubSearchQuery.setValue(newQueryStr, 'queryString')
+        })
+      }}
+      teamMemberRef={query.viewer.teamMember!}
+      menuProps={menuProps}
+    />
   )
 }
 
