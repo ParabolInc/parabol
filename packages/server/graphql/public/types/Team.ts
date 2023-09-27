7c2e1561e5d32c48d34450708488e8cab887e090
Georg Bremer <github@dschoordsch.de>
2023-09-27 17:36:56 +0200

    feat(Team Insights): Add most used retro templates (#8879)
    * feat(Team Insights): Add most used retro templates
    
    * fix empty retro template insights and order

--- packages/server/graphql/public/types/Team.ts
+++ packages/server/graphql/public/types/Team.ts
@@ -4,15 +4,28 @@ import toTeamMemberId from '../../../../client/utils/relay/toTeamMemberId'
 import {getUserId} from '../../../utils/authorization'
 
 const Team: TeamResolvers = {
-  insights: async ({id, orgId, mostUsedEmojis, meetingEngagement}, _args, {dataLoader}) => {
+  insights: async (
+    {id, orgId, mostUsedEmojis, meetingEngagement, topRetroTemplates},
+    _args,
+    {dataLoader}
+  ) => {
     const org = await dataLoader.get('organizations').load(orgId)
     if (!org?.featureFlags?.includes('teamInsights')) return null
-    if (!mostUsedEmojis && !meetingEngagement) return null
+    if (!mostUsedEmojis && !meetingEngagement && !topRetroTemplates) return null
+
+    const mappedTopRetroTemplates = Array.isArray(topRetroTemplates)
+      ? topRetroTemplates.map((template) => ({
+          ...(template as any),
+          teamId: id
+        }))
+      : null
+    mappedTopRetroTemplates?.sort((a, b) => b.count - a.count)
 
     return {
       id: TeamInsightsId.join(id),
       mostUsedEmojis,
-      meetingEngagement
+      meetingEngagement,
+      topRetroTemplates: mappedTopRetroTemplates
     } as any
   },
   viewerTeamMember: async ({id: teamId}, _args, {authToken, dataLoader}) => {
