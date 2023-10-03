6b603479273b834a32e3e0611fdaf69fb9f65287
James Taber <jmtaber129@gmail.com>
2023-10-03 14:29:42 -0500

    feat(your-work): Jira Integration pagination

--- packages/client/components/TeamPrompt/WorkDrawer/JiraIntegrationResults.tsx
+++ packages/client/components/TeamPrompt/WorkDrawer/JiraIntegrationResults.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import graphql from 'babel-plugin-relay/macro'
-import {PreloadedQuery, useFragment, usePreloadedQuery} from 'react-relay'
+import {PreloadedQuery, usePaginationFragment, usePreloadedQuery} from 'react-relay'
 import {JiraIntegrationResultsQuery} from '../../../__generated__/JiraIntegrationResultsQuery.graphql'
-import {JiraIntegrationResults_search$data} from '../../../__generated__/JiraIntegrationResults_search.graphql'
+import {JiraIntegrationResultsSearchPaginationQuery} from '../../../__generated__/JiraIntegrationResultsSearchPaginationQuery.graphql'
+import {JiraIntegrationResults_search$key} from '../../../__generated__/JiraIntegrationResults_search.graphql'
 import halloweenRetrospectiveTemplate from '../../../../../static/images/illustrations/halloweenRetrospectiveTemplate.png'
 import JiraObjectCard from './JiraObjectCard'
+import useLoadNextOnScrollBottom from '../../../hooks/useLoadNextOnScrollBottom'
+import Ellipsis from '../../Ellipsis/Ellipsis'
 
 interface Props {
   queryRef: PreloadedQuery<JiraIntegrationResultsQuery>
@@ -21,16 +24,25 @@ const JiraIntegrationResults = (props: Props) => {
     queryRef
   )
 
-  // :TODO: (jmtaber129): Implement pagination
-  const queryFrag = useFragment(
+  const paginationRes = usePaginationFragment<
+    JiraIntegrationResultsSearchPaginationQuery,
+    JiraIntegrationResults_search$key
+  >(
     graphql`
-      fragment JiraIntegrationResults_search on Query @argumentDefinitions(teamId: {type: "ID!"}) {
+      fragment JiraIntegrationResults_search on Query
+      @argumentDefinitions(
+        cursor: {type: "String"}
+        count: {type: "Int", defaultValue: 2}
+        teamId: {type: "ID!"}
+      )
+      @refetchable(queryName: "JiraIntegrationResultsSearchPaginationQuery") {
         viewer {
           teamMember(teamId: $teamId) {
             integrations {
               atlassian {
                 issues(
-                  first: 20
+                  first: $count
+                  after: $cursor
                   isJQL: true
                   queryString: "assignee = currentUser() order by updated DESC"
                 ) @connection(key: "JiraScopingSearchResults_issues") {
@@ -56,9 +68,10 @@ const JiraIntegrationResults = (props: Props) => {
     query
   )
 
-  const jira = (queryFrag as JiraIntegrationResults_search$data).viewer.teamMember?.integrations
-    .atlassian
+  const lastItem = useLoadNextOnScrollBottom(paginationRes, {}, 2)
+  const {data, hasNext} = paginationRes
 
+  const jira = data.viewer.teamMember?.integrations.atlassian
   const jiraResults = jira?.issues.edges.map((edge) => edge.node)
   const error = jira?.issues.error ?? null
 
@@ -78,6 +91,12 @@ const JiraIntegrationResults = (props: Props) => {
             <div className='mt-7 w-2/3 text-center'>
               {error?.message ? error.message : `Looks like you don’t have any issues to display.`}
             </div>
+          </div>
+        )}
+        {lastItem}
+        {hasNext && (
+          <div className='mx-auto mb-4 -mt-4 h-8 text-2xl' key={'loadingNext'}>
+            <Ellipsis />
           </div>
         )}
       </div>
