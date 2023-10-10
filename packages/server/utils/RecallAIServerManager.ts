29eba75d1fc89a33d7d0a732fa63e8b07cc225ad
Nick O'Ferrall <nickoferrall@gmail.com>
2023-10-10 14:39:02 +0100

    chore: log recall error (#8954)
    * chore/update-recall
    
    * update error message

--- packages/server/utils/RecallAIServerManager.ts
+++ packages/server/utils/RecallAIServerManager.ts
@@ -1,6 +1,8 @@
 import api from 'api'
 import axios from 'axios'
 import {ExternalLinks} from '../../client/types/constEnums'
+import appOrigin from '../appOrigin'
+import sendToSentry from './sendToSentry'
 
 const sdk = api('@recallai/v1.6#536jnqlf7d6blh')
 
@@ -40,13 +42,11 @@ class RecallAIServerManager {
       const base64Image = await getBase64Image()
       if (!base64Image) return null
 
-      const PROTOCOL = process.env.GRAPHQL_PROTOCOL || 'http'
-      const HOST = process.env.GRAPHQL_HOST || 'localhost:3000'
       const {data} = await sdk.bot_create({
         bot_name: 'Parabol Notetaker',
         real_time_transcription: {
           partial_results: false,
-          destination_url: `${PROTOCOL}://${HOST}` // this is required by the API but it's not doing anything and can be any URL. TODO: speak with recall.ai about this & fix
+          destination_url: appOrigin // this is required by the API but it's not doing anything and can be any URL. TODO: speak with recall.ai about this & fix
         },
         transcription_options: {provider: 'assembly_ai'},
         chat: {
@@ -65,7 +65,11 @@ class RecallAIServerManager {
       const {id: botId} = data
       return botId as string
     } catch (err) {
-      console.error(err)
+      const error =
+        err instanceof Error
+          ? err
+          : new Error(`Unable to create Recall bot with video meeting URL: ${videoMeetingURL}`)
+      sendToSentry(error)
       return null
     }
   }
@@ -83,7 +87,9 @@ class RecallAIServerManager {
       })
       return transcript.join('\n') as string
     } catch (err) {
-      console.error(err)
+      const error =
+        err instanceof Error ? err : new Error(`Unable to get bot transcript with botId: ${botId}`)
+      sendToSentry(error)
       return
     }
   }
