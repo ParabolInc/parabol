8241ae048651ab6c5ae975e2846cb34f10278450
Aaron Daniels <aaron@parabol.co>
2023-10-25 15:05:35 -0500

    feat(isEnterprise): add check to setIsEnterprise script to check for IS_ENTERPRISE is true

--- scripts/toolboxSrc/setIsEnterprise.ts
+++ scripts/toolboxSrc/setIsEnterprise.ts
@@ -2,13 +2,23 @@ import getRethink from '../../packages/server/database/rethinkDriver'
 import getPg from '../../packages/server/postgres/getPg'
 
 export default async function setIsEnterprise() {
+  if (process.env.IS_ENTERPRISE !== 'true') {
+    console.log(
+      'Environment variable IS_ENTERPRISE is not set to true. Exiting without updating tiers.'
+    )
+    process.exit()
+    return
+  }
+
   const r = await getRethink()
 
-  console.log('Updating tier to "enterprise" for Organization and OrganizationUser tables in RethinkDB')
+  console.log(
+    'Updating tier to "enterprise" for Organization and OrganizationUser tables in RethinkDB'
+  )
 
-  type RethinkTableKey = 'Organization' | 'OrganizationUser';
+  type RethinkTableKey = 'Organization' | 'OrganizationUser'
 
-  const tablesToUpdate: RethinkTableKey[] = ['Organization', 'OrganizationUser'];
+  const tablesToUpdate: RethinkTableKey[] = ['Organization', 'OrganizationUser']
 
   const rethinkPromises = tablesToUpdate.map(async (table) => {
     const result = await r
@@ -16,23 +26,27 @@ export default async function setIsEnterprise() {
       .update({
         tier: 'enterprise'
       })
-      .run();
+      .run()
 
-    console.log(`Updated ${result.replaced} rows in ${table} table in RethinkDB.`);
-    return result;
-  });
+    console.log(`Updated ${result.replaced} rows in ${table} table in RethinkDB.`)
+    return result
+  })
 
   const pg = getPg()
 
-  const updateUserPromise = pg.query(`UPDATE "User" SET tier = 'enterprise' RETURNING id`).then(res => {
-    console.log('Updated User tier to enterprise for:', res.rows.length, 'records in PostgreSQL.')
-    return res
-  })
+  const updateUserPromise = pg
+    .query(`UPDATE "User" SET tier = 'enterprise' RETURNING id`)
+    .then((res) => {
+      console.log('Updated User tier to enterprise for:', res.rows.length, 'records in PostgreSQL.')
+      return res
+    })
 
-  const updateTeamPromise = pg.query(`UPDATE "Team" SET tier = 'enterprise' RETURNING id`).then(res => {
-    console.log('Updated Team tier to enterprise for:', res.rows.length, 'records in PostgreSQL.')
-    return res
-  })
+  const updateTeamPromise = pg
+    .query(`UPDATE "Team" SET tier = 'enterprise' RETURNING id`)
+    .then((res) => {
+      console.log('Updated Team tier to enterprise for:', res.rows.length, 'records in PostgreSQL.')
+      return res
+    })
 
   const pgPromises = [updateUserPromise, updateTeamPromise]
 
