b9e179ac72045939968dfb7fd87da0e13a4ef214
Georg Bremer <github@dschoordsch.de>
2023-10-26 14:06:58 +0200

    chore(Team Insights): Limit most used retro templates (#8982)
    * chore(Team Insights): Limit top retro templates
    
    * Tweak tooltips

--- packages/server/graphql/mutations/helpers/updateTeamInsights.ts
+++ packages/server/graphql/mutations/helpers/updateTeamInsights.ts
@@ -7,6 +7,7 @@ import {DataLoaderWorker} from '../../graphql'
 const TEAM_INSIGHTS_PERIOD = ms('90 days')
 // let's accumulate the data over a longer period
 const TOP_RETRO_TEMPLATES_PERIOD = ms('1 year')
+const TOP_RETRO_TEMPLATES_COUNT = 3
 const MAX_NUMBER_OF_USED_EMOJIS = 3
 const MIN_NUMBER_OF_USED_EMOJIS = 2
 const MIN_EMOJI_COUNT = 5
@@ -43,7 +44,7 @@ const updateTeamInsights = async (teamId: string, dataLoader: DataLoaderWorker) 
     )
       .group('templateId')
       .count()
-      .run()
+      .run() as Promise<{group: string; reduction: number}[]>
   ])
 
   // emojis
@@ -90,12 +91,14 @@ const updateTeamInsights = async (teamId: string, dataLoader: DataLoaderWorker) 
       count: reduction
     })
   )
+  topRetroTemplates.sort((a, b) => b.count - a.count)
 
   const update = {
     insightsUpdatedAt: now,
     mostUsedEmojis: mostUsedEmojis.length >= MIN_NUMBER_OF_USED_EMOJIS ? mostUsedEmojis : null,
     meetingEngagement: meetingEngagement.all ? meetingEngagement : null,
-    topRetroTemplates: topRetroTemplates.length > 0 ? topRetroTemplates : null
+    topRetroTemplates:
+      topRetroTemplates.length > 0 ? topRetroTemplates.slice(0, TOP_RETRO_TEMPLATES_COUNT) : null
   }
 
   await pg
