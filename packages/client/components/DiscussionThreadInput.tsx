4d4033319c413da3c295b9c915b8a66f600b39e3
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-10-26 12:52:49 +0400

    fix: use SendClientSideEvent for add activity button

--- packages/client/components/DiscussionThreadInput.tsx
+++ packages/client/components/DiscussionThreadInput.tsx
@@ -30,7 +30,7 @@ import SendCommentButton from './SendCommentButton'
 import CommentEditor from './TaskEditor/CommentEditor'
 import {ReplyMention, SetReplyMention} from './ThreadedItem'
 import AddActivityButton from '~/components/AddActivityButton'
-import SendClientSegmentEventMutation from '~/mutations/SendClientSegmentEventMutation'
+import SendClientSideEvent from '../mutations/SendClientSideEvent'
 
 const Wrapper = styled('div')<{isReply: boolean; isDisabled: boolean}>(({isDisabled, isReply}) => ({
   display: 'flex',
@@ -352,7 +352,7 @@ const DiscussionThreadInput = forwardRef((props: Props, ref: any) => {
             <AddActivityButton
               onClick={() => {
                 window.open(`/activity-library/category/recommended`, '_blank', 'noreferrer')
-                SendClientSegmentEventMutation(atmosphere, 'Add Activity Button Clicked')
+                SendClientSideEvent(atmosphere, 'Add Activity Button Clicked')
               }}
               disabled={isActionsContainerDisabled}
             />
