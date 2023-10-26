241a764846fba880c8535ad246b4e8844f5928c1
Bruce Tian <tianrunhe@gmail.com>
2023-10-26 10:54:31 -0700

    Delete packages/server/postgres/queries/src/backfillPseudoIdQuery.sql

--- packages/server/postgres/queries/src/backfillPseudoIdQuery.sql
+++
@@ -1,8 +0,0 @@
-/*
-  @name backfillPseudoIdQuery
-  @param users -> ((pseudoId, id)...)
-*/
-UPDATE "User" AS u SET
-  "pseudoId" = c."pseudoId"
-FROM (VALUES :users) AS c("pseudoId", "id")
-WHERE c."id" = u."id";
