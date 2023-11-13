b5ebf0956517ef9bae8b005c8da9e731d908f669
Nick O'Ferrall <nickoferrall@gmail.com>
2023-11-13 16:37:53 +0000

    use grape 100

--- packages/client/components/ActivityLibrary/Categories.ts
+++ packages/client/components/ActivityLibrary/Categories.ts
@@ -17,7 +17,7 @@ export const DEFAULT_CARD_THEME: CardTheme = {primary: 'bg-slate-500', secondary
 export const CATEGORY_THEMES: Record<CategoryID, CardTheme> = {
   standup: {primary: 'bg-aqua-400', secondary: 'bg-aqua-100'},
   estimation: {primary: 'bg-tomato-500', secondary: 'bg-tomato-100'},
-  retrospective: {primary: 'bg-grape-500', secondary: 'bg-[#F2E1F7]'},
+  retrospective: {primary: 'bg-grape-500', secondary: 'bg-grape-100'},
   feedback: {primary: 'bg-jade-400', secondary: 'bg-jade-100'},
   strategy: {primary: 'bg-rose-500', secondary: 'bg-rose-100'},
   premortem: {primary: 'bg-gold-500', secondary: 'bg-gold-100'},
