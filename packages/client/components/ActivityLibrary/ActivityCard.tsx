97a60a6a6039e4c75a6c2e206263fe142069de6a
Nick O'Ferrall <nickoferrall@gmail.com>
2023-11-20 10:50:53 +0000

    add dummy tooltip

--- packages/client/components/ActivityLibrary/ActivityCard.tsx
+++ packages/client/components/ActivityLibrary/ActivityCard.tsx
@@ -1,9 +1,12 @@
 import clsx from 'clsx'
-import React, {PropsWithChildren} from 'react'
+import React, {PropsWithChildren, useState} from 'react'
 import {upperFirst} from '../../utils/upperFirst'
 import {MeetingTypeEnum} from '../../__generated__/NewMeetingQuery.graphql'
 import {backgroundImgMap, CategoryID, MEETING_TYPE_TO_CATEGORY} from './Categories'
 import {twMerge} from 'tailwind-merge'
+import {Tooltip} from '../../ui/Tooltip/Tooltip'
+import {TooltipTrigger} from '../../ui/Tooltip/TooltipTrigger'
+import {TooltipContent} from '../../ui/Tooltip/TooltipContent'
 
 export interface CardTheme {
   primary: string
@@ -46,12 +49,32 @@ export interface ActivityCardProps {
   type?: MeetingTypeEnum
 }
 
+const templates = [
+  {name: 'Template 1', description: 'Description for template 1', color: 'text-red-500'},
+  {name: 'Template 2', description: 'Description for template 2', color: 'text-green-500'},
+  {name: 'Template 3', description: 'Description for template 3', color: 'text-blue-500'},
+  {name: 'Template 4', description: 'Description for template 4', color: 'text-yellow-500'}
+]
+
 export const ActivityCard = (props: ActivityCardProps) => {
   const {className, theme, title, children, type, badge} = props
   const category = type && MEETING_TYPE_TO_CATEGORY[type]
+  const [showTooltip, setShowTooltip] = useState(false)
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true)
+  }
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false)
+  }
 
   return (
-    <div className='flex w-full flex-col'>
+    <div
+      className='flex w-full flex-col'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <div
         className={twMerge(
           'relative flex h-full min-w-0 flex-col overflow-hidden rounded-lg',
@@ -72,6 +95,23 @@ export const ActivityCard = (props: ActivityCardProps) => {
           </div>
         </div>
       )}
+      <Tooltip open={showTooltip}>
+        <TooltipTrigger asChild>
+          <div className='h-50 w-50 text-sky-500 hover:text-sky-700' />
+        </TooltipTrigger>
+        <TooltipContent className='rounded-lg bg-white p-4 text-left text-slate-700 shadow-lg hover:cursor-pointer'>
+          <div className='mb-2 text-left text-lg'>{title}</div>
+          {templates.map((template, index) => (
+            <div key={index} className='mb-1 flex items-start py-1'>
+              <span className={clsx('h-2 w-2 rounded-full ', template.color, 'mr-2 mt-1')}></span>
+              <div>
+                <div className='text-md font-semibold'>{template.name}</div>
+                <div className='text-sm font-normal'>{template.description}</div>
+              </div>
+            </div>
+          ))}
+        </TooltipContent>
+      </Tooltip>
     </div>
   )
 }
