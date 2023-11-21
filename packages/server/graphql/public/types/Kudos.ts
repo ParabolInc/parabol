c5abfd4521a1d45923884631d0504fcd8c2b3432
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-11-21 14:39:28 +0400

    expose senderUser

--- packages/server/graphql/public/types/Kudos.ts
+++ packages/server/graphql/public/types/Kudos.ts
@@ -3,6 +3,9 @@ import {KudosResolvers} from '../resolverTypes'
 const Kudos: KudosResolvers = {
   receiverUser: async ({receiverUserId}, _args, {dataLoader}) => {
     return dataLoader.get('users').loadNonNull(receiverUserId)
+  },
+  senderUser: async ({senderUserId}, _args, {dataLoader}) => {
+    return dataLoader.get('users').loadNonNull(senderUserId)
   }
 }
 
