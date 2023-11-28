7df19113ccb9282cf80291b068c9b9b0d448709d
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-11-28 14:53:09 +0400

    Fix type

--- packages/server/utils/analytics/analytics.ts
+++ packages/server/utils/analytics/analytics.ts
@@ -667,7 +667,12 @@ class Analytics {
     this.track(userId, 'AutoJoined Team', {userId, teamId})
   }
 
-  kudosSent = (userId: string, teamId: string, kudosId: number, receiverUserId: string) => {
+  kudosSent = (
+    userId: string,
+    teamId: string,
+    kudosId: number | string,
+    receiverUserId: string
+  ) => {
     this.track(userId, 'Kudos Sent', {
       userId,
       teamId,
