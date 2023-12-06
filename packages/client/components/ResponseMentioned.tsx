606a9a85ba8bf8f3143076581c89203ef1fdb19c
Igor Lesnenko <igor.lesnenko@gmail.com>
2023-12-06 15:39:37 +0400

    Mention notification analytics

--- packages/client/components/ResponseMentioned.tsx
+++ packages/client/components/ResponseMentioned.tsx
@@ -1,10 +1,12 @@
 import graphql from 'babel-plugin-relay/macro'
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useFragment} from 'react-relay'
 import NotificationAction from '~/components/NotificationAction'
 import useRouter from '../hooks/useRouter'
 import {ResponseMentioned_notification$key} from '../__generated__/ResponseMentioned_notification.graphql'
 import NotificationTemplate from './NotificationTemplate'
+import SendClientSideEvent from '../utils/SendClientSideEvent'
+import useAtmosphere from '~/hooks/useAtmosphere'
 
 interface Props {
   notification: ResponseMentioned_notification$key
@@ -27,14 +29,25 @@ const ResponseMentioned = (props: Props) => {
           id
           name
         }
+        type
+        status
         kudosEmojiUnicode
       }
     `,
     notificationRef
   )
   const {history} = useRouter()
-  const {meeting, response, kudosEmojiUnicode} = notification
+  const atmosphere = useAtmosphere()
+  const {meeting, response, kudosEmojiUnicode, type, status} = notification
   const {picture: authorPicture, preferredName: authorName} = response.user
+
+  useEffect(() => {
+    SendClientSideEvent(atmosphere, 'Notification Viewed', {
+      notificationType: type,
+      notificationStatus: status,
+      kudosEmojiUnicode
+    })
+  }, [])
 
   const {id: meetingId, name: meetingName} = meeting
   const goThere = () => {
