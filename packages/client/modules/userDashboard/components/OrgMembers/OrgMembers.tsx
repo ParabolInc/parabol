1c4d9d15197713cf429b73c3a748747c3de73fd8
Nick O'Ferrall <nickoferrall@gmail.com>
2023-12-06 08:55:16 +0000

    feat: release checkout flow (#9245)
    * remove checkout flow flag
    
    * remove checkout flow feature flag uses
    
    * update release tests
    
    * remove OrganizationPage

--- packages/client/modules/userDashboard/components/OrgMembers/OrgMembers.tsx
+++ packages/client/modules/userDashboard/components/OrgMembers/OrgMembers.tsx
@@ -13,9 +13,9 @@ import {ElementWidth} from '../../../../types/constEnums'
 import {APP_CORS_OPTIONS} from '../../../../types/cors'
 import OrgMemberRow from '../OrgUserRow/OrgMemberRow'
 
-const StyledPanel = styled(Panel)<{isWide: boolean}>(({isWide}) => ({
-  maxWidth: isWide ? ElementWidth.PANEL_WIDTH : 'inherit'
-}))
+const StyledPanel = styled(Panel)({
+  maxWidth: ElementWidth.PANEL_WIDTH
+})
 
 interface Props {
   queryRef: PreloadedQuery<OrgMembersQuery>
@@ -35,9 +35,6 @@ const OrgMembers = (props: Props) => {
     graphql`
       fragment OrgMembers_viewer on Query @refetchable(queryName: "OrgMembersPaginationQuery") {
         viewer {
-          featureFlags {
-            checkoutFlow
-          }
           organization(orgId: $orgId) {
             ...OrgMemberRow_organization
             name
@@ -70,8 +67,7 @@ const OrgMembers = (props: Props) => {
   )
   const {data} = paginationRes
   const {viewer} = data
-  const {organization, featureFlags} = viewer
-  const {checkoutFlow} = featureFlags
+  const {organization} = viewer
   if (!organization) return null
   const {organizationUsers, name: orgName, isBillingLeader} = organization
   const billingLeaderCount = organizationUsers.edges.reduce(
@@ -108,7 +104,6 @@ const OrgMembers = (props: Props) => {
 
   return (
     <StyledPanel
-      isWide={checkoutFlow}
       label='Organization Members'
       controls={
         isBillingLeader && (
