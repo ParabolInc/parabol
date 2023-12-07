b9ddeff458c22698dc15e7e37e0394f4cecddd3f
Nick O'Ferrall <nickoferrall@gmail.com>
2023-12-07 10:52:10 +0000

    fix: activity library colour imports (#9277)
    * fix: update activity library category colours
    
    * fix category themes background colours

--- packages/client/components/ActivityLibrary/ActivityCard.tsx
+++ packages/client/components/ActivityLibrary/ActivityCard.tsx
@@ -101,7 +101,7 @@ export const ActivityCard = (props: ActivityCardProps) => {
       <div
         className={twMerge(
           'relative flex h-full min-w-0 flex-col overflow-hidden rounded-lg',
-          `bg-${theme.secondary}`,
+          theme.secondary,
           className
         )}
       >
@@ -129,7 +129,7 @@ export const ActivityCard = (props: ActivityCardProps) => {
       {title && category && (
         <div className='mt-2 px-2 pb-2'>
           <div className='truncate pb-1 text-lg leading-5 text-slate-800'>{title}</div>
-          <div className={clsx('font-semibold italic', `text-${theme.primary}`)}>
+          <div className={clsx('font-semibold italic', `${theme.text}`)}>
             {upperFirst(category)}
           </div>
         </div>
