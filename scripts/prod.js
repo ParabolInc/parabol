d00b0ab395457008f040078a4072d482a8a0ca3d
Matt Krick <matt.krick@gmail.com>
2023-12-07 14:28:24 -0800

    chore: cleanup my ugly code
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- scripts/prod.js
+++ scripts/prod.js
@@ -1,33 +1,26 @@
-const webpack = require('webpack')
 const generateGraphQLArtifacts = require('./generateGraphQLArtifacts')
 const cp = require('child_process')
+
+const runChild = (cmd) => {
+  return new Promise((resolve) => {
+    const build = cp.exec(cmd).on('exit', resolve)
+    build.stderr.pipe(process.stderr)
+  })
+}
 
 const prod = async (isDeploy, noDeps) => {
   console.log('🙏🙏🙏      Building Production Server      🙏🙏🙏')
   await generateGraphQLArtifacts()
-  const buildServers = new Promise((resolve) => {
-    const serverBuild = cp
-      .exec(
-        `yarn webpack --config ./scripts/webpack/prod.servers.config.js --no-stats --env=noDeps=${noDeps}`
-      )
-      .on('exit', () => {
-        resolve()
-      })
-    serverBuild.stderr.pipe(process.stderr)
-  })
-  const buildClient = new Promise((resolve) => {
-    const clientBuild = cp
-      .exec(
-        `yarn webpack --config ./scripts/webpack/prod.client.config.js --no-stats --env=isDeploy=${
-          isDeploy || noDeps
-        }`
-      )
-      .on('exit', () => {
-        resolve()
-      })
-    clientBuild.stderr.pipe(process.stderr)
-  })
-  await Promise.all([buildServers, buildClient])
+  await Promise.all([
+    runChild(
+      `yarn webpack --config ./scripts/webpack/prod.servers.config.js --no-stats --env=noDeps=${noDeps}`
+    ),
+    runChild(
+      `yarn webpack --config ./scripts/webpack/prod.client.config.js --no-stats --env=isDeploy=${
+        isDeploy || noDeps
+      }`
+    )
+  ])
 }
 
 if (require.main === module) {
