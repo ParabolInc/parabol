d646abd81eb307ce97e2be4a32b7aaa396a65094
Georg Bremer <github@dschoordsch.de>
2024-01-10 17:14:21 +0100

    feat: Add custom category to activity library
    The custom category lists all team and organization custom templates.

--- packages/client/components/ActivityLibrary/CreateActivityCard.tsx
+++ packages/client/components/ActivityLibrary/CreateActivityCard.tsx
@@ -4,22 +4,11 @@ import {ActivityBadge} from './ActivityBadge'
 import {Add as AddIcon} from '@mui/icons-material'
 import clsx from 'clsx'
 import {Link} from 'react-router-dom'
-import {
-  QUICK_START_CATEGORY_ID,
-  DEFAULT_CARD_THEME,
-  CATEGORY_THEMES,
-  CategoryID,
-  CATEGORY_ID_TO_NAME
-} from './Categories'
-
-const CREATE_ACTIVITY_CARD_THEMES = {
-  [QUICK_START_CATEGORY_ID]: DEFAULT_CARD_THEME,
-  ...CATEGORY_THEMES
-}
+import {CATEGORY_THEMES, CATEGORY_ID_TO_NAME, AllCategoryID} from './Categories'
 
 interface Props {
   className?: string
-  category: CategoryID | typeof QUICK_START_CATEGORY_ID
+  category: AllCategoryID
 }
 
 const CreateActivityCard = (props: Props) => {
@@ -29,7 +18,7 @@ const CreateActivityCard = (props: Props) => {
     <Link className={clsx('flex', className)} to={`/activity-library/new-activity/${category}`}>
       <ActivityLibraryCard
         className={'flex-1 cursor-pointer'}
-        theme={CREATE_ACTIVITY_CARD_THEMES[category]}
+        theme={CATEGORY_THEMES[category]}
         badge={<ActivityBadge className='mx-2 bg-gold-300 text-grape-700'>Premium</ActivityBadge>}
       >
         <div className='flex flex-1 flex-col items-center justify-center text-center font-semibold md:mx-10'>
