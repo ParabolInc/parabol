71af08d15503d24ac4f94c4297a08c464d4808f6
Jordan Husney <jordan.husney@gmail.com>
2024-02-08 08:41:49 -0800

    fix: check for POSTGRES_USE_PGVECTOR

--- scripts/toolboxSrc/pgEnsureExtensions.ts
+++ scripts/toolboxSrc/pgEnsureExtensions.ts
@@ -3,7 +3,12 @@ import getKysely from '../../packages/server/postgres/getKysely'
 
 export default async () => {
   console.log('🔩 Postgres Extension Checks Started')
-  const pg = getKysely()
-  await sql`CREATE EXTENSION IF NOT EXISTS "vector";`.execute(pg)
+  if (process.env.POSTGRES_USE_PGVECTOR) {
+    console.log('   pgvector')
+    const pg = getKysely()
+    await sql`CREATE EXTENSION IF NOT EXISTS "vector";`.execute(pg)
+  } else {
+    console.log('   pgvector: skipping check (POSTGRES_USE_PGVECTOR !== true)')
+  }
   console.log('🔩 Postgres Extension Checks Completed')
 }
