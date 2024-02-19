923171abf5418613488f76cf4baf9b41985bc2e5
Nick O'Ferrall <nickoferrall@gmail.com>
2024-02-19 17:40:21 +0000

    remove unused threshold

--- packages/server/graphql/mutations/addReflectTemplate.ts
+++ packages/server/graphql/mutations/addReflectTemplate.ts
@@ -1,5 +1,5 @@
 import {GraphQLID, GraphQLNonNull} from 'graphql'
-import {SubscriptionChannel, Threshold} from 'parabol-client/types/constEnums'
+import {SubscriptionChannel} from 'parabol-client/types/constEnums'
 import {PALETTE} from '../../../client/styles/paletteV3'
 import getRethink from '../../database/rethinkDriver'
 import ReflectTemplate from '../../database/types/ReflectTemplate'
