ac412c22bfd27adf9b3cc6b37f7df712896b8e01
Jordan Husney <jordan.husney@gmail.com>
2024-03-01 19:25:25 -0800

    chore: msk code review update

--- packages/server/billing/helpers/removeTeamsLimitObjects.ts
+++ packages/server/billing/helpers/removeTeamsLimitObjects.ts
@@ -1,13 +1,12 @@
 import getKysely from '../../postgres/getKysely'
-import {DB} from '../../postgres/pg'
 import {r} from 'rethinkdb-ts'
 import {RValue} from '../../database/stricterR'
 import {DataLoaderWorker} from '../../graphql/graphql'
 import updateNotification from '../../graphql/public/mutations/helpers/updateNotification'
 
 const removeTeamsLimitObjects = async (orgId: string, dataLoader: DataLoaderWorker) => {
-  const removeJobTypes = ['LOCK_ORGANIZATION', 'WARN_ORGANIZATION'] as DB['ScheduledJob']['type'][]
-  const removeNotificationTypes = ['TEAMS_LIMIT_EXCEEDED', 'TEAMS_LIMIT_REMINDER']
+  const removeJobTypes = ['LOCK_ORGANIZATION', 'WARN_ORGANIZATION'] as const
+  const removeNotificationTypes = ['TEAMS_LIMIT_EXCEEDED', 'TEAMS_LIMIT_REMINDER'] as const
   const pg = getKysely()
 
   // Remove team limits jobs and existing notifications
