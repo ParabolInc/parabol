081f7a09a94a3ce2d23816e801c745040737364f
Georg Bremer <github@dschoordsch.de>
2024-03-18 21:56:37 +0100

    fix: Only read the first ip of the x-forwarded-for header (#9545)

--- packages/server/utils/uwsGetIP.ts
+++ packages/server/utils/uwsGetIP.ts
@@ -1,7 +1,7 @@
 import {HttpRequest, HttpResponse} from 'uWebSockets.js'
 
 const uwsGetIP = (res: HttpResponse, req: HttpRequest) => {
-  const clientIp = req.getHeader('x-forwarded-for')
+  const clientIp = req.getHeader('x-forwarded-for')?.split(',')[0]
   if (clientIp) return clientIp
   // returns ipv6 e.g. '0000:0000:0000:0000:0000:ffff:ac11:0001'
   return Buffer.from(res.getRemoteAddressAsText()).toString()
