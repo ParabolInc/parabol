3d246e2ef81f98da343830ca7eb5ebf78dc90cc4
Nick O'Ferrall <nickoferrall@gmail.com>
2024-03-25 18:52:54 +0000

    check if reflections exist or reflect phase is complete

--- packages/server/graphql/public/mutations/updateMeetingTemplate.ts
+++ packages/server/graphql/public/mutations/updateMeetingTemplate.ts
@@ -2,6 +2,7 @@ import {SubscriptionChannel} from '../../../../client/types/constEnums'
 import getRethink from '../../../database/rethinkDriver'
 import MeetingRetrospective from '../../../database/types/MeetingRetrospective'
 import {getUserId, isTeamMember} from '../../../utils/authorization'
+import getPhase from '../../../utils/getPhase'
 import publish from '../../../utils/publish'
 import standardError from '../../../utils/standardError'
 import {MutationResolvers} from '../resolverTypes'
@@ -19,6 +20,22 @@ const updateMeetingTemplate: MutationResolvers['updateMeetingTemplate'] = async 
   if (!meeting) return standardError(new Error('Meeting not found'), {userId: viewerId})
   if (!isTeamMember(authToken, meeting.teamId)) {
     return standardError(new Error('Team not found'), {userId: viewerId})
+  }
+  const reflections = await dataLoader.get('retroReflectionsByMeetingId').load(meetingId)
+  if (reflections.length > 0) {
+    return standardError(new Error('Cannot change template after reflections have been created'), {
+      userId: viewerId
+    })
+  }
+  const reflectPhase = getPhase(meeting.phases, 'reflect')
+  const hasCompletedReflectPhase = reflectPhase.stages.every((stage) => stage.isComplete)
+  if (hasCompletedReflectPhase) {
+    return standardError(
+      new Error('Cannot change template after reflection phase has been completed'),
+      {
+        userId: viewerId
+      }
+    )
   }
 
   await r.table('NewMeeting').get(meetingId).update({templateId}).run()
