cf22c405cb26592e9f9d9fe3cdaf28b56d424997
Matt Krick <matt.krick@gmail.com>
2024-03-28 16:23:20 -0700

    fix: chronos/tsconfig
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/types/custom.d.ts
+++ packages/server/types/custom.d.ts
@@ -1,4 +1,5 @@
 import '../../client/types/reactHTML4'
+import type AuthToken from '../database/types/AuthToken'
 export interface OAuth2Success {
   access_token: string
   token_type: string
@@ -17,4 +18,20 @@ export interface OAuth2Error {
     | 'unsupported_grant_type'
   error_description?: string
   error_uri?: string
+}
+export interface GQLRequest {
+  authToken: AuthToken
+  ip?: string
+  socketId?: string
+  variables?: {[key: string]: any}
+  docId?: string
+  query?: string
+  rootValue?: {[key: string]: any}
+  dataLoaderId?: string
+  // true if the query is on the private schema
+  isPrivate?: boolean
+  // true if the query is ad-hoc (e.g. GraphiQL, CLI)
+  isAdHoc?: boolean
+  // Datadog opentracing span of the calling server
+  carrier?: any
 }
