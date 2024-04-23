32574a650697a3ee5e43aac33478c9ac7eab6966
Matt Krick <matt.krick@gmail.com>
2024-04-23 12:14:53 -0700

    fix: support PG reconnects (#9663)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/postgres/getPg.ts
+++ packages/server/postgres/getPg.ts
@@ -1,12 +1,31 @@
 import {Pool} from 'pg'
+import sleep from '../../client/utils/sleep'
 import getPgConfig from './getPgConfig'
 
 const config = getPgConfig()
+
+const graceFullyReconnect = async () => {
+  for (let i = 0; i < 1e6; i++) {
+    const nextPool = new Pool(getPgConfig())
+    try {
+      const testClient = await nextPool.connect()
+      testClient.release()
+      nextPool.on('error', graceFullyReconnect)
+      const oldPool = pool
+      pool = nextPool
+      oldPool?.emit('changePool')
+      return
+    } catch (e) {
+      await sleep(1000)
+    }
+  }
+}
 
 let pool: Pool | undefined
 const getPg = () => {
   if (!pool) {
     pool = new Pool(config)
+    pool.on('error', graceFullyReconnect)
   }
   return pool
 }
