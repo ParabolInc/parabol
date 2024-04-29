4ad36e83c2d523be7685033873ead1e65128d4de
Matt Krick <matt.krick@gmail.com>
2024-04-29 14:24:28 -0700

    fix: remove unnecessary ignores
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- scripts/webpack/dev.servers.config.js
+++ scripts/webpack/dev.servers.config.js
@@ -60,10 +60,6 @@ module.exports = {
     new webpack.DefinePlugin({
       __PRODUCTION__: false
     }),
-    // if we need canvas for SSR we can just install it to our own package.json
-    new webpack.IgnorePlugin({resourceRegExp: /^canvas$/, contextRegExp: /jsdom$/}),
-    // native bindings might be faster, but abandonware & not currently used
-    new webpack.IgnorePlugin({resourceRegExp: /^pg-native$/, contextRegExp: /pg\/lib/}),
     new webpack.IgnorePlugin({resourceRegExp: /^exiftool-vendored$/, contextRegExp: /@dicebear/}),
     new webpack.IgnorePlugin({resourceRegExp: /^@resvg\/resvg-js$/, contextRegExp: /@dicebear/})
   ],
