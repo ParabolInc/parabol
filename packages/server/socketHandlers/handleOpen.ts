a5d4badf63781ddf9023fcc169d4b90f0f9d646f
Matt Krick <matt.krick@gmail.com>
2024-05-09 12:50:29 -0700

    fix: close websocket with reason on invalid token (#9744)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/socketHandlers/handleOpen.ts
+++ packages/server/socketHandlers/handleOpen.ts
@@ -1,9 +1,13 @@
 import {WebSocketBehavior} from 'uWebSockets.js'
+import {TrebuchetCloseReason} from '../../client/types/constEnums'
 import activeClients from '../activeClients'
 import AuthToken from '../database/types/AuthToken'
 import ConnectionContext from '../socketHelpers/ConnectionContext'
 import keepAlive from '../socketHelpers/keepAlive'
 import {sendEncodedMessage} from '../socketHelpers/sendEncodedMessage'
+import {isAuthenticated} from '../utils/authorization'
+import checkBlacklistJWT from '../utils/checkBlacklistJWT'
+import sendToSentry from '../utils/sendToSentry'
 import handleConnect from './handleConnect'
 
 const APP_VERSION = process.env.npm_package_version
@@ -11,11 +15,32 @@ export type SocketUserData = {
   connectionContext: ConnectionContext
   authToken: AuthToken
   ip: string
+  protocol: string
   done?: true
 }
 
 const handleOpen: WebSocketBehavior<SocketUserData>['open'] = async (socket) => {
-  const {authToken, ip} = socket.getUserData()
+  const {authToken, ip, protocol} = socket.getUserData()
+  if (protocol !== 'trebuchet-ws') {
+    sendToSentry(new Error(`WebSocket error: invalid protocol: ${protocol}`))
+    // WS Error 1002 is roughly HTTP 412 Precondition Failed because we can't support the req header
+    socket.end(412, 'Invalid protocol')
+    return
+  }
+
+  if (!isAuthenticated(authToken)) {
+    socket.end(401, TrebuchetCloseReason.EXPIRED_SESSION)
+    return
+  }
+
+  // ALL async calls must come after the message listener, or we'll skip out on messages (e.g. resub after server restart)
+  const {sub: userId, iat} = authToken
+  const isBlacklistedJWT = await checkBlacklistJWT(userId, iat)
+  if (isBlacklistedJWT) {
+    socket.end(401, TrebuchetCloseReason.EXPIRED_SESSION)
+    return
+  }
+
   const connectionContext = (socket.getUserData().connectionContext = new ConnectionContext(
     socket,
     authToken,
