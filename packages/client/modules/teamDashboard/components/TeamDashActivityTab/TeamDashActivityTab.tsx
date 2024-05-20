28c743274df3c8ed97e3e8dbe2677a58483a851e
Bruce Tian <tianrunhe@gmail.com>
2024-05-20 10:57:12 -0700

    chore: update tutorial card thumbnail & video links (#9746)

--- packages/client/modules/teamDashboard/components/TeamDashActivityTab/TeamDashActivityTab.tsx
+++ packages/client/modules/teamDashboard/components/TeamDashActivityTab/TeamDashActivityTab.tsx
@@ -65,6 +65,7 @@ const TeamDashActivityTab = (props: Props) => {
         ) : (
           <>
             <DemoMeetingCard />
+            <TutorialMeetingCard type='retro' />
             <TutorialMeetingCard type='standup' />
             <TutorialMeetingCard type='poker' />
           </>
