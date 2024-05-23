222d6f94b9387dfbcff347f2bbcac8eef98bfe97
Georg Bremer <github@dschoordsch.de>
2024-05-23 08:06:21 +0200

    chore: Update processRecurrence tests (#9770)

--- packages/server/__tests__/processRecurrence.test.ts
+++ packages/server/__tests__/processRecurrence.test.ts
@@ -1,6 +1,8 @@
 import ms from 'ms'
+import TeamMemberId from 'parabol-client/shared/gqlIds/TeamMemberId'
 import {RRule} from 'rrule'
 import getRethink from '../database/rethinkDriver'
+import DiscussPhase from '../database/types/DiscussPhase'
 import MeetingRetrospective from '../database/types/MeetingRetrospective'
 import MeetingTeamPrompt from '../database/types/MeetingTeamPrompt'
 import ReflectPhase from '../database/types/ReflectPhase'
@@ -71,13 +73,14 @@ test('Should not end meetings that are not scheduled to end', async () => {
   const r = await getRethink()
   const {userId} = await signUp()
   const {id: teamId} = (await getUserTeams(userId))[0]
+  const teamMemberId = TeamMemberId.join(teamId, userId)
 
   const meetingId = generateUID()
   const meeting = new MeetingTeamPrompt({
     id: meetingId,
     teamId,
     meetingCount: 0,
-    phases: [new TeamPromptResponsesPhase(['foobar'])],
+    phases: [new TeamPromptResponsesPhase([teamMemberId])],
     facilitatorUserId: userId,
     meetingPrompt: 'What are you working on today? Stuck on anything?'
   })
@@ -108,13 +111,14 @@ test('Should not end meetings that are scheduled to end in the future', async ()
   const r = await getRethink()
   const {userId} = await signUp()
   const {id: teamId} = (await getUserTeams(userId))[0]
+  const teamMemberId = TeamMemberId.join(teamId, userId)
 
   const meetingId = generateUID()
   const meeting = new MeetingTeamPrompt({
     id: meetingId,
     teamId,
     meetingCount: 0,
-    phases: [new TeamPromptResponsesPhase(['foobar'])],
+    phases: [new TeamPromptResponsesPhase([teamMemberId])],
     facilitatorUserId: userId,
     meetingPrompt: 'What are you working on today? Stuck on anything?',
     scheduledEndTime: new Date(Date.now() + ms('5m'))
@@ -148,13 +152,14 @@ test('Should end meetings that are scheduled to end in the past', async () => {
   const r = await getRethink()
   const {userId} = await signUp()
   const {id: teamId} = (await getUserTeams(userId))[0]
+  const teamMemberId = TeamMemberId.join(teamId, userId)
 
   const meetingId = generateUID()
   const meeting = new MeetingTeamPrompt({
     id: meetingId,
     teamId,
     meetingCount: 0,
-    phases: [new TeamPromptResponsesPhase(['foobar'])],
+    phases: [new TeamPromptResponsesPhase([teamMemberId])],
     facilitatorUserId: userId,
     meetingPrompt: 'What are you working on today? Stuck on anything?',
     scheduledEndTime: new Date(Date.now() - ms('5m'))
@@ -186,6 +191,7 @@ test('Should end the current team prompt meeting and start a new meeting', async
   const r = await getRethink()
   const {userId} = await signUp()
   const {id: teamId} = (await getUserTeams(userId))[0]
+  const teamMemberId = TeamMemberId.join(teamId, userId)
 
   const now = new Date()
 
@@ -194,8 +200,10 @@ test('Should end the current team prompt meeting and start a new meeting', async
     Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - 2, 9)
   )
   const recurrenceRule = new RRule({
-    freq: RRule.DAILY,
-    dtstart: startDate
+    freq: RRule.WEEKLY,
+    dtstart: startDate,
+    interval: 1,
+    byweekday: [RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR, RRule.SA, RRule.SU]
   })
 
   const meetingSeriesId = await insertMeetingSeriesQuery({
@@ -212,7 +220,7 @@ test('Should end the current team prompt meeting and start a new meeting', async
     id: meetingId,
     teamId,
     meetingCount: 0,
-    phases: [new TeamPromptResponsesPhase(['foobar'])],
+    phases: [new TeamPromptResponsesPhase([teamMemberId])],
     facilitatorUserId: userId,
     meetingPrompt: 'What are you working on today? Stuck on anything?',
     scheduledEndTime: new Date(Date.now() - ms('5m')),
@@ -269,8 +277,10 @@ test('Should end the current retro meeting and start a new meeting', async () =>
     Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - 2, 9)
   )
   const recurrenceRule = new RRule({
-    freq: RRule.DAILY,
-    dtstart: startDate
+    freq: RRule.WEEKLY,
+    dtstart: startDate,
+    interval: 1,
+    byweekday: [RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR, RRule.SA, RRule.SU]
   })
 
   const meetingSeriesId = await insertMeetingSeriesQuery({
@@ -287,7 +297,7 @@ test('Should end the current retro meeting and start a new meeting', async () =>
     id: meetingId,
     teamId,
     meetingCount: 0,
-    phases: [new ReflectPhase(teamId, [])],
+    phases: [new ReflectPhase(teamId, []), new DiscussPhase(undefined)],
     facilitatorUserId: userId,
     scheduledEndTime: new Date(Date.now() - ms('5m')),
     meetingSeriesId,
@@ -339,6 +349,7 @@ test('Should only start a new meeting if it would still be active', async () => 
   const r = await getRethink()
   const {userId} = await signUp()
   const {id: teamId} = (await getUserTeams(userId))[0]
+  const teamMemberId = TeamMemberId.join(teamId, userId)
 
   const now = new Date()
 
@@ -347,8 +358,10 @@ test('Should only start a new meeting if it would still be active', async () => 
     Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() - 5, 9)
   )
   const recurrenceRule = new RRule({
-    freq: RRule.DAILY,
-    dtstart: startDate
+    freq: RRule.WEEKLY,
+    dtstart: startDate,
+    interval: 1,
+    byweekday: [RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR, RRule.SA, RRule.SU]
   })
 
   const newMeetingSeriesId = await insertMeetingSeriesQuery({
@@ -365,7 +378,7 @@ test('Should only start a new meeting if it would still be active', async () => 
     id: meetingId,
     teamId,
     meetingCount: 0,
-    phases: [new TeamPromptResponsesPhase(['foobar'])],
+    phases: [new TeamPromptResponsesPhase([teamMemberId])],
     facilitatorUserId: userId,
     meetingPrompt: 'What are you working on today? Stuck on anything?',
     scheduledEndTime: new Date(Date.now() - ms('73h')),
@@ -403,6 +416,7 @@ test('Should not start a new meeting if the rrule has not started', async () => 
   const r = await getRethink()
   const {userId} = await signUp()
   const {id: teamId} = (await getUserTeams(userId))[0]
+  const teamMemberId = TeamMemberId.join(teamId, userId)
 
   const now = new Date()
 
@@ -411,8 +425,10 @@ test('Should not start a new meeting if the rrule has not started', async () => 
     Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate() + 1, 9)
   )
   const recurrenceRule = new RRule({
-    freq: RRule.DAILY,
-    dtstart: startDate
+    freq: RRule.WEEKLY,
+    dtstart: startDate,
+    interval: 1,
+    byweekday: [RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR, RRule.SA, RRule.SU]
   })
 
   const newMeetingSeriesId = await insertMeetingSeriesQuery({
@@ -429,7 +445,7 @@ test('Should not start a new meeting if the rrule has not started', async () => 
     id: meetingId,
     teamId,
     meetingCount: 0,
-    phases: [new TeamPromptResponsesPhase(['foobar'])],
+    phases: [new TeamPromptResponsesPhase([teamMemberId])],
     facilitatorUserId: userId,
     meetingPrompt: 'What are you working on today? Stuck on anything?',
     scheduledEndTime: new Date(Date.now() - ms('1h')),
@@ -467,6 +483,7 @@ test('Should not hang if the rrule interval is invalid', async () => {
   const r = await getRethink()
   const {userId} = await signUp()
   const {id: teamId} = (await getUserTeams(userId))[0]
+  const teamMemberId = TeamMemberId.join(teamId, userId)
 
   const now = new Date()
 
@@ -495,7 +512,7 @@ test('Should not hang if the rrule interval is invalid', async () => {
     id: meetingId,
     teamId,
     meetingCount: 0,
-    phases: [new TeamPromptResponsesPhase(['foobar'])],
+    phases: [new TeamPromptResponsesPhase([teamMemberId])],
     facilitatorUserId: userId,
     meetingPrompt: 'What are you working on today? Stuck on anything?',
     scheduledEndTime: new Date(Date.now() - ms('5m')),
