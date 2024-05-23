034ccbaab2d874f587e256729148ddb75c4125cf
Georg Bremer <github@dschoordsch.de>
2024-05-23 17:44:00 +0200

    feat: Create embeddings for meeting templates

--- packages/embedder/importHistoricalMeetingTemplates.ts
+++ packages/embedder/importHistoricalMeetingTemplates.ts
@@ -1,10 +1,10 @@
 import getKysely from 'parabol-server/postgres/getKysely'
 import {Logger} from 'parabol-server/utils/Logger'
-import {addEmbeddingsMetadataForRetrospectiveDiscussionTopic} from './addEmbeddingsMetadataForRetrospectiveDiscussionTopic'
+import {addEmbeddingsMetadataForMeetingTemplate} from './addEmbeddingsMetadataForMeetingTemplate'
 
 // Check to see if the oldest discussion topic exists in the metadata table
 // If not, get the date of the oldest discussion topic in the metadata table and import all items before that date
-export const importHistoricalRetrospectiveDiscussionTopic = async () => {
+export const importHistoricalMeetingTemplates = async () => {
   const pg = getKysely()
   const isEarliestMetadataImported = await pg
     .selectFrom('EmbeddingsMetadata')
@@ -13,10 +13,9 @@ export const importHistoricalRetrospectiveDiscussionTopic = async () => {
       eb(
         'EmbeddingsMetadata.refId',
         '=',
-        selectFrom('Discussion')
-          .select('Discussion.id')
-          .where('discussionTopicType', '=', 'reflectionGroup')
-          .orderBy(['createdAt', 'id'])
+        selectFrom('MeetingTemplate')
+          .select('MeetingTemplate.id')
+          .orderBy(['updatedAt', 'id'])
           .limit(1)
       )
     )
@@ -24,14 +23,14 @@ export const importHistoricalRetrospectiveDiscussionTopic = async () => {
     .executeTakeFirst()
 
   if (isEarliestMetadataImported) return
-  const earliestImportedDiscussion = await pg
+  const earliestImportedTemplate = await pg
     .selectFrom('EmbeddingsMetadata')
     .select(['id', 'refUpdatedAt', 'refId'])
     .where('objectType', '=', 'meetingTemplate')
     .orderBy('refUpdatedAt')
     .limit(1)
     .executeTakeFirst()
-  const endAt = earliestImportedDiscussion?.refUpdatedAt ?? undefined
-  Logger.log(`Importing discussion history up to ${endAt || 'now'}`)
-  return addEmbeddingsMetadataForRetrospectiveDiscussionTopic({endAt})
+  const endAt = earliestImportedTemplate?.refUpdatedAt ?? undefined
+  Logger.log(`Importing meeting template history up to ${endAt || 'now'}`)
+  return addEmbeddingsMetadataForMeetingTemplate({endAt})
 }
