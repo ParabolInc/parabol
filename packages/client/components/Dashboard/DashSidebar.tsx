71b17c2d05d1f75e6eff98e94d45953ad9907c6b
Nick O'Ferrall <nickoferrall@gmail.com>
2024-06-14 18:35:49 +0100

    feat: update dashboard nav item styles (#9795)
    Co-authored-by: Terry Acker <ackernaut@gmail.com>

--- packages/client/components/Dashboard/DashSidebar.tsx
+++ packages/client/components/Dashboard/DashSidebar.tsx
@@ -38,13 +38,9 @@ const NavMain = styled('div')({
   overflowY: 'auto'
 })
 
-const DashHR = styled('div')({
-  borderBottom: `solid ${PALETTE.SLATE_300} 1px`,
-  width: '100%'
-})
-
 const NavItem = styled(LeftDashNavItem)({
-  paddingLeft: 16
+  borderRadius: 44,
+  paddingLeft: 15
 })
 
 const NavList = styled(DashNavList)({
@@ -61,12 +57,11 @@ const Wrapper = styled('div')({
 })
 
 const OrgName = styled('div')({
-  paddingTop: 8,
+  color: PALETTE.SLATE_600,
   fontWeight: 600,
   fontSize: 12,
-  lineHeight: '24px',
-  color: PALETTE.SLATE_500,
-  paddingLeft: 16
+  lineHeight: '16px',
+  padding: '8px 16px'
 })
 
 interface Props {
@@ -104,40 +99,42 @@ const DashSidebar = (props: Props) => {
         <SideBarStartMeetingButton isOpen={isOpen} />
         <Nav isOpen={isOpen}>
           <Contents>
-            <NavItemsWrap>
-              <NavItem
-                icon={'arrowBack'}
-                href={'/me/organizations'}
-                label={'Organizations'}
-                exact
-              />
-              <OrgName>{name}</OrgName>
-              <NavItem
-                icon={'creditScore'}
-                href={`/me/organizations/${orgId}/${BILLING_PAGE}`}
-                label={'Plans & Billing'}
-              />
-              <NavItem
-                icon={'groups'}
-                href={`/me/organizations/${orgId}/${TEAMS_PAGE}`}
-                label={'Teams'}
-              />
-              <NavItem
-                icon={'group'}
-                href={`/me/organizations/${orgId}/${MEMBERS_PAGE}`}
-                label={'Members'}
-              />
-              <NavItem
-                icon={'work'}
-                href={`/me/organizations/${orgId}/${ORG_SETTINGS_PAGE}`}
-                label={'Organization Settings'}
-              />
-              <NavItem
-                icon={'key'}
-                href={`/me/organizations/${orgId}/${AUTHENTICATION_PAGE}`}
-                label={'Authentication'}
-              />
-            </NavItemsWrap>
+            <div className='px-3'>
+              <NavItemsWrap>
+                <NavItem
+                  icon={'arrowBack'}
+                  href={'/me/organizations'}
+                  label={'Organizations'}
+                  exact
+                />
+                <OrgName>{name}</OrgName>
+                <NavItem
+                  icon={'creditScore'}
+                  href={`/me/organizations/${orgId}/${BILLING_PAGE}`}
+                  label={'Plans & Billing'}
+                />
+                <NavItem
+                  icon={'groups'}
+                  href={`/me/organizations/${orgId}/${TEAMS_PAGE}`}
+                  label={'Teams'}
+                />
+                <NavItem
+                  icon={'group'}
+                  href={`/me/organizations/${orgId}/${MEMBERS_PAGE}`}
+                  label={'Members'}
+                />
+                <NavItem
+                  icon={'work'}
+                  href={`/me/organizations/${orgId}/${ORG_SETTINGS_PAGE}`}
+                  label={'Organization Settings'}
+                />
+                <NavItem
+                  icon={'key'}
+                  href={`/me/organizations/${orgId}/${AUTHENTICATION_PAGE}`}
+                  label={'Authentication'}
+                />
+              </NavItemsWrap>
+            </div>
           </Contents>
         </Nav>
       </Wrapper>
@@ -149,20 +146,15 @@ const DashSidebar = (props: Props) => {
       <SideBarStartMeetingButton isOpen={isOpen} />
       <Nav isOpen={isOpen}>
         <Contents>
-          <NavItemsWrap>
+          <div className='border-x border-solid border-[transparent] px-4'>
             <NavItem icon={'forum'} href={'/meetings'} label={'Meetings'} />
             <NavItem icon={'timeline'} href={'/me'} label={'History'} exact />
             <NavItem icon={'playlist_add_check'} href={'/me/tasks'} label={'Tasks'} />
-          </NavItemsWrap>
-          <DashHR />
+            <NavItem icon={'add'} href={'/newteam/1'} label={'Add a Team'} />
+          </div>
           <NavMain>
             <NavList organizationsRef={organizations} />
           </NavMain>
-          <DashHR />
-          <NavItemsWrap>
-            <NavItem icon={'add'} href={'/newteam/1'} label={'Add a Team'} />
-          </NavItemsWrap>
-          <DashHR />
         </Contents>
       </Nav>
     </Wrapper>
