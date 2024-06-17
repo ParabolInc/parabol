7264439094886e318ea57fe62a1ce2e0f82d1964
Nick O'Ferrall <nickoferrall@gmail.com>
2024-06-17 15:03:54 +0100

    fix: update promote team member copy

--- packages/client/modules/teamDashboard/components/PromoteTeamMemberModal/PromoteTeamMemberModal.tsx
+++ packages/client/modules/teamDashboard/components/PromoteTeamMemberModal/PromoteTeamMemberModal.tsx
@@ -49,20 +49,20 @@ const PromoteTeamMemberModal = (props: Props) => {
     teamMemberRef
   )
   const {preferredName, teamId, userId, team, isSelf} = teamMember
-  const {isOrgAdmin, teamLead} = team ?? {}
-  const oldLeadName = teamLead?.preferredName
-  const isOldLeadSelf = teamLead?.isSelf
+  const {isOrgAdmin, teamLead} = team!
+  const oldLeadName = teamLead.preferredName
+  const isOldLeadSelf = teamLead.isSelf
   const handleClick = () => {
     submitMutation()
     PromoteToTeamLeadMutation(atmosphere, {teamId, userId}, {onError, onCompleted})
     closePortal()
   }
 
-  const copyStart = `Are you sure? ${isOldLeadSelf ? 'You' : oldLeadName} will be removed as the team leader and ${isSelf ? 'You' : preferredName} will be promoted.`
+  const copyStart = `Are you sure? ${isOldLeadSelf ? 'You' : oldLeadName} will be removed as the team leader and ${isSelf ? 'you' : preferredName} will be promoted.`
   const copyEnd =
     isOldLeadSelf && isOrgAdmin
       ? ''
-      : `${isSelf ? 'You' : upperFirst(preferredName)} will no longer be able to change team membership.`
+      : `${isOldLeadSelf ? 'You' : upperFirst(oldLeadName)} will no longer be able to change team membership.`
   const copy = `${copyStart} ${copyEnd}`
 
   return (
