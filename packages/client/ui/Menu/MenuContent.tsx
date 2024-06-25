571c3a558a6bfd40d2777c26ac7c863e9401d233
Nick O'Ferrall <nickoferrall@gmail.com>
2024-06-25 09:06:11 +0100

    update radix menu content z index

--- packages/client/ui/Menu/MenuContent.tsx
+++ packages/client/ui/Menu/MenuContent.tsx
@@ -12,7 +12,7 @@ export const MenuContent = React.forwardRef<HTMLDivElement, MenuContentProps>(
     return (
       <DropdownMenu.Content
         className={twMerge(
-          'border-rad w-auto min-w-[200px] max-w-[400px] rounded-md bg-white shadow-lg outline-none',
+          'border-rad z-10 w-auto min-w-[200px] max-w-[400px] rounded-md bg-white shadow-lg outline-none',
           className
         )}
         ref={ref}
