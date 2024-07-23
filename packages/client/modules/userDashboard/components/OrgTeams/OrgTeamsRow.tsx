0eac79fddac14cc2085ac79bd3f070ea2aff0002
Bruce Tian <tianrunhe@gmail.com>
2024-07-23 15:55:41 -0700

    feat(orgAdmin): only team lead or org admin can archive teams

--- packages/client/modules/userDashboard/components/OrgTeams/OrgTeamsRow.tsx
+++ packages/client/modules/userDashboard/components/OrgTeams/OrgTeamsRow.tsx
@@ -11,10 +11,11 @@ import ArchiveTeamModal from './ArchiveTeamModal'
 
 type Props = {
   teamRef: OrgTeamsRow_team$key
+  isOrgAdmin: boolean
 }
 
 const OrgTeamsRow = (props: Props) => {
-  const {teamRef} = props
+  const {teamRef, isOrgAdmin} = props
   const team = useFragment(
     graphql`
       fragment OrgTeamsRow_team on Team {
@@ -34,22 +35,29 @@ const OrgTeamsRow = (props: Props) => {
   const {id: teamId, teamMembers, name} = team
   const teamMembersCount = teamMembers.length
   const viewerTeamMember = teamMembers.find((m) => m.isSelf)
-  if (!viewerTeamMember) return null
-  const {isLead: viewerIsLead, isOrgAdmin: viewerIsOrgAdmin} = viewerTeamMember
+  if (!viewerTeamMember && !isOrgAdmin) return null
+  const {isLead: viewerIsLead} = viewerTeamMember ?? {isLead: false}
   const {togglePortal, modalPortal} = useModal()
 
   return (
     <>
       <div className='flex items-center justify-between p-4 hover:bg-slate-100'>
         <div className='flex flex-col'>
-          <Link to={`teams/${teamId}`} className='inline-block text-lg font-bold text-sky-500'>
-            {name}
-          </Link>
+          <div className='inline-flex items-center items-center gap-x-2'>
+            <Link to={`teams/${teamId}`} className='inline-block text-lg font-bold text-sky-500'>
+              {name}
+            </Link>
+            {viewerIsLead && (
+              <span className='rounded-full bg-primary px-2 py-0.5 text-xs text-white'>
+                Your team
+              </span>
+            )}
+          </div>
           <div className='text-gray-600'>
             {`${teamMembersCount} ${plural(teamMembersCount, 'member')}`}
           </div>
         </div>
-        {(viewerIsLead || viewerIsOrgAdmin) && (
+        {(viewerIsLead || isOrgAdmin) && (
           <div className='flex items-center'>
             <IconButton
               onClick={togglePortal}
