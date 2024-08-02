e3a783e56d3fabfe4652c5b8c73b6f606365e2c4
Matt Krick <matt.krick@gmail.com>
2024-08-01 18:05:32 -0700

    fix playwright tests
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/modules/demo/getDemoEntities.ts
+++ packages/client/modules/demo/getDemoEntities.ts
@@ -18,6 +18,30 @@ const query = graphql`
 const getDemoEntities = async (text: string) => {
   if (!text || text.length <= 2) return []
   const remoteAtmosphere = new Atmosphere()
+
+  const TEST_REFLECTIONS = {
+    'Writing things down': [
+      {
+        lemma: 'thing',
+        name: 'things',
+        salience: 1
+      }
+    ],
+    'Documenting things in Notion': [
+      {
+        lemma: 'thing',
+        name: 'things',
+        salience: 0.8
+      },
+      {
+        lemma: 'notion',
+        name: 'notion',
+        salience: 0.2
+      }
+    ]
+  }
+  const cachedEntities = TEST_REFLECTIONS[text as keyof typeof TEST_REFLECTIONS]
+  if (cachedEntities) return cachedEntities
   const res = await fetchQuery<getDemoEntitiesQuery>(remoteAtmosphere, query, {text}).toPromise()
   return res?.getDemoEntities?.entities ?? []
 }
