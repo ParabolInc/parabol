24536bfbea8b4d1b357e1cd7b8e9be96f641ee0e
Nick O'Ferrall <nickoferrall@gmail.com>
2024-08-29 17:43:09 +0100

    add feature flag tables migration

---
+++ packages/server/postgres/migrations/1724949259778_addFeatureFlagTables.ts
@@ -0,0 +1,72 @@
+import {Kysely, PostgresDialect, sql} from 'kysely'
+import getPg from '../getPg'
+
+export async function up() {
+  const pg = new Kysely<any>({
+    dialect: new PostgresDialect({
+      pool: getPg()
+    })
+  })
+
+  await pg.schema.createType('ScopeEnum').asEnum(['User', 'Team', 'Organization']).execute()
+
+  await pg.schema
+    .createTable('FeatureFlag')
+    .addColumn('id', 'varchar(255)', (col) => col.primaryKey())
+    .addColumn('featureName', 'varchar(255)', (col) => col.notNull())
+    .addColumn('scope', sql`"ScopeEnum"`, (col) => col.notNull())
+    .addColumn('description', 'text')
+    .addColumn('expiresAt', 'timestamptz', (col) => col.notNull())
+    .addColumn('createdAt', 'timestamptz', (col) => col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`))
+    .addColumn('updatedAt', 'timestamptz', (col) => col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`))
+    .execute()
+
+  await pg.schema
+    .createTable('FeatureFlagOwner')
+    .addColumn('featureFlagId', 'varchar(255)', (col) =>
+      col.notNull().references('FeatureFlag.id').onDelete('cascade')
+    )
+    .addColumn('userId', 'varchar(255)')
+    .addColumn('teamId', 'varchar(255)')
+    .addColumn('orgId', 'varchar(255)')
+    .addColumn('createdAt', 'timestamptz', (col) => col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`))
+    .addCheckConstraint(
+      'check_feature_flag_owner_exclusivity',
+      sql`
+      (("userId" IS NOT NULL AND "teamId" IS NULL AND "orgId" IS NULL) OR
+       ("userId" IS NULL AND "teamId" IS NOT NULL AND "orgId" IS NULL) OR
+       ("userId" IS NULL AND "teamId" IS NULL AND "orgId" IS NOT NULL))
+    `
+    )
+    .execute()
+
+  await pg.schema
+    .createIndex('idx_feature_flag_owner_user')
+    .on('FeatureFlagOwner')
+    .columns(['userId', 'featureFlagId'])
+    .execute()
+
+  await pg.schema
+    .createIndex('idx_feature_flag_owner_team')
+    .on('FeatureFlagOwner')
+    .columns(['teamId', 'featureFlagId'])
+    .execute()
+
+  await pg.schema
+    .createIndex('idx_feature_flag_owner_org')
+    .on('FeatureFlagOwner')
+    .columns(['orgId', 'featureFlagId'])
+    .execute()
+}
+
+export async function down() {
+  const pg = new Kysely<any>({
+    dialect: new PostgresDialect({
+      pool: getPg()
+    })
+  })
+
+  await pg.schema.dropTable('FeatureFlag').execute()
+  await pg.schema.dropTable('FeatureFlagOwner').execute()
+  await pg.schema.dropType('ScopeEnum').execute()
+}
