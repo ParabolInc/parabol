4352f88485aaed681fbcd03d876a0f49764cd28f
Nick O'Ferrall <nickoferrall@gmail.com>
2024-09-16 16:53:45 +0100

    implement getFeatureFlags

---
+++ packages/server/graphql/private/mutations/getFeatureFlags.ts
@@ -0,0 +1,34 @@
+import getKysely from '../../../postgres/getKysely'
+import standardError from '../../../utils/standardError'
+import {QueryResolvers} from '../resolverTypes'
+
+const getFeatureFlags: QueryResolvers['getFeatureFlags'] = async (
+  _source,
+  {userId, orgId, teamId}
+) => {
+  if ([userId, orgId, teamId].length !== 1) {
+    const error = new Error('Exactly one of userId, orgId, or teamId must be provided')
+    return standardError(error)
+  }
+
+  const pg = getKysely()
+  const query = pg
+    .selectFrom('FeatureFlag as ff')
+    .innerJoin('FeatureFlagOwner as ffo', 'ff.id', 'ffo.featureFlagId')
+    .selectAll('ff')
+
+  if (userId) {
+    query.where('ffo.userId', '=', userId)
+  } else if (orgId) {
+    query.where('ffo.orgId', '=', orgId)
+  } else {
+    query.where('ffo.teamId', '=', teamId!)
+  }
+
+  const featureFlags = await query.execute()
+  const featureFlagIds = featureFlags.map((flag) => flag.id)
+
+  return {featureFlagIds}
+}
+
+export default getFeatureFlags
