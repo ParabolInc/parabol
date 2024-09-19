e80e457a66300110e23c986c56835db73d21438e
Nick O'Ferrall <nickoferrall@gmail.com>
2024-09-19 14:31:55 +0100

    add comment

--- packages/client/modules/teamDashboard/components/TeamDashInsightsTab/TeamInsights.tsx
+++ packages/client/modules/teamDashboard/components/TeamDashInsightsTab/TeamInsights.tsx
@@ -1,3 +1,4 @@
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome'
 import graphql from 'babel-plugin-relay/macro'
 import React from 'react'
 import {PreloadedQuery, usePreloadedQuery} from 'react-relay'
@@ -21,12 +22,25 @@ const query = graphql`
 const Insights = (props: Props) => {
   const {queryRef} = props
   const data = usePreloadedQuery<TeamInsightsQuery>(query, queryRef)
-  const {viewer} = data
+  // TODO: use the query rather than just console logging it
+  console.log('🚀 ~ data:', data)
 
   return (
-    <div>
-      <h2>Insights for {viewer.team?.name}</h2>
-      {/* Add your insights UI components here */}
+    <div className='space-y-6'>
+      <p className='text-sm text-slate-700'>
+        Only you (as <span className='font-bold'>Team Lead</span>) can see Team Insights. Insights
+        are auto-generated.{' '}
+        <a href='#' className='font-semibold text-sky-500 hover:underline'>
+          Give us feedback
+        </a>
+        .
+      </p>
+      <div className='mx-auto aspect-[1/1.414] max-w-3xl overflow-y-auto rounded-lg bg-white p-6 shadow-md'>
+        <h2 className='mb-4 flex items-center text-2xl font-semibold'>
+          <AutoAwesomeIcon className='mr-2 text-grape-500' />
+          <span>Insights - Aug to Sep 2024</span>
+        </h2>
+      </div>
     </div>
   )
 }
