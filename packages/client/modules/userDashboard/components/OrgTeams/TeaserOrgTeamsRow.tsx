afb46abdf24ebe9263e15f8154ae0cf9c2f902fa
Bruce Tian <tianrunhe@gmail.com>
2024-10-16 14:27:29 -0700

    Address feedback on sytle of the teaser row

--- packages/client/modules/userDashboard/components/OrgTeams/TeaserOrgTeamsRow.tsx
+++ packages/client/modules/userDashboard/components/OrgTeams/TeaserOrgTeamsRow.tsx
@@ -1,7 +1,6 @@
 import {Lock} from '@mui/icons-material'
 import React from 'react'
 import {useHistory} from 'react-router'
-import {Button} from '../../../../ui/Button/Button'
 import plural from '../../../../utils/plural'
 
 type Props = {
@@ -13,7 +12,7 @@ const TeaserOrgTeamsRow = (props: Props) => {
   const {hiddenTeamCount, orgId} = props
   const history = useHistory()
 
-  const handleSeePlansClick = () => {
+  const handleParabolEnterpriseClick = () => {
     history.push(`/me/organizations/${orgId}/billing`)
   }
 
@@ -26,26 +25,23 @@ const TeaserOrgTeamsRow = (props: Props) => {
           </div>
           <div className='flex items-center justify-between'>
             <div className='text-gray-600'>
-              Parabol Enterprise includes our Org Admin role, which allows you to{' '}
-              <strong>see all teams</strong> in your organization.
+              <a
+                href='#'
+                onClick={handleParabolEnterpriseClick}
+                className='font-bold text-sky-500 hover:text-sky-600'
+              >
+                Parabol Enterprise
+              </a>{' '}
+              includes our Org Admin role, which allows you to <strong>see all teams</strong> in
+              your organization.
             </div>
           </div>
         </div>
         <div className='flex items-center justify-center'>
-          <div className='flex h-12 w-8 flex-row items-center py-2'>
-            <Lock className='h-8 w-8 text-slate-600' />
+          <div className='flex h-8 w-6 flex-row items-center py-2'>
+            <Lock className='h-6 w-6 text-slate-600' />
           </div>
         </div>
-      </div>
-      <div className='px-4 pb-4'>
-        <Button
-          variant='primary'
-          shape='pill'
-          className='bg-pink-500 hover:bg-pink-600 px-6 py-2 text-base text-white'
-          onClick={handleSeePlansClick}
-        >
-          See Plans
-        </Button>
       </div>
     </div>
   )
