1eed39a6f833cb911e6384a76913d7488b154f03
Bruce Tian <tianrunhe@gmail.com>
2024-10-16 14:39:55 -0700

    Clarify hidden teams count

--- packages/client/modules/userDashboard/components/OrgTeams/OrgTeams.tsx
+++ packages/client/modules/userDashboard/components/OrgTeams/OrgTeams.tsx
@@ -66,7 +66,8 @@ const OrgTeams = (props: Props) => {
         <div className='bg-slate-100 px-4 py-2'>
           <div className='flex w-full justify-between'>
             <div className='flex items-center font-bold'>
-              {allTeamsCount} {plural(allTeamsCount, 'Team')}
+              {allTeamsCount} {plural(allTeamsCount, 'Team')}{' '}
+              {!showAllTeams ? `(${allTeamsCount - viewerTeamCount} hidden)` : null}
             </div>
           </div>
         </div>
