05d1f2f818916b9848ddceab726af4dd4f080939
Matt Krick <matt.krick@gmail.com>
2024-10-21 17:38:28 -0700

    support standalone migrations
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- scripts/toolboxSrc/standaloneMigrations.ts
+++ scripts/toolboxSrc/standaloneMigrations.ts
@@ -1,36 +1,12 @@
 // Should roughly match runMigrations.js except it isn't run in PM2 in dev
 // This file is bundled by webpack into a small migrate.js file which includes all migration files & their deps
 // It is used by PPMIs who are only provided with the bundles
+import {Migrator} from 'kysely'
 import path from 'path'
-import {r} from 'rethinkdb-ts'
-import {parse} from 'url'
-import cliPgmConfig from '../../packages/server/postgres/pgmConfig'
+import migratorConfig from '../../.config/kysely.config'
+import getKysely from '../../packages/server/postgres/getKysely'
 import '../webpack/utils/dotenv'
 import pgEnsureExtensions from './pgEnsureExtensions'
-import pgMigrate from './pgMigrateRunner'
-import * as rethinkMigrate from './rethinkMigrateRunner'
-
-const migrateRethinkDB = async () => {
-  console.log('👴 RethinkDB Migration Started')
-  const {hostname, port, path: urlPath} = parse(process.env.RETHINKDB_URL!)
-  process.env.host = hostname!
-  process.env.port = port!
-  process.env.db = urlPath!.slice(1)
-  process.env.r = process.cwd()
-  const context = (require as any).context(
-    '../../packages/server/database/migrations',
-    false,
-    /.(js|ts)$/
-  )
-  const collector = {}
-  context.keys().forEach((relativePath) => {
-    const {name} = path.parse(relativePath)
-    collector[name] = context(relativePath)
-  })
-  await rethinkMigrate.up({all: true, migrations: collector})
-  console.log('👴 RethinkDB Migration Complete')
-}
-
 const migratePG = async () => {
   console.log('🐘 Postgres Migration Started')
   await pgEnsureExtensions()
@@ -42,32 +18,40 @@ const migratePG = async () => {
     false,
     /.ts$/
   )
-  const collector = {}
-  context.keys().forEach((relativePath) => {
-    const {name, ext} = path.parse(relativePath)
-    const key = `${cliPgmConfig['migrations-dir']}/${name}${ext}`
-    collector[key] = context(relativePath)
+  const collector: Record<string, any> = {}
+  context.keys().forEach((relativePath: any) => {
+    const {name} = path.parse(relativePath)
+    collector[name] = context(relativePath)
   })
-  const programmaticPgmConfig = {
-    dbClient: cliPgmConfig,
-    dir: path.join(__dirname, '..', cliPgmConfig['migrations-dir']),
-    direction: 'up',
-    migrationsTable: cliPgmConfig['migrations-table'],
-    migrations: collector
-  }
-  await pgMigrate(programmaticPgmConfig as any)
-  console.log('🐘 Postgres Migration Complete')
-}
+  const pg = getKysely()
+  const migrator = new Migrator({
+    ...migratorConfig['migrations'],
+    db: pg,
+    provider: {
+      async getMigrations() {
+        return collector
+      }
+    }
+  })
+  const {error, results} = await migrator.migrateToLatest()
 
-const migrateDBs = async () => {
-  // RethinkDB must be run first because
-  // Some PG migrations depemd on the latest state of RethinkDB
-  await migrateRethinkDB()
-  await migratePG()
-  await r.getPoolMaster()?.drain()
+  results?.forEach((it) => {
+    if (it.status === 'Success') {
+      console.log(`  ✅ Migration: ${it.migrationName}`)
+    } else if (it.status === 'Error') {
+      console.error(`  ⛔️ Migration: ${it.migrationName}`)
+    }
+  })
+
+  if (error) {
+    console.log('🐘 Postgres Migration Failed')
+    console.error(error)
+  } else {
+    console.log('🐘 Postgres Migration Complete')
+  }
 }
 
 // If called via CLI
-if (require.main === module) migrateDBs()
+if (require.main === module) migratePG()
 
-export default migrateDBs
+export default migratePG
