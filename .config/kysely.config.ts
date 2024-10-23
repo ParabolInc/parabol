ad8b13dac20a2fe970bb21f8ed8031199ba1adb8
Matt Krick <matt.krick@gmail.com>
2024-10-22 18:32:04 -0700

    chore: remove kysely-ctl from prod bundle (#10398)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- .config/kysely.config.ts
+++ .config/kysely.config.ts
@@ -1,13 +1,8 @@
 import {defineConfig} from 'kysely-ctl'
 import getKysely from '../packages/server/postgres/getKysely'
+import {migrations} from './kyselyMigrations'
 
 export default defineConfig({
   kysely: getKysely(),
-  migrations: {
-    getMigrationPrefix: () => `${new Date().toISOString()}_`,
-    migrationFolder: './packages/server/postgres/migrations',
-    migrationTableSchema: 'public',
-    migrationTableName: '_migration',
-    migrationLockTableName: '_migrationLock'
-  }
+  migrations
 })
