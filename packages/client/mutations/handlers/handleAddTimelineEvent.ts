8ad165c3d348b45cc2465d094ca07bb6486bd7a0
Bruce Tian <tianrunhe@gmail.com>
2024-10-31 09:48:28 +1300

    fix: fix the issue where newly ended meeting won't show up on timeline event list

--- packages/client/mutations/handlers/handleAddTimelineEvent.ts
+++ packages/client/mutations/handlers/handleAddTimelineEvent.ts
@@ -1,4 +1,6 @@
 import {ConnectionHandler, RecordProxy, RecordSourceSelectorProxy} from 'relay-runtime'
+import {parseQueryParams} from '../../utils/useQueryParameterParser'
+import getUserTimelineEventsConn from '../connections/getUserTimelineEventsConn'
 
 const handleAddTimelineEvent = (
   meeting: RecordProxy,
@@ -8,7 +10,14 @@ const handleAddTimelineEvent = (
   const viewer = store.getRoot().getLinkedRecord('viewer')
   if (!viewer || !meeting || !timelineEvent) return
   timelineEvent.setLinkedRecord(meeting, 'meeting')
-  const timelineConnection = ConnectionHandler.getConnection(viewer, 'TimelineFeedList_timeline')
+  const {teamIds, eventTypes, showArchived} = parseQueryParams(viewer.getDataID(), window.location)
+  const timelineConnection = getUserTimelineEventsConn(
+    viewer,
+    null,
+    teamIds,
+    eventTypes,
+    showArchived
+  )
   if (timelineConnection) {
     const newEdge = ConnectionHandler.createEdge(
       store,
