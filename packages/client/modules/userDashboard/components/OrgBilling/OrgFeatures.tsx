7d2fe73c8a001ab6103bb14afb49e5f07e4ef1f9
Nick O'Ferrall <nickoferrall@gmail.com>
2024-10-31 14:23:22 +0000

    add tooltip to ai features

--- packages/client/modules/userDashboard/components/OrgBilling/OrgFeatures.tsx
+++ packages/client/modules/userDashboard/components/OrgBilling/OrgFeatures.tsx
@@ -1,9 +1,13 @@
 import styled from '@emotion/styled'
+import {Info as InfoIcon} from '@mui/icons-material'
 import React, {useState} from 'react'
 import Panel from '../../../../components/Panel/Panel'
 import Toggle from '../../../../components/Toggle/Toggle'
 import {PALETTE} from '../../../../styles/paletteV3'
 import {ElementWidth, Layout} from '../../../../types/constEnums'
+import {Tooltip} from '../../../../ui/Tooltip/Tooltip'
+import {TooltipContent} from '../../../../ui/Tooltip/TooltipContent'
+import {TooltipTrigger} from '../../../../ui/Tooltip/TooltipTrigger'
 
 const StyledPanel = styled(Panel)<{isWide: boolean}>(({isWide}) => ({
   maxWidth: isWide ? ElementWidth.PANEL_WIDTH : 'inherit'
@@ -21,6 +25,12 @@ const FeatureRow = styled('div')({
   marginBottom: 8
 })
 
+const FeatureNameGroup = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  gap: 4
+})
+
 const OrgFeatures = () => {
   const [showAIFeatures, setShowAIFeatures] = useState(false)
 
@@ -28,7 +38,15 @@ const OrgFeatures = () => {
     <StyledPanel isWide label='AI Features'>
       <PanelRow>
         <FeatureRow>
-          <span>Show AI Features</span>
+          <FeatureNameGroup>
+            <span>Show AI Features</span>
+            <Tooltip>
+              <TooltipTrigger className='bg-transparent hover:cursor-pointer'>
+                <InfoIcon className='h-4 w-4 text-slate-600' />
+              </TooltipTrigger>
+              <TooltipContent>Enable AI-powered features across your organization</TooltipContent>
+            </Tooltip>
+          </FeatureNameGroup>
           <Toggle active={showAIFeatures} onClick={() => setShowAIFeatures(!showAIFeatures)} />
         </FeatureRow>
       </PanelRow>
