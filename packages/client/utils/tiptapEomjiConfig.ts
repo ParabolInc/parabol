ef9f6f59369554b1d1386de31eed5e99b4af60f4
Matt Krick <matt.krick@gmail.com>
2024-11-08 17:24:27 -0800

    start emoji

--- packages/client/utils/tiptapEomjiConfig.ts
+++ packages/client/utils/tiptapEomjiConfig.ts
@@ -1,59 +1,13 @@
 import {MentionNodeAttrs, MentionOptions} from '@tiptap/extension-mention'
 import {ReactRenderer} from '@tiptap/react'
-import graphql from 'babel-plugin-relay/macro'
-import stringScore from 'string-score'
 import tippy, {Instance, Props} from 'tippy.js'
-import {tiptapMentionConfigQuery} from '../__generated__/tiptapMentionConfigQuery.graphql'
-import Atmosphere from '../Atmosphere'
 import MentionDropdown from '../components/MentionDropdown'
 
-const queryNode = graphql`
-  query tiptapMentionConfigQuery($teamId: ID!) {
-    viewer {
-      team(teamId: $teamId) {
-        teamMembers {
-          id
-          picture
-          preferredName
-        }
-      }
-    }
-  }
-`
-
-export const tiptapMentionConfig = (
-  atmosphere: Atmosphere,
-  teamId: string
-): Partial<MentionOptions<any, MentionNodeAttrs>> => ({
-  // renderText does not fire, bug in TipTap? Fallback to using more verbose renderHTML
-  renderHTML({options, node}) {
-    return ['span', options.HTMLAttributes, `${node.attrs.label ?? node.attrs.id}`]
-  },
+export const tiptapEmojiConfig: Partial<MentionOptions<any, MentionNodeAttrs>> = {
   suggestion: {
-    // some users have first & last name
-    allowSpaces: true,
-    decorationClass: 'mention',
+    char: ':',
     items: async ({query}) => {
-      const res = await atmosphere.fetchQuery<tiptapMentionConfigQuery>(queryNode, {teamId})
-      if (!res) return []
-      const {viewer} = res
-      const {team} = viewer
-      const {teamMembers} = team!
-      return (
-        teamMembers
-          .map((teamMember) => {
-            const score = query ? stringScore(teamMember.preferredName, query) : 1
-            return {
-              teamMember,
-              score
-            }
-          })
-          .sort((a, b) => (a.score < b.score ? 1 : -1))
-          .slice(0, 6)
-          // If you type "Foo" and the options are "Foo" and "Giraffe", remove "Giraffe"
-          .filter((obj, _idx, arr) => obj.score > 0 && arr[0]!.score - obj.score < 0.3)
-          .map((s) => ({...s.teamMember, query}))
-      )
+      return []
     },
 
     // Using radix-ui isn't possible here because radix-ui will steal focus from the editor when it opens the portal
@@ -106,4 +60,4 @@ export const tiptapMentionConfig = (
       }
     }
   }
-})
+}
