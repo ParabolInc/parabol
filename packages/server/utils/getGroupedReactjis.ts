32bd62404ec119ca26e47f3c74ce03434d0ffead
Matt Krick <matt.krick@gmail.com>
2024-11-12 08:14:24 -0800

    fix: add jsx runtime to prod
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/utils/getGroupedReactjis.ts
+++ packages/server/utils/getGroupedReactjis.ts
@@ -17,7 +17,6 @@ const getGroupedReactjis = (reactjis: ReactjiDB[], viewerId: string, idPrefix: s
       record.userIds = [userId, ...record.userIds]
     }
   })
-  console.log(agg)
   return Object.values(agg)
 }
 
