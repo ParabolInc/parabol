b94b552952bd98c1e01a5b0ac97580c548a47841
Georg Bremer <github@dschoordsch.de>
2024-11-22 20:25:24 +0100

    chore: Add module federation for mattermost plugin

---
+++ packages/mattermost-plugin/button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>Click me</button>
+}
+
+export default Button
---
+++ packages/mattermost-plugin/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
