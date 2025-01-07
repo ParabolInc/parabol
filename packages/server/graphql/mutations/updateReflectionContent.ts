585ee71e1244b23c6623721a7f5ed9caa45425d7
Nick O'Ferrall <nickoferrall@gmail.com>
2025-01-07 15:33:38 -0600

    remove entities from update reflection content

--- packages/server/graphql/mutations/updateReflectionContent.ts
+++ packages/server/graphql/mutations/updateReflectionContent.ts
@@ -2,18 +2,14 @@ import {generateText} from '@tiptap/core'
 import {GraphQLID, GraphQLNonNull, GraphQLString} from 'graphql'
 import {SubscriptionChannel} from 'parabol-client/types/constEnums'
 import isPhaseComplete from 'parabol-client/utils/meetings/isPhaseComplete'
-import stringSimilarity from 'string-similarity'
 import {serverTipTapExtensions} from '../../../client/shared/tiptap/serverTipTapExtensions'
 import getKysely from '../../postgres/getKysely'
-import {toGoogleAnalyzedEntity} from '../../postgres/helpers/toGoogleAnalyzedEntity'
 import {getUserId, isTeamMember} from '../../utils/authorization'
 import {convertToTipTap} from '../../utils/convertToTipTap'
 import publish from '../../utils/publish'
 import standardError from '../../utils/standardError'
 import {GQLContext} from '../graphql'
 import UpdateReflectionContentPayload from '../types/UpdateReflectionContentPayload'
-import {getFeatureTier} from '../types/helpers/getFeatureTier'
-import getReflectionEntities from './helpers/getReflectionEntities'
 import updateGroupTitle from './helpers/updateGroupTitle'
 
 export default {
@@ -49,13 +45,11 @@ export default {
     if (!reflectPrompt) {
       return standardError(new Error('Category not found'), {userId: viewerId})
     }
-    const {question} = reflectPrompt
     const meeting = await dataLoader.get('newMeetings').loadNonNull(meetingId)
     const {endedAt, phases, teamId} = meeting
     if (!isTeamMember(authToken, teamId)) {
       return standardError(new Error('Team not found'), {userId: viewerId})
     }
-    const team = await dataLoader.get('teams').loadNonNull(teamId)
     if (endedAt) return standardError(new Error('Meeting already ended'), {userId: viewerId})
     if (isPhaseComplete('group', phases)) {
       return standardError(new Error('Meeting phase already ended'), {userId: viewerId})
@@ -72,23 +66,11 @@ export default {
 
     // RESOLUTION
     const plaintextContent = generateText(normalizedContent, serverTipTapExtensions)
-    const isVeryDifferent =
-      stringSimilarity.compareTwoStrings(plaintextContent, reflection.plaintextContent) < 0.9
-    const entities = isVeryDifferent
-      ? await getReflectionEntities(plaintextContent)
-      : reflection.entities
-    const sentimentScore =
-      getFeatureTier(team) !== 'starter'
-        ? isVeryDifferent
-          ? await getReflectionSentimentScore(question, plaintextContent)
-          : reflection.sentimentScore
-        : undefined
+
     await pg
       .updateTable('RetroReflection')
       .set({
         content: JSON.stringify(normalizedContent),
-        entities: toGoogleAnalyzedEntity(entities),
-        sentimentScore,
         plaintextContent
       })
       .where('id', '=', reflectionId)
