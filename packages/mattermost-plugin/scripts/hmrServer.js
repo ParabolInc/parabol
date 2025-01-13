5270296cb237e408b158067920e2c495222ba7ee
Georg Bremer <github@dschoordsch.de>
2025-01-13 16:42:26 +0100

    fix: kill mattermost plugin dev server on pm2 kill
    Start the hot module reload (hmr) dev server for the mattermost plugin
    manually so that pm2 properly cleans it up.

---
+++ packages/mattermost-plugin/scripts/hmrServer.js
@@ -0,0 +1,20 @@
+require('sucrase/register')
+require('../../../scripts/webpack/utils/dotenv')
+const path = require('path')
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server/lib/Server')
+const waitForFileExists = require('../../../scripts/waitForFileExists').default
+
+const hmrServer = async () => {
+  const mattermostPluginConfig = require('../webpack.config')
+  const mattermostPluginCompiler = webpack(mattermostPluginConfig)
+  const mattermostServer = new WebpackDevServer({...mattermostPluginConfig.devServer}, mattermostPluginCompiler)
+
+  const queryMapExists = await waitForFileExists(path.join(__dirname, '../../../queryMap.json'), 20000)
+  if (!queryMapExists) throw Error('QueryMap Not Available. Run `yarn relay:build`')
+
+  await mattermostServer.start(3002, 'localhost')
+}
+
+hmrServer()
+
