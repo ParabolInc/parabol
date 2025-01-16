572e389d09e55c1019ac348352758b361653122e
Matt Krick <matt.krick@gmail.com>
2025-01-15 19:34:18 -0800

    fix: slashcommand menu styles
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/tiptap/extensions/slashCommand/SlashCommandMenu.tsx
+++ packages/client/tiptap/extensions/slashCommand/SlashCommandMenu.tsx
@@ -63,19 +63,19 @@ export const SlashCommandMenu = forwardRef(
       <div className='border-rad z-10 max-h-56 overflow-auto rounded-md bg-white py-1 shadow-lg outline-none [[data-placement="bottom-start"]_&]:animate-slideDown [[data-placement="top-start"]_&]:animate-slideUp'>
         {items.map((item, idx) => (
           <Fragment key={item.group}>
-            <div className='mx-1 px-3 py-2 text-xs font-semibold'>{item.group}</div>
+            <div className='mx-1 px-3 py-1 text-xs font-semibold'>{item.group}</div>
             {item.commands.map((command) => (
               <div className='mx-1 flex' key={command.title}>
                 <div
                   ref={command === activeItem ? activeRef : undefined}
                   data-highlighted={command === activeItem}
                   className={
-                    ' flex w-full cursor-pointer items-center rounded-md px-3 py-2 text-sm leading-8 text-slate-700 outline-none hover:!bg-slate-200 hover:text-slate-900 focus:bg-slate-200 data-highlighted:bg-slate-100 data-highlighted:text-slate-900'
+                    ' group flex w-full cursor-pointer items-center space-x-2 rounded-md px-3 py-2 text-sm leading-8 text-slate-700 outline-none hover:!bg-slate-200 hover:text-slate-900 focus:bg-slate-200 data-highlighted:bg-slate-100 data-highlighted:text-slate-900'
                   }
                   onClick={() => selectItem(idx)}
                 >
-                  <div className='flex items-center justify-center'>
-                    <command.icon className='size-11' />
+                  <div className='flex size-7 items-center justify-center rounded bg-slate-200 group-hover:bg-slate-300 group-data-highlighted:bg-slate-300'>
+                    <command.icon className='size-5' />
                   </div>
                   <div className='flex flex-col text-sm'>
                     <span>{command.title}</span>
