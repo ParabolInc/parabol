88165c90d536e30bf756e609d5edbe09b111ed74
Nick O'Ferrall <nickoferrall@gmail.com>
2025-01-22 15:38:22 +0000

    use hasContent in summary

--- packages/client/modules/email/components/SummaryEmail/MeetingSummaryEmail/WholeMeetingSummary.tsx
+++ packages/client/modules/email/components/SummaryEmail/MeetingSummaryEmail/WholeMeetingSummary.tsx
@@ -1,5 +1,8 @@
 import graphql from 'babel-plugin-relay/macro'
-import {WholeMeetingSummary_meeting$key} from 'parabol-client/__generated__/WholeMeetingSummary_meeting.graphql'
+import {
+  WholeMeetingSummary_meeting$data,
+  WholeMeetingSummary_meeting$key
+} from 'parabol-client/__generated__/WholeMeetingSummary_meeting.graphql'
 import {useFragment} from 'react-relay'
 import WholeMeetingSummaryLoading from './WholeMeetingSummaryLoading'
 import WholeMeetingSummaryResult from './WholeMeetingSummaryResult'
@@ -12,6 +15,17 @@ const isServer = typeof window === 'undefined'
 const hasAiApiKey = isServer
   ? !!process.env.OPEN_AI_API_KEY
   : !!window.__ACTION__ && !!window.__ACTION__.hasOpenAI
+
+const hasContent = (meeting: WholeMeetingSummary_meeting$data): boolean => {
+  if (meeting.__typename === 'RetrospectiveMeeting') {
+    const reflections = meeting.reflectionGroups?.flatMap((group) => group.reflections)
+    return Boolean(reflections?.length && reflections.length > 1)
+  }
+  if (meeting.__typename === 'TeamPromptMeeting') {
+    return Boolean(meeting.responses?.length)
+  }
+  return false
+}
 
 const WholeMeetingSummary = (props: Props) => {
   const {meetingRef} = props
@@ -47,19 +61,8 @@ const WholeMeetingSummary = (props: Props) => {
   const {organization} = meeting
   const {useAI} = organization
 
-  if (!useAI || !hasAiApiKey) return null
+  if (!useAI || !hasAiApiKey || !hasContent(meeting)) return null
 
-  const hasContent = (() => {
-    if (meeting.__typename === 'RetrospectiveMeeting') {
-      const reflections = meeting.reflectionGroups?.flatMap((group) => group.reflections)
-      return reflections?.length && reflections.length > 1
-    } else if (meeting.__typename === 'TeamPromptMeeting') {
-      return meeting.responses && meeting.responses.length > 0
-    }
-    return false
-  })()
-
-  if (!hasContent) return null
   if (meeting.isLoadingSummary) return <WholeMeetingSummaryLoading />
 
   return <WholeMeetingSummaryResult meetingRef={meeting} />
