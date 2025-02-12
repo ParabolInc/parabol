1b0718ea54a1b69ccfc0cdb699769838455529f9
Nick O'Ferrall <nickoferrall@gmail.com>
2025-02-12 16:33:15 +0000

    implement jaccard distance

--- packages/client/utils/smartGroup/groupReflections.ts
+++ packages/client/utils/smartGroup/groupReflections.ts
@@ -1,11 +1,5 @@
-import computeDistanceMatrix from './computeDistanceMatrix'
-import getAllLemmasFromReflections from './getAllLemmasFromReflections'
+import {computeJaccardDistanceMatrix} from './computeJaccardDistanceMatrix'
 import getGroupMatrix from './getGroupMatrix'
-import getTitleFromComputedGroup from './getTitleFromComputedGroup'
-
-/*
- * Read each reflection, parse the content for entities (i.e. nouns), group the reflections based on common themes
- */
 
 type Entity = {
   lemma: string
@@ -27,63 +21,53 @@ export type GroupingOptions = {
   maxReductionPercent?: number
 }
 
-const groupReflections = <
-  T extends {entities: any[]; reflectionGroupId: string; id: string; plaintextContent: string}
->(
-  reflections: T[],
-  groupingOptions: GroupingOptions
-) => {
-  const allReflectionEntities = reflections.map(({entities}) => entities)
-  const oldReflectionGroupIds = reflections.map(({reflectionGroupId}) => reflectionGroupId)
+function groupReflections<
+  T extends {id: string; reflectionGroupId: string; plaintextContent: string}
+>(reflections: T[], groupingOptions: GroupingOptions) {
+  const reflectionTexts = reflections.map((r) => r.plaintextContent || '')
 
-  // create a unique array of all entity names mentioned in the meeting's reflect phase
-  const uniqueLemmaArr = getAllLemmasFromReflections(allReflectionEntities)
-  // create a distance vector for each reflection
-  const distanceMatrix = computeDistanceMatrix(allReflectionEntities, uniqueLemmaArr)
+  const distanceMatrix = computeJaccardDistanceMatrix(reflectionTexts)
+
   const {
     groups: groupedArrays,
     thresh,
     nextThresh
   } = getGroupMatrix(distanceMatrix, groupingOptions)
-  // replace the arrays with reflections
-  const updatedReflections = [] as GroupedReflectionRes[]
-  const reflectionGroupMapping = {} as Record<string, string>
+
+  const updatedReflections: Array<{
+    reflectionId: string
+    oldReflectionGroupId: string
+    sortOrder: number
+    reflectionGroupId: string
+  }> = []
+
+  const reflectionGroupMapping: Record<string, string> = {}
+  const oldReflectionGroupIds = reflections.map((r) => r.reflectionGroupId)
+
   const updatedGroups = groupedArrays.map((group) => {
-    // look up the reflection by its vector, put them all in the same group
     let reflectionGroupId = ''
+
     const groupedReflectionsRes = group.map((reflectionDistanceArr, sortOrder) => {
       const idx = distanceMatrix.indexOf(reflectionDistanceArr)
       const reflection = reflections[idx]!
       reflectionGroupId = reflectionGroupId || reflection.reflectionGroupId
       return {
         reflectionId: reflection.id,
-        entities: reflection.entities,
         oldReflectionGroupId: reflection.reflectionGroupId,
         sortOrder,
         reflectionGroupId
-      } as GroupedReflectionRes
+      }
     })
 
-    const groupedReflectionEntities = groupedReflectionsRes
-      .map(({entities}) => entities)
-      .filter(Boolean)
-    const smartTitle = getTitleFromComputedGroup(
-      uniqueLemmaArr,
-      group,
-      groupedReflectionEntities,
-      reflections
-    )
-
     updatedReflections.push(...groupedReflectionsRes)
-
-    groupedReflectionsRes.forEach((groupedReflection) => {
-      reflectionGroupMapping[groupedReflection.oldReflectionGroupId] = reflectionGroupId
+    groupedReflectionsRes.forEach(({oldReflectionGroupId}) => {
+      reflectionGroupMapping[oldReflectionGroupId] = reflectionGroupId
     })
 
     return {
       id: reflectionGroupId,
-      smartTitle,
-      title: smartTitle
+      smartTitle: '',
+      title: ''
     }
   })
 
@@ -91,15 +75,16 @@ const groupReflections = <
     updatedReflections.map(({reflectionGroupId}) => reflectionGroupId)
   )
   const removedReflectionGroupIds = oldReflectionGroupIds.filter(
-    (groupId) => !newReflectionGroupIds.has(groupId)
+    (oldId) => !newReflectionGroupIds.has(oldId)
   )
+
   return {
     autoGroupThreshold: thresh,
     groups: updatedGroups,
     groupedReflectionsRes: updatedReflections,
     reflectionGroupMapping,
     removedReflectionGroupIds,
-    nextThresh: nextThresh as number
+    nextThresh
   }
 }
 
