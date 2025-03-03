8159180eb4a4283ffa8fc3e0f5fdaa5f22a1a5e7
Georg Bremer <github@dschoordsch.de>
2025-03-03 10:24:13 +0100

    chore: Add loginMattermost private mutation

--- packages/server/integrations/mattermost/mattermostWebhookHandler.ts
+++ packages/server/integrations/mattermost/mattermostWebhookHandler.ts
@@ -1,23 +1,36 @@
 import {createVerifier, httpbis} from 'http-message-signatures'
 import {HttpRequest, HttpResponse} from 'uWebSockets.js'
 import appOrigin from '../../appOrigin'
-import AuthToken from '../../database/types/AuthToken'
 import uWSAsyncHandler from '../../graphql/uWSAsyncHandler'
 import parseBody from '../../parseBody'
-import getKysely from '../../postgres/getKysely'
+import publishWebhookGQL from '../../utils/publishWebhookGQL'
+import ConnectionContext from '../../socketHelpers/ConnectionContext'
+import uwsGetIP from '../../utils/uwsGetIP'
+import checkBlacklistJWT from '../../utils/checkBlacklistJWT'
+import activeClients from '../../activeClients'
+import handleConnect from '../../socketHandlers/handleConnect'
+import getVerifiedAuthToken from '../../utils/getVerifiedAuthToken'
 import encodeAuthToken from '../../utils/encodeAuthToken'
+import {MMSocket} from '../../socketHelpers/transports/MMSocket'
 
 const MATTERMOST_SECRET = process.env.MATTERMOST_SECRET
 
+
 const login = async (email: string) => {
-  const pg = getKysely()
-  const user = await pg
-    .selectFrom('User')
-    .selectAll()
-    .where('email', '=', email)
-    .executeTakeFirstOrThrow()
-  const authToken = new AuthToken({sub: user.id, tms: user.tms})
-  return encodeAuthToken(authToken)
+  const query = `
+    mutation LoginMattermost($email: String!) {
+      loginMattermost(email: $email) {
+        error {
+          message
+        }
+        authToken
+      }
+    }
+  `
+
+  const loginResult = await publishWebhookGQL<any>(query, {email})
+  const {authToken} = loginResult?.data?.loginMattermost ?? {}
+  return authToken as string
 }
 
 const mattermostWebhookHandler = uWSAsyncHandler(async (res: HttpResponse, req: HttpRequest) => {
@@ -25,6 +38,10 @@ const mattermostWebhookHandler = uWSAsyncHandler(async (res: HttpResponse, req: 
     res.writeStatus('404').end()
     return
   }
+
+  const ip = uwsGetIP(res, req)
+  const connectionId = req.getHeader('x-correlation-id')
+
   const headers = {
     'content-type': req.getHeader('content-type'),
     'content-digest': req.getHeader('content-digest'),
@@ -66,11 +83,26 @@ const mattermostWebhookHandler = uWSAsyncHandler(async (res: HttpResponse, req: 
     return
   }
 
-  const authToken = await login(email)
+  const authToken = getVerifiedAuthToken(await login(email))
+
+  const connectionContext = new ConnectionContext(new MMSocket(connectionId), authToken, ip)
+  // TODO: Find a nicer solution to this. This is the easiest to multiplex the webhook in the MM-Plugin.
+  // If we don't use the client provided connectionId, the plugin would need to keep state to correlate userIDs with connectionIds.
+  // This might be superseeded once we establish the Parabol userId <-> Mattermost userId mapping on Parabol side.
+  connectionContext.id = connectionId
+  const {sub: userId, iat} = authToken
+  const isBlacklistedJWT = await checkBlacklistJWT(userId, iat)
+  if (isBlacklistedJWT) {
+    return
+  }
+
+  activeClients.set(connectionContext)
+  const nextAuthToken = await handleConnect(connectionContext)
+ 
   res
     .writeStatus('200')
     .writeHeader('Content-Type', 'application/json')
-    .end(JSON.stringify({authToken}))
+    .end(JSON.stringify({authToken: nextAuthToken ?? encodeAuthToken(authToken)}))
 })
 
 export default mattermostWebhookHandler
