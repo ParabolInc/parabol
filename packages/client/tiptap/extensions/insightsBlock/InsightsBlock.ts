8f8ca00198d5dacadea86c7dda85864564e4cfe4
Matt Krick <matt.krick@gmail.com>
2025-03-04 18:19:42 -0800

    feat: exitNode with ArrowDown
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/tiptap/extensions/insightsBlock/InsightsBlock.ts
+++ packages/client/tiptap/extensions/insightsBlock/InsightsBlock.ts
@@ -1,3 +1,4 @@
+import {TextSelection} from '@tiptap/pm/state'
 import {ReactNodeViewRenderer} from '@tiptap/react'
 import ms from 'ms'
 import type {MeetingTypeEnum} from '../../../__generated__/ExportToCSVQuery.graphql'
@@ -15,6 +16,14 @@ export interface InsightsBlockAttrs {
   id: string
 }
 
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    insightsBlock: {
+      setInsights: () => ReturnType
+      exitNode: () => ReturnType
+    }
+  }
+}
 export const InsightsBlock = InsightsBlockBase.extend({
   addAttributes() {
     return {
@@ -81,7 +90,33 @@ export const InsightsBlock = InsightsBlockBase.extend({
             content.push({type: 'paragraph'})
           }
           return commands.insertContent(content)
+        },
+      exitNode:
+        () =>
+        ({state, dispatch}) => {
+          const {selection, doc} = state
+          const {$head} = selection
+          // depth === 0 when cursor is at gapcursor at bottom of node
+          const nextNodePos = $head.depth === 0 ? null : $head.after()
+          const nextNode = nextNodePos ? doc.nodeAt(nextNodePos) : $head.nodeBefore
+
+          // Check if there is a next node and it is an `insightsBlock`
+          if (!(nextNode?.type.name === 'insightsBlock' && doc.lastChild === nextNode && dispatch))
+            return false
+          const insertPos = doc.nodeSize - 2
+          const tr = state.tr.insert(insertPos, state.schema.nodes.paragraph!.create())
+          tr.setSelection(TextSelection.near(tr.doc.resolve(insertPos + 1))) // Move cursor into the new paragraph
+          dispatch(tr)
+          return true
         }
+    }
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      ArrowDown: () => {
+        return this.editor.commands.exitNode()
+      }
     }
   },
 
