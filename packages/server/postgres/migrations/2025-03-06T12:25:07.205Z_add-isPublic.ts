c7249f5411481c3b8ca41f23ca9fe26100063ebd
Nick O'Ferrall <nickoferrall@gmail.com>
2025-03-10 17:30:12 +0000

    remove orgid map from isPublic migration

--- packages/server/postgres/migrations/2025-03-06T12:25:07.205Z_add-isPublic.ts
+++ packages/server/postgres/migrations/2025-03-06T12:25:07.205Z_add-isPublic.ts
@@ -6,17 +6,13 @@ export async function up(db: Kysely<any>): Promise<void> {
     .addColumn('isPublic', 'boolean', (col) => col.defaultTo(false).notNull())
     .execute()
 
-  const featureFlags = await db
+  const featureFlags = db
     .selectFrom('FeatureFlag')
     .innerJoin('FeatureFlagOwner', 'FeatureFlag.id', 'FeatureFlagOwner.featureFlagId')
     .where('FeatureFlag.featureName', '=', 'publicTeams')
     .select('FeatureFlagOwner.orgId')
-    .execute()
 
-  const orgIds = featureFlags.map((flag) => flag.orgId)
-  if (orgIds.length > 0) {
-    await db.updateTable('Team').set({isPublic: true}).where('orgId', 'in', orgIds).execute()
-  }
+  await db.updateTable('Team').set({isPublic: true}).where('orgId', 'in', featureFlags).execute()
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
