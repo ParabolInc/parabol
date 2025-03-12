1b04e56668048ab02829fbee6509e3bbb2ce2f44
Nick O'Ferrall <nickoferrall@gmail.com>
2025-03-12 17:49:30 +0000

    chore: remove google language manager (#10639)

--- packages/server/postgres/helpers/toGoogleAnalyzedEntity.ts
+++
@@ -1,6 +0,0 @@
-import {sql} from 'kysely'
-import GoogleAnalyzedEntity from '../../database/types/GoogleAnalyzedEntity'
-export const toGoogleAnalyzedEntity = (entities: GoogleAnalyzedEntity[]) =>
-  sql<
-    string[]
-  >`(select coalesce(array_agg((name, salience, lemma)::"GoogleAnalyzedEntity"), '{}') from json_populate_recordset(null::"GoogleAnalyzedEntity", ${JSON.stringify(entities)}))`
