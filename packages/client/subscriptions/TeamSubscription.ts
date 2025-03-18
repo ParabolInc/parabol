7a98cf3e02a1bd4e6ca21d30d45deccbfb55ecc9
Nick O'Ferrall <nickoferrall@gmail.com>
2025-03-18 12:26:01 +0000

    feat: can join public teams without invite (#10952)

--- packages/client/subscriptions/TeamSubscription.ts
+++ packages/client/subscriptions/TeamSubscription.ts
@@ -29,6 +29,7 @@ import {
   endSprintPokerTeamUpdater
 } from '../mutations/EndSprintPokerMutation'
 import {endTeamPromptTeamUpdater} from '../mutations/EndTeamPromptMutation'
+import {joinTeamTeamOnNext} from '../mutations/JoinTeamMutation'
 import {moveReflectTemplatePromptTeamUpdater} from '../mutations/MoveReflectTemplatePromptMutation'
 import {pushInvitationTeamOnNext} from '../mutations/PushInvitationMutation'
 import {removeAgendaItemUpdater} from '../mutations/RemoveAgendaItemMutation'
@@ -106,6 +107,9 @@ const subscription = graphql`
       }
       EndTeamPromptSuccess {
         ...EndTeamPromptMutation_team @relay(mask: false)
+      }
+      JoinTeamSuccess {
+        ...JoinTeamMutation_team @relay(mask: false)
       }
       MoveReflectTemplatePromptPayload {
         ...MoveReflectTemplatePromptMutation_team @relay(mask: false)
@@ -196,6 +200,7 @@ const onNextHandlers = {
   EndCheckInSuccess: endCheckInTeamOnNext,
   EndRetrospectiveSuccess: endRetrospectiveTeamOnNext,
   EndSprintPokerSuccess: endSprintPokerTeamOnNext,
+  JoinTeamSuccess: joinTeamTeamOnNext,
   RemoveOrgUsersSuccess: removeOrgUsersTeamOnNext,
   RemoveTeamMemberPayload: removeTeamMemberTeamOnNext,
   PushInvitationPayload: pushInvitationTeamOnNext
