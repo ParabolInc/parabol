4b21ba079f1c17030b7f23b218103d5223fcd76b
Georg Bremer <github@dschoordsch.de>
2025-03-18 15:54:59 +0100

    Fix again again again

--- packages/server/postgres/selectUsers.ts
+++ packages/server/postgres/selectUsers.ts
@@ -1,15 +1,11 @@
-import {jsonArrayFrom} from 'kysely/helpers/postgres'
 import getKysely from './getKysely'
 import {JsonObject} from './types/pg'
 
 export const selectUsers = () =>
-  getKysely()
-    .selectFrom('User')
+  getKysely().selectFrom('User')
     .selectAll()
     .select((eb) => [
-      jsonArrayFrom(
-        eb.selectFrom('TeamMember').select('teamId').where('userId', '=', 'User.id').distinct()
-      ).as('tms')
+        eb.selectFrom('TeamMember').select(({fn}) => fn.agg('array_agg', ['teamId']).as('tm')).whereRef('userId', '=', 'User.id').distinct().as('tms')
     ])
     .$narrowType<{
       identities: JsonObject[]
