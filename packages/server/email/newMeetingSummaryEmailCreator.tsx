898ee9d82e22023f87d09cee818ed92613418d85
Georg Bremer <github@dschoordsch.de>
2025-03-18 16:41:04 +0100

    Cleanup again

--- packages/server/email/newMeetingSummaryEmailCreator.tsx
+++ packages/server/email/newMeetingSummaryEmailCreator.tsx
@@ -18,10 +18,8 @@ const newMeetingSummaryEmailCreator = async (props: Props) => {
   const dataLoaderId = dataLoader.share()
 
   const newMeeting = await dataLoader.get('newMeetings').loadNonNull(meetingId)
-  const teamMembers = await dataLoader
-    .get('teamMembersByUserId')
-    .load(newMeeting.facilitatorUserId!)
-  const tms = teamMembers.map(({teamId}) => teamId)
+  const facilitator = await dataLoader.get('users').loadNonNull(newMeeting.facilitatorUserId!)
+  const {tms} = facilitator
   const authToken = new AuthToken({sub: newMeeting.facilitatorUserId!, tms, rol: 'impersonate'})
   const environment = new ServerEnvironment(authToken, dataLoaderId)
 
