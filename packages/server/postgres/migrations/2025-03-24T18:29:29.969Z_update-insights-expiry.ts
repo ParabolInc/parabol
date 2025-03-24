cdcd191fd95c6789ba01134a3f9378efc75af0c1
Nick O'Ferrall <nickoferrall@gmail.com>
2025-03-24 18:31:24 +0000

    update down migration expiration

--- packages/server/postgres/migrations/2025-03-24T18:29:29.969Z_update-insights-expiry.ts
+++ packages/server/postgres/migrations/2025-03-24T18:29:29.969Z_update-insights-expiry.ts
@@ -14,7 +14,7 @@ export async function down(db: Kysely<any>): Promise<void> {
   await db
     .updateTable('FeatureFlag')
     .set({
-      expiresAt: '2025-01-31T00:00:00.000Z'
+      expiresAt: '2025-03-31T00:00:00.000Z'
     })
     .where('featureName', '=', 'insights')
     .execute()
