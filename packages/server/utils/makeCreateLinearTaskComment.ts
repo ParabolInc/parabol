59675f9e36a8f0cea6c8a1c559089ed5e082de04
Jordan Husney <jordan.husney@gmail.com>
2025-04-03 08:39:15 -0500

    add LinearServerManager

---
+++ packages/server/utils/makeCreateLinearTaskComment.ts
@@ -0,0 +1,17 @@
+import {ExternalLinks} from '../../client/types/constEnums'
+
+const makeCreateLinearTaskComment = (
+  creator: string,
+  assignee: string,
+  teamName: string,
+  teamDashboardUrl: string
+): string => {
+  // Linear markdown might handle mentions differently, but basic sanitization is safe.
+  // Using the same sanitization as GitLab for now.
+  const sanitizedCreator = creator.replace(/#(\d+)/g, '#\u200b$1')
+  const sanitizedAssignee = assignee.replace(/#(\d+)/g, '#\u200b$1')
+
+  return `Created by ${sanitizedCreator} for ${sanitizedAssignee}\n\nSee the dashboard of [${teamName}](${teamDashboardUrl})\n\n*Powered by [Parabol](${ExternalLinks.INTEGRATIONS_LINEAR})*`
+}
+
+export default makeCreateLinearTaskComment
