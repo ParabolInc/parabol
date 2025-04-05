c9aac4e4b7f81004bb3bb7705f69a90777cbfa96
Jordan Husney <jordan.husney@gmail.com>
2025-04-05 15:50:20 -0500

    Push to Linear team and Project

---
+++ packages/server/graphql/queries/helpers/fetchLinearTeamsAndProjects.ts
@@ -0,0 +1,85 @@
+import {GraphQLResolveInfo} from 'graphql'
+import {isNotNull} from 'parabol-client/utils/predicates'
+import LinearServerManager from '../../../integrations/linear/LinearServerManager'
+import {Logger} from '../../../utils/Logger'
+import {GQLContext} from '../../graphql'
+
+export const fetchLinearProjects = async (
+  teamId: string,
+  userId: string,
+  context: GQLContext,
+  info: GraphQLResolveInfo
+) => {
+  try {
+    const {dataLoader} = context
+    const auth = await dataLoader
+      .get('teamMemberIntegrationAuthsByServiceTeamAndUserId')
+      .load({service: 'linear', teamId, userId})
+
+    if (!auth?.accessToken) {
+      return []
+    }
+
+    const manager = new LinearServerManager(auth, context, info)
+
+    const [data, error] = await manager.getProjects({})
+
+    if (error) {
+      Logger.error(
+        `Error fetching Linear projects for user ${userId} in team ${teamId}: ${error.message}`
+      )
+      return []
+    }
+
+    return (
+      data.projects?.edges
+        ?.map((edge: any) => edge?.node && {...edge.node, service: 'linear' as const})
+        .filter(isNotNull) ?? []
+    )
+  } catch (error: any) {
+    Logger.error(
+      `Unexpected error in fetchLinearProjects for user ${userId} in team ${teamId}: ${error.message}`
+    )
+    return []
+  }
+}
+
+export const fetchLinearTeams = async (
+  teamId: string,
+  userId: string,
+  context: GQLContext,
+  info: GraphQLResolveInfo
+) => {
+  try {
+    const {dataLoader} = context
+    const auth = await dataLoader
+      .get('teamMemberIntegrationAuthsByServiceTeamAndUserId')
+      .load({service: 'linear', teamId, userId})
+
+    if (!auth?.accessToken) {
+      return []
+    }
+
+    const manager = new LinearServerManager(auth, context, info)
+
+    const [data, error] = await manager.getTeams({})
+
+    if (error) {
+      Logger.error(
+        `Error fetching Linear teams for user ${userId} in team ${teamId}: ${error.message}`
+      )
+      return []
+    }
+
+    return (
+      data.teams?.edges
+        ?.map((edge: any) => edge?.node && {...edge.node, service: 'linear' as const})
+        .filter(isNotNull) ?? []
+    )
+  } catch (error: any) {
+    Logger.error(
+      `Unexpected error in fetchLinearTeamsAndProjects for user ${userId} in team ${teamId}: ${error.message}`
+    )
+    return []
+  }
+}
