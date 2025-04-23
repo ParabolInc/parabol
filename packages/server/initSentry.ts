4fd9bead901db418a604881093c8b8ed9fa68a3d
Matt Krick <matt.krick@gmail.com>
2025-04-23 10:30:34 -0700

    fix: bump sentry from v7 to v9
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/initSentry.ts
+++ packages/server/initSentry.ts
@@ -1,26 +1,13 @@
-import * as Integrations from '@sentry/integrations'
 import * as Sentry from '@sentry/node'
-
-declare global {
-  namespace NodeJS {
-    interface Global {
-      __rootdir__: string
-    }
-  }
-}
 
 Sentry.init({
   environment: 'server',
   dsn: process.env.SENTRY_DSN,
   release: __APP_VERSION__,
+  defaultIntegrations: false,
   ignoreErrors: [
     '429 Too Many Requests',
     /language \S+ is not supported/,
     'Not invited to the meeting. Cannot subscribe'
-  ],
-  integrations: [
-    new Integrations.RewriteFrames({
-      root: (global as any).__rootdir__
-    })
   ]
 })
