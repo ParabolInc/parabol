d3d0d58d9d43c49b171b0a10b6360886417c2e55
Matt Krick <matt.krick@gmail.com>
2025-04-23 10:14:32 -0700

    fix: dump sockets before dumping heap (#11213)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/private/mutations/dumpHeap.ts
+++ packages/server/graphql/private/mutations/dumpHeap.ts
@@ -2,13 +2,29 @@ import fs from 'fs'
 import inspector from 'inspector'
 import os from 'os'
 import path from 'path'
+import sleep from '../../../../client/utils/sleep'
+import {disconnectAllSockets} from '../../../disconnectAllSockets'
+import {setIsBusy} from '../../../getIsBusy'
+import {Logger} from '../../../utils/Logger'
 import {MutationResolvers} from '../resolverTypes'
 
 const {SERVER_ID} = process.env
 
-const dumpHeap: MutationResolvers['dumpHeap'] = async (_source, {isDangerous}) => {
+const dumpHeap: MutationResolvers['dumpHeap'] = async (
+  _source,
+  {isDangerous, disconnectSockets}
+) => {
   if (!isDangerous)
     return 'This action will block the server for about 1 minute, Must ack the danger!'
+  Logger.log('[Heap Dump]: Marking server as busy')
+  setIsBusy(true)
+  // wait 10 seconds for the readiness probe to fail
+  await sleep(10_000)
+  if (disconnectSockets) {
+    Logger.log('[Heap Dump]: Disconnecting Sockets')
+    await disconnectAllSockets()
+  }
+  Logger.log('[Heap Dump]: Ready to Dump')
   return new Promise((resolve) => {
     global.gc?.()
     const memoryUsage = process.memoryUsage()
@@ -27,6 +43,7 @@ const dumpHeap: MutationResolvers['dumpHeap'] = async (_source, {isDangerous}) =
     session.post('HeapProfiler.takeHeapSnapshot', undefined, (err) => {
       session.disconnect()
       fs.closeSync(fd)
+      setIsBusy(false)
       resolve(err?.toString() || pathName)
     })
   })
