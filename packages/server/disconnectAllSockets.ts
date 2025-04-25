7ffd28fd2181af0e348ab1317028c0675f14a21c
Rafa <101704572+rafaelromcar-parabol@users.noreply.github.com>
2025-04-25 17:07:14 +0100

    fix: disconnectAllSockets accepts an input parameters for the disconnect window (#11247)

--- packages/server/disconnectAllSockets.ts
+++ packages/server/disconnectAllSockets.ts
@@ -1,11 +1,11 @@
 import sleep from '../client/utils/sleep'
 import {activeClients} from './activeClients'
 
-export const disconnectAllSockets = async () => {
+export const disconnectAllSockets = async (disconnectWindow: number = 60_000) => {
   const ONDISCONNECT_LIMIT = 3000
   const connectionsToClose = Array.from(activeClients.values())
   if (connectionsToClose.length > 0) {
-    const msPerClose = (60_000 - ONDISCONNECT_LIMIT) / connectionsToClose.length
+    const msPerClose = (disconnectWindow - ONDISCONNECT_LIMIT) / connectionsToClose.length
     const closeEvery = Math.min(200, msPerClose)
     await Promise.allSettled(
       connectionsToClose.map(async (extra, idx) => {
