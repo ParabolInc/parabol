a4ef995129853671b17d7107962b8ffaed22fbd0
Matt Krick <matt.krick@gmail.com>
2025-05-14 14:38:53 -0700

    feat: no pages inside
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/components/DashNavList/LeftNavPageLink.tsx
+++ packages/client/components/DashNavList/LeftNavPageLink.tsx
@@ -2,11 +2,14 @@ import AddIcon from '@mui/icons-material/Add'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import DescriptionIcon from '@mui/icons-material/Description'
 import graphql from 'babel-plugin-relay/macro'
+import {useState} from 'react'
 import {useFragment} from 'react-relay'
 import {useRouteMatch} from 'react-router'
 import {Link} from 'react-router-dom'
 import type {LeftNavPageLink_page$key} from '../../__generated__/LeftNavPageLink_page.graphql'
 import {toSlug} from '../../shared/toSlug'
+import {cn} from '../../ui/cn'
+import {SubPagesRoot} from './SubPagesRoot'
 interface Props {
   pageRef: LeftNavPageLink_page$key
 }
@@ -27,27 +30,40 @@ export const LeftNavPageLink = (props: Props) => {
   const slug = titleSlug ? `${titleSlug}-${pageIdNum}` : pageIdNum
   const match = useRouteMatch(`/pages/:slug(.*)-${pageIdNum}`)
   const isActive = match?.isExact ?? false
+  const [showChildren, setShowChildren] = useState(false)
+  const expandChildPages = () => {
+    setShowChildren(!showChildren)
+  }
   return (
-    <Link to={`/pages/${slug}`} key={slug} className='mx-1 flex'>
-      <div
-        data-highlighted={isActive ? '' : undefined}
-        className={
-          'group flex w-full cursor-pointer items-center space-x-2 rounded-md px-3 py-1 text-sm leading-8 text-slate-700 outline-hidden hover:bg-slate-300! hover:text-slate-900 focus:bg-slate-200 data-highlighted:bg-slate-300 data-highlighted:text-slate-900'
-        }
-      >
-        <div className='flex size-6 items-center justify-center rounded-sm bg-slate-200 group-hover:bg-slate-300 group-data-highlighted:bg-slate-300 hover:bg-slate-400'>
-          <DescriptionIcon className='size-5 group-hover:hidden' />
-          <ChevronRightIcon className='hidden size-5 group-hover:block' />
-        </div>
-        <div className='flex flex-col text-sm font-medium'>
-          <span>{title}</span>
-        </div>
-        <div className='flex flex-1 items-center justify-end'>
-          <div className='flex size-6 items-center justify-center rounded-sm hover:bg-slate-400'>
-            <AddIcon className='hidden size-5 group-hover:block' />
+    <div className='mx-1'>
+      <Link to={`/pages/${slug}`} key={slug} className='my-0.5 flex'>
+        <div
+          data-highlighted={isActive ? '' : undefined}
+          className={
+            'group flex w-full cursor-pointer items-center space-x-2 rounded-md px-3 py-1 text-sm leading-8 text-slate-700 outline-hidden hover:bg-slate-300 focus:bg-slate-300 data-highlighted:bg-slate-300 data-highlighted:text-slate-900'
+          }
+        >
+          <div className='flex size-6 items-center justify-center rounded-sm bg-slate-200 group-hover:bg-slate-300 group-data-highlighted:bg-slate-300 hover:bg-slate-400'>
+            <DescriptionIcon className='size-5 group-hover:hidden no-hover:hidden' />
+            <ChevronRightIcon
+              className={cn(
+                'sm hidden size-5 transition-transform group-hover:block no-hover:block',
+                showChildren && 'rotate-90'
+              )}
+              onClick={expandChildPages}
+            />
+          </div>
+          <div className='flex flex-col text-sm font-medium'>
+            <span>{title}</span>
+          </div>
+          <div className='flex flex-1 items-center justify-end'>
+            <div className='flex size-6 items-center justify-center rounded-sm hover:bg-slate-400'>
+              <AddIcon className='hidden size-5 group-hover:block' />
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+      {showChildren && <SubPagesRoot parentPageId={id} />}
+    </div>
   )
 }
---
+++ packages/client/components/DashNavList/SubPages.tsx
@@ -0,0 +1,44 @@
+import graphql from 'babel-plugin-relay/macro'
+import {usePreloadedQuery, type PreloadedQuery} from 'react-relay'
+import query, {type SubPagesQuery} from '../../__generated__/SubPagesQuery.graphql'
+import {LeftNavPageLink} from './LeftNavPageLink'
+
+graphql`
+  query SubPagesQuery($parentPageId: ID!) {
+    viewer {
+      pages(first: 100, parentPageId: $parentPageId) {
+        edges {
+          node {
+            ...LeftNavPageLink_page
+            id
+            title
+          }
+        }
+      }
+    }
+  }
+`
+interface Props {
+  queryRef: PreloadedQuery<SubPagesQuery>
+}
+
+export const SubPages = (props: Props) => {
+  const {queryRef} = props
+  const data = usePreloadedQuery<SubPagesQuery>(query, queryRef)
+  const {viewer} = data
+  const {pages} = viewer
+  const {edges} = pages
+
+  if (edges.length === 0) {
+    return <div className='pl-8 text-sm font-medium text-slate-500'>{'No pages inside'}</div>
+  }
+  return (
+    <div>
+      {edges.map((edge) => {
+        const {node} = edge
+        const {id} = node
+        return <LeftNavPageLink key={id} pageRef={node} />
+      })}
+    </div>
+  )
+}
