fa397a3ae9ce3d08132f8c81469e08b8d5da4d9e
Nick O'Ferrall <nickoferrall@gmail.com>
2025-05-30 16:13:19 -0600

    remove unused viewerId

--- packages/server/graphql/public/mutations/setMeetingMusic.ts
+++ packages/server/graphql/public/mutations/setMeetingMusic.ts
@@ -1,15 +1,12 @@
 import {SubscriptionChannel} from '../../../../client/types/constEnums'
-import {getUserId} from '../../../utils/authorization'
 import publish from '../../../utils/publish'
 import {MutationResolvers} from '../resolverTypes'
 
 const setMeetingMusic: MutationResolvers['setMeetingMusic'] = async (
   _source,
   {meetingId, trackSrc, isPlaying, timestamp},
-  {authToken, dataLoader, socketId: mutatorId}
+  {dataLoader, socketId: mutatorId}
 ) => {
-  const viewerId = getUserId(authToken)
-
   const operationId = dataLoader.share()
   const subOptions = {mutatorId, operationId}
 
