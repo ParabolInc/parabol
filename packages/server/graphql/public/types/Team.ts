7883e0e2ae429ca911a909db57dbb7c3a4be5620
Georg Bremer <github@dschoordsch.de>
2025-06-10 11:58:51 +0200

    cherrypick to current

--- packages/server/graphql/public/types/Team.ts
+++ packages/server/graphql/public/types/Team.ts
@@ -72,6 +72,16 @@ const Team: TeamResolvers = {
       'sortOrder is not being pre-calculated! Did you call teamsWithUserSort dataloader?'
     )
     return '!'
+  },
+  activeMeetings: async ({id: teamId}, _args, {authToken, dataLoader}) => {
+    if (!isTeamMember(authToken, teamId)) return []
+    // this is by team, not by meeting member, which caused an err in dev, not sure about prod
+    // we need better perms for people to view/not view a meeting that happened before they joined the team
+    return dataLoader.get('activeMeetingsByTeamId').load(teamId)
+  },
+  activeMeetingSeries: async ({id: teamId}, _args, {authToken, dataLoader}) => {
+    if (!isTeamMember(authToken, teamId)) return []
+    return dataLoader.get('activeMeetingSeriesByTeamId').load(teamId)
   }
 }
 
