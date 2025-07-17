59382650e05369dced5a3918bdccd6662f9b9a65
Georg Bremer <github@dschoordsch.de>
2025-07-17 18:19:54 +0200

    feat: add tables to pages (#11545)

--- packages/client/hooks/useTipTapPageEditor.ts
+++ packages/client/hooks/useTipTapPageEditor.ts
@@ -8,6 +8,7 @@ import Document from '@tiptap/extension-document'
 import Focus from '@tiptap/extension-focus'
 import Mention from '@tiptap/extension-mention'
 import Placeholder from '@tiptap/extension-placeholder'
+import TableRow from '@tiptap/extension-table-row'
 import {TaskItem} from '@tiptap/extension-task-item'
 import {TaskList} from '@tiptap/extension-task-list'
 import Underline from '@tiptap/extension-underline'
@@ -31,6 +32,9 @@ import {InsightsBlock} from '../tiptap/extensions/insightsBlock/InsightsBlock'
 import {PageLinkBlock} from '../tiptap/extensions/pageLinkBlock/PageLinkBlock'
 import {PageLinkPicker} from '../tiptap/extensions/pageLinkPicker/PageLinkPicker'
 import {SlashCommand} from '../tiptap/extensions/slashCommand/SlashCommand'
+import {Table} from '../tiptap/extensions/table/Table'
+import {TableCell} from '../tiptap/extensions/table/TableCell'
+import {TableHeader} from '../tiptap/extensions/table/TableHeader'
 import {ElementWidth} from '../types/constEnums'
 import {tiptapEmojiConfig} from '../utils/tiptapEmojiConfig'
 import {tiptapMentionConfig} from '../utils/tiptapMentionConfig'
@@ -78,6 +82,12 @@ export const useTipTapPageEditor = (
         }),
         DetailsSummary,
         DetailsContent,
+        Table.configure({
+          allowTableNodeSelection: true
+        }),
+        TableRow,
+        TableHeader,
+        TableCell,
         Underline,
         TaskList,
         TaskItem.configure({
---
+++ packages/client/tiptap/extensions/table/icons/TableSVG.tsx
@@ -0,0 +1,14 @@
+import {SVGProps} from 'react'
+
+export default (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    height='24px'
+    viewBox='0 -960 960 960'
+    width='24px'
+    fill='currentColor'
+    {...props}
+  >
+    <path d='M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm240-240H200v160h240v-160Zm80 0v160h240v-160H520Zm-80-80v-160H200v160h240Zm80 0h240v-160H520v160ZM200-680h560v-80H200v80Z' />
+  </svg>
+)
