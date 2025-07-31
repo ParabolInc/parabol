905a0583c8cb453e7128f8cf2312559a8dce6670
Matt Krick <matt.krick@gmail.com>
2025-07-31 14:14:10 -0700

    fix: end meetings without insights (#11643)

--- packages/server/graphql/mutations/helpers/gatherInsights.ts
+++ packages/server/graphql/mutations/helpers/gatherInsights.ts
@@ -1,21 +1,22 @@
+import type {DataLoaderInstance} from '../../../dataloader/RootDataLoader'
 import getKysely from '../../../postgres/getKysely'
-import type {AnyMeeting} from '../../../postgres/types/Meeting'
+import type {AnyMeeting, RetrospectiveMeeting} from '../../../postgres/types/Meeting'
 import getPhase from '../../../utils/getPhase'
 import type {DataLoaderWorker} from '../../graphql'
 import isValid from '../../isValid'
 import calculateEngagement from './calculateEngagement'
 import collectReactjis from './collectReactjis'
 
-const gatherInsights = async (meeting: AnyMeeting, dataLoader: DataLoaderWorker) => {
+export const gatherRetroInsights = async (
+  meeting: RetrospectiveMeeting,
+  dataLoader: DataLoaderInstance
+) => {
   const pg = getKysely()
   const {id: meetingId, phases} = meeting
-  const discussPhase = getPhase(phases, 'discuss')
+  const discussPhase = getPhase(phases as any, 'discuss')
   const {stages} = discussPhase
   const discussionIds = stages.map((stage) => stage.discussionId)
-
-  const [usedReactjis, engagement, reflectionGroups, reflections] = await Promise.all([
-    collectReactjis(meeting, dataLoader),
-    calculateEngagement(meeting, dataLoader),
+  const [reflectionGroups, reflections] = await Promise.all([
     dataLoader.get('retroReflectionGroupsByMeetingId').load(meetingId),
     dataLoader.get('retroReflectionsByMeetingId').load(meetingId)
   ])
@@ -30,12 +31,22 @@ const gatherInsights = async (meeting: AnyMeeting, dataLoader: DataLoaderWorker)
     .where('discussionId', 'in', discussionIds)
     .executeTakeFirst()
   return {
-    usedReactjis,
-    engagement,
     commentCount,
     reflectionCount: reflections.length,
     taskCount: Number(taskCountRes?.count ?? 0),
     topicCount: reflectionGroupIds.length
+  }
+}
+
+const gatherInsights = async (meeting: AnyMeeting, dataLoader: DataLoaderWorker) => {
+  const [usedReactjis, engagement] = await Promise.all([
+    collectReactjis(meeting, dataLoader),
+    calculateEngagement(meeting, dataLoader)
+  ])
+
+  return {
+    usedReactjis,
+    engagement
   }
 }
 
