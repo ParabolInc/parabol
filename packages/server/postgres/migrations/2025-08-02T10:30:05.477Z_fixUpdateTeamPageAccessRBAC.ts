09908f94e089c64df329895d70f7ab93f5b1ba98
Georg Bremer <github@dschoordsch.de>
2025-08-02 12:40:38 +0200

    linting

--- packages/server/postgres/migrations/2025-08-02T10:30:05.477Z_fixUpdateTeamPageAccessRBAC.ts
+++ packages/server/postgres/migrations/2025-08-02T10:30:05.477Z_fixUpdateTeamPageAccessRBAC.ts
@@ -2,7 +2,6 @@ import {type Kysely, sql} from 'kysely'
 
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function up(db: Kysely<any>): Promise<void> {
-
   await sql`
 CREATE OR REPLACE FUNCTION "updateTeamPageAccessByTeamMember"() RETURNS TRIGGER AS $$
 DECLARE
