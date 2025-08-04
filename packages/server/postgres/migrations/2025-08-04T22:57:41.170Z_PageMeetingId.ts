724dfbbb456366f86605784900ecd5e6c5da0d16
Matt Krick <matt.krick@gmail.com>
2025-08-04 16:31:16 -0700

    fix: down migration typo
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/postgres/migrations/2025-08-04T22:57:41.170Z_PageMeetingId.ts
+++ packages/server/postgres/migrations/2025-08-04T22:57:41.170Z_PageMeetingId.ts
@@ -36,5 +36,5 @@ export async function up(db: Kysely<any>): Promise<void> {
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.alterTable('Page').dropColumn('summaryPageId').execute()
+  await db.schema.alterTable('Page').dropColumn('summaryMeetingId').execute()
 }
