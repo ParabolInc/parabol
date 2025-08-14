674abdf6dba1c4a4ed5732957231c81a632a2428
Matt Krick <matt.krick@gmail.com>
2025-08-14 14:06:25 -0700

    fix #11329 team/org preview objects for access
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/public/types/PageAccessTeam.ts
+++ packages/server/graphql/public/types/PageAccessTeam.ts
@@ -2,7 +2,11 @@ import type {PageAccessTeamResolvers} from '../resolverTypes'
 
 const PageAccessTeam: PageAccessTeamResolvers = {
   team: async ({teamId}, _args, {dataLoader}) => {
-    return dataLoader.get('teams').loadNonNull(teamId)
+    const team = await dataLoader.get('teams').loadNonNull(teamId)
+    return {
+      ...team,
+      id: `preview:${team.id}`
+    }
   }
 }
 
