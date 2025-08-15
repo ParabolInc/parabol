7251b6be1bfdf4c10b205122d217e79abebfe66f
Matt Krick <matt.krick@gmail.com>
2025-08-15 11:23:59 -0700

    fix: prefix breadcrumbs with team name (#11824)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/public/types/PagePartial.ts
+++ packages/server/graphql/public/types/PagePartial.ts
@@ -1,10 +1,23 @@
+import TeamMemberId from '../../../../client/shared/gqlIds/TeamMemberId'
 import {CipherId} from '../../../utils/CipherId'
 import type {ReqResolvers} from './ReqResolvers'
 
 const PagePartial: ReqResolvers<'PagePartial'> = {
   __resolveType: (source) =>
     (source as any).__typename === 'PagePreview' ? 'PagePreview' : 'Page',
-  id: ({id}) => CipherId.toClient(id, 'page')
+  id: ({id}) => CipherId.toClient(id, 'page'),
+  team: async ({teamId}, _args, {authToken, dataLoader}) => {
+    if (!teamId) return null
+    const [teamMember, team] = await Promise.all([
+      dataLoader.get('teamMembers').load(TeamMemberId.join(teamId, authToken.sub)),
+      dataLoader.get('teams').load(teamId)
+    ])
+    if (!team) return null
+    return {
+      ...team,
+      __typename: teamMember ? 'Team' : 'TeamPreview'
+    }
+  }
 }
 
 export default PagePartial
