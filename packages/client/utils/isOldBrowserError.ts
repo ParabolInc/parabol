38de79e16474643acc378686d154209fb6cc1b10
Georg Bremer <github@dschoordsch.de>
2025-08-19 15:13:27 +0200

    fix: show old browser error if Intl.Segmenter is missing (#11859)

--- packages/client/utils/isOldBrowserError.ts
+++ packages/client/utils/isOldBrowserError.ts
@@ -1,4 +1,4 @@
 export const isOldBrowserError = (message: string) => {
-  const oldBrowserErrors = ['flatMap is not a function']
+  const oldBrowserErrors = ['flatMap is not a function', 'Intl.Segmenter is not a constructor']
   return !!oldBrowserErrors.find((err) => message.includes(err))
 }
