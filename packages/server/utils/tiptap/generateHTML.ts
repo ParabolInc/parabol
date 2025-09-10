98f08926d3221e5a066c78af843b5663c37610c9
Matt Krick <matt.krick@gmail.com>
2025-09-10 13:11:54 -0700

    fix: support server usage
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/utils/tiptap/generateHTML.ts
+++ packages/server/utils/tiptap/generateHTML.ts
@@ -27,14 +27,7 @@ import {getHTMLFromFragment} from './getHTMLFromFragment'
  * const html = generateHTML(doc, extensions)
  */
 export function generateHTML(doc: JSONContent, extensions: Extensions): string {
-  if (typeof window === 'undefined') {
-    throw new Error(
-      'generateHTML can only be used in a browser environment\nIf you want to use this in a Node environment, use the `@tiptap/html/server` import instead.'
-    )
-  }
-
   const schema = getSchema(extensions)
   const contentNode = Node.fromJSON(schema, doc)
-
   return getHTMLFromFragment(contentNode, schema)
 }
