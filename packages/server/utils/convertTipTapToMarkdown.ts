dc68624b494b1f807f892a48289f39dbe395cb6c
Matt Krick <matt.krick@gmail.com>
2025-09-10 12:00:58 -0700

    fix: cleanup happy-dom with custom generateHTML
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/utils/convertTipTapToMarkdown.ts
+++ packages/server/utils/convertTipTapToMarkdown.ts
@@ -1,7 +1,7 @@
 import type {JSONContent} from '@tiptap/core'
-import {generateHTML} from '@tiptap/html/server'
 import {NodeHtmlMarkdown} from 'node-html-markdown-cloudflare'
 import {serverTipTapExtensions} from 'parabol-client/shared/tiptap/serverTipTapExtensions'
+import {generateHTML} from './tiptap/generateHTML'
 export const convertTipTapToMarkdown = (content: JSONContent) => {
   const html = generateHTML(content, serverTipTapExtensions)
   const markdown = NodeHtmlMarkdown.translate(html)
---
+++ packages/server/utils/tiptap/generateHTML.ts
@@ -0,0 +1,40 @@
+import type {Extensions, JSONContent} from '@tiptap/core'
+import {getSchema} from '@tiptap/core'
+import {Node} from '@tiptap/pm/model'
+import {getHTMLFromFragment} from './getHTMLFromFragment'
+
+/**
+ * Generates HTML from a ProseMirror JSON content object.
+ * @param doc - The ProseMirror JSON content object.
+ * @param extensions - The Tiptap extensions used to build the schema.
+ * @returns The generated HTML string.
+ * @example
+ * const doc = {
+ *   type: 'doc',
+ *   content: [
+ *     {
+ *       type: 'paragraph',
+ *       content: [
+ *         {
+ *           type: 'text',
+ *           text: 'Hello world!'
+ *         }
+ *       ]
+ *     }
+ *   ]
+ * }
+ * const extensions = [...]
+ * const html = generateHTML(doc, extensions)
+ */
+export function generateHTML(doc: JSONContent, extensions: Extensions): string {
+  if (typeof window === 'undefined') {
+    throw new Error(
+      'generateHTML can only be used in a browser environment\nIf you want to use this in a Node environment, use the `@tiptap/html/server` import instead.'
+    )
+  }
+
+  const schema = getSchema(extensions)
+  const contentNode = Node.fromJSON(schema, doc)
+
+  return getHTMLFromFragment(contentNode, schema)
+}
