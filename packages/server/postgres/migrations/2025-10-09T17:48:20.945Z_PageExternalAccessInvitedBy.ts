618e97dfab502e24782329a83d204f6b8da7d1e0
Matt Krick <matt.krick@gmail.com>
2025-10-09 11:29:17 -0700

    fix: limit external page invites by userId
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

---
+++ packages/server/postgres/migrations/2025-10-09T17:48:20.945Z_PageExternalAccessInvitedBy.ts
@@ -0,0 +1,19 @@
+import type {Kysely} from 'kysely'
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable('PageExternalAccess')
+    .addColumn('invitedBy', 'varchar(100)', (col) => col.references('User.id').onDelete('cascade'))
+    .execute()
+  await db.updateTable('PageExternalAccess').set({invitedBy: 'aGhostUser'}).execute()
+  await db.schema
+    .alterTable('PageExternalAccess')
+    .alterColumn('invitedBy', (ab) => ab.setNotNull())
+    .execute()
+}
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.alterTable('User').dropColumn('invitedBy').execute()
+}
