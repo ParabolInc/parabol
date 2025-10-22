abb8a84cc2231061ad79bddc14440f67db01e1c1
Matt Krick <matt.krick@gmail.com>
2025-10-22 14:46:51 -0700

    fix: move to an updateUser helper
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/handleFirstConnection.ts
+++ packages/server/handleFirstConnection.ts
@@ -2,7 +2,7 @@ import {sql} from 'kysely'
 import {InvoiceItemType, SubscriptionChannel} from '../client/types/constEnums'
 import adjustUserCount from './billing/helpers/adjustUserCount'
 import {getNewDataLoader} from './dataloader/getNewDataLoader'
-import getKysely from './postgres/getKysely'
+import updateUser from './postgres/queries/updateUser'
 import {analytics} from './utils/analytics/analytics'
 import {getTeamMemberUserIds} from './utils/getTeamMemberUserIds'
 import {Logger} from './utils/Logger'
@@ -12,7 +12,6 @@ export const handleFirstConnection = async (
   user: {id: string; email: string; inactive: boolean; lastSeenAt: Date},
   teamIds: string[]
 ) => {
-  const pg = getKysely()
   const userIds = await getTeamMemberUserIds(teamIds)
   userIds.forEach(({userId}) => {
     publish(SubscriptionChannel.NOTIFICATION, userId, 'User', user)
@@ -23,7 +22,7 @@ export const handleFirstConnection = async (
     const orgUsers = await dataLoader.get('organizationUsersByUserId').load(userId)
     const orgIds = orgUsers.map(({orgId}) => orgId)
     await Promise.all([
-      pg.updateTable('User').set({inactive: false, lastSeenAt: sql`CURRENT_TIMESTAMP`}).execute(),
+      updateUser({inactive: false, lastSeenAt: sql`CURRENT_TIMESTAMP`}, userId),
       adjustUserCount(userId, orgIds, InvoiceItemType.UNPAUSE_USER, dataLoader)
     ]).catch(Logger.log)
 
@@ -34,6 +33,6 @@ export const handleFirstConnection = async (
       isActive: true
     })
   } else if (new Date().toDateString() !== lastSeenAt.toDateString()) {
-    await pg.updateTable('User').set({lastSeenAt: sql`CURRENT_TIMESTAMP`}).execute()
+    updateUser({lastSeenAt: sql`CURRENT_TIMESTAMP`}, userId).catch(Logger.log)
   }
 }
