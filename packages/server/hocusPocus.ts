3d7d0a132c79ef5fa3bdb2e9d48b21776fcedfd5
Matt Krick <matt.krick@gmail.com>
2025-10-23 13:46:23 -0700

    fix: use ping/pong between origin and proxy sockets (#12278)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/hocusPocus.ts
+++ packages/server/hocusPocus.ts
@@ -59,7 +59,6 @@ export const redisHocusPocus = new RedisServerAffinity({
   redis: getRedis(),
   serverId: SERVER_ID,
   unpack,
-  proxySocketTTL: ms('1h'),
   customEvents: hocusPocusCustomEvents
 })
 export const hocuspocus = new Hocuspocus({
