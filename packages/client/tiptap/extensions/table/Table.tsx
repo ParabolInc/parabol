4deb6254019710868bfeb6411408f98beeaf7e92
Matt Krick <matt.krick@gmail.com>
2025-10-31 13:51:06 -0700

    feat: CSV Export on tables
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/tiptap/extensions/table/Table.tsx
+++ packages/client/tiptap/extensions/table/Table.tsx
@@ -1,3 +1,4 @@
+import FileDownloadIcon from '@mui/icons-material/FileDownload'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import {type TableOptions, Table as TiptapTable} from '@tiptap/extension-table'
 import {
@@ -9,6 +10,9 @@ import {
 import {useState} from 'react'
 import PlainButton from '../../../components/PlainButton/PlainButton'
 import {cn} from '../../../ui/cn'
+import {Tooltip} from '../../../ui/Tooltip/Tooltip'
+import {TooltipContent} from '../../../ui/Tooltip/TooltipContent'
+import {TooltipTrigger} from '../../../ui/Tooltip/TooltipTrigger'
 import {twStyled} from '../../../ui/twStyled'
 import AddColumnLeft from './icons/AddColumnLeftSVG'
 import AddColumnRight from './icons/AddColumnRightSVG'
@@ -86,7 +90,38 @@ function Component(props: NodeViewProps) {
 
   const focus = (what: Highlight) => () => setHighlight(what)
   const blur = () => setHighlight(null)
-
+  const exportToCSV = () => {
+    const content = node.content.content
+    const tableData: string[][] = []
+    content.forEach((row) => {
+      const rowData: string[] = []
+      row.content.forEach((rowNode) => {
+        if (['tableCell', 'tableHeader'].includes(rowNode.type.name)) {
+          const cellText = rowNode.content.content
+            .map((cellNode) => {
+              return cellNode.content.content.map((item) => item.text).join('')
+            })
+            .join('')
+          rowData.push(cellText)
+        }
+      })
+      tableData.push(rowData)
+    })
+    console.log(tableData)
+    const pageTitle = 'foo'
+    const date = new Date()
+    const numDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    const csvString = tableData.map((row) => row.join(',')).join('\n')
+    const blob = new Blob([csvString], {type: 'text/csv;charset=utf-8;'})
+    const encodedUri = URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.setAttribute('href', encodedUri)
+    link.setAttribute('download', `Parabol_${pageTitle}_table${numDate}.csv`)
+    link.style.visibility = 'hidden'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
   return (
     <NodeViewWrapper className='relative' data-highlight={highlight}>
       <NodeViewContent as={'table' as 'div'} {...props.HTMLAttributes} />
@@ -94,7 +129,7 @@ function Component(props: NodeViewProps) {
         <DropdownMenu.Trigger asChild>
           <PlainButton
             className={cn(
-              '-top-8 absolute right-0 flex size-7 items-center justify-center rounded bg-white text-slate-700 hover:bg-slate-300',
+              '-top-8 absolute right-8 flex size-7 items-center justify-center rounded bg-white text-slate-700 hover:bg-slate-300',
               {hidden: !selected}
             )}
           >
@@ -162,6 +197,20 @@ function Component(props: NodeViewProps) {
           </DropdownMenu.Content>
         </DropdownMenu.Portal>
       </DropdownMenu.Root>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <PlainButton
+            className={cn(
+              '-top-8 absolute right-0 flex size-7 items-center justify-center rounded bg-white text-slate-700 hover:bg-slate-300',
+              {hidden: !selected}
+            )}
+            onClick={exportToCSV}
+          >
+            <FileDownloadIcon />
+          </PlainButton>
+        </TooltipTrigger>
+        <TooltipContent className='text-xs'>{'Export to CSV'}</TooltipContent>
+      </Tooltip>
     </NodeViewWrapper>
   )
 }
