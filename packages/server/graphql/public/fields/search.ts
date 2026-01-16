1d61f912297af7e6cf72d09649ee562ac9e4055c
Matt Krick <matt.krick@gmail.com>
2026-01-16 15:56:53 -0800

    support searching other embeddings
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/public/fields/search.ts
+++ packages/server/graphql/public/fields/search.ts
@@ -1,196 +1,77 @@
 import {GraphQLError} from 'graphql'
-import {type ExpressionBuilder, type StringReference, sql} from 'kysely'
-import {activeEmbeddingModelId} from '../../../../embedder/activeEmbeddingModel'
-import {getEmbeddingsPagesTableName} from '../../../../embedder/getEmbeddingsTableName'
-import {getTSV} from '../../../../embedder/getSupportedLanguages'
-import numberVectorToString from '../../../../embedder/indexing/numberVectorToString'
-import {inferLanguage} from '../../../../embedder/inferLanguage'
-import getKysely from '../../../postgres/getKysely'
+import {getEmbeddingsByRRF} from '../../../postgres/queries/getEmbeddingsByRRF'
+import {getPagesByRRF} from '../../../postgres/queries/getPagesByRRF'
 import {getUserId} from '../../../utils/authorization'
+import {CipherId} from '../../../utils/CipherId'
 import {getUserQueryJobData, publishToEmbedder} from '../../mutations/helpers/publishToEmbedder'
-import type {UserResolvers} from '../resolverTypes'
+import type {SearchTypeEnum, UserResolvers} from '../resolverTypes'
 
-function RRF<DB, TB extends keyof DB>(
-  eb: ExpressionBuilder<DB, TB>,
-  kRankCol: StringReference<DB, TB>,
-  vRankCol: StringReference<DB, TB>,
-  alpha: number,
-  k: number
-) {
-  return sql<number>`
-      (${1 - alpha} * coalesce(1.0 / (${k} + ${eb.ref(vRankCol)}), 0.0)) + (${alpha} * coalesce(1.0 / (${k} + ${eb.ref(kRankCol)}), 0.0))`
+const decodeCursor = (after: string | null | undefined, type: SearchTypeEnum) => {
+  if (!after) return null
+  const decodedAfter = JSON.parse(atob(after))
+  if (!decodedAfter) return null
+  const {codes, maxScore} = decodedAfter as {codes: (number | string)[]; maxScore: number}
+  if (!Array.isArray(codes) || codes.length < 1 || !maxScore) return null
+  const afterIds = type === 'page' ? codes.map((code) => CipherId.decrypt(Number(code))) : codes
+  return {afterIds, maxScore}
 }
-
-function cosineSimilarity<DB, TB extends keyof DB>(
-  eb: ExpressionBuilder<DB, TB>,
-  column: StringReference<DB, TB>,
-  vector: Float32Array | number[]
-) {
-  return sql<number>`1 - (${eb.ref(column)} <=> ${numberVectorToString(vector)})`
-}
-
-function tsvSimilarity<DB, TB extends keyof DB>(
-  eb: ExpressionBuilder<DB, TB>,
-  column: StringReference<DB, TB>,
-  webQuery: StringReference<DB, TB>
-) {
-  return sql<number>`ts_rank_cd(${eb.ref(column)}, ${eb.ref(webQuery)}, 8)`
-}
-
-function rank<DB, TB extends keyof DB>(
-  eb: ExpressionBuilder<DB, TB>,
-  column: StringReference<DB, TB>
-) {
-  return sql<number>`row_number() over(order by ${eb.ref(column)} desc)::int`
-}
-
-export function cosineDistance<DB, TB extends keyof DB>(
-  eb: ExpressionBuilder<DB, TB>,
-  column: StringReference<DB, TB>,
-  vector: Float32Array | number[]
-) {
-  return sql<number>`${eb.ref(column)} <=> ${numberVectorToString(vector)}`
-}
-
 export const search: NonNullable<UserResolvers['search']> = async (
   _source,
-  {query, after, alpha, dateField, endAt, first, startAt, teamIds, types},
+  {query, after, alpha: inAlpha, dateField, endAt, first, startAt, teamIds, type},
   {authToken, dataLoader}
 ) => {
   const viewerId = getUserId(authToken)
-  const safeAlpha = alpha ?? 0.75
-  const k = 60 // RRF constant
+  const alpha = inAlpha ?? 0.75
 
-  if (safeAlpha < 0 || safeAlpha > 1) throw new GraphQLError('Alpha must be between 0 and 1')
-  if (query.length === 0) throw new GraphQLError('Query must not be empty')
-  if (query.length > 5000) throw new GraphQLError('Query is too long')
-  if (first || after) throw new GraphQLError('Pagination not implemented')
-  if (teamIds?.length) throw new GraphQLError('teamIds not implemented')
+  // VALIDATION
+  if (alpha < 0 || alpha > 1) throw new GraphQLError('alpha must be between 0 and 1')
+  if (query.length < 1 || query.length > 5000)
+    throw new GraphQLError('query must be between 1 and 5000 chars')
+  if (first < 1 || first > 100) throw new GraphQLError('first must be between 1 and 100')
+  if (teamIds) {
+    const hasAccessToTeams = teamIds.every((teamId) => authToken.sub.includes(teamId))
+    if (!hasAccessToTeams) {
+      throw new GraphQLError('Viewer is not a member of all teamIds requested')
+    }
+  }
 
-  const vector = await publishToEmbedder({
+  const queryVector = await publishToEmbedder({
     jobType: 'userQuery:start',
     dataLoader,
     userId: viewerId,
     data: getUserQueryJobData(query)
   })
-  const language = inferLanguage(query) || 'en'
-  const tsvLanguage = getTSV(language) || 'english'
-  const pg = getKysely()
-  const embeddingsPagesTableName = getEmbeddingsPagesTableName(activeEmbeddingModelId)
-  if (!embeddingsPagesTableName) {
-    throw new GraphQLError('No embeddings pages table found')
+  const k = 60 // RRF constant
+  const dateRange =
+    startAt || endAt
+      ? {
+          startAt: startAt || new Date(0),
+          endAt: endAt || new Date(4000),
+          dateField: dateField || 'createdAt'
+        }
+      : null
+  if (type === 'page') {
+    return getPagesByRRF({
+      query,
+      queryVector,
+      dateRange,
+      after: decodeCursor(after, type),
+      alpha,
+      k,
+      first,
+      teamIds
+    })
+  } else {
+    return getEmbeddingsByRRF({
+      query,
+      queryVector,
+      dateRange,
+      after: decodeCursor(after, type),
+      alpha,
+      k,
+      first,
+      teamIds: teamIds || ['aGhostTeam', ...authToken.tms],
+      type
+    })
   }
-  const useDateFilter = !!(startAt || endAt)
-  const safeDateField = dateField || 'createdAt'
-  const safeStartAt = startAt || new Date(0)
-  const safeEndAt = endAt || new Date(4000)
-  if (types.includes('page')) {
-    const pageResults = await pg
-      .with('Model', (qb) =>
-        qb
-          .selectFrom('PageAccess')
-          .innerJoin(
-            embeddingsPagesTableName,
-            'PageAccess.pageId',
-            `${embeddingsPagesTableName}.pageId`
-          )
-          .$if(useDateFilter, (qb) =>
-            qb
-              .innerJoin('Page', 'Page.id', 'PageAccess.pageId')
-              .where((eb) => eb.between(safeDateField, safeStartAt, safeEndAt))
-          )
-          .selectAll(embeddingsPagesTableName)
-          .select(sql<string>`websearch_to_tsquery(${tsvLanguage}, ${query})`.as('webQuery'))
-      )
-      .with('VectorSimilarity', (pg) =>
-        pg
-          .selectFrom('Model')
-          .selectAll('Model')
-          .select(({eb}) => cosineSimilarity(eb, 'embedding', vector).as('v_similarity'))
-          .orderBy('v_similarity', 'desc')
-          .limit(10)
-      )
-      .with('VectorRank', (qb) =>
-        qb
-          .selectFrom('VectorSimilarity')
-          .selectAll('VectorSimilarity')
-          .select(({eb}) => rank(eb, 'v_similarity').as('v_rank'))
-      )
-      .with('KeywordSimilarity', (qb) =>
-        qb
-          .selectFrom('Model')
-          .selectAll('Model')
-          .select((eb) => [
-            tsvSimilarity(eb, 'tsv', 'Model.webQuery').as('k_similarity'),
-            sql<string>`ts_headline(${tsvLanguage}, "embedText", "Model"."webQuery", 'StartSel=<b>, StopSel=</b>, MaxWords=35, MaxFragments=5, FragmentDelimiter=$!$')`.as(
-              'snippet'
-            )
-          ])
-          // Always do a where match, since that can be completed in the GIN index
-          .whereRef('tsv', '@@', 'Model.webQuery')
-          .orderBy('k_similarity', 'desc')
-          .limit(10)
-      )
-      .with('KeywordRank', (qb) =>
-        qb
-          .selectFrom('KeywordSimilarity')
-          .selectAll('KeywordSimilarity')
-          .select(({eb}) => rank(eb, 'k_similarity').as('k_rank'))
-      )
-      .with('RRF', (qb) =>
-        qb
-          .selectFrom('VectorRank')
-          .fullJoin('KeywordRank', 'VectorRank.id', 'KeywordRank.id')
-          .select(({eb, fn}) => [
-            fn.coalesce('VectorRank.id', 'KeywordRank.id').as('id'),
-            fn.coalesce('VectorRank.pageId', 'KeywordRank.pageId').as('pageId'),
-            fn.coalesce('VectorRank.chunkNumber', 'KeywordRank.chunkNumber').as('chunkNumber'),
-            'k_rank',
-            'v_rank',
-            'k_similarity',
-            'v_similarity',
-            'snippet',
-            RRF(eb, 'v_rank', 'k_rank', safeAlpha, k).as('score')
-          ])
-      )
-      .with('ChunkMax', (qc) =>
-        qc
-          .selectFrom('RRF')
-          .selectAll()
-          .distinctOn('pageId')
-          .orderBy('pageId')
-          .orderBy('score', 'desc')
-      )
-      .selectFrom('ChunkMax')
-      .selectAll()
-      .orderBy('score', 'desc')
-      .$call((qb) => {
-        const comp = qb.compile().sql
-        console.log(comp)
-        return qb
-      })
-      .execute()
-    console.log('pageResults', pageResults)
-    return {
-      pageInfo: {
-        hasNextPage: true,
-        hasPreviousPage: false,
-        endCursor: null,
-        startCursor: null
-      },
-      edges: pageResults.map((pr) => ({
-        nodeTypeName: 'Page',
-        nodeId: pr.pageId,
-        score: {
-          keyword: pr.k_similarity,
-          vector: pr.v_similarity,
-          vectorRank: pr.v_rank,
-          keywordRank: pr.k_rank,
-          combined: pr.score
-        },
-        snippets: pr.snippet?.split('$!$') ?? []
-      }))
-    }
-  }
-  return null as any
 }
--- packages/server/graphql/public/types/SearchResultEdge.ts
+++ packages/server/graphql/public/types/SearchResultEdge.ts
@@ -1,33 +1,49 @@
 import {GraphQLError} from 'graphql'
-import type {SearchMatchScore} from '../resolverTypes'
+import type {SearchMatchScore, SearchTypeEnum} from '../resolverTypes'
 import type {ReqResolvers} from './ReqResolvers'
 
 export type SearchResultEdgeSource = {
-  nodeTypeName: 'Page' | 'Discussion' | 'MeetingTemplate'
+  nodeTypeName: SearchTypeEnum
   nodeId: string | number
   score: SearchMatchScore
   snippets: string[]
-  cursor: string
 }
 
 const SearchResultEdge: ReqResolvers<'SearchResultEdge'> = {
   node: async ({nodeTypeName, nodeId}, _args, {dataLoader}) => {
-    const dbNode = await (async () => {
-      switch (nodeTypeName) {
-        case 'Page':
-          return dataLoader.get('pages').loadNonNull(nodeId as number)
-        case 'Discussion':
-          return dataLoader.get('discussions').loadNonNull(nodeId as string)
-        case 'MeetingTemplate':
-          return dataLoader.get('meetingTemplates').loadNonNull(nodeId as string)
-        default:
-          throw new GraphQLError(`Invalid nodeTypeName: ${nodeTypeName}`)
+    if (nodeTypeName === 'page') {
+      const pageData = await dataLoader.get('pages').loadNonNull(nodeId as number)
+      return {
+        ...pageData,
+        __typename: 'Page'
       }
-    })()
-    return {
-      ...dbNode,
-      __typename: nodeTypeName
     }
+
+    if (nodeTypeName === 'retrospectiveDiscussionTopic') {
+      // Assuming 'Discussion' is the name of the type in your schema/generated types
+      const discussionData = await dataLoader.get('discussions').loadNonNull(nodeId as string)
+      return {
+        ...discussionData,
+        __typename: 'Discussion'
+      }
+    }
+
+    if (nodeTypeName === 'meetingTemplate') {
+      const templateData = await dataLoader.get('meetingTemplates').loadNonNull(nodeId as string)
+      const {type} = templateData
+      const meetingTypeToTemplateType = {
+        action: 'FixedActivity',
+        poker: 'PokerTemplate',
+        retrospective: 'ReflectTemplate',
+        teamPrompt: 'FixedActivity'
+      } as const
+      return {
+        ...templateData,
+        __typename: meetingTypeToTemplateType[type]
+      }
+    }
+
+    throw new GraphQLError(`Invalid nodeTypeName: ${nodeTypeName}`)
   }
 }
 
