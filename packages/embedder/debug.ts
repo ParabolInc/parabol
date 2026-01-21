b128cc5579b4b4e09f1fe5faaa1ea9434f2c6a39
Matt Krick <matt.krick@gmail.com>
2026-01-20 18:43:36 -0800

    fix typecheck on debug
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/embedder/debug.ts
+++ packages/embedder/debug.ts
@@ -1,12 +1,14 @@
 // call with pnpm sucrase-node billing/debug.ts
 import '../../scripts/webpack/utils/dotenv'
-import json from '../../../tipTapJson.json'
-import {TipTapChunker} from './TipTapChunker'
+// import json from '../../../tipTapJson.json'
+// import {TipTapChunker} from './TipTapChunker'
 
 const debugFailedJob = async () => {
-  const chunker = new TipTapChunker()
-  const output = chunker.chunk(json as any)
-  console.log(output)
+  // Test chunking
+  // const chunker = new TipTapChunker()
+  // const output = chunker.chunk(json as any)
+  // console.log(output)
+  // Test most recent failure
   // const pg = getKysely()
   // const failedJob = await pg
   //   .selectFrom('EmbeddingsFailures')
@@ -14,12 +16,10 @@ const debugFailedJob = async () => {
   //   .orderBy('lastFailedAt')
   //   .limit(1)
   //   .executeTakeFirst()
-
   // if (!failedJob) {
   //   Logger.log('No failed jobs found')
   //   return
   // }
-
   // Logger.log('Debugging job:', failedJob.id)
   // const orch = new WorkflowOrchestrator()
   // await orch.runStep(failedJob as any)
