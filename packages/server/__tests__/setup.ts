b3edbbbbf68de652e5205f96347cb30fdb5988e3
Rafael Romero Carmona <rafael@parabol.co>
2026-01-22 12:55:35 +0000

    Mocking server id

--- packages/server/__tests__/setup.ts
+++ packages/server/__tests__/setup.ts
@@ -1,3 +1,10 @@
 import * as matchers from 'jest-extended'
 
 expect.extend(matchers)
+
+jest.mock('../utils/ServerIdentityManager', () => ({
+  identityManager: {
+    getId: () => 0,
+    claimIdentity: jest.fn().mockResolvedValue(0)
+  }
+}))
