739d00a6e83d5019707d2a598acc912ea0da4c34
Georg Bremer <github@dschoordsch.de>
2026-02-04 09:25:52 +0100

    feat(SCIM): Add authentication configuration (#12507)

---
+++ packages/server/postgres/migrations/2026-02-04T08:12:43.103Z_addScimAuth.ts
@@ -0,0 +1,40 @@
+import {type Kysely, sql} from 'kysely'
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createType('SCIMAuthenticationTypeEnum')
+    .asEnum(['bearerToken', 'oauthClientCredentials'])
+    .execute()
+
+  await db.schema
+    .alterTable('SAML')
+    .addColumn('scimAuthenticationType', sql`"SCIMAuthenticationTypeEnum"`)
+    .addColumn('scimBearerToken', 'varchar(1023)')
+    .addColumn('scimOAuthClientId', 'varchar(255)')
+    .addColumn('scimOAuthClientSecret', 'varchar(255)')
+    .execute()
+
+  await db
+    .insertInto('FeatureFlag')
+    .values({
+      featureName: 'SCIM',
+      description: 'Provision organization users via SCIM',
+      expiresAt: '2026-11-30T23:59:59.999Z',
+      scope: 'Organization'
+    })
+    .execute()
+}
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .alterTable('SAML')
+    .dropColumn('scimAuthenticationType')
+    .dropColumn('scimBearerToken')
+    .dropColumn('scimOAuthClientId')
+    .dropColumn('scimOAuthClientSecret')
+    .execute()
+  await db.schema.dropType('SCIMAuthenticationTypeEnum').execute()
+  await db.deleteFrom('FeatureFlag').where('featureName', '=', 'SCIM').execute()
+}
