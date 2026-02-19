8c51808b154533fc341f2fd9a32ea5d3cce26bbc
Matt Krick <matt.krick@gmail.com>
2026-02-18 17:32:25 -0800

    fix: style fixes for reflections in the reflect phase
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/components/RetroReflectPhase/HTMLReflection.tsx
+++ packages/client/components/RetroReflectPhase/HTMLReflection.tsx
@@ -11,8 +11,8 @@ export const HTMLReflection = (props: Props) => {
     <div className={cn('relative w-full overflow-auto text-slate-700 text-sm leading-5')}>
       <div
         className={cn(
-          'ProseMirror flex max-h-41 min-h-6 w-full flex-col items-start justify-center px-4 pt-3 leading-5',
-          disableAnonymity ? 'pb-0' : 'pb-3'
+          'ProseMirror flex max-h-41 min-h-6 w-full flex-col items-start justify-center px-4 pt-2.5 leading-5',
+          disableAnonymity ? 'pb-0' : 'pb-2'
         )}
         dangerouslySetInnerHTML={{__html: html}}
       ></div>
