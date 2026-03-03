fc8b2a6990fdbd4ee7ceed83eb5dc4bb9d5b9fd4
Jordan Husney <jordan@parabol.co>
2026-03-03 07:56:26 -0600

    chore: convert class components to functional, batch 1 (#12766)

--- packages/client/components/Tabs/Tabs.tsx
+++ packages/client/components/Tabs/Tabs.tsx
@@ -1,5 +1,13 @@
 import styled from '@emotion/styled'
-import {Children, Component, cloneElement, type ReactElement, type ReactNode} from 'react'
+import {
+  Children,
+  cloneElement,
+  type ReactElement,
+  type ReactNode,
+  useCallback,
+  useRef,
+  useState
+} from 'react'
 import {PALETTE} from '../../styles/paletteV3'
 import getBBox from '../RetroReflectPhase/getBBox'
 
@@ -34,54 +42,48 @@ const TabHeaders = styled('div')({
   width: '100%'
 })
 
-class Tabs extends Component<Props> {
-  activeChildRef?: HTMLDivElement
-  parentRef?: HTMLDivElement | null
+const Tabs = (props: Props) => {
+  const {activeIdx, children, className} = props
+  const [transform, setTransform] = useState('scaleX(0)')
+  const activeChildRef = useRef<HTMLElement | null>(null)
+  const parentRef = useRef<HTMLElement | null>(null)
 
-  state = {
-    transform: `scaleX(0)`
-  }
-  setChildRef = (c: HTMLDivElement | undefined) => {
-    if (c && c !== this.activeChildRef) {
-      this.moveInkBar(this.parentRef, c)
-    }
-    this.activeChildRef = c
-  }
-
-  setParentRef = (c: HTMLDivElement | null) => {
-    if (c && c !== this.parentRef) {
-      this.moveInkBar(c, this.activeChildRef)
-    }
-    this.parentRef = c
-  }
-
-  moveInkBar = (parent: HTMLDivElement | undefined | null, child: HTMLDivElement | undefined) => {
+  const moveInkBar = (parent: HTMLElement | null, child: HTMLElement | null) => {
     const childBBox = child && getBBox(child)
     const parentBBox = parent && getBBox(parent)
     if (!childBBox || !parentBBox) return
     const left = childBBox.left - parentBBox.left
-    this.setState({
-      transform: `translate3d(${left}px, 0, 0)scaleX(${childBBox.width / 1000})`
-    })
+    setTransform(`translate3d(${left}px, 0, 0)scaleX(${childBBox.width / 1000})`)
   }
 
-  render() {
-    const {activeIdx, children, className} = this.props
-    const {transform} = this.state
-    const properChildren = Children.map(children, (child, idx) => {
-      const isActive = idx === activeIdx
-      return cloneElement(child as ReactElement<any>, {
-        isActive,
-        ref: isActive ? this.setChildRef : null
-      })
+  const setChildRef = useCallback((c: HTMLElement | null) => {
+    if (c && c !== activeChildRef.current) {
+      moveInkBar(parentRef.current, c)
+    }
+    activeChildRef.current = c
+  }, [])
+
+  const setParentRef = useCallback((c: HTMLElement | null) => {
+    if (c && c !== parentRef.current) {
+      moveInkBar(c, activeChildRef.current)
+    }
+    parentRef.current = c
+  }, [])
+
+  const properChildren = Children.map(children, (child, idx) => {
+    const isActive = idx === activeIdx
+    return cloneElement(child as ReactElement<any>, {
+      isActive,
+      ref: isActive ? setChildRef : null
     })
-    return (
-      <TabsAndBar className={className} ref={this.setParentRef}>
-        <TabHeaders>{properChildren}</TabHeaders>
-        <InkBar style={{transform}} />
-      </TabsAndBar>
-    )
-  }
+  })
+
+  return (
+    <TabsAndBar className={className} ref={setParentRef}>
+      <TabHeaders>{properChildren}</TabHeaders>
+      <InkBar style={{transform}} />
+    </TabsAndBar>
+  )
 }
 
 export default Tabs
