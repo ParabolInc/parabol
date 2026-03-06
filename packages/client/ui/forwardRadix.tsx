20a737009a2e091b72f15816f1414b1e267b1d29
Jordan Husney <jordan@parabol.co>
2026-03-06 14:12:23 -0600

    chore: version bump to react 18 (#12773)

--- packages/client/ui/forwardRadix.tsx
+++ packages/client/ui/forwardRadix.tsx
@@ -2,11 +2,13 @@
 // To get type safety directly from radix
 
 import type * as React from 'react'
-import {forwardRef, type RefForwardingComponent} from 'react'
+import {type ForwardRefRenderFunction, forwardRef} from 'react'
 
 type RadixProps<T> = T extends React.ForwardRefExoticComponent<infer U> ? U : T
 type RadixAttrib<T> = T extends React.RefAttributes<infer U> ? U : T
 type RadixEl<T> = RadixAttrib<RadixProps<T>>
 
-export const forwardRadix = <T,>(c: RefForwardingComponent<RadixEl<T>, RadixProps<T>>) =>
-  forwardRef(c)
+// React 18 changed forwardRef to wrap props in PropsWithoutRef<P>.
+// RadixProps<T> already excludes ref, so the cast is safe.
+export const forwardRadix = <T,>(c: ForwardRefRenderFunction<RadixEl<T>, RadixProps<T>>) =>
+  forwardRef(c as ForwardRefRenderFunction<RadixEl<T>, React.PropsWithoutRef<RadixProps<T>>>)
