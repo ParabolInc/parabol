c47f52351aff363739b68dfdeff5edb542471caf
Jordan Husney <jordan@parabol.co>
2026-03-11 15:53:19 -0500

    chore: migrate Tasks from emotionjs to Tailwind (#12817)

--- packages/client/modules/outcomeCard/components/OutcomeCardFooter/TaskFooterUserAssignee.tsx
+++ packages/client/modules/outcomeCard/components/OutcomeCardFooter/TaskFooterUserAssignee.tsx
@@ -1,94 +1,14 @@
-import styled from '@emotion/styled'
 import {AssignmentInd as AssignmentIndIcon} from '@mui/icons-material'
 import graphql from 'babel-plugin-relay/macro'
 import {useFragment} from 'react-relay'
 import useTooltip from '~/hooks/useTooltip'
+import {cn} from '~/ui/cn'
 import type {TaskFooterUserAssignee_task$key} from '../../../../__generated__/TaskFooterUserAssignee_task.graphql'
-import BaseButton from '../../../../components/BaseButton'
 import {MenuPosition} from '../../../../hooks/useCoords'
 import useMenu from '../../../../hooks/useMenu'
 import type {UseTaskChild} from '../../../../hooks/useTaskChildFocus'
-import textOverflow from '../../../../styles/helpers/textOverflow'
-import {PALETTE} from '../../../../styles/paletteV3'
 import avatarUser from '../../../../styles/theme/images/avatar-user.svg'
 import lazyPreload from '../../../../utils/lazyPreload'
-
-const label = {
-  ...textOverflow,
-  color: PALETTE.SLATE_700,
-  display: 'block',
-  flex: 1,
-  fontSize: 12,
-  fontWeight: 400,
-  lineHeight: '24px',
-  maxWidth: '100%',
-  textAlign: 'left'
-} as const
-
-const AvatarButton = styled(BaseButton)({
-  border: 0,
-  boxShadow: 'none',
-  display: 'flex',
-  fontSize: 'inherit',
-  height: 24,
-  lineHeight: 'inherit',
-  outline: 0,
-  padding: 0,
-  maxWidth: '100%',
-  ':hover,:focus,:active': {
-    boxShadow: 'none'
-  },
-  ':hover > div,:focus > div': {
-    borderColor: PALETTE.SLATE_700,
-    color: PALETTE.SLATE_700
-  }
-})
-
-const Avatar = styled('div')<{isAssigned: boolean}>(({isAssigned}) => ({
-  backgroundColor: isAssigned ? 'transparent' : PALETTE.SLATE_600,
-  color: isAssigned ? 'transparent' : undefined,
-  borderRadius: '100%',
-  height: 24,
-  marginRight: 6,
-  width: 24
-}))
-
-const AvatarImage = styled('img')({
-  borderRadius: '100%',
-  height: 24,
-  marginRight: 4,
-  width: 24
-})
-
-const AvatarLabel = styled('div')({
-  ...label,
-  color: PALETTE.SLATE_600,
-  flex: 1,
-  fontWeight: 600,
-  minWidth: 0,
-  overflowWrap: 'break-word'
-})
-
-const TooltipToggle = styled('div')({
-  display: 'inline-flex',
-  width: '100%'
-})
-
-const StyledIcon = styled('div')({
-  alignContent: 'center',
-  alignItems: 'center',
-  color: PALETTE.WHITE,
-  cursor: 'pointer',
-  display: 'flex',
-  svg: {
-    fontSize: 22
-  },
-  height: 22,
-  width: 22,
-  justifyContent: 'center',
-  position: 'relative',
-  top: 1
-})
 
 interface Props {
   area: string
@@ -133,30 +53,39 @@ const TaskFooterUserAssignee = (props: Props) => {
   } = useTooltip<HTMLDivElement>(MenuPosition.UPPER_CENTER)
   return (
     <>
-      <TooltipToggle
+      <div
+        className='inline-flex w-full'
         onMouseEnter={openTooltip}
         onMouseLeave={closeTooltip}
         onClick={closeTooltip}
         ref={tipRef}
       >
-        <AvatarButton
+        <button
+          className='flex h-6 max-w-full border-0 bg-transparent p-0 text-inherit leading-inherit shadow-none outline-none hover:shadow-none focus:shadow-none [&:focus>div]:border-slate-700 [&:focus>div]:text-slate-700 [&:hover>div]:border-slate-700 [&:hover>div]:text-slate-700'
           aria-label='Assign this task to a teammate'
           onClick={canAssign ? togglePortal : undefined}
           onMouseEnter={TaskFooterUserAssigneeMenuRoot.preload}
           ref={originRef}
         >
-          <Avatar isAssigned={!!user}>
-            {user ? (
-              <AvatarImage alt={preferredName} src={userImage} />
-            ) : (
-              <StyledIcon>
-                <AssignmentIndIcon />
-              </StyledIcon>
+          <div
+            className={cn(
+              'mr-1.5 h-6 w-6 rounded-full',
+              user ? 'bg-transparent text-transparent' : 'bg-slate-600'
             )}
-          </Avatar>
-          <AvatarLabel>{preferredName}</AvatarLabel>
-        </AvatarButton>
-      </TooltipToggle>
+          >
+            {user ? (
+              <img className='mr-1 h-6 w-6 rounded-full' alt={preferredName} src={userImage} />
+            ) : (
+              <div className='relative top-px flex h-[22px] w-[22px] cursor-pointer items-center justify-center text-white [&_svg]:text-[22px]'>
+                <AssignmentIndIcon />
+              </div>
+            )}
+          </div>
+          <div className='block min-w-0 max-w-full flex-1 truncate break-words text-left font-semibold text-slate-600 text-xs leading-6'>
+            {preferredName}
+          </div>
+        </button>
+      </div>
       {tooltipPortal(<div>{'Reassign Responsibility'}</div>)}
       {menuPortal(
         <TaskFooterUserAssigneeMenuRoot
