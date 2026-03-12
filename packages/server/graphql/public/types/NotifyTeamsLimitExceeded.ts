686ac7ad659d2ae7afda31d3826bfc3ce94387f7
Matt Krick <matt.krick@gmail.com>
2026-03-12 13:20:48 -0700

    refactor: Remove fileStoreAsset data loader (#12839)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/public/types/NotifyTeamsLimitExceeded.ts
+++ packages/server/graphql/public/types/NotifyTeamsLimitExceeded.ts
@@ -1,11 +1,7 @@
 import type {NotifyTeamsLimitExceededResolvers} from '../resolverTypes'
 
 const NotifyTeamsLimitExceeded: NotifyTeamsLimitExceededResolvers = {
-  __isTypeOf: ({type}) => type === 'TEAMS_LIMIT_EXCEEDED',
-  orgPicture: async ({orgPicture}, _args, {dataLoader}) => {
-    if (!orgPicture) return null
-    return dataLoader.get('fileStoreAsset').load(orgPicture)
-  }
+  __isTypeOf: ({type}) => type === 'TEAMS_LIMIT_EXCEEDED'
 }
 
 export default NotifyTeamsLimitExceeded
