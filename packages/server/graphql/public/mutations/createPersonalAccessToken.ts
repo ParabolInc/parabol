da24e1c98238554d512f8205d7d9ef272640202f
Matt Krick <matt.krick@gmail.com>
2026-04-08 14:06:52 -0700

    disable pats for now
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/public/mutations/createPersonalAccessToken.ts
+++ packages/server/graphql/public/mutations/createPersonalAccessToken.ts
@@ -6,6 +6,7 @@ import {Security} from 'parabol-client/types/constEnums'
 import getKysely from '../../../postgres/getKysely'
 import type {MutationResolvers} from '../resolverTypes'
 
+let enabled = false
 export const createPersonalAccessToken: MutationResolvers['createPersonalAccessToken'] = async (
   _,
   args,
@@ -23,7 +24,11 @@ export const createPersonalAccessToken: MutationResolvers['createPersonalAccessT
     throw new GraphQLError('Expiration date cannot be more than 1 year in the future')
   }
 
-  // throw new GraphQLError('PATs not enabled for your account. Reach out to sales to enable for free')
+  if (!enabled) {
+    throw new GraphQLError(
+      'PATs not enabled for your account. Reach out to sales to enable for free'
+    )
+  }
   const rawToken = crypto.randomBytes(32).toString('base64url')
   const prefix = rawToken.slice(0, 8)
   const hashedToken = await bcrypt.hash(rawToken, Security.SALT_ROUNDS)
