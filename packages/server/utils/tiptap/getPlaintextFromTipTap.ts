1cf843f595cecd20269aa9e4339e89f3e0516b26
Matt Krick <matt.krick@gmail.com>
2026-05-20 08:50:12 -0700

    fix: add error handling and logging to plaintext generation (#13103)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/utils/tiptap/getPlaintextFromTipTap.ts
+++ packages/server/utils/tiptap/getPlaintextFromTipTap.ts
@@ -1,9 +1,16 @@
 import {generateText, type JSONContent} from '@tiptap/core'
 import {getTitleFromPageText} from '../../../client/shared/tiptap/getTitleFromPageText'
 import {serverTipTapExtensions} from '../../../client/shared/tiptap/serverTipTapExtensions'
+import {Logger} from '../Logger'
 
 export const getPlaintextFromTipTap = (content: JSONContent) => {
-  const docText = generateText(content, serverTipTapExtensions)
+  let docText = ''
+  try {
+    docText = generateText(content, serverTipTapExtensions)
+  } catch (e) {
+    Logger.log((e as Error)?.message || 'Unable to generateText', {extras: JSON.stringify(content)})
+    return {title: '', plaintextContent: ''}
+  }
   const {title, contentStartsAt} = getTitleFromPageText(docText)
   const plaintextContent = docText.slice(contentStartsAt)
   return {title, plaintextContent}
