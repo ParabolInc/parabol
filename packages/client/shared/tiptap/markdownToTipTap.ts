f3db9bdf351b9ddba8ec0cce4fbde53fcdf43ddb
Matt Krick <matt.krick@gmail.com>
2026-05-22 12:17:17 -0700

    fix: update Tiptap dependencies to 3.23.6. (#13134)
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/shared/tiptap/markdownToTipTap.ts
+++ packages/client/shared/tiptap/markdownToTipTap.ts
@@ -1,77 +1,19 @@
 // This returns a Content array of blocks, not a whole JSONContent.
-// If you need the whole doc, wrap it like {type: 'doc', content: <RETURN VALUE>}
-// We should try to move this to @tiptap/markdown instead
-import {Editor, type JSONContent} from '@tiptap/core'
-import {MarkdownParser} from '@tiptap/pm/markdown'
-import markdownit from 'markdown-it'
+import {Editor} from '@tiptap/core'
 import {serverTipTapExtensions} from './serverTipTapExtensions'
+import type {TipTapSerializedContent} from './TipTapSerializedContent'
 
-// No table support yet!
-let parser: MarkdownParser
-
-const getParser = () => {
-  if (!parser) {
+export const markdownToTipTap = (str: string): TipTapSerializedContent['content'] => {
+  try {
     const editor = new Editor({
       element: undefined,
-      content: {type: 'doc', content: []},
+      content: str,
+      contentType: 'markdown',
       extensions: serverTipTapExtensions
     })
-
-    const md = markdownit({
-      html: true,
-      linkify: true
-    })
-
-    parser = new MarkdownParser(editor.schema, md as any, {
-      blockquote: {block: 'blockquote'},
-      paragraph: {block: 'paragraph'},
-      list_item: {block: 'listItem'},
-      html_inline: {mark: 'code'}, // this is probably wrong, should move to TipTap parser
-      ordered_list: {
-        block: 'orderedList',
-        getAttrs: (t) => ({order: +t.attrGet('order')! || 1})
-      },
-      bullet_list: {block: 'bulletList'},
-      heading: {
-        block: 'heading',
-        getAttrs: (t) => ({level: +t.tag.slice(1)})
-      },
-      code_block: {block: 'codeBlock'},
-      fence: {
-        block: 'codeBlock',
-        getAttrs: (t) => ({language: t.info || null})
-      },
-      hr: {node: 'horizontalRule'},
-      image: {
-        node: 'imageBlock',
-        getAttrs: (t) => ({
-          src: t.attrGet('src'),
-          alt: t.attrGet('alt')
-        })
-      },
-      hardbreak: {node: 'hardBreak'},
-      em: {mark: 'italic'},
-      strong: {mark: 'bold'},
-      link: {
-        mark: 'link',
-        getAttrs: (t) => ({
-          href: t.attrGet('href'),
-          title: t.attrGet('title')
-        })
-      },
-      code_inline: {mark: 'code'}
-    })
-  }
-  return parser
-}
-export const markdownToTipTap = (str: string) => {
-  const parser = getParser()
-  try {
-    const parsed = parser.parse(str)
-    // remove constructors
-    return JSON.parse(JSON.stringify(parsed.content)) as JSONContent[]
+    return editor.getJSON().content as TipTapSerializedContent['content']
   } catch (e) {
     console.error(e)
-    return {}
+    return []
   }
 }
