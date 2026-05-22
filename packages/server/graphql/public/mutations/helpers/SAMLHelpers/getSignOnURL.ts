87b0a488fa05398e744d474a6806ec67b11cc580
Matt Krick <matt.krick@gmail.com>
2026-05-21 20:19:55 -0700

    fix: resolve SAML type assertion errors and missing property accessors during login flow
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/server/graphql/public/mutations/helpers/SAMLHelpers/getSignOnURL.ts
+++ packages/server/graphql/public/mutations/helpers/SAMLHelpers/getSignOnURL.ts
@@ -4,13 +4,13 @@ import getURLWithSAMLRequestParam from './getURLWithSAMLRequestParam'
 const getSignOnURL = (metadata: string, slugName: string) => {
   if (!metadata) return new Error('No metadata provided')
   const idp = samlify.IdentityProvider({metadata})
-  const {singleSignOnService} = idp.entityMeta.meta
+  const {singleSignOnService} = idp.entityMeta.meta as {singleSignOnService: Record<string, string>}
   const [fallbackKey] = Object.keys(singleSignOnService)
   if (!fallbackKey) {
     return new Error('Invalid metadata. Does not contain sign on URL')
   }
   const postKey = 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST'
-  const inputURL = singleSignOnService[postKey] || singleSignOnService[fallbackKey]
+  const inputURL = singleSignOnService[postKey] || singleSignOnService[fallbackKey]!
   try {
     new URL(inputURL)
   } catch {
