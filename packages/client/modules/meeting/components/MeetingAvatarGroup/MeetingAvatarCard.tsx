4436f7aa5f276954cc37359c37723fbcdf9e350e
Matt Krick <matt.krick@gmail.com>
2026-05-29 10:45:48 -0700

    fix: avatar fallbacks and delay opening in meeting
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/modules/meeting/components/MeetingAvatarGroup/MeetingAvatarCard.tsx
+++ packages/client/modules/meeting/components/MeetingAvatarGroup/MeetingAvatarCard.tsx
@@ -1,12 +1,14 @@
+import Avatar from '../../../../components/Avatar/Avatar'
+
 interface Props {
-  picture: string
+  picture: string | null
   preferredName: string
   email: string
 }
 
 const MeetingAvatarCard = ({picture, preferredName, email}: Props) => (
   <div className='flex items-center gap-2 px-3 py-1.5'>
-    <img src={picture} alt={preferredName} className='h-7 w-7 shrink-0 rounded-full object-cover' />
+    <Avatar picture={picture} alt={preferredName} className='h-7 w-7 shrink-0' />
     <div>
       <div className='text-slate-700 text-sm'>{preferredName}</div>
       <div className='text-slate-500 text-xs'>{email}</div>
