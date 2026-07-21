bd5e66212e7e764962d51f0c9fcda33a47a4f86e
Matt Krick <matt.krick@gmail.com>
2026-07-21 12:20:39 -0700

    fix: bump markdown-it linkify-it
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/utils/linkify.ts
+++ packages/client/utils/linkify.ts
@@ -1,7 +1,9 @@
-import linkifyIt from 'linkify-it'
+import {LinkifyIt} from 'linkify-it'
 import tlds from 'tlds'
 
-const linkify = linkifyIt()
+// v6+ disables fuzzy matching by default; keep it on to preserve the pre-v6
+// behavior of recognizing bare domains (e.g. `github.com`) and emails as links
+const linkify = new LinkifyIt({fuzzyLink: true, fuzzyEmail: true})
 linkify.tlds(tlds)
 
 export default linkify
