f61699a796d8087e7dbed772d507b6baa0ebb5da
Jordan Husney <jordan@parabol.co>
2026-07-28 15:51:24 -0500

    remove unecessary comments

--- packages/server/postgres/migrations/2026-07-27T20:35:23.080Z_widenAtlassianAuthScope.ts
+++ packages/server/postgres/migrations/2026-07-27T20:35:23.080Z_widenAtlassianAuthScope.ts
@@ -1,6 +1,5 @@
 import type {Kysely} from 'kysely'
 
-// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable('AtlassianAuth')
@@ -8,7 +7,6 @@ export async function up(db: Kysely<any>): Promise<void> {
     .execute()
 }
 
-// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function down(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable('AtlassianAuth')
--- packages/server/postgres/migrations/2026-07-27T22:27:21.746Z_addPageExport.ts
+++ packages/server/postgres/migrations/2026-07-27T22:27:21.746Z_addPageExport.ts
@@ -1,6 +1,5 @@
 import {type Kysely, sql} from 'kysely'
 
-// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable('PageExport')
@@ -44,7 +43,6 @@ export async function up(db: Kysely<any>): Promise<void> {
   await sql`ALTER TABLE "Notification" ADD COLUMN IF NOT EXISTS "pageExportId" bigint`.execute(db)
 }
 
-// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function down(db: Kysely<any>): Promise<void> {
   await sql`ALTER TABLE "Notification" DROP COLUMN IF EXISTS "pageExportId"`.execute(db)
   await db.schema.dropTable('PageExport').ifExists().execute()
