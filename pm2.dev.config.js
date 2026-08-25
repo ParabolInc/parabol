97968a719d7aac1fc980bc08a2f253f8862f43bd
Matt Krick <matt.krick@gmail.com>
2026-08-24 17:20:39 -0700

    fix: persistedquerynotfound error (#13401)

--- pm2.dev.config.js
+++ pm2.dev.config.js
@@ -1,3 +1,8 @@
+const killOrphanedDevProcesses = require('./scripts/killOrphanedDevProcesses')
+
+// leftovers from a previous stack still hold their ports, see the note in the required module
+killOrphanedDevProcesses()
+
 const {DEV_RUN_ONLY} = process.env
 // const DEV_RUN_ONLY = 'Webpack Servers,Socket Server,Dev Server'
 const runOnly = DEV_RUN_ONLY ? DEV_RUN_ONLY.split(',') : []
