70514bafbea3a2c641bc7195a1357b688dfba52c
Matt Krick <matt.krick@gmail.com>
2026-09-21 12:31:20 -0700

    fix: cleanup dead code
    Signed-off-by: Matt Krick <matt.krick@gmail.com>

--- packages/client/shared/utils/hashTeamHealthQuestionIds.ts
+++
@@ -1,8 +0,0 @@
-import {quickHash} from './quickHash'
-
-const hashTeamHealthQuestionIds = async (questionIds: readonly string[]) => {
-  const hashes = await Promise.all(questionIds.map((questionId) => quickHash([questionId])))
-  return new Map(questionIds.map((questionId, index) => [questionId, hashes[index]!]))
-}
-
-export default hashTeamHealthQuestionIds
